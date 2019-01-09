bee0568192f3762e17c4d182b8ff2243bddc576a
Alfredo Correa <correaa@llnl.gov>
2019-01-09 13:38:03 -0800

    icc workaround typecast in template argument

--- array_ref.hpp
+++ array_ref.hpp
@@ -311,8 +311,8 @@ public:
 
 template<typename T, typename ElementPtr, class Layout>
 struct basic_array<T, dimensionality_type{1}, ElementPtr, Layout> : 
-	multi::random_iterable<basic_array<T, dimensionality_type{1}, ElementPtr, Layout> >,
-	multi::partially_ordered2<basic_array<T, dimensionality_type{1}, ElementPtr, Layout>, void>,
+	multi::partially_ordered2<basic_array<T, dimensionality_type(1), ElementPtr, Layout>, void>,
+	multi::random_iterable<basic_array<T, dimensionality_type(1), ElementPtr, Layout> >,
 	array_types<T, dimensionality_type{1}, ElementPtr, Layout>
 {
 	using types = array_types<T, dimensionality_type{1}, ElementPtr, Layout>;
