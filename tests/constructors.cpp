5a6a2fbb8f9f4bf9b9a86db54bee75959d7ec9a6
Alfredo Correa <correaa@llnl.gov>
2019-01-10 02:39:59 -0800

    icc WA

--- tests/constructors.cpp
+++ tests/constructors.cpp
@@ -40,7 +40,8 @@ using boost::multi::size;
 }{  multi::array<double, 1> A({0l, 3l}); assert( size(A)==2 and A[1]==3. ); //uses init_list
 }{  multi::array<double, 1, std::allocator<double>> A(multi::index_extensions<1>{2}, std::allocator<double>{}); assert( size(A)==2 );
 }{  multi::array<double, 1, std::allocator<double>> A(multi::index_extensions<1>{{0, 3}}, std::allocator<double>{}); assert( size(A)==3 );
-}{  multi::array<double, 2> A({2, 3}); assert( num_elements(A)==6 );
+//}{  multi::array<double, 2> A(multi::index_extensions<2>{2, 3}); assert( num_elements(A)==6 );
+}{  multi::array<double, 2> A(multi::iextensions<2>{2, 3}); assert( num_elements(A)==6 );
 }{  multi::array<double, 2> A({{2, 3}}); assert( num_elements(A)==2 );
 }{  multi::array<double, 2> A(multi::index_extensions<2>{{0,2}, {0,3}}); assert( num_elements(A)==6 );
 }{  multi::array<double, 2, std::allocator<double>> A({2, 3}, std::allocator<double>{}); assert( num_elements(A)==6 );
