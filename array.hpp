4bcc2b12ea16800b5d84395db22aea4f26deb3b2
Alfredo Correa <correaa@llnl.gov>
2019-01-10 02:14:22 -0800

    icc WA

--- array.hpp
+++ array.hpp
@@ -86,7 +86,7 @@ public:
 #if defined(__INTEL_COMPILER)
 //	explicit array(std::initializer_list<typename array::index_extension> il, Alloc const& a={}) noexcept : array{multi::detail::to_tuple<D, typename array::index_extension>(il), a}{}
 //	explicit array(std::initializer_list<index> il, Alloc const& a={}) noexcept : array{multi::detail::to_tuple<D, typename array::index_extension>(il), a}{}
-	explicit array(std::array<index> arr, Alloc const& a = {}) : 
+	explicit array(std::array<index, D> arr, Alloc const& a = {}) : 
 		array{multi::detail::to_tuple<typename array::index_extension>(il), arr}{}
 #endif
 
