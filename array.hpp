7716305c19c88fe64d0cc653fecc7c4f8c670dda
Alfredo Correa <correaa@llnl.gov>
2019-01-10 02:15:01 -0800

    icc WA

--- array.hpp
+++ array.hpp
@@ -88,6 +88,8 @@ public:
 //	explicit array(std::initializer_list<index> il, Alloc const& a={}) noexcept : array{multi::detail::to_tuple<D, typename array::index_extension>(il), a}{}
 	explicit array(std::array<index, D> arr, Alloc const& a = {}) : 
 		array{multi::detail::to_tuple<typename array::index_extension>(il), arr}{}
+	explicit array(std::array<typename array::index_extension, D> arr, Alloc const& a = {}) : 
+		array{multi::detail::to_tuple<typename array::index_extension>(il), arr}{}
 #endif
 
 	template<class It> static auto distance(It a, It b){using std::distance; return distance(a, b);}
