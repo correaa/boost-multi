b3321e91150a2da7812e67d37ed5c4f786652bc6
Alfredo Correa <correaa@llnl.gov>
2019-01-10 04:13:08 -0800

    icc WA

--- tests/constructors.cpp
+++ tests/constructors.cpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION_INSTRUCTIONS
-c++ -O3 -std=c++17 -Wall -Wextra -Wpedantic -Wfatal-errors $0 -o $0.x && $0.x $@ &&rm -f $0.x; exit
+c++ -O3 -std=c++17 -Wall -Wextra -Wpedantic `#-Wfatal-errors` $0 -o $0.x && $0.x $@ &&rm -f $0.x; exit
 #endif
 
 #include<iostream>
