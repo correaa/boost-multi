4d3e224e7396934177e72c0de1708924b180a93b
Alfredo Correa <alfredo.correa@gmail.com>
2019-06-10 00:09:33 -0700

    catchify array reextent test

--- tests/array_reextent.cpp
+++ tests/array_reextent.cpp
@@ -1,37 +1,37 @@
 #ifdef COMPILATION_INSTRUCTIONS
-$CXX -O3 -std=c++17 -Wall -Wextra -Wpedantic `#-Wfatal-errors` $0 -o $0.x && $0.x $@ && rm $0.x; exit
+$CXX catch_main.o -std=c++17 -Wall -Wextra $0 -o $0x && $0x $@ && rm $0x; exit
 #endif
 
-#include<iostream>
+#include<catch2/catch.hpp>
+
 #include "../array.hpp"
 
-#include<queue>
-#include<vector>
-#include<numeric> // iota
-
 namespace multi = boost::multi;
-using std::cout;
 
-int main(){
+TEST_CASE( "Array reextent", "[array]"){
 	multi::array<double, 2> A({2, 3});
-	assert( num_elements(A)==6 );
+	REQUIRE( num_elements(A) == 6 );
+
 	A[1][2] = 6.;
+	REQUIRE( A[1][2] == 6. );
 
-//	multi::array<double, 2> B(std::tuple(2, 3)); assert(B.size() == 2);
-	multi::array<double, 2> C({2, 3}); assert(C.size() == 2 and C[0].size() == 3);
-
+	multi::array<double, 2> C({2, 3}); 
+	REQUIRE(size(C) == 2);
+	REQUIRE(size(C[0]) == 3);
 
 	A.reextent({5, 4}, 99.); 
-	assert( num_elements(A)== 20 );
-	assert( A[1][2] == 6. ); // careful, reextent preserves values when it can
-	assert( A[4][3] == 99. );
+	REQUIRE( num_elements(A)== 20 );
+	REQUIRE( A[1][2] == 6. );  // reextent preserves values when it can...
+	REQUIRE( A[4][3] == 99. ); // ...and gives selected value to the rest
 
-	A = multi::array<double, 2>(extensions(A), 123.); // this is not as inefficient as you might think
-	assert( A[1][2] == 123. );
+	A = multi::array<double, 2>(extensions(A), 123.); // this is not inefficient
+	REQUIRE( A[1][2] == 123. );
 
-	A.clear();
-	assert( num_elements(A) == 0 );
+	clear(A); // A.clear();
+	REQUIRE( num_elements(A) == 0 );
+	REQUIRE( size(A) == 0 );
+
 	A.reextent({5, 4}, 66.);
-	assert( A[4][3] == 66. );		
+	REQUIRE( A[4][3] == 66. );		
 }
 
