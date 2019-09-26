48519df4432d1b877e01ee6711bc27a13e03e6fd
Alfredo Correa <alfredo.correa@gmail.com>
2019-09-26 11:50:15 -0700

    gemv commandline

--- adaptors/blas/gemv.hpp
+++ adaptors/blas/gemv.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION_INSTRUCTIONS
-(echo "#include\""$0"\"" > $0x.cpp) && clang++ `#-DNDEBUG` -O3 -std=c++14 -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_GEMV -DADD_ $0x.cpp -o $0x.x -lblas && time $0x.x $@ && rm -f $0x.x $0x.cpp; exit
+(echo '#include"'$0'"'>$0.cpp)&& clang++ -O3 -std=c++14 -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_GEMV -DADD_ $0.cpp -o $0x -lblas `#/usr/local/Wolfram/Mathematica/12.0/SystemFiles/Libraries/Linux-x86-64/libmkl_sequential.so /usr/local/Wolfram/Mathematica/12.0/SystemFiles/Libraries/Linux-x86-64/libmkl_intel_ilp64.so /usr/local/Wolfram/Mathematica/12.0/SystemFiles/Libraries/Linux-x86-64/libmkl_core.so` &&$0x&& rm $0x $0.cpp; exit
 #endif
 // Alfredo A. Correa 2019 ©
 
@@ -40,7 +40,7 @@ Y1D&& gemv(Trans IN, T a, A2D const& A, X1D const& x, T beta, Y1D&& y){
 	assert( not(IN == 'T') or size(A) == size(y) );
 	assert( not(IN == 'N') or size(A) == size(x) );
 	assert( not(IN == 'C') or size(A) == size(x) );
-	auto e = gemv(IN, a, begin(A), end(A), begin(x), beta, begin(y));
+	auto e = gemv(IN, a, begin(A), end(A), begin(x), beta, begin(y)); (void)e;
 	assert( e == end(y) );
 	return std::forward<Y1D>(y);
 } //y := alpha*A*x + beta*y,
@@ -177,6 +177,8 @@ using std::cout;
 namespace multi = boost::multi;
 
 int main(){
+	using multi::blas::gemv;
+	using std::abs;
 	{
 		multi::array<double, 2> const M = {
 			{ 9., 24., 30., 9.},
@@ -187,10 +189,11 @@ int main(){
 		multi::array<double, 1> const X = {1.1,2.1,3.1, 4.1};
 		multi::array<double, 1> Y = {4.,5.,6.};
 		double a = 1.1, b = 1.2;
-		multi::blas::gemv('T', a, M, X, b, Y); // y = a*M*x + b*y
+		gemv('T', a, M, X, b, Y); // y = a*M*x + b*y
 
 		multi::array<double, 1> const Y3 = {214.02, 106.43, 188.37}; // = 1.1 {{9., 24., 30., 9.}, {4., 10., 12., 7.}, {14., 16., 36., 1.}}.{1.1, 2.1, 3.1, 4.1} + 1.2 {4., 5., 6.}
-		assert( std::abs(Y[1] - Y3[1]) < 1e-14 );
+//		cout << abs(Y[1] - Y3[1]) << std::endl;
+		assert( abs(Y[1] - Y3[1]) < 1e-14 );
 	}
 	{
 		multi::array<double, 2> const M = {
@@ -202,10 +205,10 @@ int main(){
 		multi::array<double, 1> const X = {1.1,2.1,3.1};
 		multi::array<double, 1> Y = {4.,5.,6., 7.};
 		double a = 1.8, b = 1.6;
-		multi::blas::gemv('N', a, M, X, b, Y); // y = a*(M^T)*x + b*y, y^T = a*(x^T)*M + b*y^T
+		using multi::blas::gemv;
+		gemv('N', a, M, X, b, Y); // y = a*(M^T)*x + b*y, y^T = a*(x^T)*M + b*y^T
 		multi::array<double, 1> const Y3 = {117.46, 182.6, 315.24, 61.06}; // =1.8 Transpose[{{9., 24., 30., 9.}, {4., 10., 12., 7.}, {14., 16., 36., 1.}}].{1.1, 2.1, 3.1} + 1.6 {4., 5., 6., 7.}
-		cout << Y[2] - Y3[2] << std::endl;
-		assert( std::abs(Y[2] - Y3[2]) < 1e-13 );
+		assert( abs(Y[2] - Y3[2]) < 1e-13 );
 	}
 	{
 		multi::array<double, 2> const M = {
@@ -217,7 +220,7 @@ int main(){
 		multi::array<double, 1> const X = {1.1,2.1,3.1, 4.1};
 		multi::array<double, 1> Y = {4.,5.,6.};
 		double a = 1.1, b = 1.2;
-		multi::blas::gemv(a, M, X, b, Y); // y = a*M*x + b*y
+		gemv(a, M, X, b, Y); // y = a*M*x + b*y
 
 		multi::array<double, 1> const Y3 = {214.02, 106.43, 188.37}; // = 1.1 {{9., 24., 30., 9.}, {4., 10., 12., 7.}, {14., 16., 36., 1.}}.{1.1, 2.1, 3.1, 4.1} + 1.2 {4., 5., 6.}
 		assert( std::abs(Y[1] - Y3[1]) < 1e-14 );
@@ -228,15 +231,14 @@ int main(){
 			{24., 10., 16.},
 			{30., 12., 36.},
 			{9., 7., 1.}
-		};
-		assert( M[0][2] == 14. );
+		}; assert( M[0][2] == 14. );
 		multi::array<double, 1> const X = {1.1,2.1,3.1, 4.1};
 		multi::array<double, 1> Y = {4.,5.,6.};
 		double a = 1.1, b = 1.2;
-		multi::blas::gemv(a, rotated(M), X, b, Y); // y = a*M*x + b*y
+		gemv(a, rotated(M), X, b, Y); // y = a*M*x + b*y
 
 		multi::array<double, 1> const Y3 = {214.02, 106.43, 188.37}; // = 1.1 {{9., 24., 30., 9.}, {4., 10., 12., 7.}, {14., 16., 36., 1.}}.{1.1, 2.1, 3.1, 4.1} + 1.2 {4., 5., 6.}
-		assert( std::abs(Y[1] - Y3[1]) < 1e-14 );
+		assert( abs(Y[1] - Y3[1]) < 1e-14 );
 	}
 
 }
