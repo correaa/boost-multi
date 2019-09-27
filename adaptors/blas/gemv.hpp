835292ff202bef13ef0afc2fb1103da0a56a4b19
Alfredo Correa <alfredo.correa@gmail.com>
2019-09-27 16:14:52 -0700

    gemm and gemv work with c-arrays

--- adaptors/blas/gemv.hpp
+++ adaptors/blas/gemv.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION_INSTRUCTIONS
-(echo '#include"'$0'"'>$0.cpp)&& clang++ -O3 -std=c++14 -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_GEMV -DADD_ $0.cpp -o $0x \
+(echo '#include"'$0'"'>$0.cpp)&&clang++ -O3 -std=c++14 -Wall -Wextra -Wpedantic `#-Wfatal-errors` -D_TEST_MULTI_ADAPTORS_BLAS_GEMV -DADD_ $0.cpp -o $0x \
 -lblas \
 `#-Wl,-rpath,/usr/local/Wolfram/Mathematica/12.0/SystemFiles/Libraries/Linux-x86-64 -L/usr/local/Wolfram/Mathematica/12.0/SystemFiles/Libraries/Linux-x86-64 -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core` \
 &&$0x&& rm $0x $0.cpp; exit
@@ -22,10 +22,10 @@ struct conj{template<class T> auto operator()(T const& t) const{using std::conj;
 template<class Trans, class T, class ItA, class Size, class Itx, class Ity>
 Ity gemv_n(Trans IN, T a, ItA A_first, Size n, Itx x_first, T beta, Ity y_first){
 	assert( IN == 'N' or IN == 'T' or IN == 'C' );
-	assert( A_first->stride() == 1 );
-	gemv(IN, A_first->size(), n, a, base(A_first), stride(A_first), base(x_first), stride(x_first), beta, base(y_first), stride(y_first));
+//	assert( A_first->stride() == 1 );
+	gemv(IN, size(*A_first), n, a, base(A_first), stride(A_first), base(x_first), stride(x_first), beta, base(y_first), stride(y_first));
 	switch(IN){
-		case 'N' : return y_first + A_first->size();
+		case 'N' : return y_first + size(*A_first);
 		case 'T' : return y_first + n;
 		case 'C' : return y_first + n;
 	}
@@ -34,7 +34,7 @@ Ity gemv_n(Trans IN, T a, ItA A_first, Size n, Itx x_first, T beta, Ity y_first)
 
 template<class Trans, class T, class ItA, class Itx, class Ity>
 Ity gemv(Trans IN, T a, ItA A_first, ItA A_last, Itx x_first, T beta, Ity y_first){
-	assert( stride(A_first) == stride(A_last) and A_first->size()==A_last->size() );
+//	assert( stride(A_first) == stride(A_last) and A_first->size()==A_last->size() );
 	return gemv_n(IN, a, A_first, std::distance(A_first, A_last), x_first, beta, y_first);
 }
 
@@ -43,6 +43,7 @@ Y1D&& gemv(Trans IN, T a, A2D const& A, X1D const& x, T beta, Y1D&& y){
 	assert( not(IN == 'T') or size(A) == size(y) );
 	assert( not(IN == 'N') or size(A) == size(x) );
 	assert( not(IN == 'C') or size(A) == size(x) );
+	using std::begin; using std::end;
 	auto e = gemv(IN, a, begin(A), end(A), begin(x), beta, begin(y)); (void)e;
 	assert( e == end(y) );
 	return std::forward<Y1D>(y);
@@ -54,7 +55,9 @@ Y1D&& gemv(T a, A2D const& A, X1D const& x, T beta, Y1D&& y){
 }
 
 template<class A2D, class X1D, class Y1D>
-Y1D&& gemv(A2D const& A, X1D const& x, Y1D&& y){return gemv(1., A, x, 0., y);}
+Y1D&& gemv(A2D const& A, X1D const& x, Y1D&& y){
+	return gemv(1., A, x, 0., std::forward<Y1D>(y));
+}
 
 //template<class A, class B, class RowIt, class ConstIt, class It>
 //It gemv(A const& a, RowIt M_first, RowIt M_last, ConstIt X_first, B const& b, It Y_first){
@@ -199,6 +202,21 @@ int main(){
 		assert( abs(Y[1] - Y3[1]) < 2e-14 );
 	}
 	{
+		double const M[3][4] = {
+			{ 9., 24., 30., 9.},
+			{ 4., 10., 12., 7.},
+			{14., 16., 36., 1.}
+		};
+		assert( M[2][0] == 14. );
+		double const X[4] = {1.1,2.1,3.1, 4.1};
+		double Y[3] = {4.,5.,6.};
+		double const a = 1.1;
+		double const b = 1.2;
+		gemv('T', a, M, X, b, Y); // y = a*M*x + b*y
+		double Y3[3] = {214.02, 106.43, 188.37};
+		assert( abs(Y[1] - Y3[1]) < 2e-14 );
+	}
+	{
 		multi::array<double, 2> const M = {
 			{ 9., 24., 30., 9.},
 			{ 4., 10., 12., 7.},
@@ -208,7 +226,6 @@ int main(){
 		multi::array<double, 1> const X = {1.1,2.1,3.1};
 		multi::array<double, 1> Y = {4.,5.,6., 7.};
 		double a = 1.8, b = 1.6;
-		using multi::blas::gemv;
 		gemv('N', a, M, X, b, Y); // y = a*(M^T)*x + b*y, y^T = a*(x^T)*M + b*y^T
 		multi::array<double, 1> const Y3 = {117.46, 182.6, 315.24, 61.06}; // =1.8 Transpose[{{9., 24., 30., 9.}, {4., 10., 12., 7.}, {14., 16., 36., 1.}}].{1.1, 2.1, 3.1} + 1.6 {4., 5., 6., 7.}
 		assert( abs(Y[2] - Y3[2]) < 1e-13 );
@@ -224,8 +241,21 @@ int main(){
 		multi::array<double, 1> Y = {4.,5.,6.};
 		double a = 1.1, b = 1.2;
 		gemv(a, M, X, b, Y); // y = a*M*x + b*y
-
 		multi::array<double, 1> const Y3 = {214.02, 106.43, 188.37}; // = 1.1 {{9., 24., 30., 9.}, {4., 10., 12., 7.}, {14., 16., 36., 1.}}.{1.1, 2.1, 3.1, 4.1} + 1.2 {4., 5., 6.}
+		assert( std::abs(Y[1] - Y3[1]) < 2e-14 );
+	}
+	{
+		double const M[3][4] = {
+			{ 9., 24., 30., 9.},
+			{ 4., 10., 12., 7.},
+			{14., 16., 36., 1.}
+		};
+		assert( M[2][0] == 14. );
+		double const X[4] = {1.1,2.1,3.1, 4.1};
+		double Y[3] = {4.,5.,6.};
+		double a = 1.1, b = 1.2;
+		gemv(a, M, X, b, Y); // y = a*M*x + b*y
+		double const Y3[3] = {214.02, 106.43, 188.37};
 		assert( std::abs(Y[1] - Y3[1]) < 2e-14 );
 	}
 	{
