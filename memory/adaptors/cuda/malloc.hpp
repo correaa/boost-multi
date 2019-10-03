b8ae81f2b8982d204845c81ed419e88dc1a0dd19
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-02 23:23:42 -0700

    pkg-config for cuda

--- memory/adaptors/cuda/malloc.hpp
+++ memory/adaptors/cuda/malloc.hpp
@@ -1,12 +1,12 @@
 #ifdef COMPILATION_INSTRUCTIONS
-(echo '#include"'$0'"'>$0.cpp)&& `#nvcc -ccbin=cuda-`c++ -D_TEST_MULTI_MEMORY_ADAPTORS_CUDA_MALLOC $0.cpp -o $0x -lcudart &&$0x&& rm $0x; exit
+(echo '#include"'$0'"'>$0.cpp)&& `#nvcc -ccbin=cuda-`c++ -D_TEST_MULTI_MEMORY_ADAPTORS_CUDA_MALLOC `pkg-config cudart --cflags --libs` $0.cpp -o $0x &&$0x&&rm $0x; exit
 #endif
 
 #ifndef MULTI_MEMORY_ADAPTORS_CUDA_MALLOC
 #define MULTI_MEMORY_ADAPTORS_CUDA_MALLOC
 
-#include "../cuda/clib.hpp"
-#include "../cuda/ptr.hpp"
+#include "../../adaptors/cuda/clib.hpp"
+#include "../../adaptors/cuda/ptr.hpp"
 
 namespace boost{namespace multi{
 namespace memory{namespace cuda{
