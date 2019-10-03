5483c13c36bb6fe058a70703f3d9e86ebc48b488
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-03 00:13:01 -0700

    rollback element access example

--- memory/adaptors/cuda/tests/array.cpp
+++ memory/adaptors/cuda/tests/array.cpp
@@ -12,6 +12,6 @@ int main(){
 	multi::array<double, 2> A2({32, 64}, double{}); 
 	A2[2][4] = 8.;
 	multi::array<double, 2, cuda::allocator<double>> A2_gpu = A2;
-//	assert( A2_gpu[2][4] == 8. );
+	assert( A2_gpu[2][4] == 8. );
 }
 
