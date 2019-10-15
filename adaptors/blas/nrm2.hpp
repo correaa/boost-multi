846a2898974806b70beb4ec997298034c1728842
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-15 10:14:44 -0700

    added nrm2 _n overload

--- adaptors/blas/nrm2.hpp
+++ adaptors/blas/nrm2.hpp
@@ -1,7 +1,7 @@
 #ifdef COMPILATION_INSTRUCTIONS
-(echo "#include\""$0"\"" > $0x.cpp) && clang++ `#-DNDEBUG` -O3 -std=c++14 -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_NRM2 -DADD_ $0x.cpp -o $0x.x -lblas && time $0x.x $@ && rm -f $0x.x $0x.cpp; exit
+(echo '#include"'$0'"'>$0.cpp)&&c++ -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_NRM2 .DCATCH_CONFIG_MAIN $0.cpp -o$0x `pkg-config --cflags --libs blas`&&$0x&&rm $0x $0.cpp; exit
 #endif
-// Alfredo A. Correa 2019 ©
+// © Alfredo A. Correa 2019
 
 #ifndef MULTI_ADAPTORS_BLAS_NRM2_HPP
 #define MULTI_ADAPTORS_BLAS_NRM2_HPP
@@ -14,30 +14,24 @@ namespace blas{
 
 template<class It, class Size>
 auto nrm2_n(It first, Size n)
-->decltype(nrm2(n, base(first), stride(first)))
-{
-	return nrm2(n, base(first), stride(first));
-}
+->decltype(nrm2(n, base(first), stride(first))){
+	return nrm2(n, base(first), stride(first));}
 
 template<class It>
-auto nrm2(It first, It last)
-->decltype(nrm2_n(first, std::distance(first, last)))
-{
-	assert( stride(first) == stride(last) );
-	return nrm2_n(first, std::distance(first, last));
-}
+auto nrm2(It f, It l)
+->decltype(nrm2_n(f, std::distance(f, l))){assert(stride(f)==stride(l));
+	return nrm2_n(f, std::distance(f, l));}
 
 template<class X1D> 
 auto nrm2(X1D const& x)
-->decltype(nrm2(begin(x), end(x)))
-{ 
-	assert( not offset(x) );
-	return nrm2(begin(x), end(x));
-}
+->decltype(nrm2(begin(x), end(x))){assert( not offset(x) );
+	return nrm2(begin(x), end(x));}
 
 }}}
 
 #if _TEST_MULTI_ADAPTORS_BLAS_NRM2
+
+#include<catch.hpp>
 
 #include "../../array.hpp"
 #include "../../utility.hpp"
@@ -53,7 +47,7 @@ auto nrm2(X1D const& x)
 using std::cout;
 namespace multi = boost::multi;
 
-int main(){
+TEST_CASE("multi_adaptor_multi", "[nrm2]"){
 	multi::array<double, 2> const cA = {
 		{1.,  2.,  3.,  4.},
 		{5.,  6.,  7.,  8.},
@@ -61,7 +55,8 @@ int main(){
 	};
 	using multi::blas::nrm2;
 	using multi::blas::dot;
-	assert( nrm2(cA[1]) == std::sqrt(dot(cA[1], cA[1])) );
+	using std::sqrt;
+	REQUIRE( nrm2(cA[1]) == sqrt(dot(cA[1], cA[1])) );
 }
 
 #endif
