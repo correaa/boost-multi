4d675d148531cc4be1fd412b29f5c4c43d6bb969
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-22 18:18:34 -0700

    simplified fill test

--- tests/array_fill.cpp
+++ tests/array_fill.cpp
@@ -1,19 +1,13 @@
 #ifdef COMPILATION_INSTRUCTIONS
-$CXX catch_main.o -std=c++17 -Wall -Wextra $0 -o$0x && $0x $@ && rm $0x; exit
+c++ .DCATCH_CONFIG_MAIN -std=c++17 -Wall -Wextra $0 -o$0x &&$0x&&rm $0x; exit
 #endif
 
-#include<catch2/catch.hpp>
-
-#include<iostream>
-
-#include "../array_ref.hpp"
 #include "../array.hpp"
+#include<catch.hpp>
 
 namespace multi = boost::multi;
 
 TEST_CASE("Array fill", "[array]"){
-	using std::fill;
-	using std::all_of;
 
 	multi::array<double, 2> d2D = {
 		{150., 16., 17., 18., 19.},
@@ -21,12 +15,18 @@ TEST_CASE("Array fill", "[array]"){
 		{100., 11., 12., 13., 14.}, 
 		{ 50.,  6.,  7.,  8.,  9.}  
 	};
-	REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto&& e){return e == 5.;}) );
+	using std::all_of;
+	REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==5.;}) );
+
+	using std::fill;
 
 	fill(begin(d2D[1]), end(d2D[1]), 8.);
-	REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto&& e){return e == 8.;}) );
+	REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==8.;}) );
 
-	fill(begin(d2D({0, 4}, 1)), end(d2D({0, 4}, 1)), 9.);
-	REQUIRE( all_of(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), [](auto&& e){return e == 9.;}) );
+	fill(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), 8.);
+	REQUIRE( all_of(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), [](auto&& e){return e==8.;}) );
+
+//	fill( d2D({0, 4}).begin(1), d2D({0, 4}).end(1), 9.);
+//	fill(begin(d2D({0, 4}, 1)), end(d2D({0, 4}, 1)), 9.);
 }
 
