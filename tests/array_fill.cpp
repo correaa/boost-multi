e4af71c20b6cea4c92ca95ed32ff88b44f633141
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-24 01:27:08 -0700

    fill test transition to Boost.Test

--- tests/array_fill.cpp
+++ tests/array_fill.cpp
@@ -1,13 +1,14 @@
 #ifdef COMPILATION_INSTRUCTIONS
-$CXX .DCATCH_CONFIG_MAIN -std=c++14 -Wall -Wextra $0 -o$0x &&$0x&&rm $0x; exit
+$CXX -std=c++14 -Wall -Wextra -Wpedantic $0 -DBOOST_TEST_DYN_LINK -lboost_unit_test_framework -o $0x &&$0x $@&&rm $0x;exit
 #endif
 
 #include "../array.hpp"
-#include<catch.hpp>
 
-namespace multi = boost::multi;
+#define BOOST_TEST_MODULE "C++ Unit Tests for Multi fill"
+#include<boost/test/unit_test.hpp>
 
-TEST_CASE("Array fill", "[array]"){
+BOOST_AUTO_TEST_CASE(fill){
+	namespace multi = boost::multi;
 
 	multi::array<double, 2> d2D = {
 		{150., 16., 17., 18., 19.},
@@ -16,15 +17,15 @@ TEST_CASE("Array fill", "[array]"){
 		{ 50.,  6.,  7.,  8.,  9.}  
 	};
 	using std::all_of;
-	REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==5.;}) );
+	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==5.;}) );
 
 	using std::fill;
 
 	fill(begin(d2D[1]), end(d2D[1]), 8.);
-	REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==8.;}) );
+	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==8.;}) );
 
 	fill(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), 8.);
-	REQUIRE( all_of(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), [](auto&& e){return e==8.;}) );
+	BOOST_REQUIRE( all_of(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), [](auto&& e){return e==8.;}) );
 
 //	fill( d2D({0, 4}).begin(1), d2D({0, 4}).end(1), 9.);
 //	fill(begin(d2D({0, 4}, 1)), end(d2D({0, 4}, 1)), 9.);
