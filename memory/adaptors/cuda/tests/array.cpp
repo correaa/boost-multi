8e853a0631eae774a209982edfb9aebc13c41338
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-30 10:40:37 -0700

    cuda managed example

--- memory/adaptors/cuda/tests/array.cpp
+++ memory/adaptors/cuda/tests/array.cpp
@@ -2,21 +2,25 @@
 nvcc -x cu --expt-relaxed-constexpr -O3 -ccbin=c++ -std=c++14 $0 -o $0x -DBOOST_TEST_DYN_LINK -lboost_unit_test_framework -D_DISABLE_CUDA_SLOW -lcudart &&$0x&&rm $0x; exit
 #endif
 
-#include "../../cuda/allocator.hpp"
-#include "../../../../array.hpp"
-
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi CUDA allocators"
 #include<boost/test/unit_test.hpp>
+
+#include "../../cuda/allocator.hpp"
+#include "../../../../../multi/array.hpp"
 
 namespace multi = boost::multi;
 namespace cuda = multi::memory::cuda;
 
 BOOST_AUTO_TEST_CASE(cuda_allocators){
-	multi::array<double, 2> A2({32, 64}, double{}); A2[2][4] = 8.;
-	multi::array<double, 2, cuda::allocator<double>> A2_gpu = A2;
-	multi::array<double, 2, cuda::managed::allocator<double>> A2_mgpu = A2;
+	multi::array<double, 2> A2({32, 64}); 
+	A2[2][4] = 8.;
 	BOOST_REQUIRE( A2[2][4] == 8. );
+
+	multi::array<double, 2, cuda::allocator<double>> A2_gpu = A2;
+
 	BOOST_REQUIRE( A2_gpu[2][4] == 8. );
+
+	multi::array<double, 2, cuda::managed::allocator<double>> A2_mgpu = A2;
 	BOOST_REQUIRE( A2_mgpu[2][4] == 8. );
 }
 
