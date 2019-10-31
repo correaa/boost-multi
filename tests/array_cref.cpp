783a111c7ec00e87bf104810e093ed69f3e2674f
Alfredo Correa <alfredo.correa@gmail.com>
2019-10-31 01:46:52 -0700

    ref test with Boost.Test

--- tests/array_cref.cpp
+++ tests/array_cref.cpp
@@ -1,44 +1,46 @@
 #ifdef COMPILATION_INSTRUCTIONS
-$CXX -O3 -std=c++17 -Wall -Wextra -Wpedantic `#-Wfatal-errors` $0 -o $0.x && $0.x $@ && rm -f $0.x; exit
+$CXX -O3 -Wall -Wextra $0 -o $0x -DBOOST_TEST_DYN_LINK -lboost_unit_test_framework &&$0x&&rm $0x; exit
 #endif
 
-#include "../array_ref.hpp"
+#define BOOST_TEST_MODULE "C++ Unit Tests for Multi References"
+#include<boost/test/unit_test.hpp>
+
 #include "../array.hpp"
 
-#include<cassert>
-#include<iostream>
 #include<vector>
 #include<complex>
 
-using std::cout; using std::cerr;
 namespace multi = boost::multi;
 
-int main(){
+BOOST_AUTO_TEST_CASE(array_cref){
+	using complex = std::complex<double>;
 
-	static_assert(std::is_same<std::pointer_traits<std::complex<double>*>::element_type, std::complex<double>>{}, "!");
-	static_assert(std::is_same<std::pointer_traits<std::complex<double>*>::rebind<std::complex<double> const>, std::complex<double> const*>{}, "!");
+	static_assert(std::is_same<std::pointer_traits<complex*>::element_type, complex>{}, "!");
+	static_assert(std::is_same<std::pointer_traits<complex*>::rebind<complex const>, complex const*>{}, "!");
 
-	std::vector<std::complex<double>> d(100);
-	std::vector<std::complex<double>> const dc(100);
+	std::vector<complex> d(100);
+	std::vector<complex> const dc(100);
 
-	multi::array_ref<std::complex<double>, 2> A2D(d.data(), multi::iextensions<2>{10, 10});
-	multi::array_ref<std::complex<double>, 2, std::complex<double>*> B2D(d.data(), {10, 10});
+	multi::array_ref<complex, 2> A2D(d.data(), multi::iextensions<2>{10, 10});
+	multi::array_ref<complex, 2, complex*> B2D(d.data(), {10, 10});
 	
-	assert( &A2D[3][4] == &B2D[3][4] );
+	BOOST_REQUIRE( &A2D[3][4] == &B2D[3][4] );
 	
 //	multi::array_ref<std::complex<double>, 2> C2D(dc.data(), {10, 10}); // error double const* -> double*
-	multi::array_ref<std::complex<double>, 2, std::complex<double> const*> D2D(dc.data(), {10, 10});
-	multi::array_cref<std::complex<double>, 2> E2D(dc.data(), {10, 10});
-	multi::array_cref<std::complex<double>, 2> F2D(d.data(), {10, 10});
+	multi::array_ref<complex, 2, complex const*> D2D(dc.data(), {10, 10});
+	multi::array_cref<complex, 2> E2D(dc.data(), {10, 10});
+	multi::array_cref<complex, 2> F2D(d.data(), {10, 10});
 //	F2D[3][4] = 4.; // error, not assignable
 	A2D[7][8] = 3.;
-	assert( F2D[7][8] == 3. );
-	assert( &A2D[7][8] == &F2D[7][8] );
+	BOOST_REQUIRE( F2D[7][8] == 3. );
+	BOOST_REQUIRE( &A2D[7][8] == &F2D[7][8] );
 
 #if __cpp_deduction_guides
-	multi::array_ref G2D(dc.data(), {10, 10}); assert( G2D == D2D );
+	multi::array_ref G2D(dc.data(), {10, 10}); 
+	BOOST_REQUIRE( G2D == D2D );
 #endif
-	auto&& H2D = multi::make_array_ref<2>(dc.data(), {10, 10}); assert( H2D == D2D );
+	auto&& H2D = multi::make_array_ref<2>(dc.data(), {10, 10}); 
+	BOOST_REQUIRE( H2D == D2D );
 
 }
 
