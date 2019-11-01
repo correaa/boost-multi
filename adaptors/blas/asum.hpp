fe82bfdac7b1fb8b9d31b73eb8e34b4bb6f99bfa
Alfredo Correa <alfredo.correa@gmail.com>
2019-11-01 02:38:00 -0700

    asum iterator interface

--- adaptors/blas/asum.hpp
+++ adaptors/blas/asum.hpp
@@ -1,7 +1,7 @@
 #ifdef COMPILATION_INSTRUCTIONS
-(echo "#include\""$0"\"" > $0x.cpp) && clang++ `#-DNDEBUG` -O3 -std=c++14 -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_SCAL -DADD_ $0x.cpp -o $0x.x -lblas && time $0x.x $@ && rm -f $0x.x $0x.cpp; exit
+(echo '#include"'$0'"'>$0.cpp)&&c++ -Wall -Wextra -Wpedantic -D_TEST_MULTI_ADAPTORS_BLAS_SCAL .DCATCH_CONFIG_MAIN $0.cpp -o$0x `pkg-config --cflags --libs blas` &&$0x&& rm $0x $0.cpp; exit
 #endif
-// Alfredo A. Correa 2019 ©
+// © Alfredo A. Correa 2019
 
 #ifndef MULTI_ADAPTORS_BLAS_DOT_HPP
 #define MULTI_ADAPTORS_BLAS_DOT_HPP
@@ -9,49 +9,73 @@
 #include "../blas/core.hpp"
 
 namespace boost{
-namespace multi{
-namespace blas{
+namespace multi{namespace blas{
+
+template<class It, typename Size>
+auto asum_n(It first, Size n)
+->decltype(asum(n, base(first), stride(first))){
+	return asum(n, base(first), stride(first));}
+
+using std::distance;
 
 template<class It>
-auto asum(It first, It last){
-	assert( stride(first) == stride(last) );
-	return asum(std::distance(first, last), base(first), stride(first));
-}
+auto asum(It f, It last)
+->decltype(asum_n(f, distance(f, last))){assert(stride(f) == stride(last));
+	return asum_n(f, distance(f, last));}
+
+using std::begin; using std::end;
 
 template<class X1D> 
-auto asum(X1D const& x){
-	assert( not offset(x) );
-//	using blas::asum;
-	return asum(size(x), origin(x), stride(x));
-}
+auto asum(X1D const& x)
+->decltype(asum(begin(x), end(x))){assert( not offset(x) );
+	return asum(begin(x), end(x));}
 
-}}}
+}}
+}
 
 #if _TEST_MULTI_ADAPTORS_BLAS_SCAL
 
+#include<catch.hpp>
+
 #include "../../array.hpp"
-#include "../../utility.hpp"
+//#include "../../utility.hpp"
 
-#include<complex>
-#include<cassert>
-#include<iostream>
-#include<numeric>
-#include<algorithm>
+#include<numeric> // accumulate
 
-using std::cout;
 namespace multi = boost::multi;
 
-int main(){
-	multi::array<double, 2> const cA = {
+using complex = std::complex<double>;
+using multi::blas::asum;
+
+TEST_CASE("multi::blas::asum double", "double"){
+	multi::array<double, 2> const A = {
 		{1.,  2.,  3.,  4.},
-		{5.,  6.,  7.,  8.},
+		{-5.,  6.,  -7.,  8.},
 		{9., 10., 11., 12.}
 	};
-	multi::array<std::complex<double>, 2> A = cA;
-	using multi::blas::asum;
-	assert(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a + std::abs(real(b)) + std::abs(imag(b));}));
-
+	REQUIRE(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a+abs(b);}));
 }
+
+TEST_CASE("multi::blas::asum complex", "complex"){
+	constexpr complex I{0, 1};
+	multi::array<complex, 2> const A = {
+		{ 1. + 1.*I,  2.,  3.,  4.},
+		{-5. + 3.*I,  6.,  -7.,  8.},
+		{ 9. - 2.*I, 10., 11., 12.}
+	};
+	REQUIRE(asum(rotated(A)[0]) == 1.+1. + 5.+3. + 9.+2.);
+}
+
+TEST_CASE("multi::blas::asum double c-array", "double"){
+	double A[3][4] = {
+		{1.,  2.,  3.,  4.},
+		{-5.,  6.,  -7.,  8.},
+		{9., 10., 11., 12.}
+	};
+	using std::begin; using std::end;
+	REQUIRE(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a+abs(b);}));
+}
+
 
 #endif
 #endif
