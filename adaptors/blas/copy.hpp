ba5c427ee20acd9dda65eb008bee0fe201e07635
Alfredo Correa <alfredo.correa@gmail.com>
2019-11-18 22:13:01 -0800

    blas copy value semantic interface

--- adaptors/blas/copy.hpp
+++ adaptors/blas/copy.hpp
@@ -1,7 +1,7 @@
 #ifdef COMPILATION_INSTRUCTIONS
-(echo "#include\""$0"\"" > $0x.cpp) && clang++ `#-DNDEBUG` -O3 -std=c++14 -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_COPY -DADD_ $0x.cpp -o $0x.x -lblas && time $0x.x $@ && rm -f $0x.x $0x.cpp; exit
+(echo '#include"'$0'"'>$0.cpp)&&c++ -Wall -Wextra -Wpedantic -Wfatal-errors -D_TEST_MULTI_ADAPTORS_BLAS_COPY $0.cpp -o $0x `pkg-config --cflags --libs blas`&&$0x&&rm $0x $0.cpp;exit
 #endif
-// Alfredo A. Correa 2019 ©
+// © Alfredo A. Correa 2019
 
 #ifndef MULTI_ADAPTORS_BLAS_COPY_HPP
 #define MULTI_ADAPTORS_BLAS_COPY_HPP
@@ -32,10 +32,10 @@ Y1D&& copy(X1D const& x, Y1D&& y){
 	return std::forward<Y1D>(y);
 }
 
-template<class X1D>
-multi::array<typename X1D::value_type, 1> copy(X1D const& x){
+template<class X1D, class Ret = typename X1D::decay_type> // TODO multi::array_traits<X1D>::decay_type
+Ret copy(X1D const& x){
 	assert( not offset(x) );
-	return copy(x, multi::array<typename X1D::value_type, 1>({0, size(x)}, 0.));
+	return copy(x, Ret(size(x), get_allocator(x)));
 }
 
 }}}
@@ -47,21 +47,37 @@ multi::array<typename X1D::value_type, 1> copy(X1D const& x){
 
 #include<complex>
 #include<cassert>
-#include<iostream>
-#include<numeric>
-#include<algorithm>
-
-#include "../blas/dot.hpp"
 
 using std::cout;
 namespace multi = boost::multi;
 
 int main(){
-	multi::array<double, 1> const A = {1., 2., 3., 4.};
-	multi::array<double, 1> B = {5., 6., 7., 8.};
-	using multi::blas::copy;
-	copy(A, B);
-	assert( B == A );
+	{
+		multi::array<double, 1> const A = {1., 2., 3., 4.};
+		multi::array<double, 1> B = {5., 6., 7., 8.};
+		using multi::blas::copy;
+		copy(A, B);
+		assert( B == A );
+	}
+	{
+		using complex = std::complex<double>;
+		multi::array<complex, 1> const A = {1., 2., 3., 4.};
+		multi::array<complex, 1> B = {5., 6., 7., 8.};
+		using multi::blas::copy;
+		copy(A, B);
+		assert( B == A );		
+	}
+	{
+		multi::array<double, 2> const A = {
+			{1., 2., 3.},
+			{4., 5., 6.},
+			{7., 8., 9.}
+		};
+		multi::array<double, 1> B(3);
+		using multi::blas::copy;
+		copy(rotated(A)[0], B);
+		assert( B == rotated(A)[0] );
+	}
 }
 
 #endif
