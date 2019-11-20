5cfc85f82ea1e42a16a0078d47d24df9cf512846
Alfredo Correa <alfredo.correa@gmail.com>
2019-11-20 02:13:10 -0800

    move real example herk to the end

--- adaptors/blas/test/herk.cpp
+++ adaptors/blas/test/herk.cpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION_INSTRUCTIONS
-`#nvcc -x cu --expt-relaxed-constexpr`clang++ -Wall -Wextra -Wpedantic -Wfatal-errors -Wno-deprecated-declarations $0 -o $0x -lcudart -lcublas -lboost_unit_test_framework \
+nvcc -x cu --expt-relaxed-constexpr`#clang++ -Wall -Wextra -Wpedantic -Wfatal-errors` -Wno-deprecated-declarations $0 -o $0x -lcudart -lcublas -lboost_unit_test_framework \
 `pkg-config --libs blas`&&$0x&&rm $0x;exit
 #endif
 // © Alfredo A. Correa 2019
