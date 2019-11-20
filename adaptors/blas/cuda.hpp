afc14d6d7510bddec9eafd91c15a715e66ba5daa
Alfredo Correa <alfredo.correa@gmail.com>
2019-11-19 18:26:42 -0800

    scal and copy in blascu adaptor

--- adaptors/blas/cuda.hpp
+++ adaptors/blas/cuda.hpp
@@ -6,9 +6,9 @@
 #ifndef MULTI_ADAPTORS_BLAS_CUDA_HPP
 #define MULTI_ADAPTORS_BLAS_CUDA_HPP
 
-#ifdef MULTI_ADAPTORS_BLAS_GEMM_HPP
-#error "blas/cuda.hpp must be included before blas/gemm.hpp"
-#endif
+//#ifdef MULTI_ADAPTORS_BLAS_GEMM_HPP
+//#error "blas/cuda.hpp must be included before blas/gemm.hpp"
+//#endif
 //#include "../../utility.hpp"
 //#include "../../array.hpp" // allocating multi::arrays for output
 #include "../../memory/adaptors/cuda/ptr.hpp"
@@ -21,7 +21,7 @@
 
 namespace boost{
 namespace multi{
-namespace blas{
+//namespace blas{
 
 template<class T> class cublas;
 
@@ -29,14 +29,22 @@ template<>
 class cublas<double>{
 public:
 	template<class... Args>
+	static auto copy(Args... args){return cublasDcopy(args...);}
+	template<class... Args>
 	static auto gemm(Args... args){return cublasDgemm(args...);}
+	template<class... Args>
+	static auto scal(Args... args){return cublasDscal(args...);}
 };
 
 template<>
 class cublas<float>{
 public:
 	template<class... Args>
+	static auto copy(Args... args){return cublasScopy(args...);}
+	template<class... Args>
 	static auto gemm(Args... args){return cublasSgemm(args...);}
+	template<class... Args>
+	static auto scal(Args... args){return cublasSscal(args...);}
 };
 
 template<>
@@ -47,6 +55,18 @@ class cublas<std::complex<double>>{
 public:
 	template<class... Args>
 	static auto gemm(Args&&... args){return cublasZgemm(to_cu(std::forward<Args>(args))...);}
+	template<class... Args>
+	static auto herk(Args&&... args){return cublasZherk(to_cu(std::forward<Args>(args))...);}
+	template<class... Args>
+	static auto copy(Args&&... args){return cublasZcopy(to_cu(std::forward<Args>(args))...);}
+	template<class... Args>
+	static auto scal(Args&&... args)
+	->decltype(cublasZscal(to_cu(std::forward<Args>(args))...)){
+		return cublasZscal(to_cu(std::forward<Args>(args))...);}
+	template<class Handle, class Size, class... Args2>
+	static auto scal(Handle h, Size s, double* alpha, Args2&&... args2){
+		return cublasZdscal(h, s, alpha, to_cu(std::forward<Args2>(args2))...);
+	}
 };
 
 template<>
@@ -57,7 +77,108 @@ class cublas<std::complex<float>>{
 public:
 	template<class... Args>
 	static auto gemm(Args&&... args){return cublasCgemm(to_cu(std::forward<Args>(args))...);}
+	template<class... Args>
+	static auto herk(Args&&... args){return cublasCherk(to_cu(std::forward<Args>(args))...);}
+	template<class... Args>
+	static auto copy(Args&&... args){return cublasCcopy(to_cu(std::forward<Args>(args))...);}
+	template<class... Args>
+	static auto scal(Args&&... args)
+	->decltype(cublasZscal(to_cu(std::forward<Args>(args))...)){
+		return cublasZscal(to_cu(std::forward<Args>(args))...);}
+	template<class Handle, class Size, class... Args2>
+	static auto scal(Handle h, Size s, float* alpha, Args2&&... args2){
+		return cublasZdscal(h, s, alpha, to_cu(std::forward<Args2>(args2))...);
+	}
 };
+
+namespace memory{
+namespace cuda{
+
+template<class T, class TA, class S> 
+void scal(S n, TA a, multi::memory::cuda::ptr<T> x, S incx){
+	cublasHandle_t handle;
+	{cublasStatus_t s = cublasCreate(&handle); assert(s==CUBLAS_STATUS_SUCCESS);}
+	cublasStatus_t s = cublas<T>::scal(handle, n, &a, static_cast<T*>(x), incx);
+	if(s!=CUBLAS_STATUS_SUCCESS){
+		std::cerr << [&](){switch(s){
+			case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
+			case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
+			case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
+			case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE";
+			case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH";
+			case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
+			case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
+			case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR";
+			case CUBLAS_STATUS_NOT_SUPPORTED: return "CUBLAS_STATUS_NOT_SUPPORTED";
+			case CUBLAS_STATUS_LICENSE_ERROR: return "CUBLAS_STATUS_LICENSE_ERROR";
+		} return "<unknown>";}() << std::endl;
+	}
+	assert( s==CUBLAS_STATUS_SUCCESS ); (void)s;
+	cublasDestroy(handle);
+}
+
+template<class Tconst, class T, class S> 
+void copy(S n, multi::memory::cuda::ptr<Tconst> x, S incx, multi::memory::cuda::ptr<T> y, S incy){
+	cublasHandle_t handle;
+	{cublasStatus_t s = cublasCreate(&handle); assert(s==CUBLAS_STATUS_SUCCESS);}
+	cublasStatus_t s = cublas<T>::copy(handle, n, static_cast<T const*>(x), incx, static_cast<T*>(y), incy);
+	if(s!=CUBLAS_STATUS_SUCCESS){
+		std::cerr << [&](){switch(s){
+			case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
+			case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
+			case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
+			case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE";
+			case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH";
+			case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
+			case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
+			case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR";
+			case CUBLAS_STATUS_NOT_SUPPORTED: return "CUBLAS_STATUS_NOT_SUPPORTED";
+			case CUBLAS_STATUS_LICENSE_ERROR: return "CUBLAS_STATUS_LICENSE_ERROR";
+		} return "<unknown>";}() << std::endl;
+	}
+	assert( s==CUBLAS_STATUS_SUCCESS ); (void)s;
+	cublasDestroy(handle);
+}
+
+//template<class T, class S> 
+//void copy(S n, multi::memory::cuda::ptr<T const> x, S incx, multi::memory::cuda::ptr<T> y, S incy){
+//}
+
+template<class T, class UL, class C, class S, class Real>
+void herk(UL ul, C transA, S n, S k, Real alpha, multi::memory::cuda::ptr<T const> A, S lda, Real beta, multi::memory::cuda::ptr<T> CC, S ldc){
+	cublasHandle_t handle;
+	{cublasStatus_t s = cublasCreate(&handle); assert(s==CUBLAS_STATUS_SUCCESS);}
+	cublasFillMode_t uplo = [ul](){
+		switch(ul){
+			case 'U': return CUBLAS_FILL_MODE_UPPER;
+			case 'L': return CUBLAS_FILL_MODE_LOWER;
+		} assert(0); return CUBLAS_FILL_MODE_UPPER;
+	}();
+	cublasOperation_t cutransA = [transA](){
+		switch(transA){
+			case 'N': return CUBLAS_OP_N;
+			case 'T': return CUBLAS_OP_T;
+			case 'C': return CUBLAS_OP_C;
+		} assert(0); return CUBLAS_OP_N;
+	}();
+	cublasStatus_t s = cublas<T>::herk(handle, uplo, cutransA, n, k, &alpha, static_cast<T const*>(A), lda, &beta, static_cast<T*>(CC), ldc);
+	if(s!=CUBLAS_STATUS_SUCCESS){
+		std::cerr << [&](){switch(s){
+			case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
+			case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
+			case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
+			case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE";
+			case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH";
+			case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
+			case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
+			case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR";
+			case CUBLAS_STATUS_NOT_SUPPORTED: return "CUBLAS_STATUS_NOT_SUPPORTED";
+			case CUBLAS_STATUS_LICENSE_ERROR: return "CUBLAS_STATUS_LICENSE_ERROR";
+		} return "<unknown>";}() << std::endl;
+	}
+	assert( s==CUBLAS_STATUS_SUCCESS ); (void)s;
+	cublasDestroy(handle);
+}
 
 template<typename T, typename AA, typename BB, class C, typename S>
 void gemm(C transA, C transB, S m, S n, S k, AA a, multi::memory::cuda::ptr<T const> A, S lda, multi::memory::cuda::ptr<T const> B, S ldb, BB beta, multi::memory::cuda::ptr<T> CC, S ldc){
@@ -98,12 +219,27 @@ void gemm(C transA, C transB, S m, S n, S k, AA a, multi::memory::cuda::ptr<T co
 	cublasDestroy(handle);
 }
 
-template<typename T, typename AA, typename BB, class C, class S>
-void gemm(C transA, C transB, S m, S n, S k, AA const& a, multi::memory::cuda::managed::ptr<T const> A, S lda, multi::memory::cuda::managed::ptr<T const> B, S ldb, BB const& beta, multi::memory::cuda::managed::ptr<T> CC, S ldc){
-	gemm(transA, transB, m, n, k, a, boost::multi::memory::cuda::ptr<T const>(A), lda, boost::multi::memory::cuda::ptr<T const>(B), ldb, beta, boost::multi::memory::cuda::ptr<T>(CC), ldc);
+
+}}}}
+
+namespace boost{namespace multi{namespace memory{namespace cuda{namespace managed{//namespace boost::multi::memory::cuda::managed{
+
+template<class T, class TA, class S> 
+void scal(S n, TA a, multi::memory::cuda::managed::ptr<T> x, S incx){
+	scal(n, a, multi::memory::cuda::ptr<T>(x), incx);
 }
 
-}}}
+template<typename AA, typename BB, class S>
+void gemm(char transA, char transB, S m, S n, S k, AA const& a, multi::memory::cuda::managed::ptr<double const> A, S lda, multi::memory::cuda::managed::ptr<double const> B, S ldb, BB const& beta, multi::memory::cuda::managed::ptr<double> CC, S ldc){
+	gemm(transA, transB, m, n, k, a, boost::multi::memory::cuda::ptr<double const>(A), lda, boost::multi::memory::cuda::ptr<double const>(B), ldb, beta, boost::multi::memory::cuda::ptr<double>(CC), ldc);
+}
+
+template<class T, class UL, class C, class S, class Real>
+void herk(UL ul, C transA, S n, S k, Real alpha, multi::memory::cuda::managed::ptr<T const> A, S lda, Real beta, multi::memory::cuda::managed::ptr<T> CC, S ldc){
+	herk(ul, transA, n, k, alpha, boost::multi::memory::cuda::ptr<T const>(A), lda, beta, boost::multi::memory::cuda::ptr<T>(CC), ldc);
+}
+
+}}}}}//}
 
 #if 0
 #define xgemm(T) template<class C, class S> v gemm(C transA, C transB, S m, S n, S k, T const& a, T const* A, S lda, T const* B, S ldb, T const& beta, T* CC, S ldc){BLAS(T##gemm)(transA, transB, BC(m), BC(n), BC(k), a, A, BC(lda), B, BC(ldb), beta, CC, BC(ldc));}
