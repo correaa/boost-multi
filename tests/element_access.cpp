786f4a13091173b2556b44358dbf5a56431556d3
Alfredo Correa <alfredo.correa@gmail.com>
2019-11-20 17:30:40 -0800

    element access/apply example

---
+++ tests/element_access.cpp
@@ -0,0 +1,28 @@
+#ifdef COMPILATION_INSTRUCTIONS
+$CXX -std=c++17 -Wall -Wextra -Wpedantic $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x; exit
+#endif
+
+#define BOOST_TEST_MODULE "C++ Unit Tests for Multi cuBLAS gemm"
+#define BOOST_TEST_DYN_LINK
+#include<boost/test/unit_test.hpp>
+
+#include "../array.hpp"
+
+#if __cpp_lib_apply>=201603
+#include<tuple> // apply
+#endif
+
+namespace multi = boost::multi;
+
+BOOST_AUTO_TEST_CASE(multi_tests_element_access){
+	multi::array<double, 2> m({3, 3}, {});
+	std::array<int, 2> p = {1, 2};
+	BOOST_REQUIRE( &m[p[0]][p[1]] == &m(p[0], p[1]) );
+
+#if __cpp_lib_apply>=201603
+	using std::apply; // needs C++17
+	BOOST_REQUIRE( &m[p[0]][p[1]] == &apply(m, p) );
+#endif
+
+}
+
