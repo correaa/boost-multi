f5fe07faeb8576ed314971148dc218efeba8522f
Alfredo Correa <alfredo.correa@gmail.com>
2019-11-21 16:43:17 -0800

    axpy asum tested with Boost.Test

--- adaptors/blas/tests/asum.cpp
+++ adaptors/blas/tests/asum.cpp
@@ -1,28 +1,29 @@
 #ifdef COMPILATION_INSTRUCTIONS
-$CXX -Wall -Wextra -Wpedantic $0 -o $0x -lblas `pkg-config --libs blas`&&$0x&&rm $0x; exit
+$CXX -Wall -Wextra -Wpedantic $0 -o $0x `pkg-config --libs blas` -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
+// © Alfredo A. Correa 2019
+
+#define BOOST_TEST_MODULE "C++ Unit Tests for Multi BLAS axpy"
+#define BOOST_TEST_DYN_LINK
+#include<boost/test/unit_test.hpp>
 
 #include "../../blas.hpp"
-
 #include "../../../array.hpp"
 
 #include<complex>
-#include<cassert>
 #include<numeric>
 
 using std::cout;
 namespace multi = boost::multi;
 
-int main(){
-	{
-		using Z = std::complex<double>; Z const I(0.,1.);
-		multi::array<Z, 2> const A = {
-			{1. + 2.*I,  2.,  3.,  4.},
-			{5.,  6. + 3.*I,  7.,  8.},
-			{9., 10., 11.+ 4.*I, 12.}
-		};
-		using multi::blas::asum;
-		assert(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a + std::abs(real(b)) + std::abs(imag(b));}));
-	}
+BOOST_AUTO_TEST_CASE(multi_blas_axpy){
+	using Z = std::complex<double>; Z const I{0, 1};
+	multi::array<Z, 2> const A = {
+		{1. + 2.*I,  2.,  3.,  4.},
+		{5.,  6. + 3.*I,  7.,  8.},
+		{9., 10., 11.+ 4.*I, 12.}
+	};
+	using multi::blas::asum;
+	BOOST_REQUIRE(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a + std::abs(real(b)) + std::abs(imag(b));}));
 }
 
