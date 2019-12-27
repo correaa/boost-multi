316aa40988d8678e0bf48c96036207013191693f
Alfredo Correa <alfredo.correa@gmail.com>
2019-12-27 01:20:34 -0800

    improved example for zero-D example

--- tests/zero_dimensionality.cpp
+++ tests/zero_dimensionality.cpp
@@ -16,32 +16,34 @@ $CXX -Wall -Wextra $0 -o$0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #include<complex>
 
 namespace multi = boost::multi;
-using std::cout; using std::cerr;
 
 BOOST_AUTO_TEST_CASE(zero_dimensionality){
-	std::vector<double> v1 = {1., 2., 3.};
-	multi::array_ref<double, 1> m1(v1.data(), 3);
-	BOOST_REQUIRE( size(m1) == 3 );
-	BOOST_REQUIRE( &m1[1] == &v1[1] );
-	BOOST_REQUIRE( num_elements(m1) == 3 );
+	{
+		std::vector<double> v1 = {1., 2., 3.};
 
-	multi::array_ref<double, 0> m0(v1.data());
-	BOOST_REQUIRE( data_elements(m0) == v1.data() );
-	BOOST_REQUIRE( num_elements(m0) == 1 );
-	m0 = 5.1;
-	BOOST_REQUIRE( v1[0] == 5.1 );
-	double d = m0;
-	BOOST_REQUIRE( d == 5.1 );
-	BOOST_REQUIRE( &m0 == v1.data() );
+		multi::array_ref<double, 1> m1(v1.data(), 3);
+		BOOST_REQUIRE( size(m1) == 3 );
+		BOOST_REQUIRE( &m1[1] == &v1[1] );
+		BOOST_REQUIRE( num_elements(m1) == 3 );
 
+		multi::array_ref<double, 0> m0(v1.data());
+		BOOST_REQUIRE( &m0 == v1.data() );
+		BOOST_REQUIRE( data_elements(m0) == v1.data() );
+		BOOST_REQUIRE( num_elements(m0) == 1 );
+
+		m0 = 5.1;
+		BOOST_REQUIRE( v1[0] == 5.1 );
+
+		double d = m0;
+		BOOST_REQUIRE( d == 5.1 );
+	}
 	{
 		multi::array<double, 0> a0 = 45.;
 		BOOST_REQUIRE( num_elements(a0) == 1 );
 		BOOST_REQUIRE( a0 == 45. );
+
 		a0 = 60.;
 		BOOST_REQUIRE( a0 == 60. );
 	}
-//	a0 = 45.;
-//	BOOST_REQUIRE( a0 == 45. );
 }
 
