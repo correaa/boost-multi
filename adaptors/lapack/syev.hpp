5f87eb942b9a9892d42c6ff23b4557cf7c8b5fae
Alfredo Correa <alfredo.correa@gmail.com>
2020-02-04 21:37:34 -0600

    added more sfinae to syev

--- adaptors/lapack/syev.hpp
+++ adaptors/lapack/syev.hpp
@@ -13,7 +13,6 @@
 
 #include<cassert>
 
-
 namespace boost{namespace multi{namespace lapack{
 
 using blas::filling;
@@ -22,7 +21,7 @@ using ::core::syev;
 
 template<class Array2D, class Array1D, class Array1DW>
 auto syev(blas::filling uplo, Array2D&& a, Array1D&& w, Array1DW&& work)
-->decltype(::core::syev('V', uplo==blas::filling::upper?'L':'U', size(a), base(a), stride(a), base(w), base(work), size(work), std::declval<int&>()), a({0l, 1l}, {0l, 1l}))
+->decltype(syev('V', uplo==blas::filling::upper?'L':'U', size(a), base(a), stride(a), base(w), base(work), size(work), std::declval<int&>()), a({0l, 1l}, {0l, 1l}))
 {
 	assert( size(work) >= std::max(1l, 3*size(a)-1l) );
 	assert( size(a) == size(w) );
@@ -30,25 +29,23 @@ auto syev(blas::filling uplo, Array2D&& a, Array1D&& w, Array1DW&& work)
 	assert( stride(work)==1 );
 	if(size(a)==0) return a();
 	int info = -1;
-	if(stride(rotated(a))==1){
-		::core::syev('V', uplo==blas::filling::upper?'L':'U', size(a), base(a), stride(a), base(w), base(work), size(work), info);
-	}else if(stride(a)==1){
-		::core::syev('V', uplo==blas::filling::upper?'U':'L', size(a), base(a), stride(rotated(a)), base(w), base(work), size(work), info);
-	}else assert(0); // case not contemplated by lapack
+	     if(stride(rotated(a))==1) syev('V', uplo==blas::filling::upper?'L':'U', size(a), base(a), stride(        a ), base(w), base(work), size(work), info);
+	else if(stride(        a )==1) syev('V', uplo==blas::filling::upper?'U':'L', size(a), base(a), stride(rotated(a)), base(w), base(work), size(work), info);
+	else                           assert(0); // case not contemplated by lapack
 	if(info < 0) assert(0); // bad argument
 	return a({0, size(a)-info}, {0, size(a)-info});
 }
 
 template<class Array2D, class Array1D, class Array1DW = typename std::decay_t<Array1D>::decay_type>
-decltype(auto) syev(blas::filling uplo, Array2D&& a, Array1D&& w){
-	return syev(uplo, a, w, Array1DW(std::max(1l, 3*size(a)-1l), get_allocator(w))); // TODO obtain automatic size from lapack info routine
-}
+auto syev(blas::filling uplo, Array2D&& a, Array1D&& w)
+->decltype(syev(uplo, std::forward<Array2D>(a), std::forward<Array1D>(w), Array1DW(std::max(1l, 3*size(a)-1l), get_allocator(w)))){
+	return syev(uplo, std::forward<Array2D>(a), std::forward<Array1D>(w), Array1DW(std::max(1l, 3*size(a)-1l), get_allocator(w)));}// TODO obtain automatic size from lapack info routine
 
 template<class Array2D, class Array1D>
 NODISCARD("because input array is const, output gives eigenvectors")
 typename Array2D::decay_type syev(blas::filling uplo, Array2D const& a, Array1D&& w){
 	auto ret = a.decay();
-	auto l = syev(uplo, ret, w);
+	auto l = syev(uplo, ret, std::forward<Array1D>(w));
 	if(size(l) != size(a)) assert(0); // failed
 	return ret;
 }
@@ -57,20 +54,17 @@ template<class Array2D>
 NODISCARD("because input array is const, output gives eigenvalues")
 auto syev(blas::filling uplo, Array2D&& a){
 	multi::array<typename std::decay_t<Array2D>::element_type, 1, decltype(get_allocator(a))> eigenvalues(size(a), get_allocator(a));
-	syev(uplo, a, eigenvalues);
+	syev(uplo, std::forward<Array2D>(a), eigenvalues);
 	return eigenvalues;
 }
 
 template<class Array2D>
-NODISCARD("because input array is const, output gives a structured binding eigenvectors and eigenvactor")
+NODISCARD("because input array is const, output gives a structured binding of eigenvectors and eigenvactor")
 auto syev(blas::filling uplo, Array2D const& a){
 	struct{
 		typename Array2D::decay_type eigenvectors;
-		multi::array<typename Array2D::element_type, 1, decltype(get_allocator(a))> eigenvalues;
-	} ret{
-		a.decay(), 
-		multi::array<typename Array2D::element_type, 1, decltype(get_allocator(a))>(size(a), get_allocator(a))
-	};
+		typename Array2D::value_type eigenvalues;
+	} ret{a, {size(a), get_allocator(a)}};
 	auto&& l = syev(uplo, ret.eigenvectors, ret.eigenvalues);
 	assert( size(l) == size(a) );
 	return ret;
