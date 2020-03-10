f0d6139a2acf0d809b11d1ab973e70be73cda800
Alfredo Correa <alfredo.correa@gmail.com>
2020-03-09 18:47:36 -0700

    added subrange test

---
+++ tests/subrange.cpp
@@ -0,0 +1,23 @@
+#ifdef COMPILATION_INSTRUCTIONS
+$CXX $0 -o $0x -DBOOST_TEST_DYN_LINK -lboost_unit_test_framework &&$0x&&rm $0x;exit
+#endif
+
+#define BOOST_TEST_MODULE "C++ Unit Tests for Multi range selection"
+#include<boost/test/unit_test.hpp>
+
+#include "../array.hpp"
+
+namespace multi = boost::multi;
+
+BOOST_AUTO_TEST_CASE(multi_array_range_section){
+
+	multi::array<double, 4> A({10, 20, 30, 40});
+
+	auto&& all = A({0, 10}, {0, 20}, {0, 30}, {0, 40});
+	BOOST_REQUIRE( &A[1][2][3][4] == &all[1][2][3][4] );
+
+	auto&& sub = A({0, 5}, {0, 10}, {0, 15}, {0, 20});
+	BOOST_REQUIRE( &sub[1][2][3][4] == &A[1][2][3][4] );
+
+}
+
