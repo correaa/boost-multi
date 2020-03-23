ddc92ee49a84b4be5561623c10728534fa17c8eb
Alfredo Correa <alfredo.correa@gmail.com>
2020-03-22 19:58:29 -0700

    indentation

--- tests/reinterpret_array_cast.cpp
+++ tests/reinterpret_array_cast.cpp
@@ -15,49 +15,51 @@ clang++ -std=c++14 --cuda-gpu-arch=sm_52 -Ofast -x cuda $0 -o $0x -ltbb -lcudart
 namespace multi = boost::multi;
 
 namespace{
-	template<class T> struct Complex_{T real; T imag; T const& re() const{return real;}};
-	BOOST_AUTO_TEST_CASE(multi_reinterpret_array_cast){
-		{
-			std::complex<double> c{1, 2};
-			auto pC = reinterpret_cast<Complex_<double>*>(&c);
-			pC->real = 11;
-			BOOST_REQUIRE(real(c)==11);
-		}
-		{
-			multi::array<std::complex<double>, 1> A(10); 
-			std::iota( begin(A), end(A), 1.);
-			BOOST_REQUIRE( A[8] == 9. );
-			auto&& A2 = multi::reinterpret_array_cast<Complex_<double>>(A);
-			A2[8].real = 1000.;
-			BOOST_REQUIRE( A[8] == 1000. );
-		}
-		{
-			multi::cuda::managed::array<std::complex<double>, 1> A(10);
-			A[8] = std::complex<double>{1000., 2000.};
-			auto&& A2 = multi::reinterpret_array_cast<Complex_<double>>(A);
-			BOOST_REQUIRE( A[8] == std::complex<double>(1000., 2000.) );
 
-			auto&& AR = multi::member_array_cast<double>(A2, &Complex_<double>::real);
-			auto&& AI = multi::member_array_cast<double>(A2, &Complex_<double>::imag);
+template<class T> struct Complex_{T real; T imag; T const& re() const{return real;}};
 
-			BOOST_REQUIRE( AR[8] == 1000. );
-			BOOST_REQUIRE( AI[8] == 2000. );
+BOOST_AUTO_TEST_CASE(multi_reinterpret_array_cast){
+{
+	std::complex<double> c{1, 2};
+	auto pC = reinterpret_cast<Complex_<double>*>(&c);
+	pC->real = 11;
+	BOOST_REQUIRE(real(c)==11);
+}
+{
+	multi::array<std::complex<double>, 1> A(10); 
+	std::iota( begin(A), end(A), 1.);
+	BOOST_REQUIRE( A[8] == 9. );
+	auto&& A2 = multi::reinterpret_array_cast<Complex_<double>>(A);
+	A2[8].real = 1000.;
+	BOOST_REQUIRE( A[8] == 1000. );
+}
+{
+	multi::cuda::managed::array<std::complex<double>, 1> A(10);
+	A[8] = std::complex<double>{1000., 2000.};
+	auto&& A2 = multi::reinterpret_array_cast<Complex_<double>>(A);
+	BOOST_REQUIRE( A[8] == std::complex<double>(1000., 2000.) );
 
-		//	auto&& ARP = multi::member_array_cast<double>(A2, &Complex_<double>::re);
-		}
-		{
-			multi::cuda::array<std::complex<double>, 1> A(10);
-			CUDA_SLOW(( A[8] = std::complex<double>{1000., 2000.} ));
-			auto&& A2 = multi::reinterpret_array_cast<Complex_<double>>(A);
-			BOOST_REQUIRE( CUDA_SLOW( A[8] == std::complex<double>(1000., 2000.) ) );
+	auto&& AR = multi::member_array_cast<double>(A2, &Complex_<double>::real);
+	auto&& AI = multi::member_array_cast<double>(A2, &Complex_<double>::imag);
 
-			auto&& AR = multi::member_array_cast<double>(A2, &Complex_<double>::real);
-			auto&& AI = multi::member_array_cast<double>(A2, &Complex_<double>::imag);
+	BOOST_REQUIRE( AR[8] == 1000. );
+	BOOST_REQUIRE( AI[8] == 2000. );
 
-			BOOST_REQUIRE( CUDA_SLOW( AR[8] == 1000. ) );
-			BOOST_REQUIRE( CUDA_SLOW( AI[8] == 2000. ) );
-		}
+//	auto&& ARP = multi::member_array_cast<double>(A2, &Complex_<double>::re);
+}
+{
+	multi::cuda::array<std::complex<double>, 1> A(10);
+	CUDA_SLOW(( A[8] = std::complex<double>{1000., 2000.} ));
+	auto&& A2 = multi::reinterpret_array_cast<Complex_<double>>(A);
+	BOOST_REQUIRE( CUDA_SLOW( A[8] == std::complex<double>(1000., 2000.) ) );
 
-	}
+	auto&& AR = multi::member_array_cast<double>(A2, &Complex_<double>::real);
+	auto&& AI = multi::member_array_cast<double>(A2, &Complex_<double>::imag);
+
+	BOOST_REQUIRE( CUDA_SLOW( AR[8] == 1000. ) );
+	BOOST_REQUIRE( CUDA_SLOW( AI[8] == 2000. ) );
+}
+}
+
 }
 
