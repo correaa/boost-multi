bccd17c2728c716332f0350029c12125d69464c6
Alfredo Correa <alfredo.correa@gmail.com>
2020-04-24 14:24:32 -0700

    array_iterator example

--- tests/array_iterator.cpp
+++ tests/array_iterator.cpp
@@ -1,7 +1,8 @@
-#ifdef COMPILATION_INSTRUCTIONS
-$CXX -Wfatal-errors $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
+#ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
+$CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
 // © Alfredo A. Correa 2018-2020
+
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi allocators"
 #define BOOST_TEST_DYN_LINK
 #include<boost/test/unit_test.hpp>
@@ -15,72 +16,52 @@ namespace multi = boost::multi;
 template<class MA>
 decltype(auto) take(MA&& ma){return ma[0];}
 
-BOOST_AUTO_TEST_CASE(nested_array){
-	multi::array<double, 2> A(120, multi::array<double, 1>(140, 123.)); 
-	BOOST_REQUIRE( size(A) == 120 );
-	BOOST_REQUIRE( std::get<1>(sizes(A)) == 140 );
-	BOOST_REQUIRE( A[119][139] == 123. );
-}
-#if 1
 BOOST_AUTO_TEST_CASE(iterator_1d){
-{
-	multi::array<double, 1> A(100, 99.); 
-	BOOST_REQUIRE( size(A) == 100 );
-	BOOST_REQUIRE( begin(A) < end(A) );
-	BOOST_REQUIRE( end(A) - begin(A) == size(A) );
-	
-	multi::array<double, 1>::const_iterator cb = cbegin(A);
-	multi::array<double, 1>::iterator b = begin(A);
-	BOOST_REQUIRE( cb == b );
-	multi::array<double, 1>::const_iterator cb2 = begin(A);
-	BOOST_REQUIRE( cb2 == cb );
+	{
+		multi::array<double, 1> A(100, 99.); 
+		BOOST_REQUIRE( size(A) == 100 );
+		BOOST_REQUIRE( begin(A) < end(A) );
+		BOOST_REQUIRE( end(A) - begin(A) == size(A) );
+		
+		multi::array<double, 1>::const_iterator cb = cbegin(A);
+		multi::array<double, 1>::iterator b = begin(A);
+		BOOST_REQUIRE( cb == b );
+		multi::array<double, 1>::const_iterator cb2 = begin(A);
+		BOOST_REQUIRE( cb2 == cb );
+	}
+	{
+		multi::array<double, 1> A({100}, 99.); 
+		BOOST_REQUIRE( size(A) == 100 );
+		BOOST_REQUIRE( begin(A) < end(A) );
+	}
 }
-{
-	multi::array<double, 1> A({100}, 99.); 
-	BOOST_REQUIRE( size(A) == 100 );
-	BOOST_REQUIRE( begin(A) < end(A) );
-}
-}
+
 BOOST_AUTO_TEST_CASE(iterator_2d){
-{
-	multi::array<double, 2> A({120, 140}, 99.); 
-	BOOST_REQUIRE( size(A) == 120 );
-	BOOST_REQUIRE( cbegin(A) < cend(A) );
-	BOOST_REQUIRE( cend(A) - cbegin(A) == size(A) );
+	{
+		multi::array<double, 2> A({120, 140}, 99.); 
+		BOOST_REQUIRE( size(A) == 120 );
+		BOOST_REQUIRE( cbegin(A) < cend(A) );
+		BOOST_REQUIRE( cend(A) - cbegin(A) == size(A) );
+	}
+	{
+		std::vector<double> v(10000);
+		multi::array_ref<double, 2> A(v.data(), {100, 100}); 
+		BOOST_REQUIRE(size(A) == 100);
+		begin(std::move(A))[4][3] = 2.; // ok 
+		using multi::static_array_cast;
+	}
 }
-{
-	std::vector<double> v(10000);
-	multi::array_ref<double, 2> A(v.data(), {100, 100}); 
-	BOOST_REQUIRE(size(A) == 100);
-	begin(A)[4][3] = 2.; // ok 
-	using multi::static_array_cast;
-//	auto const& A_const = static_array_cast<double const>(A);
-//	begin(A_const)[4][3] = 2.; // error, read only
-}
-{
-	std::vector<double> V(10000);
-	multi::array_ref<double, 2, std::vector<double>::iterator> A(begin(V), {100, 100}); 
-	BOOST_REQUIRE(size(A) == 100);
-//	begin(arr)[4][3] = 2.;
-	A[1][0] = 99.;
-	BOOST_REQUIRE( cbegin(A) < cend(A) );
-	BOOST_REQUIRE( V[100] == 99. );
-}
-{
+
+BOOST_AUTO_TEST_CASE(iterator_reverse){
 	multi::array<double, 3>::reverse_iterator rit;
 	BOOST_REQUIRE(( rit.base() == multi::array<double, 3>::reverse_iterator{}.base() ));
 	BOOST_REQUIRE(( multi::array<double, 3>::reverse_iterator{}.base() == multi::array<double, 3>::reverse_iterator{}.base() ));
 	BOOST_REQUIRE(( multi::array<double, 3>::reverse_iterator{} == multi::array<double, 3>::reverse_iterator{} ));
 	BOOST_REQUIRE(( multi::array<double, 3>::reverse_iterator{} == multi::array<double, 3>::reverse_iterator{} ));
 }
-}
-#if 0
-	return 0;
 
+BOOST_AUTO_TEST_CASE(iterator_interface){
 	multi::array<double, 3> A =
-//	#if defined(__INTEL_COMPILER)
-//		(double[3][2][2])
-//	#endif
 		{
 			{
 				{ 1.2,  1.1}, { 2.4, 1.}
@@ -93,52 +74,74 @@ BOOST_AUTO_TEST_CASE(iterator_2d){
 			}
 		}
 	;
-	assert( begin(A) < end(A) );
-	assert( cbegin(A) < cend(A) );
-	assert( begin(A[0]) < end(A[0]) );
 
-	multi::array<double, 1>::const_iterator i;
-	assert( begin(A[0]) < end(A[0]) );
+	BOOST_REQUIRE( size(A)==3 and size(A[0])==2 and size(A[0][0])==2);
+	BOOST_REQUIRE( A[0][0][1] == 1.1 );
 
-	assert( size(A) == 3 and size(A[0]) == 2 and size(A[0][0]) == 2 and A[0][0][1] == 1.1 );
-	assert(( multi::array<double, 3>::reverse_iterator{A.begin()} == rend(A) ));
+	BOOST_REQUIRE( begin(A) < end(A) );
+	BOOST_REQUIRE( cbegin(A) < cend(A) );
+	BOOST_REQUIRE( begin(A[0]) < end(A[0]) );
+	BOOST_REQUIRE( begin(A[0]) < end(A[0]) );
 
-	assert( begin(A) < end(A) );
-	assert( cbegin(A) < cend(A) );
-//	assert( crbegin(A) < crend(A) );
-//	assert( crend(A) > crbegin(A) );
-	assert( end(A) - begin(A) == size(A) );
-	assert( rend(A) - rbegin(A) == size(A) );
+	BOOST_REQUIRE(( multi::array<double, 3>::reverse_iterator{A.begin()} == rend(A) ));
 
-	assert( size(*begin(A)) == 2 );
-	assert( size(begin(A)[1]) == 2 );
-	assert( &(A[1][1].begin()[0]) == &A[1][1][0] );
-	assert( &A[0][1][0] == &A[0][1][0] );
-	assert( &((*A.begin())[1][0]) == &A[0][1][0] );
-	assert( &((*A.begin()).operator[](1)[0]) == &A[0][1][0] );
-	assert( &(A.begin()->operator[](1)[0]) == &A[0][1][0] );
-	assert( &(A.begin()->operator[](1).begin()[0]) == &A[0][1][0] );
-	assert( &((A.begin()+1)->operator[](1).begin()[0]) == &A[1][1][0] );
-	assert( &((begin(A)+1)->operator[](1).begin()[0]) == &A[1][1][0] );
-	assert( &((cbegin(A)+1)->operator[](1).begin()[0]) == &A[1][1][0] );
+	BOOST_REQUIRE( rbegin(A) < rend(A) );
 
-	multi::array<double, 3>::iterator it; assert(( it == multi::array<double, 3>::iterator{} ));
-	--it;
-	it = begin(A);                                    assert( it == begin(A) );
-	multi::array<double, 3>::iterator it2 = begin(A); assert(it == it2);
-	it = end(A);                                      assert(it != it2);
-	assert(it > it2);
-	multi::array<double, 3>::iterator it3{it};        assert( it3 == it );
-	multi::array<double, 3>::const_iterator cit;
-	cit = it3;                                        assert( cit == it3 );
-	assert((begin(A) == multi::array<double, 3>::iterator{rend(A)}));
-	{
-		std::vector<double> vv = {1.,2.,3.};
-		auto it = vv.begin();
-		auto rit = vv.rend();
-		assert(std::vector<double>::reverse_iterator{it} == rit);
-	}
+	BOOST_REQUIRE( end(A) - begin(A) == size(A) );
+	BOOST_REQUIRE( rend(A) - rbegin(A) == size(A) );
+
+	BOOST_REQUIRE( size(*begin(A)) == 2 );
+	BOOST_REQUIRE( size(begin(A)[1]) == 2 );
+
+	BOOST_REQUIRE( &(A[1][1].begin()[0]) == &A[1][1][0] );
+	BOOST_REQUIRE( &A[0][1][0] == &A[0][1][0] );
+	BOOST_REQUIRE( &((*A.begin())[1][0]) == &A[0][1][0] );
+	BOOST_REQUIRE( &((*A.begin()).operator[](1)[0]) == &A[0][1][0] );
+	BOOST_REQUIRE( &(A.begin()->operator[](1)[0]) == &A[0][1][0] );
+	BOOST_REQUIRE( &(A.begin()->operator[](1).begin()[0]) == &A[0][1][0] );
+	BOOST_REQUIRE( &((A.begin()+1)->operator[](1).begin()[0]) == &A[1][1][0] );
+	BOOST_REQUIRE( &((begin(A)+1)->operator[](1).begin()[0]) == &A[1][1][0] );
+	BOOST_REQUIRE( &((cbegin(A)+1)->operator[](1).begin()[0]) == &A[1][1][0] );
+
 }
-#endif
-#endif
+
+BOOST_AUTO_TEST_CASE(iterator_semantics){
+
+	multi::array<double, 3> A =
+		{
+			{
+				{ 1.2,  1.1}, { 2.4, 1.}
+			},
+			{
+				{11.2,  3.0}, {34.4, 4.}
+			},
+			{
+				{ 1.2,  1.1}, { 2.4, 1.}
+			}
+		}
+	;
+
+	multi::array<double, 3>::iterator it; 
+	BOOST_REQUIRE(( it == multi::array<double, 3>::iterator{} ));
+
+	--it;
+	it = begin(A);
+	BOOST_REQUIRE( it == begin(A) );
+
+	multi::array<double, 3>::iterator it2 = begin(A); 
+	BOOST_REQUIRE(it == it2);
+
+	it = end(A);
+	BOOST_REQUIRE(it != it2);
+	BOOST_REQUIRE(it > it2);
+
+	multi::array<double, 3>::iterator it3{it};
+	BOOST_REQUIRE( it3 == it );
+
+	multi::array<double, 3>::const_iterator cit;
+	cit = it3;
+	BOOST_REQUIRE( cit == it3 );
+
+	BOOST_REQUIRE((begin(A) == multi::array<double, 3>::iterator{rend(A)}));
+}
 
