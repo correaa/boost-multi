ebceaa4d991ae1bebff94f2205381f42c96b22a3
Alfredo Correa <alfredo.correa@gmail.com>
2020-04-27 12:27:04 -0700

    removed cuda from reinterpret_cast example

--- tests/reinterpret_array_cast.cpp
+++ tests/reinterpret_array_cast.cpp
@@ -3,7 +3,7 @@ $CXX $0 -o $0x -ltbb -lcudart -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
 
 #include "../array.hpp"
-#include "../adaptors/cuda.hpp"
+//#include "../adaptors/cuda.hpp"
 
 #include<complex>
 #include<numeric>
@@ -33,6 +33,7 @@ BOOST_AUTO_TEST_CASE(multi_reinterpret_array_cast){
 	A2[8].real = 1000.;
 	BOOST_REQUIRE( A[8] == 1000. );
 }
+#if 0
 {
 	multi::cuda::managed::array<std::complex<double>, 1> A(10);
 	A[8] = std::complex<double>{1000., 2000.};
@@ -47,6 +48,7 @@ BOOST_AUTO_TEST_CASE(multi_reinterpret_array_cast){
 
 //	auto&& ARP = multi::member_array_cast<double>(A2, &Complex_<double>::re);
 }
+#endif
 #if 0
 {
 	multi::cuda::array<std::complex<double>, 1> A(10);
