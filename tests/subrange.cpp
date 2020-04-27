1f575e575d1990a8369af33a5b51f096e4a278ce
Alfredo Correa <alfredo.correa@gmail.com>
2020-04-27 11:36:54 -0700

    added CR to subrange test

--- tests/subrange.cpp
+++ tests/subrange.cpp
@@ -1,6 +1,7 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
 $CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
+// © Alfredo A. Correa 2018-2020
 
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi range selection"
 #define BOOST_TEST_DYN_LINK
