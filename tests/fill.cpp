528b4b51e94e99d7238935a5238e4092aadf581f
Alfredo Correa <alfredo.correa@gmail.com>
2020-04-27 03:23:26 -0700

    removed depend on rangev3 of fill test

--- tests/fill.cpp
+++ tests/fill.cpp
@@ -1,4 +1,4 @@
-#ifdef COMPILATION_INSTRUCTIONS
+#ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
 $CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
 
@@ -15,7 +15,7 @@ $CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #include<type_traits> // enable_if_t
 #include<iostream>  //cout
 
-#include <range/v3/all.hpp>
+//#include <range/v3/all.hpp>
 
 #include <boost/iterator/transform_iterator.hpp>
 
@@ -56,37 +56,37 @@ BOOST_AUTO_TEST_CASE(fill_1d){
 		BOOST_REQUIRE( d1D[1] == 1. );
 		BOOST_REQUIRE( d1D[9] == 9. );
 		
-		auto f = [](auto e){return e + 1;};
-		copy(
-			boost::make_transform_iterator(begin(extension(d1D)), f), 
-			boost::make_transform_iterator(end  (extension(d1D)), f), 
-			begin(d1D)
-		);
-		BOOST_REQUIRE( d1D[0] == 1. );
-		BOOST_REQUIRE( d1D[1] == 2. );
-		BOOST_REQUIRE( d1D[9] == 10. );
+//		auto f = [](auto e){return e + 1;};
+//		copy(
+//			boost::make_transform_iterator(begin(extension(d1D)), f), 
+//			boost::make_transform_iterator(end  (extension(d1D)), f), 
+//			begin(d1D)
+//		);
+//		BOOST_REQUIRE( d1D[0] == 1. );
+//		BOOST_REQUIRE( d1D[1] == 2. );
+//		BOOST_REQUIRE( d1D[9] == 10. );
 
-		ranges::copy(extension(d1D), begin(d1D));
-		BOOST_REQUIRE( d1D[0] == 0. );
-		BOOST_REQUIRE( d1D[1] == 1. );
-		BOOST_REQUIRE( d1D[9] == 9. );
+//		ranges::copy(extension(d1D), begin(d1D));
+//		BOOST_REQUIRE( d1D[0] == 0. );
+//		BOOST_REQUIRE( d1D[1] == 1. );
+//		BOOST_REQUIRE( d1D[9] == 9. );
 
-		ranges::copy(extension(d1D) | ranges::views::transform([](auto e){return e+1;}), begin(d1D));
-		BOOST_REQUIRE( d1D[0] == 1. );
-		BOOST_REQUIRE( d1D[1] == 2. );
-		BOOST_REQUIRE( d1D[9] == 10. );
+//		ranges::copy(extension(d1D) | ranges::views::transform([](auto e){return e+1;}), begin(d1D));
+//		BOOST_REQUIRE( d1D[0] == 1. );
+//		BOOST_REQUIRE( d1D[1] == 2. );
+//		BOOST_REQUIRE( d1D[9] == 10. );
 
 		d1D.assign(extension(d1D));
 		BOOST_REQUIRE( d1D[0] == 0. );
 		BOOST_REQUIRE( d1D[1] == 1. );
 		BOOST_REQUIRE( d1D[9] == 9. );
 
-		d1D.assign(extension(d1D)|ranges::views::transform([](auto e){return e+1;}));
-		BOOST_REQUIRE( d1D[0] == 1. );
-		BOOST_REQUIRE( d1D[1] == 2. );
-		BOOST_REQUIRE( d1D[9] == 10. );
+//		d1D.assign(extension(d1D)|ranges::views::transform([](auto e){return e+1;}));
+//		BOOST_REQUIRE( d1D[0] == 1. );
+//		BOOST_REQUIRE( d1D[1] == 2. );
+//		BOOST_REQUIRE( d1D[9] == 10. );
 
-		fnv1a_t h;
+/*		fnv1a_t h;
 		using urd = std::uniform_real_distribution<>;
 		auto crypt = [h=h(111)](auto i){return urd{}(fnv1a_t{h}(i));};
 		copy(
@@ -99,43 +99,44 @@ BOOST_AUTO_TEST_CASE(fill_1d){
 
 		using urd = std::uniform_real_distribution<>;
 		auto crypt2 = [h=h(222)](auto i){return fnv1a_t{h}(i)();};
-		copy(
-			boost::make_transform_iterator(begin(extension(d1D)), crypt2), 
-			boost::make_transform_iterator(end  (extension(d1D)), crypt2), 
-			begin(d1D)
-		);
-		for(auto&& e : d1D) std::cout<< e <<std::endl;
-		std::cout<<"///"<<std::endl;
+*/
+//		copy(
+//			boost::make_transform_iterator(begin(extension(d1D)), crypt2), 
+//			boost::make_transform_iterator(end  (extension(d1D)), crypt2), 
+//			begin(d1D)
+//		);
+//		for(auto&& e : d1D) std::cout<< e <<std::endl;
+//		std::cout<<"///"<<std::endl;
 	}
 	{
-		auto const& xp1 = multi::index_extension(10) | ranges::views::transform([](auto e){return e+1;});
-		multi::array<double, 1> d1D(xp1.begin(), xp1.end());
-		BOOST_REQUIRE( d1D[0] == 1. );
-		BOOST_REQUIRE( d1D[1] == 2. );
-		BOOST_REQUIRE( d1D[9] == 10. );
+//		auto const& xp1 = multi::index_extension(10) | ranges::views::transform([](auto e){return e+1;});
+//		multi::array<double, 1> d1D(xp1.begin(), xp1.end());
+//		BOOST_REQUIRE( d1D[0] == 1. );
+//		BOOST_REQUIRE( d1D[1] == 2. );
+//		BOOST_REQUIRE( d1D[9] == 10. );
 	}
 	{
-		multi::array<double, 1> d1D{multi::index_extension(10) | ranges::views::transform([](auto e){return e+1;})};
-		BOOST_REQUIRE( d1D[0] == 1. );
-		BOOST_REQUIRE( d1D[1] == 2. );
-		BOOST_REQUIRE( d1D[9] == 10. );
+//		multi::array<double, 1> d1D{multi::index_extension(10) | ranges::views::transform([](auto e){return e+1;})};
+//		BOOST_REQUIRE( d1D[0] == 1. );
+//		BOOST_REQUIRE( d1D[1] == 2. );
+//		BOOST_REQUIRE( d1D[9] == 10. );
 	}
-	std::mt19937 e;
-	fnv1a_t h;
-	using urd = std::uniform_real_distribution<>;
+//	std::mt19937 e;
+//	fnv1a_t h;
+//	using urd = std::uniform_real_distribution<>;
 	{
-		multi::array<double, 1> d1D{multi::index_extension(10) | ranges::views::transform([h=h(e())](auto i){return urd{-10, 10}(fnv1a_t{h}(i));})};
-
-		BOOST_REQUIRE( d1D[1] >= -10. and d1D[1] <   10. ); 
-		for(auto i : extension(d1D)) std::cout << d1D[i] << std::endl;
+//		multi::array<double, 1> d1D{multi::index_extension(10) | ranges::views::transform([h=h(e())](auto i){return urd{-10, 10}(fnv1a_t{h}(i));})};
+//
+//		BOOST_REQUIRE( d1D[1] >= -10. and d1D[1] <   10. ); 
+//		for(auto i : extension(d1D)) std::cout << d1D[i] << std::endl;
 	}
 	std::cout << "====\n";
 	{
-		multi::array<double, 1> d1D(10);
-		d1D.assign(extension(d1D)|ranges::views::transform([h=h(e())](auto i){return urd{-10, 10}(fnv1a_t{h}(i));}));
-
-		BOOST_REQUIRE( d1D[1] >= -10. and d1D[1] < 10. );
-		for(auto&& e : d1D) std::cout<< e <<std::endl;
+//		multi::array<double, 1> d1D(10);
+//		d1D.assign(extension(d1D)|ranges::views::transform([h=h(e())](auto i){return urd{-10, 10}(fnv1a_t{h}(i));}));
+//
+//		BOOST_REQUIRE( d1D[1] >= -10. and d1D[1] < 10. );
+//		for(auto&& e : d1D) std::cout<< e <<std::endl;
 	}
 	{
 		multi::array<double, 1> d1D(begin(multi::index_extension(10)), end(multi::index_extension(10)));
@@ -172,10 +173,11 @@ BOOST_AUTO_TEST_CASE(fill){
 		{ 50.,  6.,  7.,  8.,  9.}  
 	};
 	using std::all_of;
-	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==5.;}) );
+	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]),[](auto& e){return e==5.;}) );
 
 	using std::fill;
-	fill(begin(d2D[1]), end(d2D[1]), 8.);
+	fill(d2D[1].begin(), d2D[1].end(), 8.);
+
 	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto& e){return e==8.;}) );
 
 	fill(begin(rotated(d2D)[1]), end(rotated(d2D)[1]), 8.);
@@ -187,7 +189,7 @@ BOOST_AUTO_TEST_CASE(fill){
 	std::mt19937 g{r()};
 	auto rand = [d=std::normal_distribution<>{}, g = std::mt19937{r()}]() mutable{return d(g);};
 	multi::array<double, 2> r2D({5, 5});
-	std::for_each(begin(r2D), end(r2D), [&](auto&& e){std::generate(begin(e), end(e), rand);});
+	std::for_each(begin(r2D), end(r2D), [&](auto&& e){std::generate(begin(std::move(e)), end(std::move(e)), rand);});
 
 }
 
