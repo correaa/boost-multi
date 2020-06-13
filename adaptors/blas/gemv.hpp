a4059df8550a48a85c2f154c87b11ba879e884ac
Alfredo Correa <alfredo.correa@gmail.com>
2020-06-13 05:24:27 -0700

    removed commented code gemv

--- adaptors/blas/gemv.hpp
+++ adaptors/blas/gemv.hpp
@@ -10,16 +10,10 @@ $CXX -DADD_ $0 -o $0x `pkg-config --libs blas` -lboost_unit_test_framework \
 
 #include "../blas/core.hpp"
 #include "./../../detail/../utility.hpp"
-#include "././../../detail/.././array_ref.hpp"
-//#include "../../../../detail/utility.hpp"
+#include "./../../detail/../array_ref.hpp"
 
 namespace boost{
-namespace multi{
-namespace blas{
-
-//struct trans{enum : char{N='N', T='T', C='C'};};
-
-//struct conj{template<class T> auto operator()(T const& t) const{using std::conj; return conj(t);}};
+namespace multi{namespace blas{
 
 template<class Trans, class T, class ItA, class Size, class Itx, class Ity>
 Ity gemv_n(Trans IN, T a, ItA A_first, Size n, Itx x_first, T beta, Ity y_first){
@@ -60,113 +54,6 @@ template<class A2D, class X1D, class Y1D>
 Y1D&& gemv(A2D const& A, X1D const& x, Y1D&& y){
 	return gemv(1., A, x, 0., std::forward<Y1D>(y));
 }
-
-//template<class A, class B, class RowIt, class ConstIt, class It>
-//It gemv(A const& a, RowIt M_first, RowIt M_last, ConstIt X_first, B const& b, It Y_first){
-//	using std::transform; using std::inner_product; using std::begin; using std::end;
-//	return transform(M_first, M_last, Y_first, Y_first, [&](auto const& r, auto const& e){
-//		return a*inner_product(begin(r), end(r), X_first, typename std::iterator_traits<It>::value_type{0}) + b*e;
-//	});
-//}
-//template<class A, class B, class RowIt, class ConstIt, class It, class Conj>
-//It gemv(A const& a, RowIt M_first, RowIt M_last, ConstIt X_first, B const& b, It Y_first, Conj&& /*conj*/){
-//	std::cout<< __LINE__ <<std::endl;
-//	using std::transform; using std::inner_product; using std::begin; using std::end;
-//	return transform(M_first, M_last, Y_first, Y_first, [&](auto&& r, auto&& e){
-//		return a*inner_product(begin(r), end(r), X_first, typename std::iterator_traits<It>::value_type{0}/*, std::plus<>{}, [&](auto const& a, auto const& b){return conj(a)*b;}*/) + b*e;
-//	});
-//}
-
-#if 0
-template<class AB, class RowIt, class ConstIt, class It>
-It gemv(AB const& a, RowIt M_first, RowIt M_last, ConstIt X_first, AB const& b, It Y_first){
-	assert( stride(M_first) == stride(M_last) );
-	std::cout<< __LINE__ <<std::endl;
-	using std::distance;
-#ifndef NO_BLAS
-	     if(stride(*M_first) == 1){std::cout<< __LINE__ <<std::endl; gemv(blas::trans::T, M_first->size(), std::distance(M_first, M_last), a, base(M_first), stride( M_first), base(X_first), stride(X_first), b, base(Y_first), stride(Y_first));}
-	else if(stride( M_first) == 1){std::cout<< __LINE__ <<std::endl; gemv(blas::trans::N, std::distance(M_first, M_last),M_first->size(), a, base(M_first), stride(*M_first), base(X_first), stride(X_first), b, base(Y_first), stride(Y_first));}
-	else
-#endif
-#ifdef NO_GENERICBLAS
-		assert(0);
-#else
-		gemv<AB, AB>(a, M_first, M_last, X_first, b, Y_first);
-#endif
-	return Y_first + std::distance(M_first, M_last);
-}
-
-template<class RowIt, class ConstIt, class It>
-It gemv(std::complex<double> const& a, RowIt M_first, RowIt M_last, ConstIt X_first, std::complex<double> const& b, It Y_first, blas::conj&&){
-	using AB = std::complex<double>;
-	std::cout<< __LINE__ <<std::endl;
-	assert( stride(M_first) == stride(M_last) );
-	using std::distance;
-	
-	if(stride( M_first) == 1){
-	 	std::cout<< __LINE__ << " " << stride(*M_first) << " " << std::distance(M_first, M_last) << " " << M_first->size() << std::endl;
-     	gemv(trans::C, std::distance(M_first, M_last), M_first->size(), a, base(M_first), stride(*M_first), base(X_first), stride(X_first), b, base(Y_first), stride(Y_first));
-     	std::cout<< __LINE__ << " " << stride(*M_first) << " " << std::distance(M_first, M_last) << " " << M_first->size() << std::endl;
-//			assert(0);
-     }else{
-     	gemv<AB, AB>(a, M_first, M_last, X_first, b, Y_first, blas::conj{});
-     }
-
-#if 0
-	
-#ifndef NO_BLAS
-	     if(stride( M_first) == 1){
-	     	std::cout<< __LINE__ << " " << stride(*M_first) << " " << std::distance(M_first, M_last) << " " << M_first->size() << std::endl;
-	     	gemv('C', std::distance(M_first, M_last), M_first->size(), a, base(M_first), stride(*M_first), base(X_first), stride(X_first), b, base(Y_first), stride(Y_first));
-	     	std::cout<< __LINE__ << " " << stride(*M_first) << " " << std::distance(M_first, M_last) << " " << M_first->size() << std::endl;
-//			assert(0);
-	     }
-	else
-#endif
-#ifdef NO_GENERICBLAS
-	assert(0);
-#else
-	gemv<AB, AB>(a, M_first, M_last, X_first, b, Y_first, blas::conj{});
-#endif
-#endif
- 	std::cout<< __LINE__ << " " << stride(*M_first) << " " << std::distance(M_first, M_last) << " " << M_first->size() << std::endl;
-	return Y_first;// + std::distance(M_first, M_last);
-}
-
-template<class T, class A2D, class X1D, class Y1D>
-Y1D gemv(T const& a, A2D const& A, X1D const& x, T const& b, Y1D&& y){
-	std::cout<< __LINE__ <<std::endl;
-	assert( size(x)==std::get<1>(shape(A)) and size(y)==std::get<0>(shape(A)) );
-	auto last = gemv(a, begin(A), end(A), begin(x), b, begin(y));
-	assert( last == end(y) );
-	return std::forward<Y1D>(y);
-	
-//	else if(IN == 'N') 
-//	assert( std::get<1>(strides(A)) == 1 ); // gemv is not implemented for arrays with non-leading stride != 1
-	auto m = std::get<1>(shape(A));
-	auto n = std::get<0>(shape(A));
-	if(std::get<1>(strides(A)) == 1){
-	//	if(IN=='T' or IN=='H') 
-		assert( size(x)==std::get<1>(A.shape()) and size(y)==std::get<0>(A.shape()));
-		gemv(trans::T, m, n, a, origin(A), std::get<0>(strides(A)), origin(x), stride(x), b, origin(y), stride(y));
-	}else if(std::get<0>(strides(A)) == 1){
-		assert( size(x) == std::get<0>(A.shape()) and size(y) == std::get<1>(A.shape()));
-		gemv(trans::N, m, n, a, origin(A), std::get<1>(strides(A)), origin(x), stride(x), b, origin(y), stride(y));
-	}else{assert(0);}
-	return std::forward<Y1D>(y);
-} //y := alpha*A*x + beta*y,
-
-template<class T, class A2D, class X1D, class Y1D, class Conj>
-Y1D&& gemv(T const& a, A2D const& A, X1D const& x, T const& b, Y1D&& y, Conj&& c){
-	std::cout<<__LINE__ <<std::endl;
-	assert( size(x)==std::get<1>(shape(A)) and size(y)==std::get<0>(shape(A)) );
-//	auto last = 
-	gemv(a, begin(A), end(A), begin(x), b, begin(y), std::forward<Conj>(c));
-	std::cout<< __LINE__ <<std::endl;
-//	assert( last == end(y) );
-	return std::forward<Y1D>(y);
-} //y := alpha*A*x + beta*y,
-#endif
 
 }}}
 
