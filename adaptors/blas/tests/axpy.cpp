caf326b0effecde96e85aa1e4818112d8f27353f
Alfredo Correa <alfredo.correa@gmail.com>
2020-06-16 14:18:42 -0700

    improved names in axpy test

--- adaptors/blas/tests/axpy.cpp
+++ adaptors/blas/tests/axpy.cpp
@@ -12,7 +12,10 @@ $CXX $0 -o $0x `pkg-config --libs blas` -lboost_unit_test_framework&&$0x&&rm $0x
 
 #include<complex>
 
+template<class T> constexpr std::add_const_t<T>& as_const(T&& t) noexcept{return t;}
+
 namespace multi = boost::multi;
+namespace blas = multi::blas;
 
 BOOST_AUTO_TEST_CASE(multi_blas_axpy){
 	{
@@ -23,21 +26,21 @@ BOOST_AUTO_TEST_CASE(multi_blas_axpy){
 		};
 		auto const AC = A;
 		multi::array<double, 1> const B = A[2];
-		using multi::blas::axpy;
-		axpy(2., B, A[1]); // daxpy
+		auto A1cpy = blas::axpy(2., B, as_const(A[1])); // daxpy
+		blas::axpy(2., B, A[1]); // daxpy
 		BOOST_REQUIRE( A[1][2] == 2.*B[2] + AC[1][2] );
+		assert( A1cpy == A[1] );
 	}
 	{
-		using Z = std::complex<double>;
-		multi::array<Z, 2> A = {
+		using complex = std::complex<double>;
+		multi::array<complex, 2> A = {
 			{1.,  2.,  3.,  4.},
 			{5.,  6.,  7.,  8.},
 			{9., 10., 11., 12.}
 		};
 		auto const AC = A;
-		multi::array<Z, 1> const B = A[2];
-		using multi::blas::axpy;
-		axpy(2., B, A[1]); // zaxpy (2. is promoted to 2+I*0 internally and automatically)
+		multi::array<complex, 1> const B = A[2];
+		blas::axpy(2., B, A[1]); // zaxpy (2. is promoted to 2+I*0 internally and automatically)
 		BOOST_REQUIRE( A[1][2] == 2.*B[2] + AC[1][2] );
 	}
 }
