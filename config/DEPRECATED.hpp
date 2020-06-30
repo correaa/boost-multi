618d8c0391dcf2f362d8ca4fcbee133242c5921a
Alfredo Correa <alfredo.correa@gmail.com>
2020-06-30 15:45:30 -0700

    move deprecated macro to its own header

--- config/DEPRECATED.hpp
+++ config/DEPRECATED.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-$CXX $0 -o $0x &&$0x&&rm $0x;exit
+$CXXX $CXXFLAGS $0 -o $0x &&$0x&&rm $0x;exit
 #endif
 // © Alfredo A. Correa 2019-2020
 
@@ -10,34 +10,57 @@ $CXX $0 -o $0x &&$0x&&rm $0x;exit
 #define __has_cpp_attribute(name) 0
 #endif
 
-#if (__has_cpp_attribute(nodiscard)) && (__cplusplus>=201703L)
-	#define nodiscard_(MsG) nodiscard
-	#define NODISCARD(MsG) [[nodiscard_(MsG)]]
-	#if (__has_cpp_attribute(nodiscard)>=201907) && (__cplusplus>=201703L)
-		#define nodiscard_(MsG) nodiscard(MsG)
-		#define NODISCARD(MsG) [[nodiscard_(MsG)]]
-	#endif
-#elif __has_cpp_attribute(gnu::warn_unused_result)
-#if defined(__NVCC__)
-	#define NODISCARD(MsG)
+#if __has_cpp_attribute(deprecated)
+	#define	DEPRECATED(MsG) [[deprecated(MsG)]]
 #else
-	#define nodiscard_(MsG) gnu::warn_unused_result
-	#define NODISCARD(MsG) [[nodiscard_(MsG)]]
+	#define DEPRECATED(MsG)
 #endif
+
+#if not defined(__INTEL_COMPILER)
+#define BEGIN_NO_DEPRECATED \
+\
+_Pragma("GCC diagnostic push") \
+_Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"") \
+\
+
 #else
-	#define nodiscard_(MsG)
-	#define NODISCARD(MsG)
+#define BEGIN_NO_DEPRECATED \
+_Pragma("warning push") \
+_Pragma("warning disable 1786") \
+
 #endif
+
+#if not defined(__INTEL_COMPILER)
+#define END_NO_DEPRECATED \
+\
+_Pragma("GCC diagnostic pop") \
+\
+
+#else
+#define END_NO_DEPRECATED \
+\
+_Pragma("warning pop") \
+\
+
+#endif
+
+#define BEGIN_CUDA_SLOW BEGIN_NO_DEPRECATED
+#define END_CUDA_SLOW   END_NO_DEPRECATED
+
+#define NO_DEPRECATED(ExpR) \
+	BEGIN_NO_DEPRECATED \
+	ExpR \
+	END_NO_DEPRECATED
 
 #if not __INCLUDE_LEVEL__ // _TEST_MULTI_CONFIG_NODISCARD
 
-NODISCARD("because...") int f(){return 5;}
+DEPRECATED("because...") int f(){return 5;}
 //[[nodiscard]] int g(){return 5;} // ok in g++ -std=c++14
 
 int main(){
-	int i; 
-	i = f(); // ok
-//	f();  // warning
+	int i;
+//	f(); // warning
+	NO_DEPRECATED( i = f(); ) // ok
 	++i;
 	(void)i;
 }
