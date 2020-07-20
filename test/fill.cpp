6c09404412d616bb7004c3a936da585318d75123
Alfredo Correa <alfredo.correa@gmail.com>
2020-07-20 09:29:20 -0700

    added -lm to fill test

--- test/fill.cpp
+++ test/fill.cpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-$CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
+$CXX $CXXFLAGS $0 -o $0x -lm -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
 
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi fill"
