a10aa2ec65e9a192563b23e8f01861787f6793a1
Alfredo Correa <alfredo.correa@gmail.com>
2020-07-20 11:57:21 -0700

    added maybe unused for enums in C

--- adaptors/blas/filling.hpp
+++ adaptors/blas/filling.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-$CXX $0 -o $0x `pkg-config --libs blas` -lboost_unit_test_framework&&$0x&&rm $0x;exit
+$CXXX $CXXFLAGS $0 -o $0x `pkg-config --libs blas` -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #endif
 // © Alfredo A. Correa 2019-2020
 
@@ -25,8 +25,8 @@ enum class filling : char{
 	upper = 'L'  //static_cast<char>(uplo::L)
 };
 
-static constexpr filling U = filling::upper;
-static constexpr filling L = filling::lower;
+MAYBE_UNUSED static constexpr filling U = filling::upper;
+MAYBE_UNUSED static constexpr filling L = filling::lower;
 
 filling flip(filling side){
 	switch(side){
@@ -119,7 +119,6 @@ decltype(auto) print(M const& C){
 
 namespace multi = boost::multi;
 using complex = std::complex<double>;
-auto const I = complex(0., 1.);
 
 BOOST_AUTO_TEST_CASE(multi_adaptors_blas_side){
 	return;
