1220b174e324b119bd0f5f6f37c6f5055f1d5406
Alfredo Correa <alfredo.correa@gmail.com>
2020-09-23 00:40:03 -0700

    nrm2 cleaned

--- adaptors/blas/nrm2.hpp
+++ adaptors/blas/nrm2.hpp
@@ -23,19 +23,50 @@ namespace blas{
 
 using core::nrm2;
 
-template<class Arr1D, class Arr0D>
-auto nrm2(Arr1D const& x, Arr0D&& r)
-->decltype(nrm2(size(x), base(x), stride(x), base(r)), std::forward<Arr0D>(r)){
-	return nrm2(size(x), base(x), stride(x), base(r)), std::forward<Arr0D>(r);}
+using multi::base; 
+using std::norm; // nvcc11 needs using std::FUNCTION and the FUNCTION (and it works in clang, gcc, culang, icc)
 
-using std::norm; // for some reason nvcc needs using std::norm/norm (and works in clang, gcc, culang, icc)
+template<class A1D, class A0D>
+auto nrm2(A1D const& x, A0D&& r)
+->decltype(nrm2(x.size(), x.base(), x.stride(), base(r)), std::forward<A0D>(r)){
+	return nrm2(x.size(), x.base(), x.stride(), base(r)), std::forward<A0D>(r);}
 
-template<class Arr1D, 
-	typename T = decltype(norm(std::declval<typename Arr1D::value_type>())),
-	typename Alloc = typename std::allocator_traits<typename Arr1D::default_allocator_type>::template rebind_alloc<T>
+#if 0
+template<class A1D>
+auto nrm2(A1D const& x, double& r)
+->decltype(nrm2(x.size(), x.base(), x.stride(), &r), r){
+	return nrm2(x.size(), x.base(), x.stride(), &r), r;}
+
+template<class A1D>
+auto nrm2(A1D const& x, float& r)
+->decltype(nrm2(x.size(), x.base(), x.stride(), &r), r){
+	return nrm2(x.size(), x.base(), x.stride(), &r), r;}
+#endif
+
+template<
+	class A1D, typename T = double, //decltype(norm(std::declval<typename A1D::value_type>())), 
+	class Alloc = typename std::allocator_traits<typename A1D::default_allocator_type>::template rebind_alloc<T>
 >
-auto nrm2(Arr1D const& x, Alloc const& alloc = {}){
-	return nrm2(x, multi::static_array<T, 0, Alloc>({}, alloc));
+NODISCARD("")
+auto nrm2(A1D const& x)
+//->std::decay_t<decltype(nrm2(x, multi::static_array<T, 0, Alloc>({}, x.get_allocator()) ))>{
+->std::decay_t<decltype(nrm2(x, multi::static_array<T, 0, Alloc>({})))>{ // x.get_allocator() in decltype doesn't work for icc
+	return nrm2(x, multi::static_array<T, 0, Alloc>({}, x.get_allocator()));}
+
+template<class Alloc, class A1D, typename T = decltype(norm(std::declval<typename A1D::value_type>())), 
+	class AllocR = typename std::allocator_traits<typename A1D::default_allocator_type>::template rebind_alloc<T>
+>
+NODISCARD("")
+auto nrm2(A1D const& x, AllocR const& alloc)
+->std::decay_t<decltype(blas::nrm2(x, multi::static_array<T, 0, AllocR>({}, alloc)))>{
+	return              blas::nrm2(x, multi::static_array<T, 0, AllocR>({}, alloc)) ;}
+
+namespace operators{
+	using std::norm;
+	template<class A1D>//decltype(norm(std::declval<typename A1D::value_type>()))> 
+	NODISCARD("") auto operator^(A1D const& a, int n)
+	->decltype(std::pow(blas::nrm2(a), n)){
+		return std::pow(blas::nrm2(a), n);}
 }
 
 }}}
@@ -50,7 +81,7 @@ auto nrm2(Arr1D const& x, Alloc const& alloc = {}){
 #include "../../array.hpp"
 #include "../../complex.hpp"
 
-#include<thrust/complex.h>
+//#include<thrust/complex.h>
 
 #include<boost/mpl/list.hpp>
 
@@ -84,6 +115,13 @@ BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_real){
 
 }
 
+BOOST_AUTO_TEST_CASE(multi_adaptor_blas_nrm2_operators){
+	multi::array<double, 1> X = {1.1,2.1,3.1, 4.1};
+	double n; multi::blas::nrm2(X, n);
+	BOOST_REQUIRE( n == multi::blas::nrm2(X) );
+
+}
+
 BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_complex_real_case){
 	using complex = std::complex<double>;
 	multi::array<complex, 2> const cA = {
@@ -98,6 +136,7 @@ BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_complex_real_case){
 	BOOST_REQUIRE( nrm2(rotated(cA)[1])    == n );
 }
 
+#if 0
 BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_complex_real_case_thrust){
 	using complex = thrust::complex<double>;
 	multi::array<complex, 2> const cA = {
@@ -130,6 +169,7 @@ BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_complex_real_case_types){
 		BOOST_REQUIRE( nrm2(rotated(cA)[1])    == n );
 	});
 }
+#endif
 
 BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_complex){
 	using complex = std::complex<double>; complex const I{0,1};
@@ -141,8 +181,12 @@ BOOST_AUTO_TEST_CASE(multi_adaptor_multi_nrm2_complex){
 
 	using multi::blas::nrm2;
 	double n;
-	BOOST_REQUIRE( nrm2(rotated(cA)[1], n) == std::sqrt( norm(cA[0][1]) + norm(cA[1][1]) + norm(cA[2][1]) ) );
-	BOOST_REQUIRE( nrm2(rotated(cA)[1]) == std::sqrt( norm(cA[0][1]) + norm(cA[1][1]) + norm(cA[2][1]) ) );
+	BOOST_REQUIRE( nrm2(rotated(cA)[1], n)   == std::sqrt( norm(cA[0][1]) + norm(cA[1][1]) + norm(cA[2][1]) ) );
+	BOOST_REQUIRE( nrm2(rotated(cA)[1])      == std::sqrt( norm(cA[0][1]) + norm(cA[1][1]) + norm(cA[2][1]) ) );
+
+	using namespace multi::blas::operators;
+	BOOST_TEST_REQUIRE( (rotated(cA)[1]^-1) == 1/std::sqrt(norm(cA[0][1]) + norm(cA[1][1]) + norm(cA[2][1])) , boost::test_tools::tolerance(1e-15) );
+	BOOST_TEST_REQUIRE( (rotated(cA)[1]^2) == norm(cA[0][1]) + norm(cA[1][1]) + norm(cA[2][1]) , boost::test_tools::tolerance(1e-15) );
 }
 
 #endif
