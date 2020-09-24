38b6124bb31542d3d719b8188933bbf2b3db735b
Alfredo Correa <alfredo.correa@gmail.com>
2020-09-24 16:37:50 -0700

    clean mark header

--- config/MARK.hpp
+++ config/MARK.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-$CXXX $CXXFLAGS $0 -o $0x -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -include boost/log/trivial.hpp -DBOOST_LOG_DYN_LINK -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&rm $0x;exit
+$CXXX $CXXFLAGS $0 -o $0x -include"boost/log/trivial.hpp" -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -DBOOST_LOG_DYN_LINK -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&rm $0x;exit
 #endif
 // © Alfredo A. Correa 2020
 
@@ -20,10 +20,9 @@ $CXXX $CXXFLAGS $0 -o $0x -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG
 
 #if not __INCLUDE_LEVEL__
 
-
 // #include <boost/log/trivial.hpp> or use command line `-include file.hpp`
 // for example add this to the end of compilation line to use markings for tracing
-// -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -include boost/log/trivial.hpp -DBOOST_LOG_DYN_LINK -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&
+// -include"boost/log/trivial.hpp" -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -DBOOST_LOG_DYN_LINK -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&
 
 void multi_fun(){
 	MULTI_MARK_SCOPE("function multi_fun"); //	BOOST_LOG_TRIVIAL(trace)<< "[2020-09-23 16:42:55.035034] [0x00007f6060af3a00] [trace]   function multi_fun";
@@ -36,6 +35,9 @@ void multi_gun(){
 int main(){
 	multi_fun();
 	multi_gun();
+// output: 
+//[2020-09-24 16:36:44.683215] [0x00007f54677eed40] [trace]   function multi_fun
+//[2020-09-24 16:36:44.683279] [0x00007f54677eed40] [trace]   multi_gun
 }
 
 #endif
