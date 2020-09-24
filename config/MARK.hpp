621163f7b1f40bfead4d55b72f553f046f64a6a3
Alfredo Correa <alfredo.correa@gmail.com>
2020-09-23 17:21:54 -0700

    add MARK_PRETTY_FUNCTION

--- config/MARK.hpp
+++ config/MARK.hpp
@@ -1,12 +1,10 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-$CXXX $CXXFLAGS -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -DBOOST_LOG_DYN_LINK $0 -o $0x -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&rm $0x;exit
+$CXXX $CXXFLAGS $0 -o $0x -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -include boost/log/trivial.hpp -DBOOST_LOG_DYN_LINK -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&rm $0x;exit
 #endif
 // © Alfredo A. Correa 2020
 
 #ifndef MULTI_CONFIG_MARK_HPP
 #define MULTI_CONFIG_MARK_HPP
-
-#include<cassert>
 
 #ifndef MULTI_MARK_SCOPE
 #define MULTI_MARK_SCOPE(MsG) ((void)0)
@@ -16,10 +14,16 @@ $CXXX $CXXFLAGS -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -DBOOST_
 #define MULTI_MARK_FUNCTION MULTI_MARK_SCOPE(__func__)
 #endif
 
+#ifndef MULTI_MARK_PRETTY_FUNCTION
+#define MULTI_MARK_PRETTY_FUNCTION MULTI_MARK_SCOPE(__PRETTY_FUNCTION__)
+#endif
+
 #if not __INCLUDE_LEVEL__
 
-#include <iostream>
-#include <boost/log/trivial.hpp>
+
+// #include <boost/log/trivial.hpp> or use command line `-include file.hpp`
+// for example add this to the end of compilation line to use markings for tracing
+// -D'MULTI_MARK_SCOPE(MsG)=BOOST_LOG_TRIVIAL(trace)<<MsG' -include boost/log/trivial.hpp -DBOOST_LOG_DYN_LINK -lboost_log -lboost_thread -lboost_system -lboost_log_setup -lpthread &&$0x&&
 
 void multi_fun(){
 	MULTI_MARK_SCOPE("function multi_fun"); //	BOOST_LOG_TRIVIAL(trace)<< "[2020-09-23 16:42:55.035034] [0x00007f6060af3a00] [trace]   function multi_fun";
