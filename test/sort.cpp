625933fa69d93d028bd77bea5f593f742e8ead18
Alfredo Correa <alfredo.correa@gmail.com>
2020-10-26 16:32:42 -0700

    soft for arrays in test

--- test/sort.cpp
+++ test/sort.cpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-$CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x; exit
+$CXX $CXXFLAGS $0 -o $0.$X -lboost_unit_test_framework&&$0.$X&&rm $0.$X; exit
 #endif
 // © Alfredo A. Correa 2019-2020
 
@@ -14,6 +14,47 @@ $CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x; exit
 
 namespace multi = boost::multi;
 
+BOOST_AUTO_TEST_CASE(multi_array_stable_sort){
+
+	std::vector<double> v = {1.,2.,3.};
+	multi::array<double, 2> d2D = {
+		{150, 16, 17, 18, 19},
+		{ 30,  1,  2,  3,  4}, 
+		{100, 11, 12, 13, 14}, 
+		{ 50,  6,  7,  8,  9} 
+	};
+	BOOST_REQUIRE( not std::is_sorted(begin(d2D), end(d2D) ) );
+
+	std::stable_sort( begin(d2D), end(d2D) );
+	BOOST_REQUIRE( std::is_sorted( begin(d2D), end(d2D) ) );
+
+	BOOST_REQUIRE((
+		d2D == decltype(d2D){
+			{30, 1, 2, 3, 4},
+			{50, 6, 7, 8, 9},
+			{100, 11, 12, 13, 14},
+			{150, 16, 17, 18, 19}
+		}
+	));
+
+	BOOST_REQUIRE( not std::is_sorted( begin(d2D<<1), end(d2D<<1) ) );
+
+	std::stable_sort( begin(d2D<<1), end(d2D<<1) );
+	BOOST_REQUIRE( std::is_sorted( begin(d2D<<1), end(d2D<<1) ) );
+	BOOST_REQUIRE( std::is_sorted( begin(d2D   ), end(d2D   ) ) );
+	
+	BOOST_REQUIRE((
+		d2D == decltype(d2D){
+			{1, 2, 3, 4, 30},
+			{6, 7, 8, 9, 50},
+			{11, 12, 13, 14, 100},
+			{16, 17, 18, 19, 150}
+		}
+	));
+
+
+}
+
 BOOST_AUTO_TEST_CASE(multi_array_ref_stable_sort){
 
 	std::vector<double> v = {1.,2.,3.};
@@ -23,7 +64,7 @@ BOOST_AUTO_TEST_CASE(multi_array_ref_stable_sort){
 		{100, 11, 12, 13, 14}, 
 		{ 50,  6,  7,  8,  9} 
 	};
-	multi::array_ref<double, 2> d2D_ref(&d2D[0][0], {4, 5});
+	auto&& d2D_ref = *multi::array_ptr<double, 2>(&d2D[0][0], {4, 5});
 	
 	BOOST_REQUIRE( not std::is_sorted(begin(d2D_ref), end(d2D_ref) ) );
 	std::stable_sort( begin(d2D_ref), end(d2D_ref) );
