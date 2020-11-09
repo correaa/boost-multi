3dfd2985d04546d76f8ecfd8be3b2c8e1a877464
Alfredo Correa <alfredo.correa@gmail.com>
2020-11-09 06:10:11 -0800

    command line removed -x 0 boost.test option

--- test/iterator.cpp
+++ test/iterator.cpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-$CXX $CXXFLAGS $0 -o $0x$X -lboost_unit_test_framework&&$0x$X -x 0&&rm $0x$X;exit
+$CXX $CXXFLAGS $0 -o $0.$X -lboost_unit_test_framework&&$0.$X $@&&rm $0.$X;exit
 #endif
 // © Alfredo A. Correa 2018-2020
 
