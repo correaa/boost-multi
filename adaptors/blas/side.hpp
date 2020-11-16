fadc8164496213d7f9e4c9ad330a71ae937454e1
Alfredo Correa <alfredo.correa@gmail.com>
2020-11-16 12:29:03 -0800

    removed literal R

--- adaptors/blas/side.hpp
+++ adaptors/blas/side.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-$CXX $0 -o $0x `pkg-config --libs blas` -lboost_unit_test_framework&&$0x&&rm $0x;exit
+$CXXX $CXXFLAGS $0 -o $0.$X `pkg-config --libs blas` -lboost_unit_test_framework&&$0.$X&&rm $0.$X;exit
 #endif
 // © Alfredo A. Correa 2019
 
@@ -14,11 +14,11 @@ namespace boost{
 namespace multi{
 namespace blas{
 
-enum class SIDE : char{L='L', R='R'};
+//enum class SIDE : char{L='L', R='R'};
 
 enum side : char{
-	left = static_cast<char>(SIDE::R), right = static_cast<char>(SIDE::L),
-	pre_multiply = static_cast<char>(SIDE::R), post_multiply = static_cast<char>(SIDE::L)
+	left = 'R', right = 'L',
+	pre_multiply = 'R', post_multiply = 'L'
 };
 
 side swap(side s){
@@ -34,7 +34,7 @@ side swap(side s){
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-#if not __INCLUDE_LEVEL__ // _TEST_MULTI_ADAPTORS_BLAS_SIDE
+#if defined(__INCLUDE_LEVEL__) and not __INCLUDE_LEVEL__
 
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi BLAS adaptors side"
 #define BOOST_TEST_DYN_LINK
