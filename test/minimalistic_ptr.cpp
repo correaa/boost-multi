bfdd26fa3bf44169da2e87f22dd2569314d3e643
Alfredo Correa <alfredo.correa@gmail.com>
2021-03-15 19:15:08 -0700

    solve all clang-tidy warnings

--- test/minimalistic_ptr.cpp
+++ test/minimalistic_ptr.cpp
@@ -7,6 +7,8 @@ $CXX $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 #define BOOST_TEST_DYN_LINK
 #include<boost/test/unit_test.hpp>
 
+#include<array>
+
 #include "../array.hpp"
 
 namespace multi = boost::multi;
@@ -43,18 +45,13 @@ public:
 	using default_allocator_type = std::allocator<T>;
 };
 
-}
+} // namespace minimalistic
 
 BOOST_AUTO_TEST_CASE(test_minimalistic_ptr){
 
-//	int X::* s = &X::a1; // gives warning
-//	X x{1, 2.5, 1.2};
-//	assert( x.*s == x.a1 );
+	std::array<double, 100> buffer{};
 
-//	X X::*ss = &X::X; 
-
-	double* buffer = new double[100];
-	multi::array_ptr<double, 2, minimalistic::ptr<double> > CCP(minimalistic::ptr<double>{buffer}, {10, 10});
+	multi::array_ptr<double, 2, minimalistic::ptr<double> > CCP(minimalistic::ptr<double>{buffer.data()}, {10, 10});
 	(*CCP)[2]; // requires operator+ 
 	(*CCP)[1][1]; // requires operator*
 	(*CCP)[1][1] = 9;
@@ -62,7 +59,6 @@ BOOST_AUTO_TEST_CASE(test_minimalistic_ptr){
 
 	auto&& CC2 = CCP->template static_array_cast<double, minimalistic::ptr2<double>>();
 	BOOST_REQUIRE( &CC2[1][1] == &(*CCP)[1][1] );
-	delete[] buffer;
 
 }
 
