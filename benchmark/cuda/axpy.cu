f9cdc7de88753bd0868c5e400bbe89407aed3374
Alfredo Correa <alfredo.correa@gmail.com>
2021-03-23 02:34:00 -0700

    reorganize alternatives

--- benchmark/cuda/axpy.cu
+++ benchmark/cuda/axpy.cu
@@ -14,12 +14,49 @@ sudo cpupower frequency-set --governor performance; cmake -DCMAKE_CUDA_COMPILER=
 
 namespace multi = boost::multi;
 
+////////////////////////////////////////////////////////////////////////////////
 template<class T, class X, class Y>
 Y&& axpy_cpu_transform(T alpha, X const& x, Y&& y){
 	assert( size(x) == size(y) );
 	std::transform( begin(x), end(x), begin(y), begin(y), [alpha](auto const& a, auto const& b){return a + alpha*b;} );
 	return std::forward<Y>(y);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+template<class T> 
+__global__ void axpy_gpu_legacy_kernel(int n, T alpha, T const* x, int x_stride, T* y, int y_stride){
+	int i = blockIdx.x*blockDim.x + threadIdx.x;
+	if(i < n){*(y + i*y_stride) = alpha* *(x + i*x_stride) + *(y + i*y_stride);}
+}
+
+template<class T, class X, class Y>
+Y&& axpy_gpu_legacy(T alpha, X const& x, Y&& y){
+	assert( size(x) == size(y) );
+	axpy_gpu_legacy_kernel<<<(size(x) + 255) / 256, 256>>>(size(x), alpha, raw_pointer_cast(base(x)), stride(x), raw_pointer_cast(base(y)), stride(y));
+	return std::forward<Y>(y);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template<class T, class ItX, class ItY> 
+__global__ void axpy_gpu_multi_kernel(int n, T alpha, ItX x, ItY y){
+	int i = blockIdx.x*blockDim.x + threadIdx.x;
+	if(i < n){y[i] = alpha*x[i] + y[i];}
+}
+
+template<class T, class X, class Y>
+Y&& axpy_gpu_multi_kernel(T alpha, X const& x, Y&& y){
+	assert( size(x) == size(y) );
+	axpy_gpu_multi_kernel<<<(size(x) + 255) / 256, 256>>>(size(x), alpha, begin(x), begin(y));
+	return std::forward<Y>(y);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+template<class T, class X, class Y>
+Y&& axpy_gpu_multi_thrust(T alpha, X const& x, Y&& y){
+	thrust::transform(thrust::cuda::par, begin(x), end(x), begin(y), begin(y), [alpha] __device__ (auto a, auto b){return a + alpha*b;} );
+	return std::forward<Y>(y);
+}
+
 
 static void BM_axpy_cpu_transform(benchmark::State& state){
 
@@ -41,19 +78,7 @@ static void BM_axpy_cpu_transform(benchmark::State& state){
 }
 BENCHMARK(BM_axpy_cpu_transform);
 
-template<class T> __global__ void axpy_gpu_legacy_kernel(int n, T alpha, T const* x, int x_stride, T* y, int y_stride){
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if(i < n){*(y + i*y_stride) = alpha* *(x + i*x_stride) + *(y + i*y_stride);}
-}
-
-template<class T, class X, class Y>
-Y&& axpy_gpu_legacy(T alpha, X const& x, Y&& y){
-	assert( size(x) == size(y) );
-	axpy_gpu_legacy_kernel<<<(size(x) + 255) / 256, 256>>>(size(x), alpha, raw_pointer_cast(base(x)), stride(x), raw_pointer_cast(base(y)), stride(y));
-	return std::forward<Y>(y);
-}
-
-static void BM_axpy_gpu_legacy_kernel(benchmark::State& state){
+static void BM_axpy_gpu_legacy(benchmark::State& state){
 
 	using T = double;
 	using alloc = thrust::system::cuda::allocator<double>;
@@ -71,20 +96,7 @@ static void BM_axpy_gpu_legacy_kernel(benchmark::State& state){
 	state.SetBytesProcessed(state.iterations()*size(x)*2.*sizeof(T));
 	state.SetItemsProcessed(state.iterations()*size(x)             );
 }
-BENCHMARK(BM_axpy_gpu_legacy_kernel);
-
-
-template<class T, class ItX, class ItY> __global__ void axpy_gpu_multi_kernel(int n, T alpha, ItX x, ItY y){
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if(i < n){y[i] = alpha*x[i] + y[i];}
-}
-
-template<class T, class X, class Y>
-Y&& axpy_gpu_multi_kernel(T alpha, X const& x, Y&& y){
-	assert( size(x) == size(y) );
-	axpy_gpu_multi_kernel<<<(size(x) + 255) / 256, 256>>>(size(x), alpha, begin(x), begin(y));
-	return std::forward<Y>(y);
-}
+BENCHMARK(BM_axpy_gpu_legacy);
 
 static void BM_axpy_gpu_multi_kernel(benchmark::State& state){
 
@@ -105,12 +117,6 @@ static void BM_axpy_gpu_multi_kernel(benchmark::State& state){
 	state.SetItemsProcessed(state.iterations()*size(x)             );
 }
 BENCHMARK(BM_axpy_gpu_multi_kernel);
-
-template<class T, class X, class Y>
-Y&& axpy_gpu_multi_thrust(T alpha, X const& x, Y&& y){
-	thrust::transform(thrust::cuda::par, begin(x), end(x), begin(y), begin(y), [alpha] __device__ (auto a, auto b){return a + alpha*b;} );
-	return std::forward<Y>(y);
-}
 
 static void BM_axpy_gpu_multi_thrust(benchmark::State& state){
 
