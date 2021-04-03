ccc2e6c068c97eb20588f58363f9a51d1513bc58
Alfredo Correa <alfredo.correa@gmail.com>
2021-04-02 22:59:54 -0700

    fix equality for c++20

--- array_ref.hpp
+++ array_ref.hpp
@@ -208,8 +208,10 @@ struct basic_array_ptr :
 	using Ref::base_;
 	using Ref::layout;
 	constexpr bool operator==(basic_array_ptr const& o) const{return base_==o.base_ and layout()==o.layout();}
-	template<class O> constexpr bool operator==(O const& o) const{return base()==o->base() and layout() == o->layout();}
-	template<class O> constexpr bool operator!=(O const& o) const{return not ((*this)==o);}
+//	template<class O> constexpr bool operator==(O const& o) const{return base()==o->base() and layout() == o->layout();}
+//	template<class O> constexpr bool operator!=(O const& o) const{return not ((*this)==o);}
+	template<class O> friend constexpr bool operator==(basic_array_ptr const& s, O const& o){return base()==o->base() and layout() == o->layout();}
+	template<class O> friend constexpr bool operator!=(basic_array_ptr const& s, O const& o){return not(s==o);}
 	template<class O, std::enable_if_t<not std::is_base_of<basic_array_ptr, O>{}, int> =0> friend constexpr bool operator==(O const& o, basic_array_ptr const& s){return s==o;}
 	template<class O, std::enable_if_t<not std::is_base_of<basic_array_ptr, O>{}, int> =0> friend constexpr bool operator!=(O const& o, basic_array_ptr const& s){return not(o==s);}
 protected:
