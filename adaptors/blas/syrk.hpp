8aa3b5e3d6814203f8aa47ec5dab544e51d89ebb
Alfredo Correa <correaa@llnl.gov>
2021-09-01 20:52:09 -0700

    finish tidy syrk

--- adaptors/blas/syrk.hpp
+++ adaptors/blas/syrk.hpp
@@ -10,9 +10,8 @@ $CXX $0 -o $0x -lboost_unit_test_framework -lboost_timer \
 #define MULTI_ADAPTORS_BLAS_SYRK_HPP
 
 #include "../blas/core.hpp"
-
-#include "../blas/numeric.hpp"
 #include "../blas/filling.hpp"
+#include "../blas/numeric.hpp"
 
 namespace boost{
 namespace multi{namespace blas{
@@ -23,12 +22,13 @@ template<typename AA, typename BB, class A2D, class C2D>
 auto syrk(filling c_side, AA alpha, A2D const& a, BB beta, C2D&& c)
 ->decltype(syrk('\0', '\0', size(c), size(a), alpha, base(a), stride(rotated(a)), beta, base(c), stride(c)), std::forward<C2D>(c)){
 	assert( size(c) == size(rotated(c)) );
-	if(stride(a)==1)
-		if(stride(c)==1) syrk(flip(c_side)==filling::upper?'L':'U', 'N', size(c), size(a         ), alpha, base(a), stride(rotated(a)), beta, base(c), stride(rotated(c)));
-		else             syrk(c_side      ==filling::upper?'L':'U', 'N', size(c), size(rotated(a)), alpha, base(a), stride(rotated(a)), beta, base(c), stride(        c ));
-	else
-		if(stride(c)==1) syrk(flip(c_side)==filling::upper?'L':'U', 'T', size(c), size(rotated(a)), alpha, base(a), stride(a), beta, base(c), stride(rotated(c)));
-		else             syrk(c_side      ==filling::upper?'L':'U', 'T', size(c), size(rotated(a)), alpha, base(a), stride(a), beta, base(c), stride(        c ));
+	if(stride(a)==1){
+		if(stride(c)==1){syrk(flip(c_side)==filling::upper?'L':'U', 'N', size(c), size(a         ), alpha, base(a), stride(rotated(a)), beta, base(c), stride(rotated(c)));}
+		else            {syrk(c_side      ==filling::upper?'L':'U', 'N', size(c), size(rotated(a)), alpha, base(a), stride(rotated(a)), beta, base(c), stride(        c ));}
+	}else{
+		if(stride(c)==1){syrk(flip(c_side)==filling::upper?'L':'U', 'T', size(c), size(rotated(a)), alpha, base(a), stride(a), beta, base(c), stride(rotated(c)));}
+		else            {syrk(c_side      ==filling::upper?'L':'U', 'T', size(c), size(rotated(a)), alpha, base(a), stride(a), beta, base(c), stride(        c ));}
+	}
 	return std::forward<C2D>(c);
 }
 
@@ -54,7 +54,9 @@ auto syrk(A2D const& A)
 ->decltype(syrk(1., A)){
 	return syrk(1., A);}
 
-}}}
+} // end namespace blas
+} // end namespace multi
+} // end namespace boost
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
