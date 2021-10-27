b82b63c3d87641d708ce16cc34a28859dc7d5c24
Alfredo Correa <correaa@llnl.gov>
2021-10-27 15:16:31 -0700

    use next without default argument

--- array_ref.hpp
+++ array_ref.hpp
@@ -1422,16 +1422,19 @@ struct basic_array<T, 0, ElementPtr, Layout>
 		return *this;  // lints(cppcoreguidelines-c-copy-assignment-signature,misc-unconventional-assign-operator)
 	}
 
-	constexpr auto operator==(element const& e) const& -> bool {return adl_equal(&e, std::next(&e), this->base_);}
-	constexpr auto operator!=(element const& e) const& -> bool {return not operator==(e);}
+	constexpr auto operator==(element const& e) const& -> bool {
+		assert(this->num_elements()==1);
+		return adl_equal(&e, std::next(&e, this->num_elements()), this->base());
+	}
+	constexpr auto operator!=(element const& e) const& {return not operator==(e);}
 
 	template<class TT, class=decltype(std::declval<typename basic_array::element>()==std::declval<TT>())>
 	constexpr auto operator==(TT const& e) const&
-	->decltype(adl_equal(&e, std::next(&e), this->base_)) {
-		return adl_equal(&e, std::next(&e), this->base_); }
+	->decltype(adl_equal(&e, std::next(&e, this->num_elements()), this->base() )) {assert(this->num_elements()==1);
+		return adl_equal(&e, std::next(&e, this->num_elements()), this->base() ); }
 
 	template<class TT>
-	constexpr auto operator!=(TT const& e) const&->decltype(!operator==(e)) {return !operator==(e);}
+	constexpr auto operator!=(TT const& e) const& {return not operator==(e);}
 
 	template<class Range0>
 	auto operator=(Range0&& r)& -> basic_array& {
