a78ba437018ef5fb51a2c3d34e7da33e3f89b765
Alfredo Correa <correaa@llnl.gov>
2022-01-16 18:03:51 -0800

    fix ws

--- include/multi/memory/allocator.hpp
+++ include/multi/memory/allocator.hpp
@@ -1,7 +1,5 @@
-#ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
-$CXXX $CXXFLAGS $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
-#endif
-// © Alfredo A. Correa 2019-2021
+// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
+// © Alfredo A. Correa 2019-2022
 
 #ifndef MULTI_MEMORY_ALLOCATOR_HPP
 #define MULTI_MEMORY_ALLOCATOR_HPP
@@ -11,27 +9,18 @@ $CXXX $CXXFLAGS $0 -o $0x -lboost_unit_test_framework&&$0x&&rm $0x;exit
 
 #include<cassert>
 
-#if defined(__cpp_lib_memory_resource) and __cpp_lib_memory_resource>=201603L
-#include<memory_resource> // will be standard in C++17
-#endif
+#include<memory_resource>
 
-namespace boost::multi::memory{
+namespace boost::multi::memory {
 
 template<class T, class Memory
-#if defined(__cpp_lib_memory_resource) and (__cpp_lib_memory_resource>=201603L)
-	= std::pmr::memory_resource //= std::allocator<std::byte>
-#endif
+	= std::pmr::memory_resource
 	, typename Constructor = std::allocator<T>
 >
 class allocator {
 	using memory_type = Memory;
-	memory_type* mp_ 
-#if defined(__cpp_lib_memory_resource) and (__cpp_lib_memory_resource>=201603L)
-		= std::pmr::get_default_resource() // will be standard in C++17
-#endif
-	;
+	memory_type* mp_ = std::pmr::get_default_resource();
 	using constructor_type = typename std::allocator_traits<Constructor>::template rebind_alloc<T>;
-//	using constructor_type = typename Constructor::template rebind<T>::other;
 	constructor_type ctor_;
 
  public:
@@ -42,39 +31,40 @@ class allocator {
 
 	allocator(allocator const& o) = default;
 	allocator(allocator&&) noexcept = default;
+
 	auto operator=(allocator const&) -> allocator& = default;
 	auto operator=(allocator&&) noexcept -> allocator& = default;
+
 	allocator() = default;
 	~allocator() = default;
 
-	allocator(memory_type* mp, constructor_type const& ctor) : mp_{mp}, ctor_{ctor}{}
+	allocator(memory_type* mp, constructor_type const& ctor) : mp_{mp}, ctor_{ctor} {}
 	// cppcheck-suppress noExplicitConstructor ; allocator *is* a pointer to a heap
-	allocator(memory_type* mp) : allocator(mp, constructor_type{}){} // NOLINT(google-explicit-constructor,hicpp-explicit-conversions) : to allow pointer syntax
+	allocator(memory_type* mp) : allocator(mp, constructor_type{}) {} // NOLINT(google-explicit-constructor,hicpp-explicit-conversions) : to allow pointer syntax
 
-	explicit allocator(constructor_type const& ctor) : mp_{}, ctor_{ctor}{}
+	explicit allocator(constructor_type const& ctor) : mp_{}, ctor_{ctor} {}
 
-
-	auto operator==(allocator const& o) const -> bool{return mp_ == o.mp_;}
-	auto operator!=(allocator const& o) const -> bool{return mp_ != o.mp_;}
+	auto operator==(allocator const& o) const -> bool {return mp_ == o.mp_;}
+	auto operator!=(allocator const& o) const -> bool {return mp_ != o.mp_;}
 
 	using void_pointer = typename std::pointer_traits<decltype(std::declval<memory_type*>()->allocate(0, 0))>::template rebind<void>;
 
 	NODISCARD("because otherwise it will generate a memory leak")
-	auto allocate(size_type n) -> pointer{
+	auto allocate(size_type n) -> pointer {
 		static_assert( alignof(std::max_align_t) >= 16 , "for cuda");
 		return static_cast<pointer>(static_cast<void_pointer>(mp_->allocate(
 			n*sizeof(value_type),
 			alignof(std::max_align_t) // this takes into account 
 		)));
 	}
-	void deallocate(pointer p, size_type n){
+	void deallocate(pointer p, size_type n) {
 		mp_->deallocate(p, n*sizeof(value_type));
 	}
 	template<class... Args>
-	void construct(pointer p, Args&&... args){
+	void construct(pointer p, Args&&... args) {
 		allocator_traits<Constructor>::construct(ctor_, p, std::forward<Args>(args)...);
 	}
-	void destroy(pointer p){allocator_traits<Constructor>::destroy(ctor_, p);}
+	void destroy(pointer p) {allocator_traits<Constructor>::destroy(ctor_, p);}
 };
 
 } // end namespace boost::multi::memory
