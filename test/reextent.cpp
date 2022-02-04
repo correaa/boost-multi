ab58959ff12d6b255f611c3c85d58fa5e4126274
Alfredo Correa <correaa@llnl.gov>
2022-02-03 22:45:29 -0800

    remove unused variable and fix headers

--- test/reextent.cpp
+++ test/reextent.cpp
@@ -1,6 +1,5 @@
 // -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-// Copyright 2018-2021 Alfredo A. Correa
-
+// Copyright 2018-2022 Alfredo A. Correa
 
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi reextent"
 #define BOOST_TEST_DYN_LINK
