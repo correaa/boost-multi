e07f052c95aaa1b616158e646ae5cfaa23b929c7
Alfredo Correa <correaa@llnl.gov>
2022-02-16 15:02:04 -0800

    split test

--- test/select_column.cpp
+++ test/select_column.cpp
@@ -60,6 +60,34 @@ BOOST_AUTO_TEST_CASE(multi_array_range_section_part2) {
 		{30., 31., 32.},
 	};
 
+	BOOST_REQUIRE( size( A(A.extension(), 2) ) == size(A) );
+
+	auto&& col2( A(A.extension(), 2) ); // select column #2
+	// same as A(extesion(A), 2)
+	// same as A(A.extension(0), 2);
+	// same as rotated(A)[2];
+	BOOST_REQUIRE( col2.size(0) == size(A) );
+
+	BOOST_REQUIRE( dimensionality(col2) == 1 );
+	BOOST_REQUIRE( size(col2) == size(A) );
+	BOOST_REQUIRE( col2.size() == size(A) );
+	BOOST_REQUIRE( stride(col2) == 3 );
+	BOOST_REQUIRE( col2[0] == 02. );
+	BOOST_REQUIRE( col2[1] == 12. );
+	BOOST_REQUIRE(( col2 == multi::array<double, 1>{02., 12., 22., 32.} ));
+	BOOST_REQUIRE(( col2 == multi::array<double, 1>(rotated(A)[2]) ));
+	BOOST_REQUIRE(( col2 == rotated(A)[2] ));
+	BOOST_REQUIRE(( col2 == A(A.extension(), 2) ));
+}
+
+BOOST_AUTO_TEST_CASE(multi_array_range_section_syntax) {
+	multi::array<double, 2> A = {
+		{00., 01., 02.},
+		{10., 11., 12.},
+		{20., 21., 22.},
+		{30., 31., 32.},
+	};
+
 	using multi::_;
 	using multi::U;
 
@@ -87,24 +115,5 @@ BOOST_AUTO_TEST_CASE(multi_array_range_section_part2) {
 
 	BOOST_REQUIRE( size( A(    *_<2 , 2) ) == 2 );
 	BOOST_REQUIRE( size( A(     U<2 , 2) ) == 2 );
-
-	BOOST_REQUIRE( size( A(A.extension(), 2) ) == size(A) );
-
-	auto&& col2( A(A.extension(), 2) ); // select column #2
-	// same as A(extesion(A), 2)
-	// same as A(A.extension(0), 2);
-	// same as rotated(A)[2];
-	BOOST_REQUIRE( col2.size(0) == size(A) );
-
-	BOOST_REQUIRE( dimensionality(col2) == 1 );
-	BOOST_REQUIRE( size(col2) == size(A) );
-	BOOST_REQUIRE( col2.size() == size(A) );
-	BOOST_REQUIRE( stride(col2) == 3 );
-	BOOST_REQUIRE( col2[0] == 02. );
-	BOOST_REQUIRE( col2[1] == 12. );
-	BOOST_REQUIRE(( col2 == multi::array<double, 1>{02., 12., 22., 32.} ));
-	BOOST_REQUIRE(( col2 == multi::array<double, 1>(rotated(A)[2]) ));
-	BOOST_REQUIRE(( col2 == rotated(A)[2] ));
-	BOOST_REQUIRE(( col2 == A(A.extension(), 2) ));
 }
 
