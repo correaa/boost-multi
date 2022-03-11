08f41ece703b77770de02521f572ab7fcd8f3a28
Alfredo Correa <correaa@llnl.gov>
2022-03-11 10:41:02 -0800

    add device host device performance test

--- adaptors/thrust/test/array.cu
+++ adaptors/thrust/test/array.cu
@@ -1,18 +1,19 @@
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi CUDA thrust"
-#define BOOST_TEST_DYN_LINK
 #include<boost/test/unit_test.hpp>
+
+#include "../../../adaptors/thrust/fix_complex_traits.hpp"
+#include          "../../../detail/fix_complex_traits.hpp"
 
 #include <boost/timer/timer.hpp>
 
-#include<thrust/memory.h>
-
 #include "../../../adaptors/thrust.hpp"
-//#include "../../../memory/adaptors/cuda/cached/allocator.hpp"
 
-#include <thrust/uninitialized_copy.h>
 #include <thrust/complex.h>
 #include <thrust/device_ptr.h>
-#include <thrust/system/cuda/experimental/pinned_allocator.h>
+#include <thrust/memory.h>
+#include <thrust/uninitialized_copy.h>
+
+#include <boost/mpl/list.hpp>
 
 namespace multi = boost::multi;
 
@@ -33,72 +34,144 @@ BOOST_AUTO_TEST_CASE(issue_118) {
 
 	BOOST_REQUIRE( M[1][2] == 12. );
 
+	thrust::cuda::pointer<double> cpd = thrust::device_ptr<double>{};
 }
 
-BOOST_AUTO_TEST_CASE(thrust_cpugpu_issue123){
-	using T = char;
+using types_list = boost::mpl::list<char, double, std::complex<double>, thrust::complex<double> >;
+BOOST_AUTO_TEST_CASE_TEMPLATE(thrust_cpugpu_1D_issue123, T, types_list) {
 
-	multi::array<T, 2, thrust::cuda::allocator<T>> Devc({10240, 10240}, 'a');
-	multi::array<T, 2>                             Host({10240, 10240}, 'z');
+	static_assert( multi::is_trivially_default_constructible<T>{}, "!");
+	static_assert( std::is_trivially_copy_constructible<T>{}   , "!");
+	static_assert( std::is_trivially_assignable<T&, T>{}       , "!");
 
-	BOOST_TEST_REQUIRE( Devc[0][0] == 'a' );
+	std::cout<<"==="<<std::endl;
+
+	multi::array<T, 1, thrust::cuda::allocator<T>> Devc(multi::extensions_t<1>{10240*10240});
+	multi::array<T, 1>                             Host(multi::extensions_t<1>{10240*10240}, T{});
+
+	std::cout<<"1D "<< typeid(T).name() <<" total data size: "<< Host.num_elements()*sizeof(T) / 1073741824. <<" GB\n---\n";
 	{
-		boost::timer::auto_cpu_timer t;                        // 0.010537s
 		Devc = Host;
 	}
 	{
-		boost::timer::auto_cpu_timer t;
-		Devc.sliced(0, 5120) = Host.sliced(0, 5120);           //  0.005292s
+		boost::timer::auto_cpu_timer t{""};
+		Devc = Host;
+		std::cout<<"contiguous host to devc: "<< Host.num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
 	}
 	{
-		Host[123][456] = 'x';
-		boost::timer::auto_cpu_timer t;
+		boost::timer::auto_cpu_timer t{""};
+		Devc.sliced(0, 10240*10240/2) = Host.sliced(0, 10240*10240/2);           //  0.005292s
+		std::cout<<"sliced     host to devc: "<< Host.sliced(0, 10240*10240/2).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+//	{
+//		boost::timer::auto_cpu_timer t{""};
+//		Devc.sliced(0, 5120, 2) = Host.sliced(0, 5120, 2);  // 0.002859s
+//		std::cout<<"strided host to device "<< Host.sliced(0, 5120, 2).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+//	}
+//  BOOST_TEST_REQUIRE( T{Devc[123][456]} == T{} );
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Host = Devc;
+		std::cout<<"contiguous devc to host: "<< Host.num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Host.sliced(0, 10240*10240/2) = Devc.sliced(0, 10240*10240/2);           //  0.005292s
+		std::cout<<"sliced     devc to host: "<< Host.sliced(0, 10240*10240/2).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	std::cout<<"==="<<std::endl;
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(thrust_cpugpu_issue123, T, types_list) {
+
+	multi::array<T, 2, thrust::cuda::allocator<T>> Devc({10240, 10240});
+	multi::array<T, 2>                             Host({10240, 10240}, T{});
+
+	std::cout<<"2D "<< typeid(T).name() <<" max data size "<< Host.num_elements()*sizeof(T) / 1073741824. <<" GB\n---\n";
+	{
 		Devc({0, 5120},{0, 5120}) = Host({0, 5120},{0, 5120});  // 0.002859s
 	}
-	BOOST_TEST_REQUIRE( Devc[123][456] == 'x' );
-}
-
-BOOST_AUTO_TEST_CASE(thrust_cpugpu_issue123_1D){
-	using T = char;
-
-	multi::array<T, 1, thrust::cuda::allocator<T>> Devc({10240*10240}, 'a');
-	multi::array<T, 1>                             Host({10240*10240}, 'z');
-
-	BOOST_TEST_REQUIRE( Devc[0] == 'a' );
 	{
-		boost::timer::auto_cpu_timer t;
-		Devc() = Host();
-	}
-	{
-		boost::timer::auto_cpu_timer t;
-		Devc({0, 10240*10240/2}) = Host({0, 10240*10240/2});
-	}
-}
-
-BOOST_AUTO_TEST_CASE(thrust_cpugpu_issue123_3D){
-	using T = char;
-
-	multi::array<T, 3, thrust::cuda::allocator<T>> Devc({1024, 1024, 1024}, 'a');
-	multi::array<T, 3>                             Host({1024, 1024, 1024}, 'z');
-
-	BOOST_TEST_REQUIRE( Devc[0][0][0] == 'a' );
-	{
-		boost::timer::auto_cpu_timer t;
+		boost::timer::auto_cpu_timer t{""};
 		Devc = Host;
+		std::cout<<"contiguous host to devc: "<< Host.num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
 	}
 	{
-		boost::timer::auto_cpu_timer t;
-		Devc({0, 512}, {0, 512}, {0, 512}) = Host({0, 512}, {0, 512}, {0, 512});
+		boost::timer::auto_cpu_timer t{""};
+		Devc.sliced(0, 5120) = Host.sliced(0, 5120);           //  0.005292s
+		std::cout<<"sliced     host to devc: "<< Host.sliced(0, 5120).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
 	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Devc({0, 5120},{0, 5120}) = Host({0, 5120},{0, 5120});  // 0.002859s
+		std::cout<<"strided    host to devc: "<< Host({0, 5120},{0, 5120}).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Host = Devc;
+		std::cout<<"contiguous devc to host: "<< Host.num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Host.sliced(0, 5120) = Devc.sliced(0, 5120);           //  0.005292s
+		std::cout<<"sliced     devc to host: "<< Host.sliced(0, 5120).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Host({0, 5120},{0, 5120}) = Devc({0, 5120},{0, 5120});  // 0.002859s
+		std::cout<<"strided    devc to host: "<< Host({0, 5120},{0, 5120}).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	std::cout<<"==="<<std::endl;
 }
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(thrust_cpugpu_issue123_3D, T, types_list) {
+
+	multi::array<T, 3, thrust::cuda::allocator<T>> Devc({1024, 1024, 100});
+	multi::array<T, 3>                             Host({1024, 1024, 100}, T{});
+
+	std::cout<<"3D "<< typeid(T).name() <<" max data size "<< Host.num_elements()*sizeof(T) / 1073741824. <<" GB\n---\n";
+	{
+		Devc({0, 512}, {0, 512}, {0, 512}) = Host({0, 512}, {0, 512}, {0, 512});  // 0.002859s
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Devc = Host;
+		std::cout<<"contiguous host to devc: "<< Host.num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Devc.sliced(0, 512) = Host.sliced(0, 512);           //  0.005292s
+		std::cout<<"sliced     host to devc: "<< Host.sliced(0, 512).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Devc({0, 512}, {0, 512}, {0, 512}) = Host({0, 512}, {0, 512}, {0, 512});  // 0.002859s
+		std::cout<<"strided    host to devc: "<< Host({0, 512},{0, 512}, {0, 512}).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Host = Devc;
+		std::cout<<"contiguous devc to host: "<< Host.num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Devc.sliced(0, 512) = Host.sliced(0, 512);           //  0.005292s
+		std::cout<<"sliced     devc to host: "<< Host.sliced(0, 512).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	{
+		boost::timer::auto_cpu_timer t{""};
+		Devc({0, 512}, {0, 512}, {0, 512}) = Host({0, 512}, {0, 512}, {0, 512});  // 0.002859s
+		std::cout<<"strided    devc to host: "<< Host({0, 512},{0, 512}, {0, 512}).num_elements()*sizeof(T) / (t.elapsed().wall/1e9) / 1073741824. << "GB/sec\n";
+	}
+	std::cout<<"==="<<std::endl;
+}
 
 #if 0
-namespace inq{
+namespace inq {
 	using complex = thrust::complex<double>;
 }
 
-BOOST_AUTO_TEST_CASE(thrust_complex_cached_1D){
+BOOST_AUTO_TEST_CASE(thrust_complex_cached_1D) {
 	using T = inq::complex;
 	multi::array<T, 1, boost::multi::memory::cuda::cached::allocator<T> > aa(10, T{1., 1.});
 	multi::array<T, 1, boost::multi::memory::cuda::cached::allocator<T> > bb(10, T{2., 2.});
@@ -108,7 +181,7 @@ BOOST_AUTO_TEST_CASE(thrust_complex_cached_1D){
 	BOOST_REQUIRE(( bb[0] == T{1., 1.} ));
 }
 
-BOOST_AUTO_TEST_CASE(thrust_complex_cached_without_values_1D){
+BOOST_AUTO_TEST_CASE(thrust_complex_cached_without_values_1D) {
 	using T = inq::complex;
 	multi::array<T, 1, boost::multi::memory::cuda::cached::allocator<T> > aa(10);
 	multi::array<T, 1, boost::multi::memory::cuda::cached::allocator<T> > bb(10);
@@ -120,7 +193,7 @@ BOOST_AUTO_TEST_CASE(thrust_complex_cached_without_values_1D){
 	BOOST_REQUIRE(( bb[0] == aa[0] ));
 }
 
-BOOST_AUTO_TEST_CASE(thrust_complex_cached_2D){
+BOOST_AUTO_TEST_CASE(thrust_complex_cached_2D) {
 	using T = inq::complex;
 	multi::array<T, 2, boost::multi::memory::cuda::cached::allocator<T> > aa({10, 20}, T{1., 1.});
 	multi::array<T, 2, boost::multi::memory::cuda::cached::allocator<T> > bb({10, 20}, T{2., 2.});
@@ -130,7 +203,7 @@ BOOST_AUTO_TEST_CASE(thrust_complex_cached_2D){
 	BOOST_REQUIRE(( bb[0][0] == T{1., 1.} ));
 }
 
-BOOST_AUTO_TEST_CASE(thrust_complex_cached_without_values_2D){
+BOOST_AUTO_TEST_CASE(thrust_complex_cached_without_values_2D) {
 	using T = inq::complex;
 	multi::array<T, 2, boost::multi::memory::cuda::cached::allocator<T> > aa({10, 20});
 	multi::array<T, 2, boost::multi::memory::cuda::cached::allocator<T> > bb({10, 20});
@@ -142,7 +215,7 @@ BOOST_AUTO_TEST_CASE(thrust_complex_cached_without_values_2D){
 	BOOST_REQUIRE(( bb[0][0] == aa[0][0] ));
 }
 
-BOOST_AUTO_TEST_CASE(array){
+BOOST_AUTO_TEST_CASE(array) {
 
 //{
 //	multi::thrust::cuda::array<double, 2> C({2, 3});
