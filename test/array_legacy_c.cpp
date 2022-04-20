4ea64060586e23fb0fa0d65ab2155a8bba2748e5
Alfredo Correa <correaa@llnl.gov>
2022-04-20 11:41:28 -0700

    add const attribute

--- test/array_legacy_c.cpp
+++ test/array_legacy_c.cpp
@@ -88,11 +88,11 @@ BOOST_AUTO_TEST_CASE(array_legacy_c) {
 	}
 }
 
-auto f2(multi::array_ref<double, 1>&& R)
+auto f2(multi::array_ref<double, 1>&& R) -> double&
 #ifdef __GNUC__
 __attribute__((const))
 #endif
--> double&;
+;
 
 auto f2(multi::array_ref<double, 1>&& R) -> double& {return R[2];}
 
