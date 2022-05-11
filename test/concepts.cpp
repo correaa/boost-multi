23e5a76a11a3dd767f8f7b350dea974d252b00ae
Alfredo Correa <alfredo.correa+work@gmail.com>
2022-05-11 21:30:06 +0000

    Update concepts.cpp

--- test/concepts.cpp
+++ test/concepts.cpp
@@ -26,10 +26,10 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(convertibles, NDArray, NDArrays)
 
 	using NDRef = typename NDArray::ref;
 
-	static_assert(std::is_convertible_v<NDRef, NDArray>);
+	static_assert( std::is_convertible_v<NDRef, NDArray> );
 
-	static_assert( std::is_convertible_v<typename NDRef::      reference, typename NDRef::value_type>);
-	static_assert( std::is_convertible_v<typename NDRef::const_reference, typename NDRef::value_type>);
+	static_assert( std::is_convertible_v<typename NDRef::      reference, typename NDRef::value_type> );
+	static_assert( std::is_convertible_v<typename NDRef::const_reference, typename NDRef::value_type> );
 
 	static_assert( std::is_same_v<typename NDRef::element_type, typename multi::array<double, 1>::value_type> );
 	static_assert( std::is_same_v<typename NDRef::element_ref , typename multi::array<double, 1>::reference > );
