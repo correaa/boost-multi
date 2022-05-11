bbb308880b79e0807125526806045f2b0836b52b
Alfredo Correa <alfredo.correa+work@gmail.com>
2022-05-11 21:28:42 +0000

    Update concepts.cpp

--- test/concepts.cpp
+++ test/concepts.cpp
@@ -21,13 +21,15 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(convertibles, NDArray, NDArrays)
 	static_assert( std::is_convertible_v<typename NDArray::      reference, typename NDArray::value_type> );
 	static_assert( std::is_convertible_v<typename NDArray::const_reference, typename NDArray::value_type> );
 
-	static_assert( std::is_same_v<typename NDArray::element_type, typename multi::array<double, 1>::value_type> );
-	static_assert( std::is_same_v<typename NDArray::element_ref , typename multi::array<double, 1>::reference > );
+	static_assert( std::is_same_v<typename NDArray::element_type, typename multi::array<double, 1>::value_type>);
+	static_assert( std::is_same_v<typename NDArray::element_ref , typename multi::array<double, 1>::reference >);
 
 	using NDRef = typename NDArray::ref;
 
-	static_assert( std::is_convertible_v<typename NDRef::      reference, typename NDRef::value_type> );
-	static_assert( std::is_convertible_v<typename NDRef::const_reference, typename NDRef::value_type> );
+	static_assert(std::is_convertible_v<NDRef, NDArray>);
+
+	static_assert( std::is_convertible_v<typename NDRef::      reference, typename NDRef::value_type>);
+	static_assert( std::is_convertible_v<typename NDRef::const_reference, typename NDRef::value_type>);
 
 	static_assert( std::is_same_v<typename NDRef::element_type, typename multi::array<double, 1>::value_type> );
 	static_assert( std::is_same_v<typename NDRef::element_ref , typename multi::array<double, 1>::reference > );
