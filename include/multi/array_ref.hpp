65424bd905657d7e29b1bb9acd3d5f7904039462
Alfredo Correa <correaa@llnl.gov>
2022-05-11 11:03:19 -0700

    avoid constant conversion

--- include/multi/array_ref.hpp
+++ include/multi/array_ref.hpp
@@ -233,7 +233,7 @@ struct basic_array_ptr  // NOLINT(fuchsia-multiple-inheritance) : to allow mixin
 
 	template<class, class> friend struct basic_array_ptr;
 
-	constexpr basic_array_ptr(typename Ref::element_ptr p, layout_t<typename Ref::rank{}-1> l) : Ref{l, p} {}
+	constexpr basic_array_ptr(typename Ref::element_ptr p, layout_t<Ref::rank_v - 1> l) : Ref{l, p} {}
 	constexpr basic_array_ptr(typename Ref::element_ptr p, index_extensions<typename Ref::rank{}> e) : Ref{p, e} {}
 	template<class Array>
 	// cppcheck-suppress noExplicitConstructor ; no information loss, allows comparisons
