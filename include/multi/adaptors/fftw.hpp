0989314a7e0a4d46d75d8b0bfe7069dc04b97a6f
Alfredo Correa <correaa@llnl.gov>
2022-05-16 12:29:11 -0700

    move fftw range to header file

--- include/multi/adaptors/fftw.hpp
+++ include/multi/adaptors/fftw.hpp
@@ -713,4 +713,130 @@ auto rotate(Array& a)
 }  // end namespace fftw
 }  // end namespace boost::multi
 
+namespace boost::multi::fftw {
+
+template<class MDIterator>
+class fft_iterator {
+	MDIterator base_;
+	std::array<fftw::sign, MDIterator::rank_v> which_ = {};
+
+ public:
+	using iterator_type = MDIterator;
+
+	using difference_type = typename std::iterator_traits<MDIterator>::difference_type;
+	using value_type      = typename std::iterator_traits<MDIterator>::value_type;
+	using pointer         = void*;
+	class reference {
+		typename MDIterator::reference impl_;
+		explicit reference(typename MDIterator::reference impl) : impl_{std::move(impl)} {}
+		friend class fft_iterator;
+
+	 public:
+		using extensions_type = typename MDIterator::reference::extensions_type;
+		auto extensions() const -> extensions_type {return impl_.extensions();}
+	};
+
+	using iterator_category = std::random_access_iterator_tag;  // using iterator_category = std::input_iterator_tag;
+
+	explicit fft_iterator(iterator_type base, std::array<fftw::sign, MDIterator::rank_v> which) noexcept : base_{std::move(base)}, which_{which} {}
+
+	friend auto operator-(fft_iterator const& a, fft_iterator const& b) -> difference_type {
+		return a.base_ - b.base_;
+	}
+
+	template<class ItOut>
+	friend auto copy(fft_iterator first, fft_iterator last, ItOut d_first) {
+		assert(first.which_ == last.which_);
+		fftw::dft(
+			first.which_,
+			multi::ref(first.base_, last.base_),
+			multi::ref(d_first, d_first + (last.base_ - first.base_))
+		);
+
+		return d_first + (last.base_ - first.base_);
+	}
+	template<class ItOut>
+	friend auto uninitialized_copy(fft_iterator first, fft_iterator last, ItOut d_first) {
+		return copy(first, last, d_first);
+	}
+
+	auto operator*() const {return reference{*base_};}
+};
+
+template<class A> void what(A&&) = delete;
+
+template<class Array>
+class fft_range {
+	typename Array::const_iterator first_;
+	typename Array::size_type      n_;
+	std::array<fftw::sign, Array::rank_v> which_;
+
+ public:
+	using iterator_type = typename Array::const_iterator;
+
+	using size_type = typename Array::size_type;
+	using iterator = fft_iterator<iterator_type>;
+
+	using decay_type = typename Array::decay_type;
+
+	explicit fft_range(Array const& in, std::array<fftw::sign, Array::rank_v> which) : first_{in.begin()}, n_{in.size()}, which_{which} {}
+
+	auto begin() const {return iterator{first_     , which_};}
+	auto end()   const {return iterator{first_ + n_, which_};}
+
+	auto size()       const {return n_;}
+	auto extensions() const {return multi::ref(first_, first_ + n_).extensions();}
+
+	auto operator+() const {return decay_type(*this);}
+	auto base() const {return multi::ref(first_, first_ + n_);}
+	auto rotated() const {
+		auto new_which = which_;
+		std::rotate(new_which.begin(), new_which.begin() + 1, new_which.end());
+		return fft_range<std::decay_t<decltype(multi::ref(first_, first_ + n_).rotated())>>{multi::ref(first_, first_ + n_).rotated(), new_which};
+	}
+	auto unrotated() const {
+		auto new_which = which_;
+		std::rotate(new_which.rbegin(), new_which.rbegin() + 1, new_which.rend());
+		return fft_range<std::decay_t<decltype(multi::ref(first_, first_ + n_).unrotated())>>{multi::ref(first_, first_ + n_).unrotated(), new_which};
+	}
+	auto transposed() const {
+		auto new_which = which_;
+		std::swap(std::get<0>(new_which), std::get<1>(new_which));
+		return fft_range<std::decay_t<decltype(multi::ref(first_, first_ + n_).transposed())>>{multi::ref(first_, first_ + n_).transposed(), new_which};
+	}
+
+	template<class... FBNs>
+	auto operator()(FBNs... fbns) const {
+		static_assert( sizeof...(fbns) <= Array::rank_v , "too many arguments");
+		auto new_which = which_;
+		std::array<fftw::sign, sizeof...(fbns)> fbna{fbns...};
+		std::transform(fbna.begin(), fbna.end(), new_which.begin(), new_which.begin(),
+			[](auto fbn, auto nw) {
+				if(fbn == fftw::none) {return nw;}
+				assert(nw == fftw::none);
+				return fbn;
+			}
+		);
+		return fft_range<std::decay_t<decltype(multi::ref(first_, first_ + n_)())>>{multi::ref(first_, first_ + n_)(), new_which};
+	}
+};
+
+template<class Array> 
+auto  ref(Array const& in) {return fft_range{in, {}};}
+
+template<dimensionality_type ND = 1, class Array>
+auto  fft(Array const& in) {
+	std::array<fftw::sign, Array::rank_v> which{};
+	for(auto it = which.begin(); it != which.begin() + ND; ++it) {*it = fftw::forward ;}
+	return fft_range{in, which};
+}
+template<dimensionality_type ND = 1, class Array>
+auto ifft(Array const& in) {
+	std::array<fftw::sign, Array::rank_v> which{};
+	for(auto it = which.begin(); it != which.begin() + ND; ++it) {*it = fftw::backward;}
+	return fft_range{in, which};
+}
+
+}  // end namespace boost::multi::fftw
+
 #endif
