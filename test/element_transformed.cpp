1cf27634f629a48131e3e4dd98b8d17a583ca724
Alfredo Correa <correaa@llnl.gov>
2022-05-31 03:01:46 -0700

    add test for element_transform with mutation

--- test/element_transformed.cpp
+++ test/element_transformed.cpp
@@ -60,12 +60,40 @@ struct cConjd {  // NOLINT(readability-identifier-naming) for testing
 	std::complex<double> const& c_;
 };
 
+struct  Conjd {  // NOLINT(readability-identifier-naming) for testing
+	constexpr explicit  Conjd(std::complex<double>& c) : c_{c} {}
+	operator std::complex<double>() const {return std::conj(c_);}  // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
+
+	auto operator=(std::complex<double> const& other) && -> Conjd& {c_ = std::conj(other); return *this;}
+
+	friend auto operator==(std::complex<double> const& other,  Conjd const& self) {return std::conj(self.c_) == other;}
+	friend auto operator==( Conjd const& self, std::complex<double> const& other) {return other == std::conj(self.c_);}
+
+
+ private:
+	std::complex<double>& c_;
+};
+
 constexpr struct Conj_t {  // NOLINT(readability-identifier-naming) for testing
 	constexpr auto operator()(std::complex<double> const& c) const {return cConjd{c};}  // NOLINT(readability-identifier-naming)
+	constexpr auto operator()(std::complex<double>      & c) const {return  Conjd{c};}  // NOLINT(readability-identifier-naming)
 } Conj;
 
 BOOST_AUTO_TEST_CASE(multi_array_sliced_empty_1D_ConjProxy) {
 	multi::array<complex, 1> A = { 1. + 2.*I,  3. +  4.*I};
+	BOOST_REQUIRE( A.size() == 2 );
+
+	auto&& Ac = A.element_transformed(Conj);  // NOLINT(readability-const-return-type) to disable assignment
+	BOOST_REQUIRE( std::conj(A[0]) == Ac[0] );
+	BOOST_REQUIRE( std::conj(A[1]) == Ac[1] );
+
+	Ac[0] = 5. + 4.*I;  // not allowed, no operator=
+	BOOST_REQUIRE( Ac[0] == 5. + 4.*I );
+	BOOST_REQUIRE(  A[0] == 5. - 4.*I );
+}
+
+BOOST_AUTO_TEST_CASE(multi_array_sliced_empty_1D_ConjProxy_const) {
+	multi::array<complex, 1> const A = { 1. + 2.*I,  3. +  4.*I};
 	BOOST_REQUIRE( A.size() == 2 );
 
 	auto const& Ac = A.element_transformed(Conj);  // NOLINT(readability-const-return-type) to disable assignment
