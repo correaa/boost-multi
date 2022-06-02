0e8a4aa63f6284c6a25a1b69bef7dadc7928739d
Alfredo Correa <correaa@llnl.gov>
2022-06-02 16:48:32 -0700

    Add 2d examples const correctness

--- test/nico_const_correctness.cpp
+++ test/nico_const_correctness.cpp
@@ -49,3 +49,61 @@ BOOST_AUTO_TEST_CASE(mutating_views) {
 
 	(void)coll2, (void)coll1_take3_const, (void)coll1_take3;
 }
+
+template<class Array1D>
+void print_2d(Array1D const& coll) {
+//  *(coll.begin()->begin()) = 99;  // doesn't compile "assignment of read-only location"
+
+	for(auto const& row : coll) {
+		for(auto const& e : row) {
+			std::cout<< e <<", ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+BOOST_AUTO_TEST_CASE(const_views_2d) {
+	multi::array<int, 2> coll1 = {
+		{0, 8, 15, 47, 11, 42},
+		{0, 8, 15, 47, 11, 42}
+	};
+
+	print_2d( coll1 );  // prints "0, 8, 15, 47, 11, 42"
+
+	print_2d( coll1({0, 2}, {0, 3}) );  // similar to coll1 | take(3) // prints "0, 8, 15"
+
+	auto&& coll1_take3 = coll1({0, 2}, {0, 3});
+	print_2d( coll1_take3 );  // prints "0, 8, 15"
+}
+
+template<class Array1D>
+void fill_2d_99(Array1D&& coll) {
+//  for(auto const& row : coll) { does npt work because it would make it const
+	for(auto&& row : coll) {
+		for(auto&& e : row) {
+			e = 99;
+		}
+	}
+}
+
+BOOST_AUTO_TEST_CASE(mutating_views_2d) {
+	multi::array<int, 2> coll1 = {
+		{0, 8, 15, 47, 11, 42},
+		{0, 8, 15, 47, 11, 42}
+	};
+
+	fill_2d_99( coll1 );
+	fill_2d_99( coll1({0, 2}, {0, 3}) );
+
+	auto&& coll1_take3 = coll1({0, 2}, {0, 3});
+	fill_2d_99( coll1_take3 );
+
+	auto const& coll2 = coll1;
+//  fill_99( coll2 );  // doesn't compile because coll2 is const ("assignment of read-only" inside fill_99)
+//  fill_99( coll2({0, 3}) );  // similar to coll2 | take(3) doesn't compile ("assignment of read-only")
+
+	auto const& coll1_take3_const = coll1({0, 2}, {0, 3});
+//  fill_99( coll1_take3_const );  // doesn't compile because coll1_take3_const is const ("assignment of read-only")
+
+	(void)coll2, (void)coll1_take3_const, (void)coll1_take3;
+}
