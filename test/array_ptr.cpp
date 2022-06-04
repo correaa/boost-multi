dce38d8795b0161572508ea6ec8f29f0faa6ed40
Alfredo Correa <correaa@llnl.gov>
2022-06-04 13:24:41 -0700

    remove commands and macro

--- test/array_ptr.cpp
+++ test/array_ptr.cpp
@@ -1,10 +1,7 @@
-#ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-$CXX $CXXFLAGS $0 -o $0.$X -lboost_unit_test_framework&&$0.$X&&rm $0.$X;exit
-#endif
-// © Alfredo A. Correa 2019-2020
+// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
+// © Alfredo A. Correa 2019-2022
 
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi array pointer"
-#define BOOST_TEST_DYN_LINK
 #include<boost/test/unit_test.hpp>
 
 #include "multi/array.hpp"
@@ -91,7 +88,7 @@ BOOST_AUTO_TEST_CASE(multi_array_ptr) {
 		BOOST_REQUIRE( (*ps[2])[4] == 19 );
 		BOOST_REQUIRE( ps[2]->operator[](4) == 19 );
 	}
-	 {
+	{
 		std::vector<double> v1(100, 3.);
 		std::vector<double> const v2(100, 4.);
 		multi::array_ptr<double, 2> v1P2D(v1.data(), {10, 10});
