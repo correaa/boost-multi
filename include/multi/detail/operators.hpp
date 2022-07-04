20338c1224419b6cc72abdd41d651ee718c3fc0e
Alfredo Correa <alfredo.correa@gmail.com>
2022-07-03 19:21:37 -0700

    use long names in operators

--- include/multi/detail/operators.hpp
+++ include/multi/detail/operators.hpp
@@ -15,55 +15,18 @@ template<class Self> struct selfable {
 	using self_type = Self;
 	constexpr auto self() const -> self_type const& {return static_cast<self_type const&>(*this);}
 	constexpr auto self()       -> self_type      & {return static_cast<self_type      &>(*this);}
-	friend constexpr auto self(selfable const& s) -> self_type const& {return s.self();}
+	friend constexpr auto self(selfable const& self) -> self_type const& {return self.self();}
 };
 
 template<class Self, class U> struct equality_comparable2;
 
-//template<class Self, class Other>
-//struct equality_comparable2 : virtual selfable<Self> {
-//	friend constexpr auto operator==(equality_comparable2 const& s, Other const& o) {return     s.self() == o ;}
-//	friend constexpr auto operator!=(equality_comparable2 const& s, Other const& o) {return not(s.self() == o);}
-
-//	friend constexpr auto operator!=(Other const& o, equality_comparable2 const& s) {return not(s.self() == o);}
-//	friend constexpr auto operator==(Other const& o, equality_comparable2 const& s) {return     s.self() == o ;}
-//};
-
 template<class Self>
 struct equality_comparable2<Self, Self> : selfable<Self> {
-//	friend constexpr auto operator==(equality_comparable2 const& s, equality_comparable2 const& o) {return     s.self() == o.self() ;}
-	friend constexpr auto operator!=(equality_comparable2 const& s, equality_comparable2 const& o) {return not(s.self() == o.self());}
+//	friend constexpr auto operator==(equality_comparable2 const& self, equality_comparable2 const& other) {return     self.self() == other.self() ;}
+	friend constexpr auto operator!=(equality_comparable2 const& self, equality_comparable2 const& other) {return not(self.self() == other.self());}
 };
 
 template<class Self> struct equality_comparable : equality_comparable2<Self, Self> {};
-
-//template<class T>
-//struct equality_comparable2<T, void> {
-//	template<class Other> friend constexpr auto operator==(equality_comparable2 const& s, Other const& o) {return     s.self() == o ;}
-//	template<class Other> friend constexpr auto operator!=(equality_comparable2 const& s, Other const& o) {return not(s.self() == o);}
-
-//	template<class Other> friend constexpr auto operator!=(Other const& o, equality_comparable2 const& s) {return not(s.self() == o);}
-//	template<class Other> friend constexpr auto operator==(Other const& o, equality_comparable2 const& s) {return     s.self() == o ;}
-//};
-
-//template<class T, class V> struct partially_ordered2;
-
-//template <class T>
-//struct partially_ordered2<T, void>{
-//	template<class U, typename = std::enable_if_t<not std::is_base_of<T, U>{}>>
-//	friend constexpr auto operator>(const U& x, const T& y) -> bool {return y < x;}
-//	template<class U, typename = std::enable_if_t<not std::is_base_of<T, U>{}>>
-//	friend constexpr auto operator<(const U& x, const T& y) -> bool {return y > x;}
-
-//	template<class U>
-//	friend constexpr auto operator<=(T&& x, U&& y) -> bool{return (std::forward<T>(x) < std::forward<T>(y)) or (std::forward<T>(x) == std::forward<T>(y));}
-//	template<class U, typename = std::enable_if_t<not std::is_base_of<T, U>{}>>
-//	friend constexpr auto operator<=(const U& x, const T& y) -> bool {return (y > x) or (y == x);}
-//	template<class U>
-//	friend constexpr auto operator>=(const T& x, const U& y) -> bool {return (x > y) or (x == y);}
-//	template<class U, typename = std ::enable_if_t<not std::is_base_of<T, U>{}>>
-//	friend constexpr auto operator>=(const U& x, const T& y) -> bool {return (y < x) or (y == x);}
-//};
 
 template<class T, class V> struct totally_ordered2;
 
@@ -72,14 +35,14 @@ struct totally_ordered2<Self, Self> : equality_comparable2<totally_ordered2<Self
 	using self_type = Self;
 	constexpr auto self() const -> self_type const& {return static_cast<self_type const&>(*this);}
 
-//	friend auto operator< (totally_ordered2 const& s, totally_ordered2 const& o) -> bool {return     s.self() < o.self() ;}
-	friend auto operator==(totally_ordered2 const& s, totally_ordered2 const& o) -> bool {return not(s.self() < o.self()) and not(o.self() < s.self());}
-//	friend auto operator!=(totally_ordered2 const& s, totally_ordered2 const& o) {return    (s.self() < o.self()) or     (o.self() < s.self());}
+//	friend auto operator< (totally_ordered2 const& self, totally_ordered2 const& other) -> bool {return     self.self() < other.self() ;}
+	friend auto operator==(totally_ordered2 const& self, totally_ordered2 const& other) -> bool {return not(self.self() < other.self()) and not(other.self() < self.self());}
+//	friend auto operator!=(totally_ordered2 const& self, totally_ordered2 const& other) {return    (s.self() < o.self()) or     (o.self() < s.self());}
 
-	friend auto operator<=(totally_ordered2 const& s, totally_ordered2 const& o) -> bool {return not(o.self() < s.self());}
+	friend auto operator<=(totally_ordered2 const& self, totally_ordered2 const& other) -> bool {return not(other.self() < self.self());}
 
-	friend auto operator> (totally_ordered2 const& s, totally_ordered2 const& o) -> bool {return not(s.self() < o.self()) and not(s.self() == o.self());}
-	friend auto operator>=(totally_ordered2 const& s, totally_ordered2 const& o) -> bool {return not(s.self() < o.self());}
+	friend auto operator> (totally_ordered2 const& self, totally_ordered2 const& other) -> bool {return not(self.self() < other.self()) and not(self.self() == other.self());}
+	friend auto operator>=(totally_ordered2 const& self, totally_ordered2 const& other) -> bool {return not(self.self() < other.self());}
 };
 
 template<class Self> using totally_ordered = totally_ordered2<Self, Self>;
@@ -87,11 +50,11 @@ template<class Self> using totally_ordered = totally_ordered2<Self, Self>;
 template<class T>
 struct totally_ordered2<T, void> {
 	template<class U>
-	friend constexpr auto operator<=(const T& x, const U& y) {return (x < y) or (x == y);}
+	friend constexpr auto operator<=(const T& self, const U& other) {return (self < other) or (self == other);}
 	template<class U>
-	friend constexpr auto operator>=(const T& x, const U& y) {return (y < x) or (x == y);}
+	friend constexpr auto operator>=(const T& self, const U& other) {return (other < self) or (self == other);}
 	template<class U>
-	friend constexpr auto operator> (const T& x, const U& y) {return y < x;}
+	friend constexpr auto operator> (const T& self, const U& other) {return  other < self;}
 };
 
 template<class T>
@@ -107,14 +70,8 @@ struct weakly_decrementable {
 //  friend T& operator--(weakly_decrementable& t){return --static_cast<T&>(t);}
 };
 
-//template<class T>
-//struct incrementable : weakly_incrementable<T> {
-//	template<class U, typename = std::enable_if_t<not std::is_base_of<T, U>{}>>
-//	friend constexpr auto operator++(U& self, int) -> T {T tmp{self}; ++self; return tmp;}
-//};
-
 template<class Self> struct incrementable : totally_ordered<Self> {//, self_mutable<Self> {
-	friend constexpr auto operator++(incrementable& s, int) -> Self {Self tmp{s.self()}; ++s.self(); assert(s.self() > tmp); return tmp;}
+	friend constexpr auto operator++(incrementable& self, int) -> Self {Self tmp{self.self()}; ++self.self(); assert(self.self() > tmp); return tmp;}
 };
 
 template<class T>
@@ -129,8 +86,8 @@ struct steppable : totally_ordered<Self> {
 	constexpr auto self() const -> self_type const& {return static_cast<self_type const&>(*this);}
 	constexpr auto self()       -> self_type      & {return static_cast<self_type      &>(*this);}
 
-	friend constexpr auto operator++(steppable& s, int) -> Self {Self tmp{s.self()}; ++s.self(); return tmp;}
-	friend constexpr auto operator--(steppable& s, int) -> Self {Self tmp{s.self()}; --s.self(); return tmp;}
+	friend constexpr auto operator++(steppable& self, int) -> Self {Self tmp{self.self()}; ++self.self(); return tmp;}
+	friend constexpr auto operator--(steppable& self, int) -> Self {Self tmp{self.self()}; --self.self(); return tmp;}
 };
 
 template<class Self, typename Difference>
@@ -141,33 +98,26 @@ struct affine_with_unit : steppable<Self> {//affine_with_unit<Self, Difference> 
 	constexpr auto  self()       -> self_type      & {return static_cast<self_type      &>(*this);}
 
 	using difference_type = Difference;
-	friend constexpr auto operator++(affine_with_unit& s) -> Self& {return s.self() += difference_type{1};}
-	friend constexpr auto operator--(affine_with_unit& s) -> Self& {return s.self() -= difference_type{1};}
+	friend constexpr auto operator++(affine_with_unit& self) -> Self& {return self.self() += difference_type{1};}
+	friend constexpr auto operator--(affine_with_unit& self) -> Self& {return self.self() -= difference_type{1};}
 
-	friend constexpr auto operator-(affine_with_unit const& s, difference_type const& d) -> Self {
-		auto ret{s.self()};
-		ret += (-d);
+	friend constexpr auto operator-(affine_with_unit const& self, difference_type const& diff) -> Self {
+		auto ret{self.self()};
+		ret += (-diff);
 		return ret;
 	}
-	constexpr auto operator+(difference_type const& d) const -> Self {
+	constexpr auto operator+(difference_type const& diff) const -> Self {
 		auto ret{cself()};
-		ret += d;
+		ret += diff;
 		return ret;
 	}
-//	friend
-////  constexpr
-//	auto operator+(affine_with_unit const& s, difference_type const& d) -> Self {
-//		auto ret{s.self()};
-//		ret += d;
-//		return ret;
-//	}
-	friend constexpr auto operator+(difference_type const& d, affine_with_unit const& s) -> Self {
-		auto ret{s.self()};
-		ret += d;
+	friend constexpr auto operator+(difference_type const& diff, affine_with_unit const& self) -> Self {
+		auto ret{self.self()};
+		ret += diff;
 		return ret;
 	}
-	friend constexpr auto operator<(affine_with_unit const& s, affine_with_unit const& o) -> bool {
-		return difference_type{0} < o.self() - s.self();
+	friend constexpr auto operator<(affine_with_unit const& self, affine_with_unit const& other) -> bool {
+		return difference_type{0} < other.self() - self.self();
 	}
 };
 
@@ -194,7 +144,7 @@ struct random_accessable  // NOLINT(fuchsia-multiple-inheritance)
 	constexpr auto self() const -> self_type const& {return static_cast<self_type const&>(*this);}
 	constexpr auto self()       -> self_type      & {return static_cast<self_type      &>(*this);}
 
-	constexpr auto operator[](difference_type d) const -> reference {return *(self() + d);}
+	constexpr auto operator[](difference_type idx) const -> reference {return *(self() + idx);}
 };
 
 //template<class T, class Reference>
@@ -207,16 +157,16 @@ template<class T, class D>
 struct addable2 {
 	using difference_type = D;
 	template<class TT, typename = std::enable_if_t<std::is_base_of<T, TT>{}> >
-	friend constexpr auto operator+(TT&& t, difference_type const& d) -> T {T tmp{std::forward<TT>(t)}; tmp+=d; return tmp;}
+	friend constexpr auto operator+(TT&& self, difference_type const& diff) -> T {T tmp{std::forward<TT>(self)}; tmp += diff; return tmp;}
 	template<class TT, typename = std::enable_if_t<std::is_base_of<T, TT>{}> >
-	friend constexpr auto operator+(difference_type const& d, TT&& t) -> T {return std::forward<TT>(t) + d;}
+	friend constexpr auto operator+(difference_type const& diff, TT&& self) -> T {return std::forward<TT>(self) + diff;}
 };
 
 template<class T, class D>
 struct subtractable2 {
 	using difference_type = D;
 	template<class TT, class = T>
-	friend auto operator-(TT&& t, difference_type const& d) -> T {T tmp{std::forward<TT>(t)}; tmp-=d; return tmp;}
+	friend auto operator-(TT&& self, difference_type const& diff) -> T {T tmp{std::forward<TT>(self)}; tmp -= diff; return tmp;}
 };
 
 template<class T, class Difference>
@@ -226,15 +176,10 @@ struct affine : addable2<T, Difference>, subtractable2<T, Difference> {
 
 template<class T>
 struct random_iterable {
-//         constexpr auto rbegin()&    {return typename T::reverse_iterator{static_cast<T&>(*this).end  ()};}
-//         constexpr auto rend  ()&    {return typename T::reverse_iterator{static_cast<T&>(*this).begin()};}
-//  friend /*consx*/ auto rbegin(T& s) {return static_cast<random_iterable&>(s).rbegin();}
-//  friend /*consx*/ auto rend  (T& s) {return static_cast<random_iterable&>(s).rend  ();}
-
-	       constexpr auto cfront() const&    -> decltype(auto) {return static_cast<T const&>(*this).front();}
-	       constexpr auto cback () const&    -> decltype(auto) {return static_cast<T const&>(*this).back() ;}
-	friend constexpr auto cfront(T const& s) -> decltype(auto) {return s.cfront();}
-	friend constexpr auto cback (T const& s) -> decltype(auto) {return s.cback() ;}
+	       constexpr auto cfront() const&       -> decltype(auto) {return static_cast<T const&>(*this).front();}
+	       constexpr auto cback () const&       -> decltype(auto) {return static_cast<T const&>(*this).back() ;}
+	friend constexpr auto cfront(T const& self) -> decltype(auto) {return self.cfront();}
+	friend constexpr auto cback (T const& self) -> decltype(auto) {return self.cback() ;}
 };
 
 template<class Self, class Value, class Reference = Value&, class Pointer = Value*, class Difference = std::ptrdiff_t>
