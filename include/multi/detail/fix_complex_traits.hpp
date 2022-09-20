b74c20f7c9882c5f989a9d4b436280953d618cb4
Alfredo Correa <correaa@llnl.gov>
2022-09-20 14:45:38 -0700

    fix traits

--- include/multi/detail/fix_complex_traits.hpp
+++ include/multi/detail/fix_complex_traits.hpp
@@ -1,7 +1,8 @@
 // -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
 // Copyright 2022 Alfredo A. Correa
 
-#pragma once
+#ifndef MULTI_DETAIL_FIX_COMPLEX_TRAITS_HPP
+#define MULTI_DETAIL_FIX_COMPLEX_TRAITS_HPP
 
 #include "../detail/type_traits.hpp"
 
@@ -9,16 +10,20 @@
 
 #include<complex>
 
-namespace boost {
-namespace multi {
+namespace boost::multi {
 
+#ifndef NDEBUG
+// NOLINTNEXTLINE(clang-diagnostic-#pragma-messages)
 #pragma message "By including this header, the behavior of initialization of std::complex<T> in multi::array's changes. std::complex<T>  elements will not be initialized."
+#endif
 
 template<class T> struct is_trivially_default_constructible<std::complex<T>> : std::is_trivially_default_constructible<T> {};
 template<class T> struct is_trivial<std::complex<T>> : std::is_trivial<T> {};
 
-}
-}
+template<class T> constexpr bool is_trivially_default_constructible_v = boost::multi::is_trivially_default_constructible<T>::value;
+template<class T> constexpr bool is_trivial_v = boost::multi::is_trivial<T>::value;
+
+}  // end namespace boost::multi
 
 static_assert(not std::is_trivially_default_constructible<::std::complex<double>>::value);
 static_assert(not std::is_trivially_default_constructible<::std::complex<float >>::value);
@@ -35,4 +40,5 @@ static_assert(std::is_trivially_assignable<::std::complex<float >&, ::std::compl
 static_assert(std::is_trivially_copyable<::std::complex<double>>::value);
 static_assert(std::is_trivially_copyable<::std::complex<float >>::value);
 
+#endif
 
