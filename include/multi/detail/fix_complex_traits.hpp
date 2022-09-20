af8906f1cf5150796c96bccebee2708f391e022e
Alfredo Correa <correaa@llnl.gov>
2022-09-19 23:55:32 -0700

    add checks and trait

--- include/multi/detail/fix_complex_traits.hpp
+++ include/multi/detail/fix_complex_traits.hpp
@@ -15,8 +15,24 @@ namespace multi {
 #pragma message "By including this header, the behavior of initialization of std::complex<T> in multi::array's changes. std::complex<T>  elements will not be initialized."
 
 template<class T> struct is_trivially_default_constructible<std::complex<T>> : std::is_trivially_default_constructible<T> {};
-
-static_assert(is_trivially_default_constructible<::std::complex<double>>::value);
+template<class T> struct is_trivial<std::complex<T>> : std::is_trivial<T> {};
 
 }
 }
+
+static_assert(not std::is_trivially_default_constructible<::std::complex<double>>::value);
+static_assert(not std::is_trivially_default_constructible<::std::complex<float >>::value);
+
+static_assert(boost::multi::is_trivially_default_constructible<::std::complex<double>>::value);
+static_assert(boost::multi::is_trivially_default_constructible<::std::complex<float >>::value);
+
+static_assert(boost::multi::is_trivial<::std::complex<double>>::value);
+static_assert(boost::multi::is_trivial<::std::complex<float >>::value);
+
+static_assert(std::is_trivially_assignable<::std::complex<double>&, ::std::complex<double>>::value);
+static_assert(std::is_trivially_assignable<::std::complex<float >&, ::std::complex<float >>::value);
+
+static_assert(std::is_trivially_copyable<::std::complex<double>>::value);
+static_assert(std::is_trivially_copyable<::std::complex<float >>::value);
+
+
