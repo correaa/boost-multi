76c6b82e810f6dcb8b2e1ec8e642ca6ffaf18ab5
Alfredo Correa <correaa@llnl.gov>
2022-10-20 12:37:11 -0700

    lint ifdef guard

--- include/multi/config/UNREACHABLE.hpp
+++ include/multi/config/UNREACHABLE.hpp
@@ -1,11 +1,8 @@
-#ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-echo $X
-$CXXX $CXXFLAGS $0 -o $0.$X &&$0.$X&&rm $0.$X;exit
-#endif
-// © Alfredo A. Correa 2020
+// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
+// Copyright 2019-2021 Alfredo A. Correa
 
-#ifndef MULTI_CONFIG_NODISCARD_HPP
-#define MULTI_CONFIG_NODISCARD_HPP
+#ifndef MULTI_CONFIG_UNREACHABLE_HPP_
+#define MULTI_CONFIG_UNREACHABLE_HPP_
 
 #if defined(__GNUC__) || defined(__clang__) || defined(__INTEL_COMPILER) || defined(__NVCC__)
 #define MULTI_UNREACHABLE __builtin_unreachable()
@@ -17,17 +14,16 @@ $CXXX $CXXFLAGS $0 -o $0.$X &&$0.$X&&rm $0.$X;exit
 
 enum color{red, green, blue};
 
-int f(enum color c){
-	switch(c){
-	case red  : return 1;
-	case green: return 2;
-	case blue : return 3; // comment case make gcc, clang, culang causes -Wswitch warning
-	} MULTI_UNREACHABLE;  // commnet unreachable in gcc and nvcc causes -Wreturn-type warning 
+int f(enum color c) {
+	switch(c) {
+		case red  : return 1;
+		case green: return 2;
+		case blue : return 3; // comment case make gcc, clang, culang causes -Wswitch warning
+	} MULTI_UNREACHABLE;  // commnet unreachable in gcc and nvcc causes -Wreturn-type warning
 }
 
-int main(){
-}
+int main() {}
 
 #endif
-#endif
+#endif  // MULTI_CONFIG_UNREACHABLE_HPP_
 
