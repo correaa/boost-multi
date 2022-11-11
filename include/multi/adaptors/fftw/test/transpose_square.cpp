2ef7c469fa93e922ecdef24593f50bae5d130bae
Alfredo Correa <correaa@llnl.gov>
2022-11-10 23:48:29 -0800

    more tidy

--- include/multi/adaptors/fftw/test/transpose_square.cpp
+++ include/multi/adaptors/fftw/test/transpose_square.cpp
@@ -13,6 +13,8 @@
 
 namespace multi = boost::multi;
 
+using namespace std::string_literals;
+
 using fftw_fixture = multi::fftw::environment;
 BOOST_TEST_GLOBAL_FIXTURE( fftw_fixture );
 
@@ -54,7 +56,7 @@ BOOST_AUTO_TEST_CASE(fftw_transpose) {
 			multi::array<complex, 2> out = in;
 			auto* data = out.data_elements();
 			 {
-				watch unnamed{"fftw trans mve 1 thread  %ws wall, CPU (%p%)\n"};
+				watch unnamed{"fftw trans mve 1 thread  %ws wall, CPU (%p%)\n"s};
 				multi::fftw::transpose( out );
 				BOOST_REQUIRE( out.data_elements() == data );
 				BOOST_REQUIRE( out[35][79] == in[79][35] );
@@ -100,7 +102,7 @@ BOOST_AUTO_TEST_CASE(fftw_transpose) {
 			multi::array<complex, 2> out = in;
 			multi::array<complex, 2> aux(extensions(out));
 			{
-				watch unnamed{"auxiliary copy           %ws wall, CPU (%p%)\n"};
+				watch unnamed{"auxiliary copy           %ws wall, CPU (%p%)\n"s};
 				aux = ~out;
 				out = std::move(aux);
 				BOOST_REQUIRE( out[35][79] == in[79][35] );
@@ -110,7 +112,7 @@ BOOST_AUTO_TEST_CASE(fftw_transpose) {
 		{
 			multi::array<complex, 2> out = in;
 			{
-				watch unnamed{"transposition with loop   %ws wall, CPU (%p%)\n"};
+				watch unnamed{"transposition with loop   %ws wall, CPU (%p%)\n"s};
 				std::for_each(extension(out).begin(), extension(out).end(), [&out](auto idx) {
 					auto ext = multi::extension_t(0L, idx);
 					std::for_each(ext.begin(), ext.end(), [&out, idx](auto jdx) {
@@ -124,7 +126,7 @@ BOOST_AUTO_TEST_CASE(fftw_transpose) {
 		{
 			multi::array<complex, 2> out = in;
 			{
-				watch unnamed{"transposition with loop 2 %ws wall, CPU (%p%)\n"};
+				watch unnamed{"transposition with loop 2 %ws wall, CPU (%p%)\n"s};
 				std::for_each(extension(out).begin(), extension(out).end(), [&out](auto idx) {
 					auto ext = multi::extension_t(idx + 1, out.size());
 					std::for_each(ext.begin(), ext.end(), [&out, idx](auto jdx) {
