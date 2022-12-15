11dc00eada864cc98d6db0bba4ad0bc6d56af2ea
Alfredo Correa <correaa@llnl.gov>
2022-12-15 02:39:05 -0800

    tidy kernel

--- include/multi/adaptors/thrust/test/set_identity_kernel.cu
+++ include/multi/adaptors/thrust/test/set_identity_kernel.cu
@@ -1,6 +1,7 @@
 #include <multi/array.hpp>
 #include <multi/adaptors/thrust.hpp>
 
+#include <thrust/complex.h>
 #include <thrust/system/cuda/memory.h>
 
 namespace multi = boost::multi;
@@ -21,33 +22,6 @@ inline void require(bool code, const char *file, int line, bool abort=true) {
     }
 }
 
-// template<typename T>
-// __global__ void kernel_setIdentity(int m, int n, T* A, int lda)
-// {
-//   int i = threadIdx.x + blockDim.x * blockIdx.x;
-//   int j = threadIdx.y + blockDim.y * blockIdx.y;
-//   if ((i < m) && (j < n))
-//     if (i == j)
-//     {
-//       A[i * lda + i] = T(1.0);
-//     }
-//     else
-//     {
-//       A[j * lda + i] = T(0.0);
-//     }
-// }
-
-// void set_identity(int m, int n, double* A, int lda) {
-//     int xblock_dim = 16;
-//     int xgrid_dim  = (m + xblock_dim - 1) / xblock_dim;
-//     int ygrid_dim  = (n + xblock_dim - 1) / xblock_dim;
-//     dim3 block_dim(xblock_dim, xblock_dim);
-//     dim3 grid_dim(xgrid_dim, ygrid_dim);
-//     kernel_setIdentity<<<grid_dim, block_dim>>>(m, n, A, lda);
-//     CUDA_CHECKED(cudaGetLastError());
-//     CUDA_CHECKED(cudaDeviceSynchronize());
-// }
-
 template<typename Array2DCursor>
 __global__ void kernel_setIdentity(Array2DCursor home, int m, int n) {
     int i = threadIdx.x + blockDim.x * blockIdx.x;
@@ -62,7 +36,7 @@ __global__ void kernel_setIdentity(Array2DCursor home, int m, int n) {
 }
 
 template<class Array2D>
-void set_identity(Array2D&& arr) {
+auto set_identity(Array2D&& arr) -> Array2D&&{
     int xblock_dim = 16;
     auto [m, n] = arr.sizes();
     int xgrid_dim  = (m + xblock_dim - 1) / xblock_dim;
@@ -71,11 +45,12 @@ void set_identity(Array2D&& arr) {
     dim3 grid_dim(xgrid_dim, ygrid_dim);
     kernel_setIdentity<<<grid_dim, block_dim>>>(arr.home(), m, n);
     CUDA_CHECKED(cudaGetLastError());
-    CUDA_CHECKED(cudaDeviceSynchronize());
+    // CUDA_CHECKED(cudaDeviceSynchronize());
+    return std::forward<Array2D>(arr);
 }
 
 int main() {
-    using T = double;
+    using T = thrust::complex<double>;
 
     {
         multi::array<double, 2, thrust::cuda::allocator<double>> A({10000, 10000});
@@ -86,16 +61,14 @@ int main() {
 
         thrust::fill(A.elements().begin(), A.elements().end(), 0.0);
         thrust::fill(A.diagonal().begin(), A.diagonal().end(), 1.0);
-        CUDA_CHECKED(cudaGetLastError());
-        CUDA_CHECKED(cudaDeviceSynchronize());
-
-        std::chrono::duration<double> time = std::chrono::high_resolution_clock::now() - start_time;
-        auto rate = size/time.count();
-        std::cout<<"algorithm rate = "<< rate <<" GB/s (ratio = 1)\n";
 
         REQUIRE( A[0][0] == 1.0 );
         REQUIRE( A[1][1] == 1.0 );
         REQUIRE( A[2][1] == 0.0 );
+
+        std::chrono::duration<double> time = std::chrono::high_resolution_clock::now() - start_time;
+        auto rate = size/time.count();
+        std::cout<<"algorithm rate = "<< rate <<" GB/s (ratio = 1)\n";
     }
 
     {
@@ -105,13 +78,13 @@ int main() {
 
         auto start_time = std::chrono::high_resolution_clock::now();
         set_identity(A);
-        std::chrono::duration<double> time = std::chrono::high_resolution_clock::now() - start_time;
-        auto rate = size/time.count();
-        std::cout<<"kernel rate = "<< rate <<" GB/s (ratio = 1)\n";
 
         REQUIRE( A[0][0] == 1.0 );
         REQUIRE( A[1][1] == 1.0 );
         REQUIRE( A[2][1] == 0.0 );
-    }
 
+        std::chrono::duration<double> time = std::chrono::high_resolution_clock::now() - start_time;
+        auto rate = size/time.count();
+        std::cout<<"kernel rate = "<< rate <<" GB/s (ratio = 1)\n";
+    }
 }
