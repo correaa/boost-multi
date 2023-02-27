6840caf3249e2c16cbcab8c31a55c6ba61809143
Alfredo Correa <alfredo.correa@gmail.com>
2023-02-27 14:56:46 -0800

    fix constexpr friend for nvcc

--- include/multi/array.hpp
+++ include/multi/array.hpp
@@ -365,7 +365,11 @@ struct static_array  // NOLINT(fuchsia-multiple-inheritance) : multiple inherita
 
 	       constexpr auto base()                 &       -> typename static_array::element_ptr       {return ref::base();}
 	       constexpr auto base()            const&       -> typename static_array::element_const_ptr {return typename static_array::element_const_ptr{ref::base()};}
-	friend constexpr auto base(static_array      & self) -> typename static_array::element_ptr       {return self.base();}
+	friend
+	#if not defined(__NVCC__)
+	constexpr
+	#endif
+	auto base(static_array      & self) -> typename static_array::element_ptr       {return self.base();}
 	friend
 	#if not defined(__NVCC__)
 	constexpr
