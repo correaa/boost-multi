3fe7690dff2f4faab8bed336bd3f53faa4a556ef
Alfredo Correa <alfredo.correa@gmail.com>
2023-03-09 13:43:33 -0800

    finish asum

--- include/multi/adaptors/blas/asum.hpp
+++ include/multi/adaptors/blas/asum.hpp
@@ -1,7 +1,5 @@
 // -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-// Copyright 2019-2021 Alfredo A. Correa
-
-// TODO(correaa) : make it work with thrust complex
+// Copyright 2019-2023 Alfredo A. Correa
 
 #ifndef MULTI_ADAPTORS_BLAS_ASUM_HPP
 #define MULTI_ADAPTORS_BLAS_ASUM_HPP
@@ -10,72 +8,60 @@
 
 namespace boost::multi::blas {
 
-template<class It, typename Size>
-auto asum_n(It first, Size n)
-->decltype(asum(n, base(first), stride(first))) {
-	return asum(n, base(first), stride(first)); }
-
-using std::distance;
-
-template<class It>
-auto asum(It first, It last)
-->decltype(asum_n(first, distance(first, last))) {assert(stride(first) == stride(last));
-	return asum_n(first, distance(first, last)); }
+template<class It, typename Size, class A0D>
+auto asum_n(It first, Size n, A0D res)
+->decltype(blas::default_context_of(base(first))->asum(n, base(first), stride(first), res), std::next(res)) {
+	return blas::default_context_of(base(first))->asum(n, base(first), stride(first), res), std::next(res); }
 
 using std::begin; using std::end;
 
-template<class X1D>
-auto asum(X1D const& x)  // NOLINT(readability-identifier-length) x conventional blas name
-->decltype(asum(begin(x), end(x))) {assert( not offset(x) );
-	return asum(begin(x), end(x)); }
+template<class X1D, class A0D>
+auto asum(X1D const& x, A0D&& res)  // NOLINT(readability-identifier-length) x conventional blas name
+//->decltype(asum_n(x.begin(), x.size(), &res)) {
+{   return asum_n(x.begin(), x.size(), &res); }
 
-} // end namespace boost::multi::blas
+template<class A1D>
+struct asum_ptr {
+	A1D const* xp_;  // NOLINT(misc-non-private-member-variables-in-classes)
 
-//#if defined(__INCLUDE_LEVEL__) and not __INCLUDE_LEVEL__
+	explicit operator bool() const {return xp_;}
 
-//#define BOOST_TEST_MODULE "C++ Unit Tests for Multi.BLAS asum"
-//#define BOOST_TEST_DYN_LINK
-//#include<boost/test/unit_test.hpp>
-////#include<boost/test/tools/floating_point_comparison.hpp>
+	template<class ItOut, class Size2>
+	friend auto copy_n(asum_ptr first, Size2 count, ItOut d_first)
+	->decltype(blas::asum_n(typename A1D::iterator{}, typename A1D::size_type{}, d_first)) {assert(count == 1);
+		return blas::asum_n(first.xp_->begin()      , first.xp_->size()        , d_first); }
 
-//#include "../../array.hpp"
-////#include "../../utility.hpp"
+	template<class... As> friend auto uninitialized_copy_n(As... as) {return copy_n(as...);}
+};
 
-//#include<numeric> // accumulate
+template<class A1D>
+[[nodiscard]]
+auto asum(A1D const& x) {  // NOLINT(readability-identifier-length) BLAS naming
+	struct ref {
+		A1D const& x_;  // NOLINT(misc-non-private-member-variables-in-classes)
+		auto operator&() const& {return asum_ptr<A1D>{&x_};}  // NOLINT(google-runtime-operator) reference type
+		using decay_type = decltype(abs(std::declval<typename A1D::value_type>()));
+		operator decay_type() const {decay_type ret; blas::asum(x_, ret); return ret;}  // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
+		auto operator+() const -> decay_type {return operator decay_type();}
+	};
 
-//namespace multi = boost::multi;
-//using multi::blas::asum;
+	return ref{x};
+}
 
-//BOOST_AUTO_TEST_CASE(multi_blas_asum_double){
-//	multi::array<double, 2> const A = {
-//		{1.,  2.,  3.,  4.},
-//		{-5.,  6.,  -7.,  8.},
-//		{9., 10., 11., 12.}
-//	};
-//	BOOST_REQUIRE(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a+std::abs(b);}));
-//}
+namespace operators {
+	template<class A1D>
+	auto operator==(A1D const& self, void***** zero) -> bool {
+		assert( zero == nullptr );
+		return blas::asum(self) < 1.0e-7;
+	}
 
-//BOOST_AUTO_TEST_CASE(multi_blas_asum_complex){
+	template<class A1D>
+	auto operator!=(A1D const& self, void***** zero) -> bool {
+		assert( zero == nullptr );
+		return not(self == 0);
+	}
+}  // end namespace operators
 
-//	using complex = std::complex<double>; complex const I{0, 1};
-//	multi::array<complex, 2> const A = {
-//		{ 1. + 1.*I,  2.,  3.,  4.},
-//		{-5. + 3.*I,  6.,  -7.,  8.},
-//		{ 9. - 2.*I, 10., 11., 12.}
-//	};
-//	BOOST_REQUIRE(asum(rotated(A)[0]) == 1.+1. + 5.+3. + 9.+2.);
+}  // end namespace boost::multi::blas
 
-//}
-
-//BOOST_AUTO_TEST_CASE(multi_blas_asum_double_carray){
-////	double A[3][4] = {
-////		{1.,  2.,  3.,  4.},
-////		{-5.,  6.,  -7.,  8.},
-////		{9., 10., 11., 12.}
-////	}; (void)A;
-////	using std::begin; using std::end;
-////	BOOST_REQUIRE(asum(A[1]) == std::accumulate(begin(A[1]), end(A[1]), 0., [](auto&& a, auto&& b){return a+abs(b);}));
-//}
-
-//#endif
 #endif
