d864ffdfba8c58d0337f9589ac967e2b980c1988
Alfredo Correa <alfredo.correa@gmail.com>
2023-07-23 20:32:52 -0700

    fixes for tidy and gcc 7

--- test/diagonal.cpp
+++ test/diagonal.cpp
@@ -13,18 +13,18 @@ namespace multi = boost::multi;
 template<class Array2D>
 auto trace_with_indices(Array2D const& arr) {
 	typename Array2D::element_type sum{0};
-	for(auto i : extension(arr)) {
+	for(auto i : extension(arr)) {  // NOLINT(altera-unroll-loops) testing loops
 		sum += arr[i][i];
-	}  // NOLINT(altera-unroll-loops) testing loops
+	}
 	return sum;
 }
 
 template<class Array2D>
 auto trace_with_diagonal(Array2D const& arr) {
 	typename Array2D::element_type sum{0};
-	for(auto aii : arr.diagonal()) {
+	for(auto aii : arr.diagonal()) {  // NOLINT(altera-unroll-loops) testing loops
 		sum += aii;
-	}  // NOLINT(altera-unroll-loops) testing loops
+	}
 	return sum;
 }
 
@@ -33,26 +33,27 @@ auto trace_with_accumulate(Array2D const& arr) {
 	return std::accumulate(arr.diagonal().begin(), arr.diagonal().end(), 0);
 }
 
-template<class Array2D>
-auto trace_with_reduce(Array2D const& arr) {
-	return std::reduce(arr.diagonal().begin(), arr.diagonal().end(), 0);
-}
+// g++ 7 defect
+// template<class Array2D>
+// auto trace_with_reduce(Array2D const& arr) {
+//  return std::reduce(arr.diagonal().begin(), arr.diagonal().end(), 0);
+// }
 
 BOOST_AUTO_TEST_CASE(trace_test) {
-	multi::array<int, 2> A({5, 5}, 0);
+	multi::array<int, 2> arr({5, 5}, 0);
 
-	auto [is, js] = extensions(A);
-	for(auto i : is) {
-		for(auto j : js) {
-			A[i][j] = static_cast<int>(10 * i + j);
+	auto [is, js] = extensions(arr);
+	for(auto i : is) {  // NOLINT(altera-unroll-loops) testing loops
+		for(auto j : js) {  // NOLINT(altera-unroll-loops) testing loops
+			arr[i][j] = static_cast<int>(10 * i + j);
 		}
 	}
 
-	auto tr = trace_with_diagonal(A);
+	auto tr = trace_with_diagonal(arr);
 
 	BOOST_REQUIRE( tr == 00 + 11 + 22 + 33 + 44 );
 
-	BOOST_REQUIRE( trace_with_diagonal(A) == trace_with_indices(A) );
-	BOOST_REQUIRE( trace_with_diagonal(A) == trace_with_accumulate(A) );
-	BOOST_REQUIRE( trace_with_diagonal(A) == trace_with_reduce(A) );
+	BOOST_REQUIRE( trace_with_diagonal(arr) == trace_with_indices(arr) );
+	BOOST_REQUIRE( trace_with_diagonal(arr) == trace_with_accumulate(arr) );
+//  BOOST_REQUIRE( trace_with_diagonal(arr) == trace_with_reduce(arr) );
 }
