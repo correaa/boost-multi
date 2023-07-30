1a63a287b01e836239097d21aea4fb1691bf7012
Alfredo Correa <alfredo.correa@gmail.com>
2023-07-30 02:35:01 -0700

    more const

--- include/multi/array_ref.hpp
+++ include/multi/array_ref.hpp
@@ -2470,7 +2470,7 @@ struct array_ref  // TODO(correaa) : inheredit from multi::partially_ordered2<ar
 		return *this;
 	}
 
-	constexpr auto operator=(array_ref&& other) &  // NOLINT(hicpp-noexcept-move,performance-noexcept-move-constructor)  // NOSONAR
+	constexpr auto operator=(array_ref&& other) &  // NOLINT(hicpp-noexcept-move,performance-noexcept-move-constructor)  //NOSONAR
 	-> array_ref& {
 		if(this == std::addressof(other)) {return *this;}  // lints(cert-oop54-cpp)
 		operator=(std::as_const(other));
