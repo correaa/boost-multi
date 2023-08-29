5c5b68232009eafc5cb3fee716358530ca66447c
Alfredo Correa <alfredo.correa@gmail.com>
2023-08-28 19:31:32 -0700

    add implicit convertible

--- include/multi/utility.hpp
+++ include/multi/utility.hpp
@@ -17,6 +17,9 @@
 
 namespace boost::multi {
 
+template<class From, class To> constexpr bool is_implicitly_convertible_v = std::is_convertible_v<From, To>;
+template<class From, class To> constexpr bool is_explicitly_convertible_v = std::is_constructible_v<To, From>;
+
 template<class To, class From, std::enable_if_t<std::is_convertible<From, To>::value, int> =0>  // ::value (not _v) needed by intel's icpc 19
 constexpr auto implicit_cast(From&& r) -> To {return static_cast<To>(r);}  // NOLINT(readability-identifier-length) std naming
 
