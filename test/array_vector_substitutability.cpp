fb5f3bc177c0755e2a3de1dd94b0c15a9ac84557
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-11-01 15:42:55 -0700

    more circle exception

--- test/array_vector_substitutability.cpp
+++ test/array_vector_substitutability.cpp
@@ -214,11 +214,13 @@ BOOST_AUTO_TEST_CASE(construct_from_vector_2D) {
 		auto const aa(AA().operator std::vector<std::vector<double>>());
 	}
 	{
+	#if not defined(__circle_build__)
 		multi::array<double, 2> const               AA = {{1.0, 2.0}, {3.0, 4.0}};
 		BOOST_REQUIRE( AA.num_elements() == 4 );
 
 		std::vector<std::vector<double>> const aa(AA);
 		// std::vector<std::vector<double>> const aaa = AA;  // doesn't compile, needs implicit conversion
+	#endif
 	}
 	{
 		multi::array<double, 2> const               AA = {{1.0, 2.0}, {3.0, 4.0}};
