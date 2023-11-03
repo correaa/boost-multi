1b2e3058403048542c709d80610dd2070b1884ec
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-11-03 13:17:34 -0700

    guard

--- include/multi/array.hpp
+++ include/multi/array.hpp
@@ -15,7 +15,7 @@
 #include<tuple>      // needed by a deprecated function
 #include<utility>    // for move
 
-#if not defined(__GLIBCXX__) or (__GLIBCXX__ >= 20170502)
+#if not defined(__GLIBCXX__) or (__GLIBCXX__ >= 20170814)
 #include<memory_resource>
 #endif
 
