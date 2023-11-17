2da03cf35bcdd4d02ecf72b2e8de16f6ee4b8dc0
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-11-16 21:31:50 -0800

    fist conan version

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+from conan.tools.files import copy
+
+
+class MultiConan(ConanFile):
+    name = "multi"
+    version = "0.80.1"
+    # No settings/options are necessary, this is header only
+    exports_sources = "include/*"
+    no_copy_source = True
+
+    def package(self):
+        # This will also copy the "include" folder
+        copy(self, "*.hpp", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        # For header-only packages, libdirs and bindirs are not used
+        # so it's recommended to set those as empty.
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
