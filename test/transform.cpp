dba92481c3bc01da1c49f382bac6ddbcff688f1f
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-11-28 00:00:51 -0800

    fix ws

--- test/transform.cpp
+++ test/transform.cpp
@@ -90,11 +90,18 @@ template<class Ref> using negated = involuted<std::negate<>, Ref>;
 template<class It> using negater  = involuter<std::negate<>, It>;
 
 class basic_conjugate_t {
+	// clang-format off
 	template<int N> struct prio : std::conditional_t<N != 0, prio<N - 1>, std::true_type> {};
-	template<class T> static auto _(prio<0> /**/, T const& value) DECLRETURN(std::conj(value)) template<class T> static auto _(prio<1> /**/, T const& value) DECLRETURN(conj(value)) template<class T> static auto _(prio<2> /**/, T const& value) DECLRETURN(T::conj(value)) template<class T> static auto _(prio<3> /**/, T const& value) DECLRETURN(value.conj())
 
-		public : template<class T>
-		         static auto _(T const& value) DECLRETURN(_(prio<3>{}, value))
+	template<class T> static auto _(prio<0> /**/, T const& value) DECLRETURN( std::conj(value))
+	template<class T> static auto _(prio<1> /**/, T const& value) DECLRETURN(      conj(value))
+	template<class T> static auto _(prio<2> /**/, T const& value) DECLRETURN(   T::conj(value))
+	template<class T> static auto _(prio<3> /**/, T const& value) DECLRETURN(value.conj()     )
+
+ public:
+	template<class T>
+	static auto _(T const& value) DECLRETURN(_(prio<3>{}, value))
+	// clang-format on
 };
 
 template<class T = void>
