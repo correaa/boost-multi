15c5ed44437bd0bbbe84c7fb368d50f028b7fb1f
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-12-15 17:19:27 +0000

    add nolint for macro

--- include/multi/config/ASSERT.hpp
+++ include/multi/config/ASSERT.hpp
@@ -6,14 +6,14 @@
 #include<cassert>
 
 #if defined(MULTI_ACCESS_NDEBUG) or defined(__CUDACC__)
-	#define MULTI_ACCESS_ASSERT(Expr)
+	#define MULTI_ACCESS_ASSERT(Expr)  // NOLINT(cppcoreguidelines-macro-usage
 #else
 	// #include<stacktrace>
 	// // NOLINTNEXTLINE(cppcoreguidelines-macro-usage) this is for very inefficient asserts
 	// #if defined(__cpp_lib_stacktrace) && (__cpp_lib_stacktrace >= 202011L)
 	// #define MULTI_ACCESS_ASSERT(Expr) assert((std::cerr<<std::stacktrace()<<std::endl) && (Expr))
 	// #else
-	#define MULTI_ACCESS_ASSERT(Expr) assert(Expr)
+	#define MULTI_ACCESS_ASSERT(Expr) assert(Expr)  // NOLINT(cppcoreguidelines-macro-usage)
 	// #endif
 #endif
 
