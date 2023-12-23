36f68d753e7d8d3c3fc10b2f4be8a517e361a067
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-12-23 01:08:42 -0800

    deprecate message

--- include/multi/array_ref.hpp
+++ include/multi/array_ref.hpp
@@ -1408,18 +1408,23 @@ struct subarray : array_types<T, D, ElementPtr, Layout> {
 
 	// template<class T2, class P2 = typename std::pointer_traits<element_ptr>::template rebind<T2>>  // TODO(correaa) should it be rebind<T2 const>?
 	// constexpr auto static_array_cast() const & {  // name taken from std::static_pointer_cast
-	// 	#if not defined(H5_USE_110_API)  // TODO(correaa) workaround for qmc!! remove as soon as possible
-	// 	return subarray<T2, D, P2>(this->layout(), static_cast<P2>(this->base()));
-	// 	#else
-	// 	P2 p2;
-	// 	auto b = this->base();
-	// 	std::memcpy(std::addressof(p2), std::addressof(b), sizeof(p2));
-	// 	return subarray<T2, D, P2>(this->layout(), p2);
-	// 	#endif
+	//  #if not defined(H5_USE_110_API)  // TODO(correaa) workaround for qmc!! remove as soon as possible
+	//  return subarray<T2, D, P2>(this->layout(), static_cast<P2>(this->base()));
+	//  #else
+	//  P2 p2;
+	//  auto b = this->base();
+	//  std::memcpy(std::addressof(p2), std::addressof(b), sizeof(p2));
+	//  return subarray<T2, D, P2>(this->layout(), p2);
+	//  #endif
 	// }
 
-	template<class T2, class P2 = typename std::pointer_traits<element_ptr>::template rebind<T2>>
+	template<class T2, class P2 = typename std::pointer_traits<element_ptr>::template rebind<T2>, std::enable_if_t<  std::is_const_v<typename std::pointer_traits<P2>::element_type>,int> =0>
 	constexpr auto static_array_cast() const & {  // name taken from std::static_pointer_cast
+		return subarray<T2, D, P2>(this->layout(), static_cast<P2>(this->base_));  // TODO(correaa) might violate constness
+	}
+
+	template<class T2, class P2 = typename std::pointer_traits<element_ptr>::template rebind<T2>, std::enable_if_t<! std::is_const_v<typename std::pointer_traits<P2>::element_type>,int> =0>
+	[[deprecated("violates constness")]] constexpr auto static_array_cast() const & {  // name taken from std::static_pointer_cast
 		return subarray<T2, D, P2>(this->layout(), static_cast<P2>(this->base_));  // TODO(correaa) might violate constness
 	}
 
