be8cc2ef829eb05fcd8d4c56fc6de665c87c19ec
Alfredo A. Correa <correaa@llnl.gov>
2024-01-02 19:00:34 -0800

    first version hip

---
+++ include/multi/adaptors/hipfft.hpp
@@ -0,0 +1,64 @@
+// Copyright 2020-2023 Alfredo A. Correa
+
+#ifndef MULTI_ADAPTORS_HIPFFT_HPP
+#define MULTI_ADAPTORS_HIPFFT_HPP
+
+#include <hipfft/hipfft.h>
+#include <hipfft/hipfftXt.h>
+
+
+// #define CONCAT_(A, B) A ## B
+// #define CONCAT(A, B) CONCAT_(A, B)
+
+// #define kufft(A) CONCAT(cufft, Result)
+
+// hipdefine(fftResult) -> #define cufftResult hipfftResult
+
+using cudaError_t = hipError_t;
+
+#define cufftResult        hipfftResult
+#define CUFFT_INVALID_PLAN HIPFFT_INVALID_PLAN
+#define CUFFT_SUCCESS      HIPFFT_SUCCESS
+#define CUFFT_Z2Z          HIPFFT_Z2Z
+
+template<class... As> inline auto cufftSetWorkArea(As&&... as) noexcept(noexcept(hipfftSetWorkArea(std::forward<As>(as)...))) -> decltype(hipfftSetWorkArea(std::forward<As>(as)...)) {return hipfftSetWorkArea(std::forward<As>(as)...);}
+template<class... As> inline auto cufftPlanMany   (As&&... as) noexcept(noexcept(hipfftPlanMany   (std::forward<As>(as)...))) -> decltype(hipfftPlanMany   (std::forward<As>(as)...)) {return hipfftPlanMany   (std::forward<As>(as)...);}
+
+using cufftHandle        = hipfftHandle;
+using cufftDoubleComplex = hipfftDoubleComplex;
+
+constexpr static auto const& cudaDeviceReset  = hipDeviceReset;
+constexpr static auto const& cudaSuccess = hipSuccess;
+
+#define cu2hip_fft(NamE) constexpr static auto const& cufft ## NamE  = hipfft ## NamE
+
+cu2hip_fft(Create);
+cu2hip_fft(Destroy);
+// cu2hip_fft(DeviceReset);
+cu2hip_fft(GetSize);
+cu2hip_fft(ExecZ2Z);
+cu2hip_fft(SetAutoAllocation);
+
+#undef cu2hip_fft
+
+#define CU2HIPFFT_(NamE) constexpr static auto const& CUFFT_ ## NamE  = HIPFFT_ ## NamE
+
+CU2HIPFFT_(ALLOC_FAILED);
+CU2HIPFFT_(BACKWARD);
+
+constexpr static auto const& CUFFT_INVERSE = HIPFFT_BACKWARD;
+
+CU2HIPFFT_(EXEC_FAILED);
+CU2HIPFFT_(FORWARD);
+CU2HIPFFT_(INTERNAL_ERROR);
+CU2HIPFFT_(INVALID_SIZE);
+CU2HIPFFT_(INVALID_TYPE);
+CU2HIPFFT_(INVALID_VALUE);
+CU2HIPFFT_(UNALIGNED_DATA);
+CU2HIPFFT_(SETUP_FAILED);
+
+#undef CU2HIPFFT_
+
+#include "cufft.hpp"
+
+#endif
