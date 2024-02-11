ba9d55823d75d2357263b49d50a78bba00884fdb
Alfredo Correa <alfredo.correa@gmail.com>
2024-02-11 01:36:48 -0800

    allow overload &

--- include/multi/adaptors/blas/asum.hpp
+++ include/multi/adaptors/blas/asum.hpp
@@ -45,7 +45,7 @@ auto asum(A1D const& x) {  // NOLINT(readability-identifier-length) BLAS naming
 		A1D const& x_;  // NOLINT(misc-non-private-member-variables-in-classes,cppcoreguidelines-avoid-const-or-ref-data-members)
 		auto operator&() const& {return asum_ptr<A1D>{&x_};}  // NOLINT(google-runtime-operator) reference type //NOSONAR
 		using decay_type = decltype(abs(std::declval<typename A1D::value_type>()));
-		operator decay_type() const {decay_type ret; blas::asum(x_, ret); return ret;}  // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
+		operator decay_type() const {decay_type ret; blas::asum(x_, ret); return ret;}  //NOSONAR // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)  allow terse syntax double a = asum(v);
 		auto operator+() const -> decay_type {return operator decay_type();}
 	};
 
