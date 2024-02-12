af9f34a144dd63dbcfe2f2e873ee1b5c9cc754af
Alfredo Correa <alfredo.correa@gmail.com>
2024-02-11 21:57:44 -0800

    bump version needed for launder

--- include/multi/array_ref.hpp
+++ include/multi/array_ref.hpp
@@ -2708,20 +2708,25 @@ struct array_ref  // TODO(correaa) : inheredit from multi::partially_ordered2<ar
 			check_sizes<TTN, DD + 1>();
 		}
 	}
+
+	template<class T> static auto launder(T* pointer) -> T* {
+		#if(defined(__cpp_lib_launder) and ( __cpp_lib_launder >= 201606L)) 
+		return std::launder(pointer);
+		#else
+		return              pointer ;
+		#endif
+	}
+
 	template<class TTN>
 	constexpr auto to_carray()& -> TTN& {
 		check_sizes<TTN>();
-		#if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20210101))
-		return *std::launder(reinterpret_cast<TTN*>(array_ref::base_));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
-		#else
-		return *             reinterpret_cast<TTN*>(array_ref::base_) ;  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
-		#endif
+		return *launder(reinterpret_cast<TTN*>(array_ref::base_));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
 	}
 
 	template<class TTN>
 	constexpr auto to_carray() const -> TTN const& {
 		check_sizes<TTN>();
-		return *std::launder(reinterpret_cast<TTN const*>(array_ref::base_));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+		return *launder(reinterpret_cast<TTN const*>(array_ref::base_));  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
 	}
 
  public:
