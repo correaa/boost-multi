14482c1ca67e3d222b9a83f188d382944ebc170a
Alfredo Correa <alfredo.correa+work@gmail.com>
2024-02-15 05:14:47 +0000

    Update is_complex.hpp

--- include/multi/adaptors/blas/numeric/is_complex.hpp
+++ include/multi/adaptors/blas/numeric/is_complex.hpp
@@ -1,8 +1,8 @@
-// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
-// Copyright 2020-2022 Alfredo A. Correa
+// Copyright 2020-2024 Alfredo A. Correa
 
 #ifndef MULTI_ADAPTORS_BLAS_NUMERIC_IS_COMPLEX_HPP
 #define MULTI_ADAPTORS_BLAS_NUMERIC_IS_COMPLEX_HPP
+#pragma once
 
 #include<complex>
 #include<type_traits>
@@ -33,7 +33,7 @@ template<class T> struct has_imag : decltype(has_imag_aux(std::declval<T>())){};
 template<class T> constexpr bool has_imag_v = has_imag<T>::value;
 
 template<class T> struct is_complex : std::integral_constant<bool, 
-	(has_real_v<T> or has_real_fun_v<T>) and (has_imag_v<T> or has_imag_fun_v<T>)
+	(has_real_v<T> || has_real_fun_v<T>) && (has_imag_v<T> || has_imag_fun_v<T>)
 >{};
 
 template<class V, class T> auto real_is_aux(T const& value) -> typename std::is_same<decltype(value.real()), V>;
@@ -44,7 +44,7 @@ template<class V, class T> auto imag_is_aux(T const& value) -> typename std::is_
 template<class>            auto imag_is_aux(...           ) -> false_type;
 template<class T, class V> struct imag_is : decltype(imag_is_aux<V>(std::declval<T>())){};
 
-template<class T, class V> struct is_complex_of : std::integral_constant<bool, real_is<T, V>::value and imag_is<T, V>::value>{};
+template<class T, class V> struct is_complex_of : std::integral_constant<bool, real_is<T, V>::value && imag_is<T, V>::value>{};
 
 } // end namespace boost::multi::blas::numeric
 
