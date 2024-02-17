7acb4f61384152ae9b55df380a89314570058196
Alfredo Correa <alfredo.correa@gmail.com>
2024-02-16 18:07:07 -0800

    put get inside namespace

--- include/multi/detail/layout.hpp
+++ include/multi/detail/layout.hpp
@@ -370,6 +370,7 @@ constexpr auto array_size_impl(const boost::multi::extensions_t<D>&)
 
 }  // end namespace boost::multi
 
+
 template<boost::multi::dimensionality_type D>
 struct std::tuple_size<boost::multi::extensions_t<D>> : std::integral_constant<std::size_t, static_cast<std::size_t>(D)> {};
 
@@ -378,16 +379,17 @@ struct std::tuple_element<Index, boost::multi::extensions_t<D>> {
 	using type = typename std::tuple_element<Index, typename boost::multi::extensions_t<D>::base_>::type;
 };
 
-template<std::size_t Index, boost::multi::dimensionality_type D>
-constexpr auto std::get(boost::multi::extensions_t<D> const& self) -> typename std::tuple_element<Index, boost::multi::extensions_t<D>>::type {
-	using boost::multi::detail::get;
-	return get<Index>(self.base());
-}
-
-// namespace std {
-
-
-// }  // end namespace std
+// NOLINTBEGIN(cert-dcl58-cpp) to implement structured bindings
+namespace std {
+	// this function cannot be outside the namespace because clang complains "out-of-line definition of 'get' does not match any declaration in namespace 'std'"
+	template<std::size_t Index, boost::multi::dimensionality_type D>
+	constexpr auto get(::boost::multi::extensions_t<D> const& self)
+	-> typename std::tuple_element<Index, ::boost::multi::extensions_t<D>>::type {
+		using boost::multi::detail::get;
+		return get<Index>(self.base());
+	}
+}  // end namespace std
+// NOLINTEND(cert-dcl58-cpp) define stuff in STD
 
 namespace boost::multi {
 
