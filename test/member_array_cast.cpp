612a9dbb03ccffd460ec6661a48da243f9ed2238
Alfredo Correa <alfredo.correa@gmail.com>
2024-02-21 15:41:24 -0800

    tidy

--- test/member_array_cast.cpp
+++ test/member_array_cast.cpp
@@ -34,7 +34,7 @@ BOOST_AUTO_TEST_CASE(member_array_cast_soa_aos) {
 			auto operator+() const { return operator particle(); }
 
 			reference(double& mss, v3d& pos) : mass{mss}, position{pos} {}  // NOLINT(google-runtime-references)
-			reference(particle& p) : mass{p.mass}, position{p.position} {}
+			reference(particle& other) : reference{other.mass, other.position} {}  // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
 
 		 private:  // NOLINT(whitespace/indent) nested class
 			friend class particles_soa;
