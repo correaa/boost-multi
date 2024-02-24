e2eec8e4bec85e9be6eeba17b61a54e5750e566e
Alfredo Correa <alfredo.correa@gmail.com>
2024-02-24 01:33:11 -0800

    fix fill

--- test/fill.cpp
+++ test/fill.cpp
@@ -19,10 +19,6 @@ auto fnv1a(unsigned char const* first, std::ptrdiff_t len, std::size_t hash) noe
 }
 }  // namespace
 
-// static constexpr auto fnv1a(void const* key, std::size_t len) noexcept {
-//  return fnv1a(key, len, 14695981039346656037U);
-// }
-
 class fnv1a_t {
 	std::size_t h = 14695981039346656037U;  // offset
 
@@ -42,46 +38,54 @@ class fnv1a_t {
 	explicit operator result_type() const& noexcept { return h; }
 };
 
-BOOST_AUTO_TEST_CASE(fill_1d) {
+BOOST_AUTO_TEST_CASE(fill_1d_a) {
 	namespace multi = boost::multi;
-	{
-		multi::array<multi::index, 1> d1D(multi::extensions_t<1>{multi::iextension{10}});
-		static_assert(std::is_same_v<std::iterator_traits<decltype(begin(d1D))>::value_type, multi::index>, "!");
 
-		using std::copy;
-		copy(begin(extension(d1D)), end(extension(d1D)), begin(d1D));
-		BOOST_REQUIRE( d1D[0] == 0 );
-		BOOST_REQUIRE( d1D[1] == 1 );
-		BOOST_REQUIRE( d1D[9] == 9 );
+	multi::array<multi::index, 1> d1D(multi::extensions_t<1>{multi::iextension{10}});
+	static_assert(std::is_same_v<std::iterator_traits<decltype(begin(d1D))>::value_type, multi::index>, "!");
 
-		d1D.assign(extension(d1D));
-		BOOST_REQUIRE( d1D[0] == 0 );
-		BOOST_REQUIRE( d1D[1] == 1 );
-		BOOST_REQUIRE( d1D[9] == 9 );
-	}
-	{
-		multi::array<multi::index, 1> d1D(begin(multi::index_extension(10)), end(multi::index_extension(10)));
-		BOOST_REQUIRE( size(d1D) == 10 );
-		BOOST_REQUIRE( d1D[0] == 0 );
-		BOOST_REQUIRE( d1D[1] == 1 );
-		BOOST_REQUIRE( d1D[9] == 9 );
-	}
-	{
-		multi::array<multi::index, 1> d1D(multi::extensions_t<1>{multi::iextension{10}});
-		BOOST_REQUIRE( size(d1D) == 10 );
+	using std::copy;
+	copy(begin(extension(d1D)), end(extension(d1D)), begin(d1D));
+	BOOST_REQUIRE( d1D[0] == 0 );
+	BOOST_REQUIRE( d1D[1] == 1 );
+	BOOST_REQUIRE( d1D[9] == 9 );
 
-		d1D.assign(begin(extension(d1D)), end(extension(d1D)));
-		BOOST_REQUIRE( d1D[0] == 0 );
-		BOOST_REQUIRE( d1D[1] == 1 );
-		BOOST_REQUIRE( d1D[9] == 9 );
-	}
-	{
-		multi::array<multi::index, 1> d1D(multi::extensions_t<1>{multi::iextension{10}});
-		d1D.assign(extension(d1D));
-		BOOST_REQUIRE( d1D[0] == 0 );
-		BOOST_REQUIRE( d1D[1] == 1 );
-		BOOST_REQUIRE( d1D[9] == 9 );
-	}
+	d1D.assign(extension(d1D));
+	BOOST_REQUIRE( d1D[0] == 0 );
+	BOOST_REQUIRE( d1D[1] == 1 );
+	BOOST_REQUIRE( d1D[9] == 9 );
+}
+
+BOOST_AUTO_TEST_CASE(fill_1d_b) {
+	namespace multi = boost::multi;
+
+	multi::array<multi::index, 1> d1D(begin(multi::index_extension(10)), end(multi::index_extension(10)));
+	BOOST_REQUIRE( size(d1D) == 10 );
+	BOOST_REQUIRE( d1D[0] == 0 );
+	BOOST_REQUIRE( d1D[1] == 1 );
+	BOOST_REQUIRE( d1D[9] == 9 );
+}
+
+BOOST_AUTO_TEST_CASE(fill_1d_c) {
+	namespace multi = boost::multi;
+
+	multi::array<multi::index, 1> d1D(multi::extensions_t<1>{multi::iextension{10}});
+	BOOST_REQUIRE( size(d1D) == 10 );
+
+	d1D.assign(begin(extension(d1D)), end(extension(d1D)));
+	BOOST_REQUIRE( d1D[0] == 0 );
+	BOOST_REQUIRE( d1D[1] == 1 );
+	BOOST_REQUIRE( d1D[9] == 9 );
+}
+
+BOOST_AUTO_TEST_CASE(fill_1d_d) {
+	namespace multi = boost::multi;
+
+	multi::array<multi::index, 1> d1D(multi::extensions_t<1>{multi::iextension{10}});
+	d1D.assign(extension(d1D));
+	BOOST_REQUIRE( d1D[0] == 0 );
+	BOOST_REQUIRE( d1D[1] == 1 );
+	BOOST_REQUIRE( d1D[9] == 9 );
 }
 
 BOOST_AUTO_TEST_CASE(fill_member) {
@@ -135,17 +139,18 @@ BOOST_AUTO_TEST_CASE(fill) {
 	fill(begin((d2D.rotated())[1]), end((d2D.rotated())[1]), 8.0);
 	BOOST_REQUIRE( all_of(begin((d2D.rotated())[1]), end((d2D.rotated())[1]), [](auto&& elem) { return elem == 8.0;}) );
 
-	auto rand = [gauss = std::normal_distribution<>{}, gen = std::mt19937_64(randdev())]() mutable { return gauss(gen); };  //NOSONAR
+	auto rand = [gauss = std::normal_distribution<>{}, gen = std::mt19937_64(randdev())]() mutable { return gauss(gen); };  // NOSONAR
 
 	multi::array<double, 2> r2D({5, 5});
-	std::for_each(begin(r2D), end(r2D), [&](auto&& elem) { std::generate(begin(elem), end(elem), rand); });
+	std::for_each(begin(r2D), end(r2D), [&](decltype(r2D)::reference elem) { std::generate(begin(elem), end(elem), rand); });
 }
 
 namespace multi = boost::multi;
 
 BOOST_AUTO_TEST_CASE(fill_1D) {
 	multi::array<double, 1> const arr = {1.0, 2.0, 3.0};
-	multi::array<double, 2>       arr2({10, 3});
+
+	multi::array<double, 2> arr2({10, 3});
 
 	std::fill(begin(arr2), end(arr2), arr);
 
@@ -171,41 +176,42 @@ auto broadcast(BinaryOp op, Column const& col, Array const& in, Out&& out) -> Ou
 BOOST_AUTO_TEST_CASE(julia_broadcast, *boost::unit_test::tolerance(0.00001)) {
 	multi::array<double, 2> const col = {
 		{0.1},
-		{0.2}};
+		{0.2},
+	};
 	multi::array<double, 2> arr = {
 		{1.10813, 1.72068, 1.15387},
 		{1.36851, 1.66401, 1.47846},
 	};
-	{  // "broadcast"
-		multi::array<double, 2> arr2(extensions(arr));
-		broadcast(std::plus<>{}, col, arr, arr2);
 
-		BOOST_TEST( arr2[0][0] == 1.20813 );
-		BOOST_TEST( arr2[0][1] == 1.82068 );
-		BOOST_TEST( arr2[0][2] == 1.25387 );
-		BOOST_TEST( arr2[1][0] == 1.56851 );
-		BOOST_TEST( arr2[1][1] == 1.86401 );
-		BOOST_TEST( arr2[1][2] == 1.67846 );
-	}
-	{  // inefficient: replicate the vector before summing elementwise
-		multi::array<double, 2> ax3({2, 3});
+	// "broadcast"
+	multi::array<double, 2> arr2(extensions(arr));
+	broadcast(std::plus<>{}, col, arr, arr2);
 
-		std::fill(begin(~ax3), end(~ax3), (~col)[0]);
-		BOOST_TEST( ax3[0][0] == 0.1 );
-		BOOST_TEST( ax3[0][1] == 0.1 );
-		BOOST_TEST( ax3[0][2] == 0.1 );
-		BOOST_TEST( ax3[1][0] == 0.2 );
-		BOOST_TEST( ax3[1][1] == 0.2 );
-		BOOST_TEST( ax3[1][2] == 0.2 );
+	BOOST_TEST( arr2[0][0] == 1.20813 );
+	BOOST_TEST( arr2[0][1] == 1.82068 );
+	BOOST_TEST( arr2[0][2] == 1.25387 );
+	BOOST_TEST( arr2[1][0] == 1.56851 );
+	BOOST_TEST( arr2[1][1] == 1.86401 );
+	BOOST_TEST( arr2[1][2] == 1.67846 );
 
-		multi::array<double, 2> Ap(extensions(arr));
-		std::transform(begin(arr.elements()), end(arr.elements()), begin(ax3.elements()), begin(Ap.elements()), std::plus<>{});
+	// inefficient: replicate the vector before summing elementwise
+	multi::array<double, 2> ax3({2, 3});
 
-		BOOST_TEST( Ap[0][0] == 1.20813 );
-		BOOST_TEST( Ap[0][1] == 1.82068 );
-		BOOST_TEST( Ap[0][2] == 1.25387 );
-		BOOST_TEST( Ap[1][0] == 1.56851 );
-		BOOST_TEST( Ap[1][1] == 1.86401 );
-		BOOST_TEST( Ap[1][2] == 1.67846 );
-	}
+	std::fill(begin(~ax3), end(~ax3), (~col)[0]);
+	BOOST_TEST( ax3[0][0] == 0.1 );
+	BOOST_TEST( ax3[0][1] == 0.1 );
+	BOOST_TEST( ax3[0][2] == 0.1 );
+	BOOST_TEST( ax3[1][0] == 0.2 );
+	BOOST_TEST( ax3[1][1] == 0.2 );
+	BOOST_TEST( ax3[1][2] == 0.2 );
+
+	multi::array<double, 2> Ap(extensions(arr));
+	std::transform(begin(arr.elements()), end(arr.elements()), begin(ax3.elements()), begin(Ap.elements()), std::plus<>{});
+
+	BOOST_TEST( Ap[0][0] == 1.20813 );
+	BOOST_TEST( Ap[0][1] == 1.82068 );
+	BOOST_TEST( Ap[0][2] == 1.25387 );
+	BOOST_TEST( Ap[1][0] == 1.56851 );
+	BOOST_TEST( Ap[1][1] == 1.86401 );
+	BOOST_TEST( Ap[1][2] == 1.67846 );
 }
