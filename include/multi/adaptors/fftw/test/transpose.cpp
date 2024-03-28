1bd97cf72cde485ce4d723428cba648a6c0fe4c0
Alfredo Correa <alfredo.correa@gmail.com>
2024-03-28 10:10:19 -0700

    add nolint for destructor

--- include/multi/adaptors/fftw/test/transpose.cpp
+++ include/multi/adaptors/fftw/test/transpose.cpp
@@ -1,6 +1,6 @@
 // Copyright 2020-2024 Alfredo A. Correa
 
-#include<boost/test/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include <multi/adaptors/fftw.hpp>
 
@@ -14,20 +14,20 @@ namespace multi = boost::multi;
 class watch  // NOLINT(cppcoreguidelines-special-member-functions,hicpp-special-member-functions)
 : private std::chrono::high_resolution_clock {
 	std::string label_;
-	time_point start_ = now();
+	time_point  start_ = now();
 
  public:
 	explicit watch(std::string label) : label_{std::move(label)} {}  // NOLINT(fuchsia-default-arguments-calls)
 
-	watch(watch const&) = delete;
+	watch(watch const&)                    = delete;
 	auto operator=(watch const&) -> watch& = delete;
 
-	auto elapsed_sec() const {return std::chrono::duration<double>(now() - start_).count();}
-	~watch() {std::cerr<< label_ <<": "<< elapsed_sec() <<" sec"<<std::endl;}
+	auto elapsed_sec() const { return std::chrono::duration<double>(now() - start_).count(); }
+	~watch() { std::cerr << label_ << ": " << elapsed_sec() << " sec" << std::endl; }  // NOLINT(cpp:S4963)
 };
 
 using fftw_fixture = multi::fftw::environment;
-BOOST_TEST_GLOBAL_FIXTURE( fftw_fixture );
+BOOST_TEST_GLOBAL_FIXTURE(fftw_fixture);
 
 BOOST_AUTO_TEST_CASE(fftw_transpose) {
 	using namespace std::string_literals;  // NOLINT(build/namespaces) for ""s
@@ -36,8 +36,9 @@ BOOST_AUTO_TEST_CASE(fftw_transpose) {
 
 	auto const in = std::invoke([] {
 		multi::array<complex, 2> ret({101, 99});  // ({1013, 997});  // ({10137, 9973});
-		std::generate(ret.data_elements(), ret.data_elements() + ret.num_elements(),
-			[eng = std::default_random_engine{std::random_device{}()}, uniform_01 = std::uniform_real_distribution<>{}]() mutable{
+		std::generate(
+			ret.data_elements(), ret.data_elements() + ret.num_elements(),
+			[eng = std::default_random_engine{std::random_device{}()}, uniform_01 = std::uniform_real_distribution<>{}]() mutable {
 				return complex{uniform_01(eng), uniform_01(eng)};
 			}
 		);
@@ -47,7 +48,9 @@ BOOST_AUTO_TEST_CASE(fftw_transpose) {
 	multi::array<complex, 2> out = in;
 
 	watch const unnamed{"transposition with aux   %ws wall, CPU (%p%)\n"s};
+
 	multi::array<complex, 2> aux = ~out;
+
 	out = std::move(aux);
 	BOOST_REQUIRE( out[35][79] == in[79][35] );
 }
