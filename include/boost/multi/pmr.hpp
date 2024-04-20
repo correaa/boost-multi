7a8928d6d8b8c7038014425dd5cdc6911eb10da7
Alfredo Correa <alfredo.correa@gmail.com>
2024-04-20 13:12:40 -0700

    move pmr out of core headers

---
+++ include/boost/multi/pmr.hpp
@@ -0,0 +1,26 @@
+// Copyright 2018-2024 Alfredo A. Correa
+
+#ifndef MULTI_ARRAY_PMR_HPP_
+#define MULTI_ARRAY_PMR_HPP_
+
+#include <multi/array.hpp>
+
+#if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20210601)) && (!defined(_LIBCPP_VERSION) || (_LIBCPP_VERSION > 14000))
+#include <memory_resource>
+#endif
+
+#if(defined(__cpp_lib_memory_resource) && (__cpp_lib_memory_resource >= 201603))
+namespace boost::multi::pmr {
+
+template<class T, boost::multi::dimensionality_type D>
+using array = boost::multi::array<T, D, std::pmr::polymorphic_allocator<T>>;
+
+}  // end namespace boost::multi::pmr
+#else
+namespace boost::multi::pmr {
+template<class T, boost::multi::dimensionality_type D>
+struct [[deprecated("no PMR allocator")]] array;  // your version of C++ doesn't provide polymorphic_allocators
+}  // end namespace boost::multi::pmr
+#endif
+
+#endif
