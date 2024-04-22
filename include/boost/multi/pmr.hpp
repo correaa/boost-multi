0050b0bfba9aa10cf409a243ad849c7687c9c262
Matt Borland <matt@mattborland.com>
2024-04-22 11:46:33 +0200

    Rename file path

--- include/boost/multi/pmr.hpp
+++ include/boost/multi/pmr.hpp
@@ -3,7 +3,7 @@
 #ifndef MULTI_PMR_HPP_
 #define MULTI_PMR_HPP_
 
-#include <multi/array.hpp>
+#include <boost/multi/array.hpp>
 
 #if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20210601)) && (!defined(_LIBCPP_VERSION) || (_LIBCPP_VERSION > 14000))
 #include <memory_resource>
