ea0d3b133ad9042be217d4d46c79ee017b7a4d02
Alfredo Correa <alfredo.correa@gmail.com>
2024-04-22 22:24:03 -0700

    add boost lic

--- test/layout.cpp
+++ test/layout.cpp
@@ -1,4 +1,6 @@
-// Copyright 2018-2023 Alfredo A. Correa
+// Copyright 2018-2024 Alfredo A. Correa
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/test/unit_test.hpp>
 
