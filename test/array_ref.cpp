64e438642b2c33c0d5872892004bf36fa56f5640
Matt Borland <matt@mattborland.com>
2024-04-23 11:49:05 +0200

    Disable terminating test on homebrew GCC

--- test/array_ref.cpp
+++ test/array_ref.cpp
@@ -514,6 +514,8 @@ BOOST_AUTO_TEST_CASE(array_ref_cast_carray) {
 	BOOST_REQUIRE( &other_darr2[1][0] == &darr[1][0] );
 	BOOST_REQUIRE( &other_darr3[1][0] == &darr[1][0] );
 
+    // Homebrew GCC-13 terminates rather than having the expected exception caught.
+    #if !(defined(__GNUC__) && __GNUC__ >= 5 && defined(__APPLE__))
 	BOOST_REQUIRE_THROW(
 		([&] {
 			double(&other_darr4)[3][3](ref);  // NOLINT(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays) test legacy type
@@ -521,6 +523,7 @@ BOOST_AUTO_TEST_CASE(array_ref_cast_carray) {
 		}()),
 		std::bad_cast
 	);
+    #endif
 }
 
 BOOST_AUTO_TEST_CASE(array_ref_original_tests_const_carray) {
