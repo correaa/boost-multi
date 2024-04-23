b1c84b20aa90f8049f12d9248c5053ef6cd6ca08
Alfredo Correa <alfredo.correa+work@gmail.com>
2024-04-23 05:04:39 +0000

    Update pmr.hpp

--- include/boost/multi/pmr.hpp
+++ include/boost/multi/pmr.hpp
@@ -5,7 +5,7 @@
 #ifndef BOOST_MULTI_PMR_HPP_
 #define BOOST_MULTI_PMR_HPP_
 
-#include <multi/array.hpp>
+#include <boost/multi/array.hpp>
 
 #if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20210601)) && (!defined(_LIBCPP_VERSION) || (_LIBCPP_VERSION > 14000))
 #include <memory_resource>
