18ad0aa49f35a4c47e2e8d955002203f8fa2b502
Alfredo Correa <alfredo.correa@gmail.com>
2024-04-23 21:03:42 -0700

    guards

--- include/boost/multi/adaptors/blas/traits.hpp
+++ include/boost/multi/adaptors/blas/traits.hpp
@@ -2,8 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#ifndef MULTI_ADAPTORS_BLAS_TRAITS_HPP
-#define MULTI_ADAPTORS_BLAS_TRAITS_HPP
+#ifndef BOOST_MULTI_ADAPTORS_BLAS_TRAITS_HPP
+#define BOOST_MULTI_ADAPTORS_BLAS_TRAITS_HPP
 #pragma once
 
 #include<complex>
