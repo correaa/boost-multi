b6358ba3303f483ed79739530b73250102164fef
Matt Borland <matt@mattborland.com>
2024-04-29 11:05:16 +0200

    Change apple PMR detection conditions

--- include/boost/multi/pmr.hpp
+++ include/boost/multi/pmr.hpp
@@ -10,7 +10,7 @@
 #if __has_include(<memory_resource>)
 #  include <memory_resource>
 // Apple clang provides the header but not the compiled library prior to version 16
-#  if (defined(__cpp_lib_memory_resource) && (__cpp_lib_memory_resource >= 201603)) && !(defined(__APPLE_CC__) && __APPLE_CC__ <= 15)
+#  if (defined(__cpp_lib_memory_resource) && (__cpp_lib_memory_resource >= 201603)) && !(defined(__APPLE__) && defined(__clang_major__) && __clang_major__ <= 15)
 #    define BOOST_MULTI_HAS_MEMORY_RESOURCE
 #  endif
 #endif
