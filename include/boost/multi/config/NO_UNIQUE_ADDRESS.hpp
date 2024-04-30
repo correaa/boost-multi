00c1734f20813ec54024a1653d9fcdc91597cbdb
Matt Borland <matt@mattborland.com>
2024-04-30 11:35:26 +0200

    Refactor

--- include/boost/multi/config/NO_UNIQUE_ADDRESS.hpp
+++ include/boost/multi/config/NO_UNIQUE_ADDRESS.hpp
@@ -1,4 +1,5 @@
 // Copyright 2019-2024 Alfredo A. Correa
+// Copyright 2024 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
@@ -6,16 +7,14 @@
 #define BOOST_MULTI_CONFIG_NO_UNIQUE_ADDRESS_HPP_
 
 // clang-format off
-#ifndef __has_cpp_attribute
-	#define __has_cpp_attribute(name) 0
-#endif
-
-#if __has_cpp_attribute(no_unique_address) >=201803 && ! defined(__NVCC__) && ! defined(__PGI)
+#ifdef __has_cpp_attribute
+#  if __has_cpp_attribute(no_unique_address) >= 201803L && ! defined(__NVCC__) && ! defined(__PGI) && (__cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)) 
 	// NOLINTNEXTLINE(cppcoreguidelines-macro-usage) this macro will be needed until C++20
 	#define BOOST_MULTI_NO_UNIQUE_ADDRESS   [[no_unique_address]]
-#else
-	// NOLINTNEXTLINE(cppcoreguidelines-macro-usage) this macro will be needed until C++20
-	#define BOOST_MULTI_NO_UNIQUE_ADDRESS /*[[no_unique_address]]*/
+#endif
+
+#ifndef BOOST_MULTI_NO_UNIQUE_ADDRESS
+#  define BOOST_MULTI_NO_UNIQUE_ADDRESS
 #endif
 // clang-format on
 
