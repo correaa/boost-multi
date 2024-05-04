8d7a4c5aa2540b5d61a0ec898a219e0bcbe36db5
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-04 06:59:49 -0700

    fix constructor

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -1011,7 +1011,8 @@ struct array : static_array<T, D, Alloc> {
 	array(array&& other, typename array::allocator_type const& alloc) noexcept : static_array<T, D, Alloc>{std::move(other), alloc} {}
 	array(array&& other) noexcept : array{std::move(other), other.get_allocator()} {}
 
-	array(typename array::extensions_type exts, typename array::allocator_type const& alloc) : static_array<T, D, typename array::allocator_type>{exts, alloc} {}  // needed by MSVC?
+	array(typename array::extensions_type exts, Alloc const& alloc)
+	: static_array<T, D, Alloc>{exts, alloc} {}  // needed by MSVC?
 
 	friend auto get_allocator(array const& self) -> typename array::allocator_type { return self.get_allocator(); }
 
