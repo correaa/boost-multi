1e30b84b6d7a4515521878d2924a680bd6a32c6c
Alfredo Correa <alfredo.correa+work@gmail.com>
2024-05-10 01:47:12 +0000

    Update index_range.cpp

--- test/index_range.cpp
+++ test/index_range.cpp
@@ -38,7 +38,7 @@
 namespace multi = boost::multi;
 
 BOOST_AUTO_TEST_CASE(multi_range) {
-#if defined(__cpp_deduction_guides) and __cpp_deduction_guides and not defined(__NVCC__)
+#if defined(__cpp_deduction_guides) && __cpp_deduction_guides && ! defined(__NVCC__)
 	BOOST_REQUIRE(( multi::range{5, 5}.empty() ));
 #else
 	BOOST_REQUIRE(( multi::range<std::ptrdiff_t>{5, 5}.empty() ));
@@ -83,7 +83,7 @@ BOOST_AUTO_TEST_CASE(multi_range_in_constexpr) {
 	multi::range<int> const irng{5, 12};
 
 	BOOST_REQUIRE( irng.contains(6) );
-	BOOST_REQUIRE( not irng.contains(12) );
+	BOOST_REQUIRE( ! irng.contains(12) );
 
 	BOOST_REQUIRE( * irng.begin()      ==  5 );
 	BOOST_REQUIRE( *(irng.begin() + 1) ==  6 );
