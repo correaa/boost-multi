3057ddced8a1bf2be2e22e0acea3f36a9dcef279
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-10 11:47:16 -0700

    remove another operator

--- include/boost/multi/detail/operators.hpp
+++ include/boost/multi/detail/operators.hpp
@@ -59,8 +59,8 @@ template<class T>
 struct totally_ordered2<T, void> {
 	// template<class U>
 	// friend constexpr auto operator<=(T const& self, U const& other) { return (self < other) || (self == other); }
-	template<class U>
-	friend constexpr auto operator>=(T const& self, U const& other) { return (other < self) || (self == other); }
+	//template<class U>
+	//friend constexpr auto operator>=(T const& self, U const& other) { return (other < self) || (self == other); }
 	template<class U>
 	friend constexpr auto operator>(T const& self, U const& other) { return other < self; }
 };
