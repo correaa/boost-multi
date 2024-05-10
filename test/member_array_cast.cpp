c05d41c675c1d046cf735e5b9138b95567c268fa
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-10 11:27:25 -0700

    use !

--- test/member_array_cast.cpp
+++ test/member_array_cast.cpp
@@ -108,7 +108,7 @@ BOOST_AUTO_TEST_CASE(member_array_cast_soa_aos) {
 	SoA(1, 1) = SoA(0, 0);
 	BOOST_REQUIRE(SoA(1, 1).mass == SoA(0, 0).mass);
 	BOOST_REQUIRE(SoA(1, 1) == SoA(0, 0));
-	BOOST_REQUIRE(not(SoA(1, 1) != SoA(0, 0)));
+	BOOST_REQUIRE( ! (SoA(1, 1) != SoA(0, 0)));
 }
 
 struct employee_dummy {
@@ -168,7 +168,7 @@ BOOST_AUTO_TEST_CASE(member_array_cast_soa_aos_employee) {
 }
 #endif
 
-#if not defined(__circle_build__)
+#if ! defined(__circle_build__)
 BOOST_AUTO_TEST_CASE(element_transformed_from_member) {
 	struct record {
 		int    id;
