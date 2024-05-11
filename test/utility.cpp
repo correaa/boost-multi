db26f094a127a1cfea8e92702ca0f352a5db952c
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-11 00:23:50 -0700

    workaround msvc

--- test/utility.cpp
+++ test/utility.cpp
@@ -47,12 +47,14 @@ BOOST_AUTO_TEST_CASE(std_array_extensions_3d) {
 
 	BOOST_REQUIRE(( multi::extensions(arr) == decltype(multi::extensions(arr)){3, 4, 5} ));
 
+#ifndef _MSC_VER  // problem with 14.3 c++17
 	using multi::data_elements;
-	BOOST_REQUIRE( arr.data_elements() == &arr[0][0][0] );  // NOLINT(readability-container-data-pointer)
-	BOOST_REQUIRE( arr.data_elements() ==  arr[0][0].data() );
+	BOOST_REQUIRE( data_elements(arr) == &arr[0][0][0] );  // NOLINT(readability-container-data-pointer)
+	BOOST_REQUIRE( data_elements(arr) ==  arr[0][0].data() );
 
 	using multi::num_elements;
 	BOOST_REQUIRE( num_elements(arr) == 60 );
+#endif
 
 	multi::array<double, 3> const marr(
 #ifdef _MSC_VER  // problem with 14.3 c++17
