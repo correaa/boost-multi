bd0ad46a01c6f45f0a61f762c86c5cced0ee94d9
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-11 00:03:16 -0700

    more msvc workarounds

--- test/array_ref.cpp
+++ test/array_ref.cpp
@@ -324,12 +324,12 @@ BOOST_AUTO_TEST_CASE(array_ref_2D_from_vector_with_offset) {
 
 	{
 		auto exts = aref.extensions();
-	#ifndef _MSC_VER
+#ifndef _MSC_VER
 		auto const [exts0, exts1] = exts;
-	#else
+#else
 		auto const exts0 = std::get<0>(exts);
 		auto const exts1 = std::get<1>(exts);
-	#endif
+#endif
 		BOOST_REQUIRE( exts0 == multi::iextension(1, 3) );
 
 		BOOST_REQUIRE( exts1.first()  == 1 );
@@ -516,8 +516,8 @@ BOOST_AUTO_TEST_CASE(array_ref_cast_carray) {
 	BOOST_REQUIRE( &other_darr2[1][0] == &darr[1][0] );
 	BOOST_REQUIRE( &other_darr3[1][0] == &darr[1][0] );
 
-    // Homebrew GCC-13 terminates rather than having the expected exception caught.
-    #if !(defined(__GNUC__) && __GNUC__ >= 5 && defined(__APPLE__))
+// Homebrew GCC-13 terminates rather than having the expected exception caught.
+#if !(defined(__GNUC__) && __GNUC__ >= 5 && defined(__APPLE__))
 	BOOST_REQUIRE_THROW(
 		([&] {
 			double(&other_darr4)[3][3](ref);  // NOLINT(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays) test legacy type
@@ -525,7 +525,7 @@ BOOST_AUTO_TEST_CASE(array_ref_cast_carray) {
 		}()),
 		std::bad_cast
 	);
-    #endif
+#endif
 }
 
 BOOST_AUTO_TEST_CASE(array_ref_original_tests_const_carray) {
@@ -814,7 +814,14 @@ BOOST_AUTO_TEST_CASE(as_span) {
 		print_me1(arr2);
 		print_me1(*multi::array_ptr<int, 1>{arr2.data(), {6}});
 
-		multi::static_array<int, 1> marr({10}, 99);
+		multi::static_array<int, 1> marr(
+#ifdef _MSC_VER  // problems with MSVC 14.3 c++17
+			multi::extensions_t<1>
+#endif
+			{10},
+			99
+		);
+
 		print_me1(*multi::array_ptr<int, 1>(marr.data_elements(), 10));
 
 		auto& alias = marr;
