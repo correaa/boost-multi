77de0775aa017505d90a4b78ddbc1386636da743
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-10 17:11:17 -0700

    use member

--- test/zero_dimensionality.cpp
+++ test/zero_dimensionality.cpp
@@ -1,5 +1,4 @@
-// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
-// Copyright 2019-2023 Alfredo A. Correa
+// Copyright 2019-2024 Alfredo A. Correa
 // Copyright 2024 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
@@ -75,7 +74,7 @@ BOOST_AUTO_TEST_CASE(zero_dimensionality_part1) {
 BOOST_AUTO_TEST_CASE(zero_dimensionality_part2) {
 	{
 		multi::array<std::complex<double>, 2> const arr({1, 2}, std::allocator<std::complex<double>>{});
-		BOOST_REQUIRE( size(arr) == 1 );
+		BOOST_REQUIRE( arr.size() == 1 );
 	}
 	{
 		double doub = 2.0;
