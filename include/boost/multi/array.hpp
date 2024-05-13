52e1985b808d67397fd9c1dcbd2f94b824388f93
Alfredo Correa <alfredo.correa@gmail.com>
2024-05-13 00:51:11 -0700

    separate lines

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -267,7 +267,16 @@ struct static_array  // NOLINT(fuchsia-multiple-inheritance) : multiple inherita
 	: static_array(typename static_array::extensions_type{}, elem, alloc) {}
 
 	constexpr static_array(typename static_array::extensions_type exts, typename static_array::element const& elem)
-	: array_alloc{}, ref(array_alloc::allocate(static_cast<typename multi::allocator_traits<allocator_type>::size_type>(typename static_array::layout_t(exts).num_elements()), nullptr), exts) {
+	:
+	array_alloc{}, 
+	ref(
+		array_alloc::allocate(
+			static_cast<typename multi::allocator_traits<allocator_type>::size_type>(typename static_array::layout_t(exts).num_elements()),
+			nullptr
+		),
+		exts
+	)
+	{
 		array_alloc::uninitialized_fill_n(this->base(), static_cast<typename multi::allocator_traits<allocator_type>::size_type>(this->num_elements()), elem);
 	}
 
