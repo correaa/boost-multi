bad219e25d6d1df5f0d4a8a39f2cac592490d83e
Alfredo Correa <alfredo.correa@gmail.com>
2024-06-10 17:54:04 -0700

    format test

--- test/broadcast.cpp
+++ test/broadcast.cpp
@@ -5,19 +5,19 @@
 
 // Suppress warnings from boost.test
 #if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wold-style-cast"
-#pragma clang diagnostic ignored "-Wundef"
-#pragma clang diagnostic ignored "-Wconversion"
-#pragma clang diagnostic ignored "-Wsign-conversion"
+	#pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Wold-style-cast"
+	#pragma clang diagnostic ignored "-Wundef"
+	#pragma clang diagnostic ignored "-Wconversion"
+	#pragma clang diagnostic ignored "-Wsign-conversion"
 #elif defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wundef"
-#pragma GCC diagnostic ignored "-Wconversion"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wstringop-overflow="
-#pragma GCC diagnostic ignored "-Warray-bounds="
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wold-style-cast"
+	#pragma GCC diagnostic ignored "-Wundef"
+	#pragma GCC diagnostic ignored "-Wconversion"
+	#pragma GCC diagnostic ignored "-Wsign-conversion"
+	#pragma GCC diagnostic ignored "-Wstringop-overflow="
+	#pragma GCC diagnostic ignored "-Warray-bounds="
 #endif
 
 #include <boost/multi/array.hpp>
@@ -25,33 +25,33 @@
 #include <algorithm>  // for std::ranges::fold_left
 
 #ifndef BOOST_TEST_MODULE
-#define BOOST_TEST_MAIN
+	#define BOOST_TEST_MAIN
 #endif
 
 #include <boost/test/unit_test.hpp>
 
 #if defined(__clang__)
-#pragma clang diagnostic pop
+	#pragma clang diagnostic pop
 #elif defined(__GNUC__)
-#pragma GCC diagnostic pop
+	#pragma GCC diagnostic pop
 #endif
 
 BOOST_AUTO_TEST_CASE(broadcast_as_fill) {
 	namespace multi = boost::multi;
-    multi::array<int, 1> b = {10, 11};
 
-    multi::array<int, 2> B({10, 2});
- 
- 
-    // std::fill  (B.begin(), B.end(), b);                                       // canonical way
-    std::fill_n(B.begin(), B.size(), b);                                      // canonical way
+	multi::array<int, 1> b = { 10, 11 };
 
-    // std::copy_n(b.broadcasted().begin(), B.size(), B.begin());                // doesn't work because faulty implementation of copy_n
-    // thrust::copy_n(b.broadcasted().begin(), B.size(), B.begin());                // equivalent, using broadcast
+	multi::array<int, 2> B({ 10, 2 });
 
-    // std::copy_n(b.broadcasted().begin(), b.broadcasted().size(), B.begin());  // incorrect, undefined behavior, no useful size()
-    // std::copy  (b.broadcasted().begin(), b.broadcasted().end(), B.begin());   // incorrect, undefined behavior, non-terminating loop (end is not reacheable)
-    // B = b.broadcasted();
+	// std::fill  (B.begin(), B.end(), b);                                       // canonical way
+	std::fill_n(B.begin(), B.size(), b);  // canonical way
+
+	// std::copy_n(b.broadcasted().begin(), B.size(), B.begin());                // doesn't work because faulty implementation of copy_n
+	// thrust::copy_n(b.broadcasted().begin(), B.size(), B.begin());                // equivalent, using broadcast
+
+	// std::copy_n(b.broadcasted().begin(), b.broadcasted().size(), B.begin());  // incorrect, undefined behavior, no useful size()
+	// std::copy  (b.broadcasted().begin(), b.broadcasted().end(), B.begin());   // incorrect, undefined behavior, non-terminating loop (end is not reacheable)
+	// B = b.broadcasted();
 
 	BOOST_REQUIRE( B[0] == b );
 	BOOST_REQUIRE( B[1] == b );
