f12fa5d876838643504393fb630c1b16d5cdfd23
Alfredo Correa <alfredo.correa+work@gmail.com>
2024-06-14 18:49:04 +0000

    Update concepts.cpp

--- test/concepts.cpp
+++ test/concepts.cpp
@@ -39,10 +39,12 @@
 	#pragma clang diagnostic push
 	#pragma clang diagnostic ignored "-Wold-style-cast"
 	#pragma clang diagnostic ignored "-Wsign-conversion"
+	#pragma clang "-Wconversion"
 #elif defined(__GNUC__)
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wold-style-cast"
 	#pragma GCC diagnostic ignored "-Wsign-conversion"
+	#pragma GCC diagnostic "-Wconversion"
 #endif
 
 #include <boost/concept/assert.hpp>  // for BOOST_CONCEPT_ASSERT
