547b97e960598790e2c9dd43991322b699502eec
Alfredo Correa <alfredo.correa@gmail.com>
2024-06-17 23:57:23 -0700

    finish iwyu

--- test/overload.cpp
+++ test/overload.cpp
@@ -1,44 +1,47 @@
-// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
-// Copyright 2018-2023 Alfredo A. Correa
+// Copyright 2018-2024 Alfredo A. Correa
 // Copyright 2024 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/multi/array.hpp>
-
-#include <complex>
-
-// Suppress warnings from boost.test
 #if defined(__clang__)
-#  pragma clang diagnostic push
-#  pragma clang diagnostic ignored "-Wold-style-cast"
-#  pragma clang diagnostic ignored "-Wundef"
-#  pragma clang diagnostic ignored "-Wconversion"
-#  pragma clang diagnostic ignored "-Wsign-conversion"
-#  pragma clang diagnostic ignored "-Wfloat-equal"
+	#pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Wold-style-cast"
+	#pragma clang diagnostic ignored "-Wundef"
+	#pragma clang diagnostic ignored "-Wconversion"
+	#pragma clang diagnostic ignored "-Wsign-conversion"
 #elif defined(__GNUC__)
-#  pragma GCC diagnostic push
-#  pragma GCC diagnostic ignored "-Wold-style-cast"
-#  pragma GCC diagnostic ignored "-Wundef"
-#  pragma GCC diagnostic ignored "-Wconversion"
-#  pragma GCC diagnostic ignored "-Wsign-conversion"
-#  pragma GCC diagnostic ignored "-Wfloat-equal"
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wold-style-cast"
+	#pragma GCC diagnostic ignored "-Wundef"
+	#pragma GCC diagnostic ignored "-Wconversion"
+	#pragma GCC diagnostic ignored "-Wsign-conversion"
 #endif
 
 #ifndef BOOST_TEST_MODULE
-#  define BOOST_TEST_MAIN
+	#define BOOST_TEST_MAIN
 #endif
 
 #include <boost/test/unit_test.hpp>
 
+#if defined(__clang__)
+	#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+	#pragma GCC diagnostic pop
+#endif
+
+#include <boost/multi/array.hpp>  // for array
+
+#include <complex>  // for complex
+#include <string>   // for operator==, string
+
 namespace multi = boost::multi;
 
-inline auto what_is(multi::array<double              , 2> const& /*arr*/) { return std::string{"real"}; }  // std::string NOLINT(fuchsia-default-arguments-calls)
-inline auto what_is(multi::array<std::complex<double>, 2> const& /*arr*/) { return std::string{"complex"}; }  // std::string NOLINT(fuchsia-default-arguments-calls)
+inline auto what_is(multi::array<double, 2> const& /*arr*/) { return std::string{ "real" }; }                   // std::string NOLINT(fuchsia-default-arguments-calls)
+inline auto what_is(multi::array<std::complex<double>, 2> const& /*arr*/) { return std::string{ "complex" }; }  // std::string NOLINT(fuchsia-default-arguments-calls)
 
 BOOST_AUTO_TEST_CASE(multi_array_overload) {
-	multi::array<double, 2> const               real_A({10, 20});
-	multi::array<std::complex<double>, 2> const cplx_A({10, 20});
+	multi::array<double, 2> const               real_A({ 10, 20 });
+	multi::array<std::complex<double>, 2> const cplx_A({ 10, 20 });
 
 	std::string const real_str    = what_is(real_A);
 	std::string const complex_str = what_is(cplx_A);
