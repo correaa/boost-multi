61546533cbb28861e3db6494e5ee3819c9a9f0bc
Alfredo Correa <alfredo.correa@gmail.com>
2024-07-24 14:49:16 -0700

    more gcc 7

--- test/static_array_cast.cpp
+++ test/static_array_cast.cpp
@@ -10,7 +10,9 @@
 	#pragma clang diagnostic ignored "-Wundef"
 #elif defined(__GNUC__)
 	#pragma GCC diagnostic push
-	#pragma GCC diagnostic ignored "-Wcast-function-type"
+	#if (__GNUC__ > 7)
+		#pragma GCC diagnostic ignored "-Wcast-function-type"
+	#endif
 	#pragma GCC diagnostic ignored "-Wconversion"
 	#pragma GCC diagnostic ignored "-Wold-style-cast"
 	#pragma GCC diagnostic ignored "-Wsign-conversion"
