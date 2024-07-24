f565d90ab6deb1de2559719605ddb50d9abe831f
Alfredo Correa <alfredo.correa@gmail.com>
2024-07-24 11:25:50 -0700

    more protection gcc 7

--- test/broadcast.cpp
+++ test/broadcast.cpp
@@ -13,7 +13,9 @@
 #elif defined(__GNUC__)
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Warray-bounds="
-	#pragma GCC diagnostic ignored "-Wcast-function-type"
+	#if (__GNUC__ > 7)
+		#pragma GCC diagnostic ignored "-Wcast-function-type"
+	#endif
 	#pragma GCC diagnostic ignored "-Wconversion"
 	#pragma GCC diagnostic ignored "-Wold-style-cast"
 	#pragma GCC diagnostic ignored "-Wsign-conversion"
