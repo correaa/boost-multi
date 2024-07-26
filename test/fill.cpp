e9e9d0f39491bbf90b5309a04fce35bff1290fc3
Alfredo Correa <alfredo.correa@gmail.com>
2024-07-25 17:55:34 -0700

    convert fill test to boost lwt

--- test/fill.cpp
+++ test/fill.cpp
@@ -3,6 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#if 0
 #if defined(__clang__)
 	#pragma clang diagnostic push
 	#pragma clang diagnostic ignored "-Wunknown-warning-option"
@@ -39,6 +40,10 @@
 #elif defined(_MSC_VER)
 	#pragma warning(pop)
 #endif
+#endif
+
+#include <boost/core/lightweight_test.hpp>
+#define BOOST_AUTO_TEST_CASE(ArG) ); std::invoke([]
 
 #include <boost/multi/array.hpp>  // for array, apply, operator==
 
@@ -89,6 +94,8 @@ class fnv1a_t {
 	explicit operator result_type() const& noexcept { return h_; }
 };
 
+int main() {((void)0
+
 BOOST_AUTO_TEST_CASE(fill_1d_a) {
 	namespace multi = boost::multi;
 
@@ -97,36 +104,36 @@ BOOST_AUTO_TEST_CASE(fill_1d_a) {
 
 	using std::copy;
 	copy(begin(extension(d1D)), end(extension(d1D)), begin(d1D));
-	BOOST_REQUIRE( d1D[0] == 0 );
-	BOOST_REQUIRE( d1D[1] == 1 );
-	BOOST_REQUIRE( d1D[9] == 9 );
+	BOOST_TEST( d1D[0] == 0 );
+	BOOST_TEST( d1D[1] == 1 );
+	BOOST_TEST( d1D[9] == 9 );
 
 	d1D.assign(extension(d1D));
-	BOOST_REQUIRE( d1D[0] == 0 );
-	BOOST_REQUIRE( d1D[1] == 1 );
-	BOOST_REQUIRE( d1D[9] == 9 );
+	BOOST_TEST( d1D[0] == 0 );
+	BOOST_TEST( d1D[1] == 1 );
+	BOOST_TEST( d1D[9] == 9 );
 }
 
 BOOST_AUTO_TEST_CASE(fill_1d_b) {
 	namespace multi = boost::multi;
 
 	multi::array<multi::index, 1> d1D(begin(multi::index_extension(10)), end(multi::index_extension(10)));
-	BOOST_REQUIRE( size(d1D) == 10 );
-	BOOST_REQUIRE( d1D[0] == 0 );
-	BOOST_REQUIRE( d1D[1] == 1 );
-	BOOST_REQUIRE( d1D[9] == 9 );
+	BOOST_TEST( size(d1D) == 10 );
+	BOOST_TEST( d1D[0] == 0 );
+	BOOST_TEST( d1D[1] == 1 );
+	BOOST_TEST( d1D[9] == 9 );
 }
 
 BOOST_AUTO_TEST_CASE(fill_1d_c) {
 	namespace multi = boost::multi;
 
 	multi::array<multi::index, 1> d1D(multi::extensions_t<1>{ multi::iextension{ 10 } });
-	BOOST_REQUIRE( size(d1D) == 10 );
+	BOOST_TEST( size(d1D) == 10 );
 
 	d1D.assign(begin(extension(d1D)), end(extension(d1D)));
-	BOOST_REQUIRE( d1D[0] == 0 );
-	BOOST_REQUIRE( d1D[1] == 1 );
-	BOOST_REQUIRE( d1D[9] == 9 );
+	BOOST_TEST( d1D[0] == 0 );
+	BOOST_TEST( d1D[1] == 1 );
+	BOOST_TEST( d1D[9] == 9 );
 }
 
 BOOST_AUTO_TEST_CASE(fill_1d_d) {
@@ -134,9 +141,9 @@ BOOST_AUTO_TEST_CASE(fill_1d_d) {
 
 	multi::array<multi::index, 1> d1D(multi::extensions_t<1>{ multi::iextension{ 10 } });
 	d1D.assign(extension(d1D));
-	BOOST_REQUIRE( d1D[0] == 0 );
-	BOOST_REQUIRE( d1D[1] == 1 );
-	BOOST_REQUIRE( d1D[9] == 9 );
+	BOOST_TEST( d1D[0] == 0 );
+	BOOST_TEST( d1D[1] == 1 );
+	BOOST_TEST( d1D[9] == 9 );
 }
 
 BOOST_AUTO_TEST_CASE(fill_member) {
@@ -152,15 +159,15 @@ BOOST_AUTO_TEST_CASE(fill_member) {
 		{ 500,  60,  70,  80,  90},
 	};
 
-	BOOST_REQUIRE(   d2D.elements().size()  == d2D.num_elements()  );
-	BOOST_REQUIRE(   d2D.elements().base()  == d2D.base()          );
-	BOOST_REQUIRE(   d2D.elements()[3]      == 180                 );
-	BOOST_REQUIRE( &*d2D.elements().begin() == d2D.data_elements() );
-	BOOST_REQUIRE( &*d2D.elements().end()   == d2D.data_elements() + d2D.num_elements() );
+	BOOST_TEST(   d2D.elements().size()  == d2D.num_elements()  );
+	BOOST_TEST(   d2D.elements().base()  == d2D.base()          );
+	BOOST_TEST(   d2D.elements()[3]      == 180                 );
+	BOOST_TEST( &*d2D.elements().begin() == d2D.data_elements() );
+	BOOST_TEST( &*d2D.elements().end()   == d2D.data_elements() + d2D.num_elements() );
 
 	std::fill(d2D.elements().begin(), d2D.elements().end(), 990);
 
-	BOOST_REQUIRE( d2D[1][1] == 990 );
+	BOOST_TEST( d2D[1][1] == 990 );
 }
 
 BOOST_AUTO_TEST_CASE(fill) {
@@ -175,18 +182,18 @@ BOOST_AUTO_TEST_CASE(fill) {
 		{ 500,  60,  70,  80,  90},
 	};
 	using std::all_of;
-	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto const& elem) { return elem == 50;}) );
+	BOOST_TEST( all_of(begin(d2D[1]), end(d2D[1]), [](auto const& elem) { return elem == 50;}) );
 
 	using std::fill;
 	fill(d2D[1].begin(), d2D[1].end(), 80);
 
-	BOOST_REQUIRE( all_of(begin(d2D[1]), end(d2D[1]), [](auto const& elem) { return elem == 80;}) );
+	BOOST_TEST( all_of(begin(d2D[1]), end(d2D[1]), [](auto const& elem) { return elem == 80;}) );
 
 	fill(begin(d2D.rotated()[1]), end(d2D.rotated()[1]), 80);
-	BOOST_REQUIRE( all_of(begin(d2D.rotated()[1]), end(d2D.rotated()[1]), [](auto&& elem) { return elem == 80;}) );
+	BOOST_TEST( all_of(begin(d2D.rotated()[1]), end(d2D.rotated()[1]), [](auto&& elem) { return elem == 80;}) );
 
 	fill(begin((d2D.rotated())[1]), end((d2D.rotated())[1]), 80);
-	BOOST_REQUIRE( all_of(begin((d2D.rotated())[1]), end((d2D.rotated())[1]), [](auto&& elem) { return elem == 80;}) );
+	BOOST_TEST( all_of(begin((d2D.rotated())[1]), end((d2D.rotated())[1]), [](auto&& elem) { return elem == 80;}) );
 
 	auto rand = [gauss = std::uniform_int_distribution<>(0, 10), gen = std::mt19937_64(randdev())]() mutable { return gauss(gen); };  // NOSONAR
 
@@ -194,18 +201,19 @@ BOOST_AUTO_TEST_CASE(fill) {
 	std::for_each(begin(r2D), end(r2D), [&](decltype(r2D)::reference elem) { std::generate(begin(elem), end(elem), rand); });
 }
 
-namespace multi = boost::multi;
-
 BOOST_AUTO_TEST_CASE(fill_1D) {
+	namespace multi = boost::multi;
+
 	multi::array<double, 1> const arr = { 1.0, 2.0, 3.0 };
 
 	multi::array<double, 2> arr2({ 10, 3 });
 
 	std::fill(begin(arr2), end(arr2), arr);
 
-	BOOST_REQUIRE( arr2[0] == arr );
-	BOOST_REQUIRE( arr2[1] == arr );
+	BOOST_TEST( arr2[0] == arr );
+	BOOST_TEST( arr2[1] == arr );
 
-	BOOST_REQUIRE( arr2[9] == arr );
+	BOOST_TEST( arr2[9] == arr );
 }
 
+); return boost::report_errors();}
