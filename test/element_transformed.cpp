13599ce5df2faaa44feb0d7068f2f69290eaba47
Alfredo Correa <alfredo.correa@gmail.com>
2024-07-25 22:44:20 -0700

    more lwt

--- test/element_transformed.cpp
+++ test/element_transformed.cpp
@@ -3,102 +3,49 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#if defined(__clang__)
-	#pragma clang diagnostic push
-	#pragma clang diagnostic ignored "-Wunknown-warning-option"
-	#pragma clang diagnostic ignored "-Wconversion"
-	#pragma clang diagnostic ignored "-Wextra-semi-stmt"
-	#pragma clang diagnostic ignored "-Wignored-qualifiers"
-	#pragma clang diagnostic ignored "-Wold-style-cast"
-	#pragma clang diagnostic ignored "-Wsign-conversion"
-	#pragma clang diagnostic ignored "-Wswitch-default"
-	#pragma clang diagnostic ignored "-Wundef"
-#elif defined(__GNUC__)
-	#pragma GCC diagnostic push
-	#if (__GNUC__ > 7)
-		#pragma GCC diagnostic ignored "-Wcast-function-type"
-	#endif
-	#pragma GCC diagnostic ignored "-Wconversion"
-	#pragma GCC diagnostic ignored "-Wold-style-cast"
-	#pragma GCC diagnostic ignored "-Wsign-conversion"
-	#pragma GCC diagnostic ignored "-Wundef"
-#elif defined(_MSC_VER)
-	#pragma warning(push)
-	#pragma warning(disable : 4244)
-#endif
+// #if defined(__clang__)
+//  #pragma clang diagnostic push
+//  #pragma clang diagnostic ignored "-Wunknown-warning-option"
+//  #pragma clang diagnostic ignored "-Wconversion"
+//  #pragma clang diagnostic ignored "-Wextra-semi-stmt"
+//  #pragma clang diagnostic ignored "-Wignored-qualifiers"
+//  #pragma clang diagnostic ignored "-Wold-style-cast"
+//  #pragma clang diagnostic ignored "-Wsign-conversion"
+//  #pragma clang diagnostic ignored "-Wswitch-default"
+//  #pragma clang diagnostic ignored "-Wundef"
+// #elif defined(__GNUC__)
+//  #pragma GCC diagnostic push
+//  #if (__GNUC__ > 7)
+//      #pragma GCC diagnostic ignored "-Wcast-function-type"
+//  #endif
+//  #pragma GCC diagnostic ignored "-Wconversion"
+//  #pragma GCC diagnostic ignored "-Wold-style-cast"
+//  #pragma GCC diagnostic ignored "-Wsign-conversion"
+//  #pragma GCC diagnostic ignored "-Wundef"
+// #elif defined(_MSC_VER)
+//  #pragma warning(push)
+//  #pragma warning(disable : 4244)
+// #endif
 
-#ifndef BOOST_TEST_MODULE
-	#define BOOST_TEST_MAIN
-#endif
+// #ifndef BOOST_TEST_MODULE
+//  #define BOOST_TEST_MAIN
+// #endif
 
-#include <boost/test/included/unit_test.hpp>
+// #include <boost/test/included/unit_test.hpp>
 
-#if defined(__clang__)
-	#pragma clang diagnostic pop
-#elif defined(__GNUC__)
-	#pragma GCC diagnostic pop
-#elif defined(_MSC_VER)
-	#pragma warning(pop)
-#endif
+// #if defined(__clang__)
+//  #pragma clang diagnostic pop
+// #elif defined(__GNUC__)
+//  #pragma GCC diagnostic pop
+// #elif defined(_MSC_VER)
+//  #pragma warning(pop)
+// #endif
 
 #include <boost/multi/array.hpp>  // for transform_ptr, array, subarray
 
 #include <complex>  // for complex, operator*, operator+
 #include <utility>  // for declval, forward  // IWYU pragma: no_include <type_traits>  // for declval  // in utility
 #include <vector>   // for vector
-
-namespace multi = boost::multi;
-
-BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_function_reference) {
-	using complex = std::complex<double>;
-	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
-
-	multi::array<complex, 1> arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
-
-	constexpr auto conj = static_cast<complex (&)(complex const&)>(std::conj<double>);
-
-	auto const& conjd_arr = arr.element_transformed(conj);
-	BOOST_REQUIRE( conjd_arr[0] == conj(arr[0]) );
-	BOOST_REQUIRE( conjd_arr[1] == conj(arr[1]) );
-
-	//  Ac[0] = 5. + 4.*I;  // this doesn't compile, good!
-	BOOST_REQUIRE( conjd_arr[0] == 1.0 - 2.0*I );
-
-	// BOOST_REQUIRE_CLOSE(real(std::inner_product(arr.begin(), arr.end(), conjd_arr.begin(), complex{ 0.0, 0.0 })), std::norm(arr[0]) + std::norm(arr[1]), 1E-6);
-	// BOOST_REQUIRE_CLOSE(imag(std::inner_product(arr.begin(), arr.end(), conjd_arr.begin(), complex{ 0.0, 0.0 })), 0.0, 1E-6);
-
-	// BOOST_TEST_REQUIRE( std::inner_product(arr.begin(), arr.end(), conjd_arr.begin(), complex{0.0, 0.0}) == std::norm(arr[0]) + std::norm(arr[1]) );
-}
-
-BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_lambda) {
-	using complex = std::complex<double>;
-	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
-
-	multi::array<complex, 1> arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
-
-	// g++ -std=20 needs the transformation (lambda) to be noexcept
-	auto const& conjd_arr = arr.element_transformed([](auto const& cee) noexcept { return std::conj(cee); });
-	BOOST_REQUIRE( conjd_arr[0] == std::conj(arr[0]) );
-	BOOST_REQUIRE( conjd_arr[1] == std::conj(arr[1]) );
-
-	//  Ac[0] = 5. + 4.*I;  // this doesn't compile, good!
-	BOOST_REQUIRE( conjd_arr[0] == 1.0 - 2.0*I );
-}
-
-BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_lambda_with_const_return) {
-	using complex = std::complex<double>;
-	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
-
-	multi::array<complex, 1> arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
-
-	// g++ -std=20 needs the transformation (lambda) to be noexcept
-	auto&& conjd_arr = arr.element_transformed([](auto const& cee) noexcept /* a const return would be unmovable */ { return std::conj(cee); });
-	BOOST_REQUIRE( conjd_arr[0] == std::conj(arr[0]) );
-	BOOST_REQUIRE( conjd_arr[1] == std::conj(arr[1]) );
-
-	// conjd_arr[0] = 5.0 + 4.0*I;  // this compiles, but classes the implement operator= naively can be misleading here
-	BOOST_REQUIRE( conjd_arr[0] == 1.0 - 2.0*I );
-}
 
 template<typename ComplexRef> struct Conjd;  // NOLINT(readability-identifier-naming) for testing
 
@@ -137,6 +84,63 @@ struct Conjd {  // NOLINT(readability-identifier-naming) for testing
 	friend decltype(Conj);
 };
 
+namespace multi = boost::multi;
+
+#include <boost/core/lightweight_test.hpp>
+#define BOOST_AUTO_TEST_CASE(ArG) [[maybe_unused]] void* ArG ;
+
+int main() {
+BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_function_reference) {
+	using complex = std::complex<double>;
+	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
+
+	multi::array<complex, 1> arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
+
+	constexpr auto conj = static_cast<complex (&)(complex const&)>(std::conj<double>);
+
+	auto const& conjd_arr = arr.element_transformed(conj);
+	BOOST_TEST( conjd_arr[0] == conj(arr[0]) );
+	BOOST_TEST( conjd_arr[1] == conj(arr[1]) );
+
+	//  Ac[0] = 5. + 4.*I;  // this doesn't compile, good!
+	BOOST_TEST( conjd_arr[0] == 1.0 - 2.0*I );
+
+	// BOOST_REQUIRE_CLOSE(real(std::inner_product(arr.begin(), arr.end(), conjd_arr.begin(), complex{ 0.0, 0.0 })), std::norm(arr[0]) + std::norm(arr[1]), 1E-6);
+	// BOOST_REQUIRE_CLOSE(imag(std::inner_product(arr.begin(), arr.end(), conjd_arr.begin(), complex{ 0.0, 0.0 })), 0.0, 1E-6);
+
+	// BOOST_TEST_REQUIRE( std::inner_product(arr.begin(), arr.end(), conjd_arr.begin(), complex{0.0, 0.0}) == std::norm(arr[0]) + std::norm(arr[1]) );
+}
+
+BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_lambda) {
+	using complex = std::complex<double>;
+	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
+
+	multi::array<complex, 1> arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
+
+	// g++ -std=20 needs the transformation (lambda) to be noexcept
+	auto const& conjd_arr = arr.element_transformed([](auto const& cee) noexcept { return std::conj(cee); });
+	BOOST_TEST( conjd_arr[0] == std::conj(arr[0]) );
+	BOOST_TEST( conjd_arr[1] == std::conj(arr[1]) );
+
+	//  Ac[0] = 5. + 4.*I;  // this doesn't compile, good!
+	BOOST_TEST( conjd_arr[0] == 1.0 - 2.0*I );
+}
+
+BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_lambda_with_const_return) {
+	using complex = std::complex<double>;
+	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
+
+	multi::array<complex, 1> arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
+
+	// g++ -std=20 needs the transformation (lambda) to be noexcept
+	auto&& conjd_arr = arr.element_transformed([](auto const& cee) noexcept /* a const return would be unmovable */ { return std::conj(cee); });
+	BOOST_TEST( conjd_arr[0] == std::conj(arr[0]) );
+	BOOST_TEST( conjd_arr[1] == std::conj(arr[1]) );
+
+	// conjd_arr[0] = 5.0 + 4.0*I;  // this compiles, but classes the implement operator= naively can be misleading here
+	BOOST_TEST( conjd_arr[0] == 1.0 - 2.0*I );
+}
+
 BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_proxy) {
 	using complex = std::complex<double>;
 	auto const I  = complex{ 0.0, 1.0 };  // NOLINT(readability-identifier-length) I imaginary unit
@@ -144,11 +148,11 @@ BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_proxy) {
 	multi::array<complex, 1> const arr = { 1.0 + 2.0 * I, 3.0 + 4.0 * I };
 
 	auto const& conj_arr = arr.element_transformed(Conj);
-	BOOST_REQUIRE( std::conj(arr[0]) == conj_arr[0] );
-	BOOST_REQUIRE( std::conj(arr[1]) == conj_arr[1] );
+	BOOST_TEST( std::conj(arr[0]) == conj_arr[0] );
+	BOOST_TEST( std::conj(arr[1]) == conj_arr[1] );
 
 	//  Ac[0] = 5. + 4.*I;  // not allowed, compile error, Ac is const
-	BOOST_REQUIRE( conj_arr[0] == 1.0 - 2.0*I );
+	BOOST_TEST( conj_arr[0] == 1.0 - 2.0*I );
 }
 
 BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_mutable_proxy) {
@@ -159,12 +163,12 @@ BOOST_AUTO_TEST_CASE(element_transformed_1D_conj_using_mutable_proxy) {
 
 	auto&& conj_arr = arr.element_transformed(Conj);  // NOLINT(readability-const-return-type) to disable assignment
 
-	BOOST_REQUIRE( std::conj(arr[0]) == conj_arr[0] );
-	BOOST_REQUIRE( std::conj(arr[1]) == conj_arr[1] );
+	BOOST_TEST( std::conj(arr[0]) == conj_arr[0] );
+	BOOST_TEST( std::conj(arr[1]) == conj_arr[1] );
 
 	conj_arr[0] = 5.0 + 4.0 * I;
-	BOOST_REQUIRE( conj_arr[0] == 5.0 + 4.0*I );
-	BOOST_REQUIRE(  arr[0] == 5.0 - 4.0*I );
+	BOOST_TEST( conj_arr[0] == 5.0 + 4.0*I );
+	BOOST_TEST(  arr[0] == 5.0 - 4.0*I );
 }
 
 BOOST_AUTO_TEST_CASE(transform_ptr_single_value) {
@@ -177,7 +181,7 @@ BOOST_AUTO_TEST_CASE(transform_ptr_single_value) {
 	constexpr auto conj_ro = [](auto const& zee) noexcept { return std::conj(zee); };  // g++ -std=20 needs the transformation (lambda) to be noexcept
 
 	multi::transform_ptr<complex, decltype(conj_ro), complex*> const conjd_ceeP{ &cee, conj_ro };
-	BOOST_REQUIRE( *conjd_ceeP == std::conj(1.0 + 2.0*I) );
+	BOOST_TEST( *conjd_ceeP == std::conj(1.0 + 2.0*I) );
 }
 
 BOOST_AUTO_TEST_CASE(transform_ptr_1D_array) {
@@ -190,8 +194,8 @@ BOOST_AUTO_TEST_CASE(transform_ptr_1D_array) {
 	constexpr auto conj_ro = [](auto const& zee) noexcept { return std::conj(zee); };  // g++ -std=20 needs the transformation (lambda) to be noexcept
 
 	auto const& conjd_arr = arr.element_transformed(conj_ro);
-	BOOST_REQUIRE( conjd_arr[0] == conj_ro(arr[0]) );
-	BOOST_REQUIRE( conjd_arr[1] == conj_ro(arr[1]) );
+	BOOST_TEST( conjd_arr[0] == conj_ro(arr[0]) );
+	BOOST_TEST( conjd_arr[1] == conj_ro(arr[1]) );
 
 	//  Ac[0] = 5. + 4.i;  // doesn't compile thanks to the `auto const` in the `conj` def
 }
@@ -206,10 +210,10 @@ BOOST_AUTO_TEST_CASE(arthur_odwyer_array_transform_int) {
 	auto&&             ref = arr.element_transformed(&S::a);
 	ref[0]                 = 990;
 
-	BOOST_REQUIRE( arr[0].a == 990 );
+	BOOST_TEST( arr[0].a == 990 );
 
 	auto const& cref = arr.element_transformed(&S::a);
-	BOOST_REQUIRE( cref[0] == 990 );
+	BOOST_TEST( cref[0] == 990 );
 	//  cr[0] = 99.;  // compile error "assignment of read-only location"
 }
 
@@ -225,11 +229,11 @@ BOOST_AUTO_TEST_CASE(arthur_odwyer_array_transform_int_array) {
 
 	ref[0][1] = 990;
 
-	BOOST_REQUIRE( ref[0][1] == 990 );
-	BOOST_REQUIRE( vec[0].a[1] == 990 );
+	BOOST_TEST( ref[0][1] == 990 );
+	BOOST_TEST( vec[0].a[1] == 990 );
 
 	auto const& cref = vec.element_transformed(&S::a);
-	BOOST_REQUIRE( cref[0][1] == 990 );
+	BOOST_TEST( cref[0][1] == 990 );
 	//  cref[0][1] = 990;  // compile error "assignment of read-only location"
 }
 
@@ -242,23 +246,23 @@ BOOST_AUTO_TEST_CASE(indirect_transformed) {
 
 	auto&& indirect_v = arr.element_transformed([&vec](index_t idx) noexcept -> int& { return vec[idx]; });
 
-	BOOST_REQUIRE(  indirect_v[1] ==  vec[3] );
-	BOOST_REQUIRE( &indirect_v[1] == &vec[3] );
+	BOOST_TEST(  indirect_v[1] ==  vec[3] );
+	BOOST_TEST( &indirect_v[1] == &vec[3] );
 
 	indirect_v[1] = 990;
-	BOOST_REQUIRE(  vec[3] ==  990 );
+	BOOST_TEST(  vec[3] ==  990 );
 
 	//  for(auto&& elem : indirect_v) {elem = 88.;}
 	//  std::fill(indirect_v.begin(), indirect_v.end(), 88.0);
 
 #if !defined(_MSC_VER)
 	indirect_v.fill(880);
-	BOOST_REQUIRE(  vec[3] ==  880 );
+	BOOST_TEST(  vec[3] ==  880 );
 
 	auto const& const_indirect_v = indirect_v;
 	(void)const_indirect_v;
 	// const_indirect_v[1] = 9990;  // does not compile, good!
-	BOOST_REQUIRE(const_indirect_v[3] ==  880);
+	BOOST_TEST(const_indirect_v[3] ==  880);
 #endif
 }
 
@@ -279,14 +283,16 @@ BOOST_AUTO_TEST_CASE(indirect_transformed_carray) {
 	// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays)
 	auto&& indirect_v = arr.element_transformed([&carr](index_t idx) noexcept -> int(&)[3] { return carr[idx]; });
 
-	BOOST_REQUIRE( &indirect_v[1][2] ==  &carr[3][2] );
-	BOOST_REQUIRE(  indirect_v[1][2] ==  320 );
+	BOOST_TEST( &indirect_v[1][2] ==  &carr[3][2] );
+	BOOST_TEST(  indirect_v[1][2] ==  320 );
 
 	indirect_v[1][2] = 111110;
 	BOOST_TEST   (  indirect_v[1][2] ==  111110 );
 
 	auto const& const_indirect_v = indirect_v;
 
-	BOOST_REQUIRE(  const_indirect_v[1][2] ==  111110 );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic) testing legacy type
-	                                                //  const_indirect_v[1][2] = 999.;  // doesn't compile, good!
+	BOOST_TEST(  const_indirect_v[1][2] ==  111110 );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic) testing legacy type
+	//  const_indirect_v[1][2] = 999.;  // doesn't compile, good!
 }
+
+return boost::report_errors();}
