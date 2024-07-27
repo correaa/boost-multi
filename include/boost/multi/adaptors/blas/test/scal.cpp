49ec12571d1e14f5ceae46c863c6bf7fabfb52eb
Alfredo Correa <alfredo.correa@gmail.com>
2024-07-27 00:43:37 -0700

    more lwt

--- include/boost/multi/adaptors/blas/test/scal.cpp
+++ include/boost/multi/adaptors/blas/test/scal.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/test/included/unit_test.hpp>
+// #include <boost/test/included/unit_test.hpp>
 
 #include <boost/multi/adaptors/blas/scal.hpp>
 
@@ -13,16 +13,22 @@
 namespace multi = boost::multi;
 namespace blas  = multi::blas;
 
+#include <boost/core/lightweight_test.hpp>
+#define BOOST_AUTO_TEST_CASE(CasenamE) [[maybe_unused]] void* CasenamE;
+#define BOOST_REQUIRE_CLOSE(X, Y, ToL) BOOST_TEST( std::abs( (X) - (Y) ) < (ToL) )
+// #define BOOST_REQUIRE_SMALL(X, ToL) BOOST_TEST( std::abs( X ) < (ToL) )
+
+int main() {
 BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_n) {
 	multi::array<double, 2> arr = {
 		{ 1.0,  2.0,  3.0,  4.0 },
 		{ 5.0,  6.0,  7.0,  8.0 },
 		{ 9.0, 10.0, 11.0, 12.0 },
 	};
-	BOOST_REQUIRE( (arr[0][2] == 3.0) && (arr[2][2] == 11.0) );
+	BOOST_TEST( (arr[0][2] == 3.0) && (arr[2][2] == 11.0) );
 
 	blas::scal_n(2.0, arr[2].begin(), arr[2].size());
-	BOOST_REQUIRE( arr[0][2] == 3. && arr[2][2] == 11.0*2.0 );
+	BOOST_TEST( arr[0][2] == 3. && arr[2][2] == 11.0*2.0 );
 }
 
 BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_it) {
@@ -31,12 +37,12 @@ BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_it) {
 		{ 5.0,  6.0,  7.0,  8.0 },
 		{ 9.0, 10.0, 11.0, 12.0 },
 	};
-	BOOST_REQUIRE( arr[0][2] ==  3.0 );
-	BOOST_REQUIRE( arr[2][2] == 11.0 );
+	BOOST_TEST( arr[0][2] ==  3.0 );
+	BOOST_TEST( arr[2][2] == 11.0 );
 
 	blas::scal(2.0, arr[2].begin(), arr[2].end());
-	BOOST_REQUIRE( arr[0][2] == 3.0 );
-	BOOST_REQUIRE(arr[2][2] == 11.0*2.0 );
+	BOOST_TEST( arr[0][2] == 3.0 );
+	BOOST_TEST(arr[2][2] == 11.0*2.0 );
 }
 
 BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_real) {
@@ -45,17 +51,17 @@ BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_real) {
 		{ 5.0,  6.0,  7.0,  8.0 },
 		{ 9.0, 10.0, 11.0, 12.0 },
 	};
-	BOOST_REQUIRE( arr[0][2] ==  3.0 );
-	BOOST_REQUIRE( arr[2][2] == 11.0 );
+	BOOST_TEST( arr[0][2] ==  3.0 );
+	BOOST_TEST( arr[2][2] == 11.0 );
 
-	BOOST_REQUIRE(  blas::scal(1.0, arr[2]) ==  arr[2] );
-	BOOST_REQUIRE( &blas::scal(1.0, arr[2]) == &arr[2] );
-	BOOST_REQUIRE( +blas::scal(1.0, arr[2]) ==  arr[2] );
+	BOOST_TEST(  blas::scal(1.0, arr[2]) ==  arr[2] );
+	BOOST_TEST( &blas::scal(1.0, arr[2]) == &arr[2] );
+	BOOST_TEST( +blas::scal(1.0, arr[2]) ==  arr[2] );
 
 	blas::scal(2.0, arr[2]);
-	BOOST_REQUIRE( arr[0][2] == 3.0 && arr[2][2] == 11.0*2.0 );
+	BOOST_TEST( arr[0][2] == 3.0 && arr[2][2] == 11.0*2.0 );
 
-	BOOST_REQUIRE( &blas::scal(1.0, arr[2]) == &arr[2] );
+	BOOST_TEST( &blas::scal(1.0, arr[2]) == &arr[2] );
 }
 
 BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_real_2D) {
@@ -64,13 +70,13 @@ BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_real_2D) {
 		{ 5.0,  6.0,  7.0,  8.0 },
 		{ 9.0, 10.0, 11.0, 12.0 },
 	};
-	BOOST_REQUIRE( arr[0][2] ==  3.0 );
-	BOOST_REQUIRE( arr[2][2] == 11.0 );
+	BOOST_TEST( arr[0][2] ==  3.0 );
+	BOOST_TEST( arr[2][2] == 11.0 );
 
 	blas::scal(2.0, arr.elements());
 
-	BOOST_REQUIRE( arr[0][2] ==  6.0 );
-	BOOST_REQUIRE( arr[2][2] == 22.0 );
+	BOOST_TEST( arr[0][2] ==  6.0 );
+	BOOST_TEST( arr[2][2] == 22.0 );
 }
 
 BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_complex_2D) {
@@ -81,11 +87,12 @@ BOOST_AUTO_TEST_CASE(multi_adaptors_blas_test_scal_complex_2D) {
 		{ 5.0 + 0.0 * I,  6.0 + 0.0 * I,  7.0 + 0.0 * I,  8.0 + 0.0 * I },
 		{ 9.0 + 0.0 * I, 10.0 + 0.0 * I, 11.0 + 0.0 * I, 12.0 + 0.0 * I },
 	};
-	BOOST_REQUIRE( arr[0][2] ==  3.0 );
-	BOOST_REQUIRE( arr[2][2] == 11.0 );
+	BOOST_TEST( arr[0][2] ==  3.0 );
+	BOOST_TEST( arr[2][2] == 11.0 );
 
 	blas::scal(2.0, arr.elements());
 
-	BOOST_REQUIRE( arr[0][2] ==  6.0 );
-	BOOST_REQUIRE( arr[2][2] == 22.0 );
+	BOOST_TEST( arr[0][2] ==  6.0 );
+	BOOST_TEST( arr[2][2] == 22.0 );
 }
+return boost::report_errors();}
