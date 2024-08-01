40b282bb24116dea3c0b6a37ed21fa7548af8124
Alfredo Correa <alfredo.correa@gmail.com>
2024-08-01 03:06:58 -0700

    more tidy

--- test/iterator.cpp
+++ test/iterator.cpp
@@ -19,9 +19,9 @@ namespace multi = boost::multi;
 template<class Array> auto take(Array&& array) -> auto& { return std::forward<Array>(array)[0]; }
 
 #include <boost/core/lightweight_test.hpp>
-#define BOOST_AUTO_TEST_CASE(CasenamE) [[maybe_unused]] void* CasenamE;
+#define BOOST_AUTO_TEST_CASE(CasenamE) [[maybe_unused]] void* (CasenamE);
 
-int main() {
+auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugprone-exception-escape)
 	BOOST_AUTO_TEST_CASE(iterator_1d) {
 		{
 			multi::array<double, 1> arr(multi::extensions_t<1>{multi::iextension{100}}, 99.0);
