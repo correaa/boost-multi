4573b514505dfe317af70386a9106bfd7fcccb6c
Alfredo Correa <alfredo.correa@gmail.com>
2024-08-03 19:56:06 -0700

    add ptr overflow test with diagonal

--- test/diagonal.cpp
+++ test/diagonal.cpp
@@ -61,6 +61,17 @@ BOOST_AUTO_TEST_CASE(trace_test) {
 
 BOOST_AUTO_TEST_CASE(broadcasted) {
 	multi::array<int, 2> const arr = {
+		{0, 1,  2},
+		{4, 5,  6},
+		{8, 9, 10},
+	};
+
+	BOOST_TEST( arr.diagonal().begin() != arr.diagonal().end() ); 
+	BOOST_TEST( arr.diagonal().end() - arr.diagonal().begin() == 3 );
+}
+
+BOOST_AUTO_TEST_CASE(broadcasted) {
+	multi::array<int, 2> const arr = {
 		{0, 1,  2,  3},
 		{4, 5,  6,  7},
 		{8, 9, 10, 11},
