57ee27a183c3f4cab97e25a4f504b93f85ccae9d
Alfredo Correa <alfredo.correa@gmail.com>
2024-08-25 00:11:45 -0700

    fix warns

--- include/boost/multi/adaptors/lapack/test/potrf.cpp
+++ include/boost/multi/adaptors/lapack/test/potrf.cpp
@@ -188,9 +188,10 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		print(C, "recover");      // NOLINT(fuchsia-default-arguments-calls)
 
 		for(auto i = 0; i != 4; ++i) {
-			for(auto j = i; j != 4; ++j) {  // NOLINT(altera-unroll-loops,altera-id-dependent-backward-branch)  // only compare upper part of the reference array (the other half is garbage)
+			// only compare upper part of the reference array (the other half is garbage)
+			for(auto j = i; j != 4; ++j) {  // NOLINT(altera-unroll-loops,altera-id-dependent-backward-branch)
 				BOOST_TEST_CLOSE(real(A_gold[i][j]), real(C[i][j]), 0.0000001);
-				BOOST_TEST_CLOSE(imag(A_gold[i][j]), imag(C[i][j]), 0.0000001);  // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+				BOOST_TEST_CLOSE(imag(A_gold[i][j]), imag(C[i][j]), 0.0000001);  // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay,readability-simplify-boolean-expr) bug in clang-tidy 14
 			}
 		}
 	}
