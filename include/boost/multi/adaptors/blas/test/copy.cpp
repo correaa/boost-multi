10ec16d6c7d52cee7b4ab7d5ae869959ead39402
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-03 13:10:53 -0700

    add benchmark headers

--- include/boost/multi/adaptors/blas/test/copy.cpp
+++ include/boost/multi/adaptors/blas/test/copy.cpp
@@ -5,14 +5,14 @@
 #include <boost/multi/adaptors/blas/copy.hpp>  // for copy, copy_n
 #include <boost/multi/array.hpp>               // for array, layout_t, subarray
 
-#if defined(NDEBUG) && !defined(__NVCC__) && !(defined(__clang__) && defined(__CUDA__))
-	#include <algorithm>  // for transform
-	#include <chrono>     // for duration, high_resolution...
+#if defined(NDEBUG) && !defined(__CUDA__)
+	#include <algorithm>  // for transform  // IWYU pragma: keep  // NOLINT(misc-include-cleaner)
+	#include <chrono>     // for duration, high_resolution...  // IWYU pragma: keep  // NOLINT(misc-include-cleaner)
 	#if __has_include(<execution>)
-		#include <execution>  // for execution_policy
+		#include <execution>  // for execution_policy  // IWYU pragma: keep  // NOLINT(misc-include-cleaner)
 	#endif
-	#include <functional>  // for invoke  // IWYU pragma: keep
-	#include <iostream>    // for basic_ostream, endl, cout
+	#include <functional>  // for invoke  // IWYU pragma: keep  // NOLINT(misc-include-cleaner)
+	#include <iostream>    // for basic_ostream, endl, cout  // IWYU pragma: keep  // NOLINT(misc-include-cleaner)
 #endif
 
 #include <complex>   // for operator*, operator+
@@ -96,7 +96,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		BOOST_TEST( arr[0][2] == 3.0 + 5.0*I );
 	}
 
-#if defined(NDEBUG)
+#if defined(NDEBUG) && !defined(__CUDA__)
 	/* transform copy */ {
 		multi::array<double, 2> A2D({10000, 10000}, 55.5);
 		auto&&                  A2D_block = A2D({1000, 9000}, {1000, 5000});
@@ -125,16 +125,16 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 		BOOST_TEST( A2D_block == B2D_block );
 
-	#if defined(__cpp_lib_execution) && !defined(__NVCC__) && !(defined(__clang__) && defined(__CUDA__)) && !defined(__NVCOMPILER)
-		std::cout << "std::transform par BLAS\n"
-				  << std::invoke([&, start_time = high_resolution_clock::now()] {
-						 std::transform(std::execution::par, A2D_block.begin(), A2D_block.end(), B2D_block.begin(), [](auto& row) { return multi::blas::copy(row); });
-						 return duration<double>{high_resolution_clock::now() - start_time};
-					 }).count()
-				  << '\n';
+// //  #if defined(__cpp_lib_execution) && !defined(__NVCC__) && !(defined(__clang__) && defined(__CUDA__)) && !defined(__NVCOMPILER)
+//      std::cout << "std::transform par BLAS\n"
+//                << std::invoke([&, start_time = high_resolution_clock::now()] {
+//                       std::transform(std::execution::par, A2D_block.begin(), A2D_block.end(), B2D_block.begin(), [](auto& row) { return multi::blas::copy(row); });
+//                       return duration<double>{high_resolution_clock::now() - start_time};
+//                   }).count()
+//                << '\n';
 
-		BOOST_TEST( A2D_block == B2D_block );
-	#endif
+//      BOOST_TEST( A2D_block == B2D_block );
+// //  #endif
 
 		std::cout << "std::copy\n"
 				  << std::invoke([&, start_time = high_resolution_clock::now()] {
@@ -145,7 +145,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 		BOOST_TEST( A2D_block == B2D_block );
 
-	#if defined(__cpp_lib_execution) && !defined(__NVCC__) && !(defined(__clang__) && defined(__CUDA__)) && !defined(__NVCOMPILER)
+//  #if defined(__cpp_lib_execution) && !defined(__NVCC__) && !(defined(__clang__) && defined(__CUDA__)) && !defined(__NVCOMPILER)
 		std::cout << "std::copy par\n"
 				  << std::invoke([&, start_time = high_resolution_clock::now()] {
 						 std::copy(std::execution::par, A2D_block.begin(), A2D_block.end(), B2D_block.begin());
@@ -176,7 +176,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 				  << '\n';
 
 		BOOST_TEST( A2D_block == B2D_block );
-	#endif
+//  #endif
 
 		std::cout << "Multi element assignment\n"
 				  << std::invoke([&, start_time = high_resolution_clock::now()] {
