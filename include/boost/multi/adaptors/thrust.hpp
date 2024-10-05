ed69b0e98201e7c2d9e3abb550c3c2555237c50c
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-05 00:33:34 -0700

    fix namespace path in iterator_system detection

--- include/boost/multi/adaptors/thrust.hpp
+++ include/boost/multi/adaptors/thrust.hpp
@@ -160,14 +160,14 @@ namespace thrust {
 
 // template<class It> struct iterator_system;  // not needed in cuda 12.0, doesn't work on cuda 12.5
 
-template<class T, boost::multi::dimensionality_type D, class Pointer, bool IsConst>
-struct iterator_system<boost::multi::array_iterator<T, D, Pointer, IsConst>>{
+template<class T, ::boost::multi::dimensionality_type D, class Pointer, bool IsConst>
+struct iterator_system<::boost::multi::array_iterator<T, D, Pointer, IsConst>>{
 	using type = typename ::thrust::iterator_system<typename boost::multi::array_iterator<T, D, Pointer, IsConst>::element_ptr>::type;
 };
 
 template<typename Pointer, class LayoutType>
-struct iterator_system<boost::multi::elements_iterator_t<Pointer, LayoutType>> {  // TODO(correaa) might need changes for IsConst templating
-	using type = typename ::thrust::iterator_system<typename boost::multi::elements_iterator_t<Pointer, LayoutType>::pointer>::type;
+struct iterator_system<::boost::multi::elements_iterator_t<Pointer, LayoutType>> {  // TODO(correaa) might need changes for IsConst templating
+	using type = typename ::thrust::iterator_system<typename ::boost::multi::elements_iterator_t<Pointer, LayoutType>::pointer>::type;
 };
 
 // namespace detail {
