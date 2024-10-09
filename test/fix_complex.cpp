327312d6fddaf33d3b87b7fa0fa0b88a8bdb1ea3
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-09 16:03:03 -0700

    supress mac warnings

--- test/fix_complex.cpp
+++ test/fix_complex.cpp
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE(pmr_double_uninitialized) {
 		BOOST_TEST         ( &Aarr[0][0] == buffer.data() );
 		BOOST_TEST( std::abs( Aarr[0][0] - 4.0 ) < 1E-6);
 	#elif defined(_LIBCPP_VERSION)
-		BOOST_TEST         ( &Aarr[0][0] == buffer.data() + (buffer.size() - 4) );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		BOOST_TEST         ( &Aarr[0][0] == &buffer[buffer.size() - 4] );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 		BOOST_TEST( std::abs( Aarr[0][0] - 996.0 ) < 1E-6 );
 	#endif
 	}
@@ -81,7 +81,7 @@ BOOST_AUTO_TEST_CASE(pmr_double_uninitialized) {
 		BOOST_TEST( std::abs( buffer[buffer.size()-3] - 0.0 ) < 1E-6 );
 		BOOST_TEST( std::abs( buffer[buffer.size()-5] - 11.0 ) < 1E-6 );
 
-		BOOST_TEST( &Aarr[0][0] == buffer.data() + (buffer.size() - 4) );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+		BOOST_TEST( &Aarr[0][0] == &buffer[buffer.size() - 4] );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 	#endif
 
 		BOOST_TEST( std::abs( Aarr[0][0] - 0.0 ) < 1E-6);
@@ -137,9 +137,9 @@ BOOST_AUTO_TEST_CASE(pmr_complex_initialized_4) {
 	BOOST_TEST( std::abs( buffer[1] - 5.0 ) < 1E-6 );
 
 	#if defined(__GLIBCXX__)
-	BOOST_TEST( static_cast<void*>(buffer.data()) == static_cast<void*>(&Aarr[0][0]) );
+	BOOST_TEST( static_cast<void*>(&buffer[0]) == static_cast<void*>(&Aarr[0][0]) );
 	#elif defined(_LIBCPP_VERSION)
-	BOOST_TEST( static_cast<void*>(buffer.data() + 4) == static_cast<void*>(&Aarr[0][0]) );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+	BOOST_TEST( static_cast<void*>(&buffer[4]) == static_cast<void*>(&Aarr[0][0]) );  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 	#endif
 }
 
