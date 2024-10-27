e2db54870d5b036525a8faf1c0cff25efb6f0c32
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-27 02:31:52 -0700

    print configuration message

--- test/redux.cpp
+++ test/redux.cpp
@@ -208,7 +208,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 			return ret;
 		}();
 
-		for(multi::array<double, 2>::index ix = 0; ix != nx; ++ix) {
+		for(multi::array<double, 2>::index ix = 0; ix != nx; ++ix) {  // NOLINT(altera-unroll-loops)
 			BOOST_TEST( std::abs( accumulator[ix] - static_cast<double>(ix) * ny * (ny - 1.0) / 2.0 ) < 1.0e-8);
 		}
 	}
