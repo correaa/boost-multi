e8874685c3b0f2680e00d15cff5dcc6d44b9f3ba
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-26 22:57:50 -0700

    fixes dot

--- test/redux.cpp
+++ test/redux.cpp
@@ -4,20 +4,19 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #if defined(__GNUC__)
-#pragma GCC diagnostic ignored "-Wdouble-promotion"
+	#pragma GCC diagnostic ignored "-Wdouble-promotion"
 #endif
 
-#include <boost/multi/array.hpp>  // for array, implicit_cast, explicit_cast
-
 #include <boost/multi/adaptors/blas.hpp>  // IWYU pragma: keep
+#include <boost/multi/array.hpp>          // for array, implicit_cast, explicit_cast
 
 #include <boost/core/lightweight_test.hpp>
 
 #include <algorithm>  // IWYU pragma: keep
-#include <chrono>  // NOLINT(build/c++11)
-#include <cmath>  // IWYU pragma: keep
+#include <chrono>     // NOLINT(build/c++11)
+#include <cmath>      // IWYU pragma: keep
 #include <iostream>
-#include <numeric>  // IWYU pragma: keep
+#include <numeric>    // IWYU pragma: keep
 #include <string>
 #include <utility>
 
@@ -38,17 +37,17 @@ namespace multi = boost::multi;
 class watch {
 	std::chrono::time_point<std::chrono::high_resolution_clock> start_ = std::chrono::high_resolution_clock::now();
 
-	std::string                                                 msg_;
+	std::string msg_;
 
  public:
 	explicit watch(std::string msg) : msg_(std::move(msg)) {}
 	~watch() {
 		std::cerr << msg_ << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_).count() << " ms\n";
 	}
-	watch(watch const&) = delete;
-	watch(watch&&)      = delete;
+	watch(watch const&)          = delete;
+	watch(watch&&)               = delete;
 	auto operator=(watch const&) = delete;
-	auto operator=(watch&&) = delete;
+	auto operator=(watch&&)      = delete;
 	//  non-default destructor but does not define a copy constructor, a copy assignment operator, a move constructor or a move assignment operator
 };
 
@@ -174,7 +173,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		}
 	}
 
-#if (!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502))
+	#if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502))
 	{
 		auto const accumulator = [&] {
 			watch _("reduce transform forward");
@@ -194,7 +193,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 			BOOST_TEST( std::abs( accumulator[ix] - static_cast<double>(ix) * ny * (ny - 1.0) / 2.0 ) < 1.0e-8);
 		}
 	}
-#endif
+	#endif
 
 	{
 		auto const accumulator = [&] {
@@ -212,7 +211,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		}
 	}
 
-#if (!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502))
+	#if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502))
 	{
 		auto const accumulator = [&] {
 			watch const             _("transform reduce element zero");
@@ -228,7 +227,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 			BOOST_TEST( std::abs( accumulator[ix] - static_cast<double>(ix) * ny * (ny - 1.0) / 2.0 ) < 1.0e-8);
 		}
 	}
-#endif
+	#endif
 
 	{
 		auto const accumulator = [&](auto&& init) {
@@ -245,7 +244,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		}
 	}
 
-#if (!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502))
+	#if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502))
 	{
 		auto const accumulator = [&](auto&& init) {
 			watch const _("> transform reduce");
@@ -260,18 +259,19 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 			BOOST_TEST( std::abs( accumulator[ix] - static_cast<double>(ix) * ny * (ny - 1.0) / 2.0 ) < 1.0e-8);
 		}
 	}
-#endif
+	#endif
 
-#if (defined __has_include && __has_include(<execution>))
-#if !defined(__NVCC__) && !defined(__NVCOMPILER)
-#if (!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502)) && (!defined(__clang_major__) || (__clang_major__ > 7)) && !defined(_LIBCPP_VERSION)
+	#if(defined __has_include && __has_include(<execution>))
+		#if !defined(__NVCC__) && !defined(__NVCOMPILER)
+			#if(!defined(__GLIBCXX__) || (__GLIBCXX__ >= 20190502)) && !(defined(__clang__) && defined(__CUDA__)) && (!defined(__clang_major__) || (__clang_major__ > 7)) && !defined(_LIBCPP_VERSION) && !defined(__apple_build_version__)
 	{
 		auto const accumulator = [&] {
-			watch const             _("transform reduce[unseq]");
+			watch const _("transform reduce[unseq]");
+
 			multi::array<double, 1> ret(K2D.extension(), 0.0);
 			std::transform(
 				K2D.begin(), K2D.end(), ret.begin(), ret.begin(),
-				[](auto const& row, auto rete) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(rete)); }
+				[](auto const& row, auto rete) {return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(rete)); }
 			);
 			return ret;
 		}();
@@ -283,7 +283,8 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	{
 		auto const accumulator = [&] {
-			watch const             _("transform reduce[par]");
+			watch const _("transform reduce[par]");
+
 			multi::array<double, 1> ret(K2D.extension(), 0.0);
 			std::transform(
 				K2D.begin(), K2D.end(), ret.begin(), ret.begin(),
@@ -299,7 +300,8 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	{
 		auto const accumulator = [&] {
-			watch const             _("transform reduce[par_unseq]");
+			watch const _("transform reduce[par_unseq]");
+
 			multi::array<double, 1> ret(K2D.extension(), 0.0);
 			std::transform(
 				K2D.begin(), K2D.end(), ret.begin(), ret.begin(),
@@ -315,13 +317,10 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	{
 		auto const accumulator = [&]() {
-			watch const             _("transform[par] reduce");
+			watch const _("transform[par] reduce");
+
 			multi::array<double, 1> ret(K2D.extension(), 0.0);
-			std::transform(std::execution::par,
-				K2D.begin(), K2D.end(),
-				ret.begin(), ret.begin(),
-				[](auto const& row, auto rete) { return std::reduce(row.begin(), row.end(), std::move(rete)); }
-			);
+			std::transform(std::execution::par, K2D.begin(), K2D.end(), ret.begin(), ret.begin(), [](auto const& row, auto rete) { return std::reduce(row.begin(), row.end(), std::move(rete)); });
 			return ret;
 		}();
 
@@ -332,12 +331,8 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	{
 		auto const accumulator = [&](auto ret) {
-			watch _("* transform[par] reduce[unseq]");
-			std::transform(std::execution::par,
-				K2D.begin(), K2D.end(),
-				ret.begin(), ret.begin(),
-				[](auto const& row, auto rete) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(rete)); }
-			);
+			watch const _("* transform[par] reduce[unseq]");
+			std::transform(std::execution::par, K2D.begin(), K2D.end(), ret.begin(), ret.begin(), [](auto const& row, auto rete) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(rete)); });
 			return ret;
 		}(multi::array<double, 1>(K2D.extension(), 0.0));
 
@@ -349,12 +344,8 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 	{
 		multi::array<double, 1> accumulator(K2D.extension(), 0.0);
 		[&](auto acc_begin) {
-			watch _("transform[par] reduce[unseq] iterator");
-			return std::transform(std::execution::par,
-				K2D.begin(), K2D.end(),
-				acc_begin, acc_begin,
-				[](auto const& row, auto rete) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(rete)); }
-			);
+			watch const _("transform[par] reduce[unseq] iterator");
+			return std::transform(std::execution::par, K2D.begin(), K2D.end(), acc_begin, acc_begin, [](auto const& row, auto rete) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(rete)); });
 		}(accumulator.begin());
 
 		for(multi::array<double, 2>::index ix = 0; ix != nx; ++ix) {
@@ -364,14 +355,10 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	{
 		auto const accumulator = [&](auto zero_elem) {
-			watch _("transform[par] reduce[unseq] element zero");
+			watch const _("transform[par] reduce[unseq] element zero");
 
 			multi::array<double, 1> ret(K2D.extension());
-			std::transform(std::execution::par,
-				K2D.begin(), K2D.end(),
-				ret.begin(),
-				[zz = std::move(zero_elem)](auto const& row) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(zz)); }
-			);
+			std::transform(std::execution::par, K2D.begin(), K2D.end(), ret.begin(), [zz = std::move(zero_elem)](auto const& row) { return std::reduce(std::execution::unseq, row.begin(), row.end(), std::move(zz)); });
 			return ret;
 		}(0.0);
 
@@ -379,14 +366,14 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 			BOOST_TEST( std::abs( accumulator[ix] - static_cast<double>(ix) * ny * (ny - 1.0) / 2.0 ) < 1.0e-8);
 		}
 	}
-#endif
-#endif
-#endif
+			#endif
+		#endif
+	#endif
 #endif  // __NVCC__
 
 	{
 		auto const accumulator = [&](auto&& init) {
-			watch _("blas gemv");
+			watch const             _("blas gemv");
 			multi::array<double, 1> ones({init.extension()}, 1.0);
 			multi::blas::gemv_n(1.0, K2D.begin(), K2D.size(), ones.begin(), 0.0, init.begin());
 			return std::forward<decltype(init)>(init);
@@ -399,7 +386,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	{
 		auto const accumulator = [&](auto&& init) {
-			watch _("blas gemv smart");
+			watch const _("blas gemv smart");
 			multi::blas::gemv_n(1.0, K2D.begin(), K2D.size(), init[0].begin(), 0.0, init[1].begin());
 			return +init[1];
 		}(multi::array<double, 2>({2, K2D.extension()}, 1.0));
