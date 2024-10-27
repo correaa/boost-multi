9b28c545d4a4756155cf6e62d89f9c763a943660
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-27 14:40:42 -0700

    check for nan in cdotu test

--- include/boost/multi/adaptors/blas/core.hpp
+++ include/boost/multi/adaptors/blas/core.hpp
@@ -294,11 +294,11 @@ template<class XP, class X = typename std::pointer_traits<XP>::element_type, cla
 		std::clog << "using dot void\n";
 		std::array<std::complex<float>, 3> const v1 = {std::complex<float>{1.0F, 2.0F}, std::complex<float>{3.0F,  4.0F}, std::complex<float>{ 5.0F,  6.0F}};
 		std::array<std::complex<float>, 3> const v2 = {std::complex<float>{7.0F, 8.0F}, std::complex<float>{9.0F, 10.0F}, std::complex<float>{11.0F, 12.0F}};
-		Complex_float rr;
+		Complex_float rr{-1.0F, -2.0F};
 		BLAS(cdotu)(&rr, 3, v1.data(), 1, v2.data(), 1);
 		std::clog << "dot is (" << rr.real << ", " << rr.imag << ") should be " << v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] << "\n";
-		if( std::abs(rr.imag - std::imag(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error("[imag] cdotu should be configured as non-void returing"); }
-		if( std::abs(rr.real - std::real(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error("[real] cdotu should be configured as non-void returing"); }
+		if( std::isnan(rr.imag) || std::abs(rr.imag - std::imag(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error("[imag] cdotu should be configured as non-void returing"); }
+		if( std::isnan(rr.real) || std::abs(rr.real - std::real(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error("[real] cdotu should be configured as non-void returing"); }
 		return true;
 	}();
 	BLAS(cdotu)(reinterpret_cast<Complex_float *>(rp), n, reinterpret_cast<c const*>(static_cast<X*>(xp)), incx, reinterpret_cast<c const*>(static_cast<Y*>(yp)), incy);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
@@ -315,10 +315,11 @@ template<class XP, class X = typename std::pointer_traits<XP>::element_type, cla
 		std::clog << "using dot non void\n";
 		std::array<std::complex<float>, 3> const v1 = {std::complex<float>{1.0F, 2.0F}, std::complex<float>{3.0F,  4.0F}, std::complex<float>{ 5.0F,  6.0F}};
 		std::array<std::complex<float>, 3> const v2 = {std::complex<float>{7.0F, 8.0F}, std::complex<float>{9.0F, 10.0F}, std::complex<float>{11.0F, 12.0F}};
-		auto const rr = BLAS(cdotu)(3, v1.data(), 1, v2.data(), 1);
+		Complex_float rr{-1.0F, -2.0F};
+		rr = BLAS(cdotu)(3, v1.data(), 1, v2.data(), 1);
 		std::clog << "dot is (" << rr.real << ", " << rr.imag << ") should be " << v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] << "\n";
-		if( std::abs(rr.imag - std::imag(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error{"[imag] cdotu should be configured as void"}; }
-		if( std::abs(rr.real - std::real(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error{"[real] cdotu should be configured as void"}; }
+		if( std::isnan(rr.imag) || std::abs(rr.imag - std::imag(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error{"[imag] cdotu should be configured as void"}; }
+		if( std::isnan(rr.imag) || std::abs(rr.real - std::real(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) > 1.0e-8 ) { throw std::logic_error{"[real] cdotu should be configured as void"}; }
 		return true;
 	}();
 	auto const rr = BLAS(cdotu)(                                      n, reinterpret_cast<c const*>(static_cast<X*>(x)), incx, reinterpret_cast<c const*>(static_cast<Y*>(y)), incy); std::memcpy(reinterpret_cast<std::array<float , 2>*>(static_cast<R*>(rp))->data(), &rr, sizeof(rr)); static_assert(sizeof(rr)==sizeof(*rp));
