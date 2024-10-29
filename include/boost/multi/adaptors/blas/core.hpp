03c64fdb8df50dd6ba1638083f11af18a2368d14
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-28 17:07:00 -0700

    dot tests

--- include/boost/multi/adaptors/blas/core.hpp
+++ include/boost/multi/adaptors/blas/core.hpp
@@ -282,7 +282,18 @@ using std::enable_if_t;
 using std::is_assignable;
 
 // NOLINTBEGIN(modernize-use-constraints) TODO(correaa) for C++20
-template<class XP, class X = typename std::pointer_traits<XP*>::element_type, class YP, class Y = typename std::pointer_traits<YP*>::element_type, class RP, class R = typename std::pointer_traits<RP*>::element_type, enable_if_t<is_s<X>{} && is_s<Y>{} && is_assignable<R&, decltype(0.0F+X{}*Y{}+X{}*Y{})>{}, int> =0> void dot (ssize_t n, XP* x, ptrdiff_t incx, YP* y, ptrdiff_t incy, RP* r) {auto const rr = BLAS(sdot )(n, reinterpret_cast<s const*>(static_cast<X*>(x)), incx, reinterpret_cast<s const*>(static_cast<Y*>(y)), incy); std::memcpy(reinterpret_cast<float  *    >(static_cast<R*>(r)), &rr, sizeof(rr)); static_assert(sizeof(rr)==sizeof(*r));} // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays,google-readability-casting,readability-identifier-length)  // NOSONAR
+template<class XP, class X = typename std::pointer_traits<XP*>::element_type, class YP, class Y = typename std::pointer_traits<YP*>::element_type, class RP, class R = typename std::pointer_traits<RP*>::element_type, enable_if_t<is_s<X>{} && is_s<Y>{} && is_assignable<R&, decltype(0.0F+X{}*Y{}+X{}*Y{})>{}, int> =0> void dot (ssize_t n, XP* xp, ptrdiff_t incx, YP* yp, ptrdiff_t incy, RP* rp) {
+	// Apple Accelerate BLAS is known to have bugs in single precission function
+	// `sdot` and `smrm2`, be careful:
+	// https://stackoverflow.com/a/77017238/225186,
+	// https://fortran-lang.discourse.group/t/how-many-blas-libraries-have-this-error/4454/23,
+	// https://forums.developer.apple.com/forums/thread/717757
+#ifdef MULTI_BLAS_USE_SDOT  // disable workararound for Apple Accelerate framework bug
+	auto const rr = BLAS(sdot )(n, reinterpret_cast<s const*>(static_cast<X*>(xp)), incx, reinterpret_cast<s const*>(static_cast<Y*>(yp)), incy); std::memcpy(reinterpret_cast<float  *    >(static_cast<R*>(r)), &rr, sizeof(rr)); static_assert(sizeof(rr)==sizeof(*r));
+#else
+	BLAS(sgemv)('N', 1, n, 1.0F, reinterpret_cast<s const*>(static_cast<X*>(xp)), incx, reinterpret_cast<s const*>(static_cast<Y*>(yp)), incy, 0.0F, reinterpret_cast<s*>(static_cast<R*>(rp)), 1);  // NOLINT(readability-suspicious-call-argument,cppcoreguidelines-pro-type-reinterpret-cast) 
+#endif
+}
 template<class XP, class X = typename std::pointer_traits<XP*>::element_type, class YP, class Y = typename std::pointer_traits<YP*>::element_type, class RP, class R = typename std::pointer_traits<RP*>::element_type, enable_if_t<is_d<X>{} && is_d<Y>{} && is_assignable<R&, decltype(0.0 +X{}*Y{}+X{}*Y{})>{}, int> =0> void dot (ssize_t n, XP* x, ptrdiff_t incx, YP* y, ptrdiff_t incy, RP* r) {auto const rr = BLAS(ddot )(n, reinterpret_cast<d const*>(static_cast<X*>(x)), incx, reinterpret_cast<d const*>(static_cast<Y*>(y)), incy); std::memcpy(reinterpret_cast<double *    >(static_cast<R*>(r)), &rr, sizeof(rr)); static_assert(sizeof(rr)==sizeof(*r));} // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays,google-readability-casting,readability-identifier-length)  // NOSONAR
 // NOLINTEND(modernize-use-constraints) TODO(correaa) for C++20
 
@@ -312,34 +323,34 @@ template<class XP, class X = typename std::pointer_traits<XP>::element_type, cla
 // TODO(correaa) implement workaround for bug in Apple Accelerate BLAS ? https://stackoverflow.com/a/77017238/225186
 template<class XP, class X = typename std::pointer_traits<XP>::element_type, class YP, class Y = typename std::pointer_traits<YP>::element_type, class RP, class R = typename std::pointer_traits<RP>::element_type, enable_if_t<is_c<X>{} && is_c<Y>{} && is_assignable<R&, decltype(/*0.0F+*/ X{}*Y{}+X{}*Y{})>{}, int> =0> void dotu(ssize_t n, XP x, ptrdiff_t incx, YP y, ptrdiff_t incy, RP rp) {
 	// [[maybe_unused]] static bool const use_cdotu = []{
-	// 	std::array<std::complex<float>, 3> const v1 = {std::complex<float>{1.0F, 2.0F}, std::complex<float>{3.0F,  4.0F}, std::complex<float>{ 5.0F,  6.0F}};
-	// 	std::array<std::complex<float>, 3> const v2 = {std::complex<float>{7.0F, 8.0F}, std::complex<float>{9.0F, 10.0F}, std::complex<float>{11.0F, 12.0F}};
+	//  std::array<std::complex<float>, 3> const v1 = {std::complex<float>{1.0F, 2.0F}, std::complex<float>{3.0F,  4.0F}, std::complex<float>{ 5.0F,  6.0F}};
+	//  std::array<std::complex<float>, 3> const v2 = {std::complex<float>{7.0F, 8.0F}, std::complex<float>{9.0F, 10.0F}, std::complex<float>{11.0F, 12.0F}};
 
-	// 	Complex_float rr{-1.0F, -2.0F};
-	// 	rr = BLAS(cdotu)(3, v1.data(), 1, v2.data(), 1);
+	//  Complex_float rr{-1.0F, -2.0F};
+	//  rr = BLAS(cdotu)(3, v1.data(), 1, v2.data(), 1);
 
-	// 	if( !(std::abs(rr.real - std::real(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) < 1.0e-8)
-	// 		|| !(std::abs(rr.imag - std::imag(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) < 1.0e-8) ) {
-	// 		std::clog
-	// 			<< "multi::blas setup warning: when using cdotu that returns non-void,\n"
-	// 			<< "cdotu returned (" << rr.real << ", " << rr.imag << ", it should return " << v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] << '\n'
-	// 			<< "This problem appears with BLAS and OpenBLAS 32bit.\n"
-	// 			<< "... falling back to cgemv\n";
-	// 		{
-	// 			std::complex<float> gemv_rr{-12.345F, -54.321F};
-	// 			BLAS(cgemv)('N', 1, v1.size(), std::complex<float>{1.0F, 0.0F}, v1.data(), 1, v2.data(), 1, std::complex<float>{0.0F, 0.0F}, &gemv_rr, 1);
-	// 			std::clog << "cgemv gives " << gemv_rr << ", it should give " << v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] << '\n';
+	//  if( !(std::abs(rr.real - std::real(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) < 1.0e-8)
+	//    || !(std::abs(rr.imag - std::imag(v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) < 1.0e-8) ) {
+	//    std::clog
+	//      << "multi::blas setup warning: when using cdotu that returns non-void,\n"
+	//      << "cdotu returned (" << rr.real << ", " << rr.imag << ", it should return " << v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] << '\n'
+	//      << "This problem appears with BLAS and OpenBLAS 32bit.\n"
+	//      << "... falling back to cgemv\n";
+	//    {
+	//      std::complex<float> gemv_rr{-12.345F, -54.321F};
+	//      BLAS(cgemv)('N', 1, v1.size(), std::complex<float>{1.0F, 0.0F}, v1.data(), 1, v2.data(), 1, std::complex<float>{0.0F, 0.0F}, &gemv_rr, 1);
+	//      std::clog << "cgemv gives " << gemv_rr << ", it should give " << v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] << '\n';
 			
-	// 			if( !(std::abs(gemv_rr - (v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) < 1.0e-8) ) {
-	// 				std::clog << "gemv also failed" << '\n';
-	// 			}
-	// 			return false;  // dot not use cdotu
-	// 		}
-	// 	}
-	// 	return true;  // use cdotu
+	//      if( !(std::abs(gemv_rr - (v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2])) < 1.0e-8) ) {
+	//        std::clog << "gemv also failed" << '\n';
+	//      }
+	//      return false;  // dot not use cdotu
+	//    }
+	//  }
+	//  return true;  // use cdotu
 	// }();
 	// if(use_cdotu) {
-	// 	Complex_float const rr = BLAS(cdotu)(                                      n, reinterpret_cast<c const*>(static_cast<X*>(x)), incx, reinterpret_cast<c const*>(static_cast<Y*>(y)), incy); std::memcpy(reinterpret_cast<std::array<float , 2>*>(static_cast<R*>(rp))->data(), &rr, sizeof(rr)); static_assert(sizeof(rr)==sizeof(*rp));
+	//  Complex_float const rr = BLAS(cdotu)(                                      n, reinterpret_cast<c const*>(static_cast<X*>(x)), incx, reinterpret_cast<c const*>(static_cast<Y*>(y)), incy); std::memcpy(reinterpret_cast<std::array<float , 2>*>(static_cast<R*>(rp))->data(), &rr, sizeof(rr)); static_assert(sizeof(rr)==sizeof(*rp));
 	// } else {
 		BLAS(cgemv)('N', 1, n, std::complex<float>{1.0F, 0.0F}, reinterpret_cast<c const*>(static_cast<X*>(x)), incx, reinterpret_cast<c const*>(static_cast<Y*>(y)), incy, std::complex<float>{0.0F, 0.0F}, reinterpret_cast<c*>(static_cast<R*>(rp)), 1);  // NOLINT(readability-suspicious-call-argument)
 	// }
