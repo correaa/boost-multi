3a74ab8c18306398adbd372891d97af66ce458f1
Alfredo Correa <alfredo.correa@gmail.com>
2024-11-07 10:20:04 -0800

    remove iostream

--- include/boost/multi/adaptors/blas/test/dot.cpp
+++ include/boost/multi/adaptors/blas/test/dot.cpp
@@ -14,7 +14,6 @@
 #include <cmath>
 #include <complex>     // for complex, operator*
 #include <functional>  // for plus  // IWYU pragma: keep
-#include <iostream>
 #include <iterator>    // for begin, end
 #include <limits>      // for numeric_limits
 #include <memory>      // for allocator
