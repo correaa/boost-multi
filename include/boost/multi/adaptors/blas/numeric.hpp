337b27e27f02314642f929addbb3267f0bf69740
Alfredo Correa <alfredo.correa@gmail.com>
2024-11-11 14:02:18 -0800

    workaround for nvcc

--- include/boost/multi/adaptors/blas/numeric.hpp
+++ include/boost/multi/adaptors/blas/numeric.hpp
@@ -201,10 +201,10 @@ class involuter {
 		return *this;
 	}
 
-	template<class DifferenceType = difference_type>  // workaround for nvcc
-	constexpr friend auto operator+(involuter lhs, DifferenceType n) { return lhs += n; }
-	template<class DifferenceType = difference_type>  // workaround for nvcc
-	constexpr friend auto operator-(involuter lhs, DifferenceType n) { return lhs -= n; }
+	template<class = void>  // workaround for nvcc
+	constexpr friend auto operator+(involuter lhs, difference_type n) { return lhs += n; }
+	template<class = void>  // workaround for nvcc
+	constexpr friend auto operator-(involuter lhs, difference_type n) { return lhs -= n; }
 
 	auto operator-(involuter const& other) const { return it_ - other.it_; }
 
