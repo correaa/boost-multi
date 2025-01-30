4b901f2dd137b51bc4f807d824850cd9f2c59867
Alfredo Correa <alfredo.correa@gmail.com>
2025-01-29 19:44:50 -0800

    fix for circle

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -2373,13 +2373,12 @@ struct array_iterator<Element, 1, Ptr, IsConst, IsMove, Stride>  // NOLINT(fuchs
 	static constexpr dimensionality_type dimensionality = 1;
 
 	#if __cplusplus >= 202002L
-	template<class T = void,
-		std::enable_if_t<sizeof(T*) && std::is_base_of_v<std::contiguous_iterator_tag, iterator_category>, int> =0>  // NOLINT(modernize-use-constraints) TODO(correaa) for C++20
-	constexpr explicit operator Ptr() const& { return ptr_; }
-
-	template<class T = void,
-		std::enable_if_t<sizeof(T*) && std::is_base_of_v<std::contiguous_iterator_tag, iterator_category>, int> =0>  // NOLINT(modernize-use-constraints) TODO(correaa) for C++20
-	constexpr explicit operator Ptr() && { return ptr_; }
+	// template<class T = void,
+	//  std::enable_if_t<sizeof(T*) && std::is_base_of_v<std::contiguous_iterator_tag, iterator_category>, int> =0>  // NOLINT(modernize-use-constraints) TODO(correaa) for C++20
+	constexpr explicit operator Ptr() const& {
+		static_assert(std::is_base_of_v<std::contiguous_iterator_tag, iterator_category>, "iterator must be continuous");
+		return ptr_;
+	}
 	#endif
 
 	array_iterator() = default;  // NOLINT(cppcoreguidelines-pro-type-member-init,hicpp-member-init)
