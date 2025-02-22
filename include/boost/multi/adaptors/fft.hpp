b4a92be40077a1d43977a440024f8d8cdce2cc73
Alfredo Correa <alfredo.correa@gmail.com>
2025-02-22 12:05:17 -0800

    restore cufft test

--- include/boost/multi/adaptors/fft.hpp
+++ include/boost/multi/adaptors/fft.hpp
@@ -25,17 +25,17 @@ namespace fft {
 
 	template<std::size_t I> struct priority : std::conditional_t<I==0, std::true_type, struct priority<I-1>>{};
 
-	template<class... Args> auto dft_aux_(priority<0>, Args&&... args) BOOST_MULTI_DECLRETURN_(  fftw::dft_backward(std::forward<Args>(args)...))
-	template<class... Args> auto dft_aux_(priority<1>, Args&&... args) BOOST_MULTI_DECLRETURN_(cufft ::dft_backward(std::forward<Args>(args)...))
+	template<class... Args> auto dft_aux_(priority<0>, Args&&... args) BOOST_MULTI_DECLRETURN_(                  fftw::dft_backward(std::forward<Args>(args)...))
+//	template<class... Args> auto dft_aux_(priority<1>, Args&&... args) BOOST_MULTI_DECLRETURN_(::boost::multi::cufft ::dft_backward(std::forward<Args>(args)...))
 	template<class... Args> auto dft(Args&&... args) BOOST_MULTI_DECLRETURN_(dft_backward_aux_(priority<1>{}, std::forward<Args>(args)...))
 	template<class In, class... Args> auto dft(std::array<bool, std::decay_t<In>::dimensionality> which, In const& in, Args&&... args) -> decltype(auto) {return dft_aux_(priority<1>{}, which, in, std::forward<Args>(args)...);}
 
 	template<class... Args> auto dft_forward_aux_(priority<0>, Args&&... args) BOOST_MULTI_DECLRETURN_(  fftw::dft_forward(std::forward<Args>(args)...))
-	template<class... Args> auto dft_forward_aux_(priority<1>, Args&&... args) BOOST_MULTI_DECLRETURN_(cufft ::dft_forward(std::forward<Args>(args)...))
+//	template<class... Args> auto dft_forward_aux_(priority<1>, Args&&... args) BOOST_MULTI_DECLRETURN_(cufft ::dft_forward(std::forward<Args>(args)...))
 	template<class In, class... Args> auto dft_forward(std::array<bool, In::dimensionality> which, In const& in, Args&&... args) -> decltype(auto) {return dft_forward_aux_(priority<1>{}, which, in, std::forward<Args>(args)...);}
 
 	template<class... Args> auto dft_backward_aux_(priority<0>, Args&&... args) BOOST_MULTI_DECLRETURN_(  fftw::dft_backward(std::forward<Args>(args)...))
-	template<class... Args> auto dft_backward_aux_(priority<1>, Args&&... args) BOOST_MULTI_DECLRETURN_(cufft ::dft_backward(std::forward<Args>(args)...))
+//	template<class... Args> auto dft_backward_aux_(priority<1>, Args&&... args) BOOST_MULTI_DECLRETURN_(cufft ::dft_backward(std::forward<Args>(args)...))
 	template<class In, class... Args> auto dft_backward(std::array<bool, In::dimensionality> which, In const& in, Args&&... args) -> decltype(auto) {return dft_backward_aux_(priority<1>{}, which, in, std::forward<Args>(args)...);}
 
 }}}
