258aa136637eeea4cc8316599da7fe901712dc38
Alfredo Correa <alfredo.correa@gmail.com>
2025-03-14 18:38:57 -0700

    simplify + for iterator

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -2511,16 +2511,16 @@ struct array_iterator<Element, 1, Ptr, IsConst, IsMove, Stride>  // NOLINT(fuchs
 	stride_type stride_;  // = {1};  // = {0};  // TODO(correaa) change to make it trivially default constructible
 
  public:
-	BOOST_MULTI_HD constexpr auto operator+(difference_type n) const -> array_iterator { array_iterator ret{*this}; ret+=n; return ret; }
-	BOOST_MULTI_HD constexpr auto operator-(difference_type n) const -> array_iterator { array_iterator ret{*this}; ret-=n; return ret; }
+	BOOST_MULTI_HD constexpr auto operator+(difference_type n) const { return array_iterator{*this} += n; }
+	BOOST_MULTI_HD constexpr auto operator-(difference_type n) const { return array_iterator{*this} -= n; }
 
 	BOOST_MULTI_HD constexpr auto base() const {return static_cast<pointer>(ptr_);}
 
 	[[deprecated("use base() for iterator")]]
 	BOOST_MULTI_HD constexpr auto data() const {return base();}
 
-	BOOST_MULTI_FRIEND_CONSTEXPR
-	auto base(array_iterator const& self) { return self.base(); }
+	// BOOST_MULTI_FRIEND_CONSTEXPR
+	// auto base(array_iterator const& self) { return self.base(); }
 
 	BOOST_MULTI_HD constexpr auto stride() const -> stride_type {return      stride_;}
 	friend    constexpr auto stride(array_iterator const& self) -> stride_type { return self.stride_; }
