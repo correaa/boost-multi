3dd5020ce7b0f2aa909d0fe1df76843a79d61d90
Alfredo Correa <alfredo.correa@gmail.com>
2025-03-17 13:37:13 -0700

    remove ws

--- test/partitioned.cpp
+++ test/partitioned.cpp
@@ -647,29 +647,6 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		BOOST_TEST(( border == multi::array<int, 1>{6, 7} ));
 	}
 
-	// tiled 1D with 4 elements per tile
-	{
-		multi::array<int, 1> const arr = {0, 1, 2, 3, /**/ 4, 5, 6, 7, /**/ 8, 9};
-
-		BOOST_TEST( arr.size() == 10 );
-
-		BOOST_TEST(( arr.tiled(4).quotient[0].size() == 4 ));
-		BOOST_TEST(( arr.tiled(4).quotient[1].size() == 4 ));
-
-		BOOST_TEST(( arr.tiled(4).remainder.size() == 2 ));
-
-		auto [tiles, border] = arr.tiled(4);
-		BOOST_TEST( tiles.size() == 2 );
-		BOOST_TEST(( tiles[0].size() == 4 ));
-		BOOST_TEST(( tiles[1].size() == 4 ));
-
-		BOOST_TEST(( tiles[0] == multi::array<int, 1>{0, 1, 2, 3} ));
-		BOOST_TEST(( tiles[1] == multi::array<int, 1>{4, 5, 6, 7} ));
-
-		BOOST_TEST( border.size() == 2 );
-		BOOST_TEST(( border == multi::array<int, 1>{8, 9} ));
-	}
-
 	// tiled 1D with 5 elements per tile
 	{
 		multi::array<int, 1> const arr = {0, 1, 2, 3, 4, /**/ 5, 6, 7, 8, 9, /**/ 10, 11, 12};
