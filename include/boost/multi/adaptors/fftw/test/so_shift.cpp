5c79eca5888ac4c5240fb94ff90879cd0089c3b6
Alfredo Correa <alfredo.correa@gmail.com>
2025-05-14 02:23:02 -0700

    8 as ext

--- include/boost/multi/adaptors/fftw/test/so_shift.cpp
+++ include/boost/multi/adaptors/fftw/test/so_shift.cpp
@@ -17,7 +17,7 @@ auto main() -> int {  // NOLINT(bugprone-exception-escape)
 
 	// input array
 	auto const x = std::invoke([] {  // NOLINT(readability-identifier-length)
-		multi::array<complex, 1> ret(8);
+		multi::array<complex, 1> ret({8});
 		// fill the first array with some numbers
 		std::iota(ret.begin(), ret.end(), 1.0);
 		return ret;
