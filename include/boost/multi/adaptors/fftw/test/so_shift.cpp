31c665d80d6d86a2645ac7cd9214a72e473b5d8d
Alfredo Correa <alfredo.correa@gmail.com>
2025-05-21 13:19:35 -0700

    fix circle

--- include/boost/multi/adaptors/fftw/test/so_shift.cpp
+++ include/boost/multi/adaptors/fftw/test/so_shift.cpp
@@ -17,9 +17,9 @@ auto main() -> int {  // NOLINT(bugprone-exception-escape)
 
 	// input array
 	auto const x = std::invoke([] {  // NOLINT(readability-identifier-length)
-		multi::static_array<complex, 1> const ret2(multi::static_array<complex, 1>::extensions_type{{0L, 8L}}); (void)ret2;
+		multi::static_array<complex, 1> const ret2(multi::static_array<complex, 1>::extensions_type{{0, 8}}); (void)ret2;
 
-		multi::array<complex, 1> ret({8}, {});
+		multi::array<complex, 1> ret({8});
 		// fill the first array with some numbers
 		std::iota(ret.begin(), ret.end(), 1.0);
 		return ret;
