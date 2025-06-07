e27fcf4469dd4ca1fd4edd0c7ff4bc9021f1cf93
Alfredo Correa <alfredo.correa@gmail.com>
2025-06-06 22:02:08 -0700

    rm bt

--- test/concepts.cpp
+++ test/concepts.cpp
@@ -1,39 +1,7 @@
-// Copyright 2022-2024 Alfredo A. Correa
+// Copyright 2022-2025 Alfredo A. Correa
 // Copyright 2024 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
-
-// #if defined(__clang__)
-//  #pragma clang diagnostic push
-//  #pragma clang diagnostic ignored "-Wunknown-warning-option"
-//  #pragma clang diagnostic ignored "-Wconversion"
-//     #pragma clang diagnostic ignored "-Wextra-semi-stmt"
-//  #pragma clang diagnostic ignored "-Wold-style-cast"
-//  #pragma clang diagnostic ignored "-Wsign-conversion"
-//     #pragma clang diagnostic ignored "-Wswitch-default"
-//  #pragma clang diagnostic ignored "-Wundef"
-// #elif defined(__GNUC__)
-//  #pragma GCC diagnostic push
-//  #if (__GNUC__ > 7)
-//      #pragma GCC diagnostic ignored "-Wcast-function-type"
-//  #endif
-//  #pragma GCC diagnostic ignored "-Wconversion"
-//  #pragma GCC diagnostic ignored "-Wold-style-cast"
-//  #pragma GCC diagnostic ignored "-Wsign-conversion"
-//  #pragma GCC diagnostic ignored "-Wundef"
-// #endif
-
-// #ifndef BOOST_TEST_MODULE
-//  #define BOOST_TEST_MAIN
-// #endif
-
-// #include <boost/test/included/unit_test.hpp>
-
-// #if defined(__clang__)
-//  #pragma clang diagnostic pop
-// #elif defined(__GNUC__)
-//  #pragma GCC diagnostic pop
-// #endif
 
 #if defined(__clang__)
 	#pragma clang diagnostic push
@@ -63,12 +31,6 @@
 #endif
 
 #include <boost/multi/array.hpp>  // for operator!=, implicit...
-
-// #include <boost/mp11.hpp>  // Boost.Test 1.67 needs test cases to be mpl list
-// #include <boost/mpl/list.hpp>  // for list
-
-// #include <cstddef>  // for ptrdiff_t  // IWYU pragma: keep
-// #include <vector>   // for vector  // IWYU pragma: keep
 
 #include <type_traits>  // for is_same_v, is_convertib...
 
