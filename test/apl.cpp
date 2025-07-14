6f33327822564b716c75edf044482067388f57c0
Alfredo Correa <alfredo.correa@gmail.com>
2025-07-14 16:48:51 -0700

    g++-8

--- test/apl.cpp
+++ test/apl.cpp
@@ -24,7 +24,8 @@ namespace {
 
 template<multi::dimensionality_type D, class Extensions = multi::extensions_t<D>>
 constexpr auto iota(Extensions const& exts) {
-	auto beg = multi::extension_t{std::ptrdiff_t{0}, exts.num_elements()}.begin();  // std::views::iota(0, exts.num_elements()).begin();
+	auto ext = multi::extension_t{std::ptrdiff_t{0}, exts.num_elements()};
+	auto beg = ext.begin();  // std::views::iota(0, exts.num_elements()).begin();
 	return multi::array_ref<typename decltype(beg)::value_type, Extensions::dimensionality, std::remove_const_t<decltype(beg)>>(exts, beg);
 }
 
