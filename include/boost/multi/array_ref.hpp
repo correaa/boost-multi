79ca34e584cdc1c7323d249bd0ad875e6e930f48
Alfredo Correa <alfredo.correa@gmail.com>
2025-07-24 11:32:56 -0700

    formats for clang-format 19

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -2690,10 +2690,12 @@ struct const_subarray<T, 1, ElementPtr, Layout>  // NOLINT(fuchsia-multiple-inhe
 
 	template<class TT, std::enable_if_t<std::is_same_v<ElementPtr, TT const*>, int> = 0>  // NOLINT(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays,modernize-use-constraints) for C++20
 	BOOST_MULTI_HD constexpr const_subarray(std::initializer_list<TT> const& il)
-	: array_types<T, 1, ElementPtr, Layout>{
-		  layout_type(multi::extensions_t<1>{{0, static_cast<size_type>(std::size(il))}}
-          ), std::data(il)
-    } {}
+	: array_types<T, 1, ElementPtr, Layout>(
+		  layout_type(multi::extensions_t<1>(
+			  {0, static_cast<size_type>(std::size(il))}
+		  )),
+		  std::data(il)
+	  ) {}
 
 	// boost serialization needs `delete`. void boost::serialization::extended_type_info_typeid<T>::destroy(const void*) const [with T = boost::multi::subarray<double, 1, double*, boost::multi::layout_t<1> >]
 	// void operator delete(void* ptr) noexcept = delete;
