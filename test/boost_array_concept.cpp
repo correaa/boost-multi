797b8cab12ed48f137f365080173e9fa3d479cc0
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-01 15:23:54 -0700

    more iwyu

--- test/boost_array_concept.cpp
+++ test/boost_array_concept.cpp
@@ -19,10 +19,12 @@
 #endif
 
 #include <boost/multi/array.hpp>  // for operator!=, implicit...
+
 // IWYU pragma: no_include <boost/multi_array/subarray.hpp>        // for const_sub_array, sub_array
+// IWYU pragma: no_include <algorithm>                             // for fill_n
 
 #include <boost/multi_array.hpp>                 // for multi_array
-#include <boost/multi_array/base.hpp>            // for multi_array
+// #include <boost/multi_array/base.hpp>            // for multi_array
 #include <boost/multi_array/concept_checks.hpp>  // for ConstMultiArrayConcept
 
 #if defined(__clang__)
