bcd7d96303ef4058bafd25c0e3d7a911ece05df6
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-03 21:31:12 -0700

    fixes for clang

--- include/boost/multi/detail/layout.hpp
+++ include/boost/multi/detail/layout.hpp
@@ -1,4 +1,4 @@
-// Copyright 2018-2024 Alfredo A. Correa
+// Copyright 2018-2025 Alfredo A. Correa
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
