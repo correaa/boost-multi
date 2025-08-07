a7d7b7b46c1f7d8d5cb420e2ea7d20e6af6c52eb
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-07 01:51:39 -0700

    cu fix

--- include/boost/multi/adaptors/thrust/test/universal.cu
+++ include/boost/multi/adaptors/thrust/test/universal.cu
@@ -1,5 +1,6 @@
-// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
-// Copyright 2023 Alfredo A. Correa
+// Copyright 2023-2025 Alfredo A. Correa
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
 
 #define BOOST_TEST_MODULE "C++ Unit Tests for Multi CUDA thrust universal"
 // #include<boost/test/unit_test.hpp>
@@ -18,11 +19,11 @@
 namespace multi = boost::multi;
 
 #include <boost/core/lightweight_test.hpp>
-#define BOOST_AUTO_TEST_CASE(CasenamE) /**/
 
 auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugprone-exception-escape)
 
-BOOST_AUTO_TEST_CASE(thrust_universal_ptr) {
+// BOOST_AUTO_TEST_CASE(thrust_universal_ptr)
+{
 	multi::array<double, 2                                           > Host({1024, 1024});
 	std::iota(Host.data_elements(), Host.data_elements() + Host.num_elements(), 12.0);
 
@@ -33,10 +34,11 @@ BOOST_AUTO_TEST_CASE(thrust_universal_ptr) {
 	multi::array<double, 2                                           > Hos2({1024, 1024});
 	Hos2({0, 10}, {0, 20}) = Univ({0, 10}, {0, 20});
 
-	BOOST_TEST( Hos2[0][0] == 12.0 );
+	BOOST_TEST( std::abs( Hos2[0][0] - 12.0 ) < 1e-10 );
 }
 
-BOOST_AUTO_TEST_CASE(thrust_universal_ptr_initializer_list) {
+// BOOST_AUTO_TEST_CASE(thrust_universal_ptr_initializer_list)
+{
 	multi::array<double, 1                                           > Host = {1.0, 2.0, 3.0};
 	BOOST_TEST( Host.size() == 3 );
 	{
@@ -73,14 +75,6 @@ BOOST_AUTO_TEST_CASE(thrust_universal_ptr_initializer_list) {
 
 		BOOST_TEST( Host[0] == 3.0 );
 	}
-//  what( thrust::cuda::universal_allocator<double>{} );
-//  {
-//      multi::array<double, 1, thrust::cuda::universal_allocator<double>> Univ = {3., 2., 1.};
-
-//      Host() = Univ();
-
-//      BOOST_TEST( Host[0] == 3. );
-//  }
 }
 
 return boost::report_errors();
