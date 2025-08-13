538c79bb098710d371c769e3e1811e44bab87666
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-13 00:20:24 -0700

    more nolint

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -777,9 +777,8 @@ struct static_array<T, ::boost::multi::dimensionality_type{0}, Alloc>  // NOLINT
 	: static_array(multi::iextensions<0>{}, elem) {}
 
 	template<
-		class Singleton,
-		std::enable_if_t<!std::is_base_of_v<static_array, Singleton> && !std::is_same_v<Singleton, typename static_array::element_type>, int> = 0,
-		class                                                                                                                                 = decltype(adl_copy_n(&std::declval<Singleton>(), 1, typename static_array::element_ptr{}))>
+		class Singleton, std::enable_if_t<!std::is_base_of_v<static_array, Singleton> && !std::is_same_v<Singleton, typename static_array::element_type>, int> = 0,  // NOLINT(modernize-type-traits) for C++20
+		class = decltype(adl_copy_n(&std::declval<Singleton>(), 1, typename static_array::element_ptr{}))>
 	// cppcheck-suppress noExplicitConstructor ; to allow terse syntax  // NOLINTNEXTLINE(runtime/explicit)
 	/*implict*/ static_array(Singleton const& single)  // NOLINT(google-explicit-constructor,hicpp-explicit-conversions) this is used by the
 	: ref(static_array::allocate(1), typename static_array::extensions_type{}) {
