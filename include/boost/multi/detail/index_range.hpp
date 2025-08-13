eb8040081f027d126930ad8f338c783465f2d5cb
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-13 00:20:24 -0700

    more nolint

--- include/boost/multi/detail/index_range.hpp
+++ include/boost/multi/detail/index_range.hpp
@@ -127,7 +127,7 @@ class range {
 	// range(range const&) = default;
 
 	template<class Range,
-	         std::enable_if_t<!std::is_base_of_v<range, std::decay_t<Range>>, int> = 0,
+	         std::enable_if_t<!std::is_base_of_v<range, std::decay_t<Range>>, int> = 0,  // NOLINT(modernize-type-traits) for C++20
 	         decltype(detail::implicit_cast<IndexType>(std::declval<Range&&>().first()),
 	                  detail::implicit_cast<IndexTypeLast>(std::declval<Range&&>().last())
 	         )*                                                                    = nullptr>
