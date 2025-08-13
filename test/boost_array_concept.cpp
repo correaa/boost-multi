43367eb4cc439a45742b2d7be1b2c6e5cbbb99a7
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-12 18:11:49 -0700

    restore base

--- test/boost_array_concept.cpp
+++ test/boost_array_concept.cpp
@@ -24,9 +24,8 @@
 // IWYU pragma: no_include <boost/multi_array/subarray.hpp>        // for const_sub_array, sub_array
 // IWYU pragma: no_include <algorithm>                             // for fill_n
 
-#include <boost/multi_array.hpp>  // for multi_array
-// #include <boost/multi_array/base.hpp>            // for multi_array
-
+#include <boost/multi_array.hpp>                 // for multi_array
+#include <boost/multi_array/base.hpp>            // for multi_array  // IWYU pragma: keep
 #include <boost/multi_array/concept_checks.hpp>  // for ConstMultiArrayConcept
 
 #if defined(__clang__)
