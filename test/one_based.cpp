0c0a9af9b2b15c0139eaab631132edb53567c790
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-14 14:28:30 -0700

    wa 14

--- test/one_based.cpp
+++ test/one_based.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#if defined(__GNUC__)
+#if defined(__GNUC__)  // for gcc 14
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 #endif
 
