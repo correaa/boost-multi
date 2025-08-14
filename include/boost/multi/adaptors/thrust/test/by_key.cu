7369a9697211688bf40eadf0a3f49d7da0ac3583
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-13 21:47:10 -0700

    use division

--- include/boost/multi/adaptors/thrust/test/by_key.cu
+++ include/boost/multi/adaptors/thrust/test/by_key.cu
@@ -3,6 +3,7 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/core/lightweight_test.hpp>
+#include <cstddef>
 
 #include <boost/multi/adaptors/thrust.hpp>
 #include <boost/multi/array.hpp>
@@ -37,18 +38,27 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 	BOOST_TEST(true);
 	// M.extensions().elements();
 
-	auto row_ids_begin =
+	auto row_ids_begin_ref =
 	    thrust::make_transform_iterator(
 			M.extensions().elements().begin(),
-	        [] __host__ __device__ (decltype(M)::indexes const& e) { using std::get; return get<0>(e); }
+	        [] __host__ __device__ (decltype(M)::indexes e) -> std::ptrdiff_t { using std::get; return get<0>(e); }
 	    )
 	;
-	auto row_ids_end =
-		thrust::make_transform_iterator(
-			M.extensions().elements().end(),
-			[] __host__ __device__ (decltype(M)::indexes const& e) { using std::get; return get<0>(e); }
-		)
+
+	auto row_ids_begin =
+	    thrust::make_transform_iterator(
+			thrust::make_counting_iterator(std::ptrdiff_t{0}),
+	        [] __host__ __device__ (std::ptrdiff_t e) -> std::ptrdiff_t { return e / 4; }  // std::get; return get<0>(e); }
+	    )
 	;
+	auto row_ids_end = row_ids_begin + M.num_elements();
+
+	auto row_ids_end_ref = row_ids_begin_ref + M.num_elements();
+
+	BOOST_TEST( thrust::equal(row_ids_begin, row_ids_end, row_ids_begin_ref) );  // , row_ids_end_ref) );
+	BOOST_TEST( thrust::equal(row_ids_begin+1, row_ids_end, row_ids_begin_ref+1) );  // row_ids_end_ref) );
+
+	BOOST_TEST( row_ids_end - row_ids_begin == M.num_elements() );
 
 	multi::thrust::universal_array<float, 1> sums(M.size());
 
@@ -66,6 +76,9 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 	//     thrust::make_discard_iterator(),
 	//     sums.data()
 	// );
+
+	BOOST_TEST( sums[0] == M[0][0] + M[0][1] + M[0][2] + M[0][3] );
+	BOOST_TEST( sums[1] == M[1][0] + M[1][1] + M[1][2] + M[1][3] );
 
 	return boost::report_errors();
 }
