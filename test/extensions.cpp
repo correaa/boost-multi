2fa0e1346fa5809b3eff4014654f04939b65a294
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-14 23:11:07 -0700

    more mull

--- test/extensions.cpp
+++ test/extensions.cpp
@@ -250,7 +250,7 @@ auto main() -> int {  // NOLINT(bugprone-exception-escape,readability-function-c
 		BOOST_TEST( 1 == get<0>(*(it + 5)) );
 		BOOST_TEST( 2 == get<1>(*(it + 5)) );
 
-		auto const it2 = it + 5;
+		auto const it2  = it + 5;
 		auto const it22 = it - (-5);
 		BOOST_TEST( it2 == it22 );
 
@@ -266,7 +266,7 @@ auto main() -> int {  // NOLINT(bugprone-exception-escape,readability-function-c
 		BOOST_TEST( 1 == get<0>(*(it2-2)) );
 		BOOST_TEST( 0 == get<1>(*(it2-2)) );
 
-		auto const it3 = it2 - 5;
+		auto const it3  = it2 - 5;
 		auto const it33 = it2 + (-5);
 		BOOST_TEST( it3 == it33 );
 
