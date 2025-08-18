2adea9382f353ffac3b692eee4df0679503b6056
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-17 21:40:59 -0700

    remove unusued, move what

---
+++ include/boost/multi/detail/what.hpp
@@ -0,0 +1,14 @@
+// Copyright 2025 Alfredo A. Correa
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_MULTI_DETAIL_WHAT_HPP
+#define BOOST_MULTI_DETAIL_WHAT_HPP
+#pragma once
+
+namespace boost::multi::detail {
+	template<class... Ts> auto what() -> std::tuple<Ts&&...>        = delete;
+	template<class... Ts> auto what(Ts&&...) -> std::tuple<Ts&&...> = delete;  // NOLINT(cppcoreguidelines-missing-std-forward)
+}  // namespace boost::multi::detail
+
+#endif  // BOOST_MULTI_DETAIL_WHAT_HPP
