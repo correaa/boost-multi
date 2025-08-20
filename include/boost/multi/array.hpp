a84b97ee5fb632bac525eb579bcec6e198bad819
Alfredo Correa <alfredo.correa@gmail.com>
2025-08-19 17:17:55 -0700

    use const ref for extensions construction

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -319,7 +319,7 @@ struct static_array                                                             
 	explicit static_array(typename static_array::index_extension const& extension, ValueType const& value)                                                                               // fill constructor
 	: static_array(extension, value, allocator_type{}) {}
 
-	explicit static_array(::boost::multi::extensions_t<D> extensions, allocator_type const& alloc)
+	explicit static_array(::boost::multi::extensions_t<D> const& extensions, allocator_type const& alloc)
 	: array_alloc{alloc}, ref(array_alloc::allocate(static_cast<typename multi::allocator_traits<allocator_type>::size_type>(typename static_array::layout_t{extensions}.num_elements())), extensions) {
 		uninitialized_default_construct();
 	}
@@ -337,19 +337,15 @@ struct static_array                                                             
 		adl_alloc_uninitialized_copy_n(static_array::alloc(), other.elements().begin(), this->num_elements(), this->data_elements());
 	}
 
-	// template<
-	// 	class ArrayElementsLike, class = typename ArrayElementsLike::elements_t,
-	// 	// std::enable_if_t<std::is_assignable_v<typename static_array::element, typename ArrayElementsLike::element>, int> = 0,
-	// 	std::enable_if_t<!std::is_base_of_v<boost::multi::extensions_t<D>, ArrayElementsLike>, int> = 0  // NOLINT(modernize-use-constraints) for C++20
-	// 	>
-	// constexpr static_array(ArrayElementsLike const& other, allocator_type const& alloc)
-	// : array_alloc{alloc},
-	//   ref(
-	// 	  array_alloc::allocate(static_cast<typename multi::allocator_traits<allocator_type>::size_type>(typename static_array::layout_t{other.extensions()}.num_elements())),
-	// 	  other.extensions()
-	//   ) {
-	// 	adl_alloc_uninitialized_copy_n(static_array::alloc(), other.elements().begin(), this->num_elements(), this->data_elements());
-	// }
+	template<class F>  // TODO(correaa) make more generic, e.g.: take ArrayWithElementsLike
+	constexpr static_array(multi::f_extensions_t<D, F> const& other, allocator_type const& alloc)
+	: array_alloc{alloc},
+	  ref(
+		  array_alloc::allocate(static_cast<typename multi::allocator_traits<allocator_type>::size_type>(typename static_array::layout_t{other.extensions()}.num_elements())),
+		  other.extensions()
+	  ) {
+		adl_alloc_uninitialized_copy_n(static_array::alloc(), other.elements().begin(), this->num_elements(), this->data_elements());
+	}
 
 	template<class ArrayElementsLike, class = typename ArrayElementsLike::elements_t>
 	// cppcheck-suppress noExplicitConstructor  // NOLINTNEXTLINE(runtime/explicit)
