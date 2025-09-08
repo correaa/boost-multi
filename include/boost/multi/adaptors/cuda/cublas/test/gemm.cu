2ea6718d7954e7d0ce07b90b05bcb70e76c214f1
Alfredo Correa <alfredo.correa@gmail.com>
2025-09-07 20:35:08 -0700

    fix shadow variable

--- include/boost/multi/adaptors/cuda/cublas/test/gemm.cu
+++ include/boost/multi/adaptors/cuda/cublas/test/gemm.cu
@@ -2,28 +2,24 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/multi/adaptors/cuda/cublas.hpp>
-
 #include <boost/multi/adaptors/blas/axpy.hpp>
 #include <boost/multi/adaptors/blas/gemm.hpp>
 #include <boost/multi/adaptors/blas/gemv.hpp>
 #include <boost/multi/adaptors/blas/nrm2.hpp>
-
+#include <boost/multi/adaptors/cuda/cublas.hpp>
 #include <boost/multi/adaptors/thrust.hpp>
 
 #include <thrust/complex.h>
+
+#include <boost/core/lightweight_test.hpp>
 
 #include <numeric>  // for std::iota
 
 namespace multi = boost::multi;
 
-#include <boost/core/lightweight_test.hpp>
-#define BOOST_AUTO_TEST_CASE(CasenamE) /**/
-
-#define BOOST_REQUIRE_CLOSE(X, Y, ToL) BOOST_TEST( std::abs( (X) - (Y) ) < (ToL) )
-
 int main() {
-	BOOST_AUTO_TEST_CASE(multi_blas_gemv_complex) {
+	//	BOOST_AUTO_TEST_CASE(multi_blas_gemv_complex)
+	{
 		namespace blas = multi::blas;
 		using complex  = thrust::complex<double>;
 		// NOLINTNEXTLINE(readability-identifier-length) BLAS naming
@@ -51,15 +47,15 @@ int main() {
 		multi::array<complex, 1> const Y_copy = Y_gpu;
 
 		using blas::operators::operator-;
-		BOOST_REQUIRE_CLOSE(+blas::nrm2(Y_copy - multi::array<complex, 1>{
+		BOOST_TEST(+blas::nrm2(Y_copy - multi::array<complex, 1>{
 													 {214.02, 0.0},
 													 {106.43, 0.0},
 													 {188.37, 0.0}
-        }),
-							0.0, 1e-13);
+        }) < 1e-13);
 	}
 
-	BOOST_AUTO_TEST_CASE(cublas_gemv_real) {
+	// BOOST_AUTO_TEST_CASE(cublas_gemv_real)
+	{
 		namespace blas = multi::blas;
 		using T        = double;
 		// NOLINTNEXTLINE(readability-identifier-length) BLAS naming
@@ -78,10 +74,11 @@ int main() {
 		multi::array<T, 1> const Y_copy = Y_gpu;
 
 		using blas::operators::operator-;
-		BOOST_REQUIRE_CLOSE(+blas::nrm2(Y_copy - multi::array{214.02, 106.43, 188.37}), 0.0, 1e-13);
+		BOOST_TEST( +blas::nrm2(Y_copy - multi::array{214.02, 106.43, 188.37}) < 1e-13);
 	}
 
-	BOOST_AUTO_TEST_CASE(cublas_gemm_nh) {
+	// BOOST_AUTO_TEST_CASE(cublas_gemm_nh)
+	{
 		namespace blas = multi::blas;
 
 		using complex = thrust::complex<double>;
@@ -115,45 +112,45 @@ int main() {
 			BOOST_TEST( c_copy[1][0] == 16.0 -  2.0*I );
 			BOOST_TEST( c_copy[0][1] == 14.0 - 38.0*I );
 		}
+	}
 
-		// Chris
-		{
-			int m   = 12;
-			int n   = 10;
-			int ell = 8;
+	// Chris
+	{
+		int m   = 12;
+		int n   = 10;
+		int ell = 8;
 
-			multi::array<double, 3> a({m, n, ell});
-			multi::array<double, 2> b({n, ell});
+		multi::array<double, 3> a({m, n, ell});
+		multi::array<double, 2> b({n, ell});
 
-			std::iota(a.elements().begin(), a.elements().end(), 20.0);
-			std::iota(b.elements().begin(), b.elements().end(), 30.0);
+		std::iota(a.elements().begin(), a.elements().end(), 20.0);
+		std::iota(b.elements().begin(), b.elements().end(), 30.0);
 
-			multi::array<double, 1> c_gold(m, 0.0);
+		multi::array<double, 1> c_gold(m, 0.0);
 
-			for(int k = 0; k != m; ++k) {
-				for(int j = 0; j != n; ++j) {
-					for(int i = 0; i != ell; ++i) {
-						c_gold[k] += a[k][j][i] * b[j][i];
-					}
+		for(int k = 0; k != m; ++k) {
+			for(int j = 0; j != n; ++j) {
+				for(int i = 0; i != ell; ++i) {
+					c_gold[k] += a[k][j][i] * b[j][i];
 				}
 			}
-
-			multi::array<double, 1> c_flat(m, 0.0);
-
-			for(int k = 0; k != m; ++k) {
-				for(int ji = 0; ji != a[k].elements().size(); ++ji) {
-					c_flat[k] += a[k].elements()[ji] * b.elements()[ji];
-				}
-			}
-
-			BOOST_TEST( c_gold == c_flat );
-
-			// gpu::run(m, n, [...] (auto k, auto j) {
-			//      for (int i =0; i <l ; ++i) {
-			//        c[k] +=a[k][j][i]) * b[j][i];
-			//        }
-			//  });
 		}
+
+		multi::array<double, 1> c_flat(m, 0.0);
+
+		for(int k = 0; k != m; ++k) {
+			for(int ji = 0; ji != a[k].elements().size(); ++ji) {
+				c_flat[k] += a[k].elements()[ji] * b.elements()[ji];
+			}
+		}
+
+		BOOST_TEST( c_gold == c_flat );
+
+		// gpu::run(m, n, [...] (auto k, auto j) {
+		//      for (int i =0; i <l ; ++i) {
+		//        c[k] +=a[k][j][i]) * b[j][i];
+		//        }
+		//  });
 	}
 
 	return boost::report_errors();
