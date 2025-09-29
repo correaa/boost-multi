fc6aed4a5fa27dce51d7813bf6d8bfafb51d87ca
Alfredo Correa <alfredo.correa@gmail.com>
2025-09-28 23:32:32 -0700

    fix format

--- test/extensions.cpp
+++ test/extensions.cpp
@@ -366,8 +366,8 @@ auto main() -> int {  // NOLINT(bugprone-exception-escape,readability-function-c
 		);
 	}
 	{
-		multi::array<int, 2> const arr({3, 4});
 		multi::extensions_t<3> const xs{3, 4, 5};
+
 		BOOST_TEST( xs.sub() == multi::extensions_t<2>(4, 5) );
 	}
 	{
