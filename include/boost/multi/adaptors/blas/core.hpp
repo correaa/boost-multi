3995c58b46149e526f747cb57e0eeae328194819
Alfredo Correa <alfredo.correa@gmail.com>
2025-09-29 23:01:45 -0700

    fixes for conversion in cublas dotu

--- include/boost/multi/adaptors/blas/core.hpp
+++ include/boost/multi/adaptors/blas/core.hpp
@@ -1,4 +1,4 @@
-// Copyright 2019-2024 Alfredo A. Correa
+// Copyright 2019-2025 Alfredo A. Correa
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
