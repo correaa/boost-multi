527eb0c6d316557d7d642bf3cb33b4bf09772277
Alfredo Correa <alfredo.correa@gmail.com>
2025-09-30 14:55:19 -0700

    use multi size for reduce by index code

--- include/boost/multi/detail/tuple_zip.hpp
+++ include/boost/multi/detail/tuple_zip.hpp
@@ -146,7 +146,7 @@ template<class T0, class... Ts> class tuple<T0, Ts...> : tuple<Ts...> {  // NOLI
 	}
 
 	template<class F, std::size_t... I>
-	BOOST_MULTI_HD constexpr auto apply_impl_(F&& fn, std::index_sequence<I...> /*012*/) &&-> decltype(auto) {  // NOLINT(cert-dcl58-cpp) normal idiom to defined tuple get
+	BOOST_MULTI_HD constexpr auto apply_impl_(F&& fn, std::index_sequence<I...> /*012*/) && -> decltype(auto) {  // NOLINT(cert-dcl58-cpp) normal idiom to defined tuple get
 		return std::forward<F>(fn)(std::move(*this).template get<I>()...);
 	}
 
