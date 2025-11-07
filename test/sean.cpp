19decf4fd62ab8c9a92f21c76e079fb896ca4ac6
Alfredo Correa <alfredo.correa@gmail.com>
2025-11-07 11:03:05 -0800

    do some test even if there is no ranges

--- test/sean.cpp
+++ test/sean.cpp
@@ -17,10 +17,13 @@ namespace multi = boost::multi;
 
 auto main() -> int {  // NOLINT(bugprone-exception-escape,readability-function-cognitive-complexity)
 	{
-#if defined(__cpp_lib_ranges) && (__cpp_lib_ranges >= 201911L) && !defined(_MSC_VER)
 		auto rst = [](auto ii, auto jj) { return (10 * ii) + jj; } ^ multi::extensions_t(5, 5);
 
 		multi::array<int, 2> const AA = rst;
+
+		BOOST_TEST( AA.size() == rst.size() );
+
+	#if defined(__cpp_lib_ranges) && (__cpp_lib_ranges >= 201911L) && !defined(_MSC_VER)
 		multi::array<int, 2> const BB = rst | std::views::reverse;
 
 		BOOST_TEST( AA[0] == BB[4] );  // as A[0][0] == B[4][0] && A[0][1] == B[4][1] ...
