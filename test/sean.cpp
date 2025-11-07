8bc0f7799861daff43edb7b0f3bc4328fbf62604
Alfredo Correa <alfredo.correa@gmail.com>
2025-11-06 22:39:08 -0800

    fix sean

--- test/sean.cpp
+++ test/sean.cpp
@@ -10,29 +10,24 @@
 #include <iterator>   // IWYU pragma: keep
 
 #if defined(__cplusplus) && (__cplusplus >= 202002L)
-#include <concepts>  // for totally_ordered
 #include <ranges>    // IWYU pragma: keep
 #endif
-
-#include <tuple>        // IWYU pragma: keep
-#include <type_traits>  // for std::is_same_v
-// IWYU pragma: no_include <variant>        // for get, iwyu bug
 
 namespace multi = boost::multi;
 
 auto main() -> int {  // NOLINT(bugprone-exception-escape,readability-function-cognitive-complexity)
 	{
 		#if defined(__cpp_lib_ranges) && (__cpp_lib_ranges >= 201911L) && !defined(_MSC_VER)
-		auto rst = [](auto i, auto j) { return (10 * i) + j; } ^ multi::extensions_t(5, 5);
+		auto rst = [](auto ii, auto jj) { return (10 * ii) + jj; } ^ multi::extensions_t(5, 5);
 
-		multi::array<int, 2> const A = rst;
-		multi::array<int, 2> const B = rst | std::views::reverse;
+		multi::array<int, 2> const AA = rst;
+		multi::array<int, 2> const BB = rst | std::views::reverse;
 
-		BOOST_TEST( A[0] == B[4] );  // as A[0][0] == B[4][0] && A[0][1] == B[4][1] ...
-		BOOST_TEST( A[1] == B[3] );  // as A[1][0] == B[3][0] && A[1][1] == B[3][1] ...
-		BOOST_TEST( A[2] == B[2] );  // ...
-		BOOST_TEST( A[3] == B[1] );
-		BOOST_TEST( A[4] == B[0] );
+		BOOST_TEST( AA[0] == BB[4] );  // as A[0][0] == B[4][0] && A[0][1] == B[4][1] ...
+		BOOST_TEST( AA[1] == BB[3] );  // as A[1][0] == B[3][0] && A[1][1] == B[3][1] ...
+		BOOST_TEST( AA[2] == BB[2] );  // ...
+		BOOST_TEST( AA[3] == BB[1] );
+		BOOST_TEST( AA[4] == BB[0] );
 		#endif
 	}
 	return boost::report_errors();
