37af9ae4edfad718f95a682dbf97fe31c193e5ca
Alfredo Correa <alfredo.correa@gmail.com>
2025-11-20 16:54:33 -0800

    uwyu

--- test/initializer_list.cpp
+++ test/initializer_list.cpp
@@ -8,7 +8,7 @@
 #include <boost/core/lightweight_test.hpp>
 
 // IWYU pragma: no_include <algorithm>  // for copy  // bug in iwyu 14.0.6? with GNU stdlib
-#include <algorithm>         // for std::copy  // IWYU pragma: keep
+#include <algorithm>         // IWYU pragma: keep
 #include <array>             // for array
 #include <complex>           // for operator*, operator+, complex
 #include <initializer_list>  // for initializer_list, begin, end
