6ed6494a12d967f381aae74ff30d734e96c33916
Alfredo Correa <alfredo.correa@gmail.com>
2025-12-07 01:09:36 -0800

    handle both arguments

--- include/boost/multi/broadcast.hpp
+++ include/boost/multi/broadcast.hpp
@@ -119,6 +119,8 @@ template<class F, class A, class B, typename = decltype(std::declval<F&&>()(std:
 constexpr auto apply_broadcast(F&& fun, A&& a, B&& b) {
 	if constexpr(!multi::has_dimensionality<std::decay_t<A>>::value) {
 		return apply_broadcast(std::forward<F>(fun), [a = std::forward<A>(a)](){ return a; } ^ multi::extensions_t<0>{}, std::forward<B>(b));
+	} else if constexpr(!multi::has_dimensionality<std::decay_t<B>>::value) {
+		return apply_broadcast(std::forward<F>(fun), std::forward<A>(a), [b = std::forward<B>(a)](){ return b; } ^ multi::extensions_t<0>{});
 	} else {
 		if constexpr(std::decay_t<A>::dimensionality < std::decay_t<B>::dimensionality) {
 			return apply_broadcast(std::forward<F>(fun), a.repeated(b.size()), b);
