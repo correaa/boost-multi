1ebdbe284c97efdaac3048c5c4f297dc6588f1c9
Alfredo Correa <alfredo.correa@gmail.com>
2025-12-12 23:22:51 -0800

    master header

---
+++ include/multi.hpp
@@ -0,0 +1,12 @@
+// Copyright 2018-2025 Alfredo A. Correa
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_MULTI_HPP
+#define BOOST_MULTI_HPP
+
+#include "boost/multi/array.hpp"
+#include "boost/multi/array_ref.hpp"
+#include "boost/multi/restriction.hpp"
+
+#endlf  //  BOOST_MULTI_HPP
