ccfe94ff87501fede4d028a9cba6daf47c92cf95
Alfredo Correa <alfredo.correa@gmail.com>
2025-12-14 20:48:51 -0800

    more edg

--- include/boost/multi/detail/tuple_zip.hpp
+++ include/boost/multi/detail/tuple_zip.hpp
@@ -212,13 +212,13 @@ template<class T0, class... Ts> class tuple<T0, Ts...> : tuple<Ts...> {  // NOLI
 	}
 
 
-#ifdef __NVCC__  // in place of global -Xcudafe \"--diag_suppress=implicit_return_from_non_void_function\"
+#ifdef __NVCC__
 	#pragma nv_diagnostic push
-	#pragma nv_diag_suppress = implicit_return_from_non_void_function
+	#pragma nv_diag_suppress = implicit_return_from_non_void_function  // in place of global -Xcudafe \"--diag_suppress=implicit_return_from_non_void_function\"
 #elif defined(__NVCOMPILER)
 	#pragma diagnostic push
 	#pragma diag_suppress = implicit_return_from_non_void_function
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) || !defined(__EDG__)
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wreturn-type"
 #endif
@@ -246,7 +246,7 @@ template<class T0, class... Ts> class tuple<T0, Ts...> : tuple<Ts...> {  // NOLI
 #pragma nv_diagnostic pop
 #elif defined(__NVCOMPILER)
 #pragma diagnostic pop
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) || !defined(__EDG__)
 #pragma GCC diagnostic pop
 #endif
 
@@ -337,13 +337,13 @@ BOOST_MULTI_HD constexpr auto tail(tuple<T0, Ts...>&& t) -> decltype(std::move(t
 template<class T0, class... Ts>
 BOOST_MULTI_HD constexpr auto tail(tuple<T0, Ts...>& t) -> decltype(t.tail()) { return t.tail(); }  // NOLINT(readability-identifier-length) std naming
 
-#ifdef __NVCC__  // in place of global -Xcudafe \"--diag_suppress=implicit_return_from_non_void_function\"
+#ifdef __NVCC__
 	#pragma nv_diagnostic push
-	#pragma nv_diag_suppress = implicit_return_from_non_void_function
+	#pragma nv_diag_suppress = implicit_return_from_non_void_function  // in place of global -Xcudafe \"--diag_suppress=implicit_return_from_non_void_function\"
 #elif defined(__NVCOMPILER)
 	#pragma diagnostic push
 	#pragma diag_suppress = implicit_return_from_non_void_function
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) || !defined(__EDG__)
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wreturn-type"
 #endif
@@ -380,7 +380,7 @@ BOOST_MULTI_HD constexpr auto get(tuple<T0, Ts...>&& tup) -> auto&& {
 	#pragma nv_diagnostic pop
 #elif defined(__NVCOMPILER)
 	#pragma diagnostic pop
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) || !defined(__EDG__)
 	#pragma GCC diagnostic pop
 #endif
 
