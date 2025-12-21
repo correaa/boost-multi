f16dddc5a73c6c1e26ff34533fe5395a761192fd
Alfredo Correa <alfredo.correa@gmail.com>
2025-12-21 00:11:16 -0800

    add enable_if

--- include/boost/multi/broadcast.hpp
+++ include/boost/multi/broadcast.hpp
@@ -220,7 +220,7 @@ class exp_bind_t {
 
 template<class A> exp_bind_t(A) -> exp_bind_t<A>;
 
-template<class A>
+template<class A, std::enable_if_t<multi::has_dimensionality<std::decay_t<A>>::value, int> =0>  // NOLINT(modernize-use-constraints) for C++23
 BOOST_MULTI_HD constexpr auto exp(A&& alpha) {
 	auto xs = alpha.extensions();
 	// auto hm = alpha.home();
