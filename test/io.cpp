bd6615791ab5c656b34fe6931ae0119fde660f7d
Alfredo Correa <alfredo.correa@gmail.com>
2026-01-01 22:06:45 -0800

    finish io

--- test/io.cpp
+++ test/io.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/core/lightweight_test.hpp>
 
+#include <iostream>
 #include <sstream>
 #include <type_traits>  // for is_same_v, is_same
 
@@ -16,24 +17,26 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 	// matlab 1d
 	{
-		multi::array<double, 1> arr = {1.0, 2.0, 3.0};
-		std::ostringstream      oss;
+		multi::array<double, 1> const arr = {1.0, 2.0, 3.0};
+
+		std::ostringstream oss;
 		multi::detail::print(oss, arr, "\t", "\t", "\a", "\a", 0);
-		std::cout << "arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "arr = " << oss.str() << "; end\n";
 	}
 	// matlab 2d
 	{
-		multi::array<double, 2> arr = {
+		multi::array<double, 2> const arr = {
 			{1,   3},
 			{2, -10},
 		};
+
 		std::ostringstream oss;
 		multi::detail::print(oss, arr, "\t", "\t", "\a", "\a", 0);
-		std::cout << "arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "arr = " << oss.str() << "; end\n";
 	}
 	// matlab 3d
 	{
-		multi::array<double, 3> arr = {
+		multi::array<double, 3> const arr = {
 			{
              {1.0, 2.0, 3.0},
              {4.0, 5.0, 6.0},
@@ -46,60 +49,63 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 		std::ostringstream oss;
 		multi::detail::print(oss, arr, "\t", "\t", "\a", "\a", 0);
-		std::cout << "arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "arr = " << oss.str() << "; end\n";
 	}
 	// fortran 2d
 	{
-		multi::array<double, 2> arr = {
+		multi::array<double, 2> const arr = {
 			{1,   3},
 			{2, -10},
 		};
+
 		std::ostringstream oss;
 		multi::detail::print(oss, arr, " ", " ", " ", "\a", 0);
-		std::cout << "fortran 2d arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "fortran 2d arr = " << oss.str() << "; end\n";
 	}
 	// julia 1d
 	{
-		multi::array<double, 1> arr = {1, 2, 3};
-		std::ostringstream      oss;
+		multi::array<double, 1> const arr = {1, 2, 3};
+
+		std::ostringstream oss;
 		multi::detail::print(oss, arr, "[", ",", "]", "\t", 0);
-		std::cout << "julia 1d arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "julia 1d arr = " << oss.str() << "; end\n";
 	}
 	// julia 2d
 	{
-		multi::array<double, 2> arr = {
+		multi::array<double, 2> const arr = {
 			{1,   3},
 			{2, -10},
 		};
+
 		std::ostringstream oss;
 		multi::detail::print(oss, arr, "[", ",", "]", "\a", 0);
-		std::cout << "julia 2d arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "julia 2d arr = " << oss.str() << "; end\n";
 	}
 	// numpy 2d
 	{
-		multi::array<double, 2> arr = {
+		multi::array<double, 2> const arr = {
 			{1,   3},
 			{2, -10},
 		};
 		std::ostringstream oss;
 		multi::detail::print(oss, arr, "[", " ", "]", " ", 0);
-		std::cout << "numpy 2d arr = " << oss.str() << "; end" << std::endl;
+		std::cout << "numpy 2d arr = " << oss.str() << "; end\n";
 	}
 	{
-		multi::array<double, 1> arr = {1.0, 2.0, 3.0};
+		multi::array<double, 1> const arr = {1.0, 2.0, 3.0};
 
 		std::cout << "A1D = " << arr << "; no more, no less.\n";
 	}
 	{
-		multi::array<double, 2> arr = {
+		multi::array<double, 2> const arr = {
 			{1.0, 2.0, 3.0},
 			{4.0, 5.0, 6.0}
 		};
 
-		std::cout << "A2D = " << arr << "; no more, no less " << std::endl;
+		std::cout << "A2D = " << arr << "; no more, no less\n";
 	}
 	{
-		multi::array<double, 3> arr = {
+		multi::array<double, 3> const arr = {
 			{
              {1.0, 2.0, 3.0},
              {4.0, 5.0, 6.0},
@@ -110,14 +116,14 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 			 },
 		};
 
-		std::cout << "A3D = " << arr << "; no more, no less " << std::endl;
+		std::cout << "A3D = " << arr << "; no more, no less\n";
 	}
 	{
-		multi::array<double, 0> arr{5.0};
-		std::cout << "A0D = " << arr << "; no more, no less " << std::endl;
+		multi::array<double, 0> const arr{5.0};
+		std::cout << "A0D = " << arr << "; no more, no less\n";
 	}
 	{
-		multi::array<double, 4> arr = {
+		multi::array<double, 4> const arr = {
 			{
              {
              {1.0, 2.0, 3.0},
@@ -144,7 +150,6 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 		std::cout << "A4D.extesion() = " << arr.extension() << '\n';
 	}
-
 
 	return boost::report_errors();
 }
