e6886093c3618db323e6f8ff1bf0c5be3d33092b
Alfredo Correa <alfredo.correa@gmail.com>
2026-01-19 20:55:35 -0800

    add cutensor

--- include/boost/multi/restriction.hpp
+++ include/boost/multi/restriction.hpp
@@ -14,6 +14,12 @@ constexpr auto make_restriction(std::initializer_list<T> const& il) {
 	return [il](multi::index i0) { return il.begin()[i0]; } ^ multi::extensions(il);
 }
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#endif
+
 template<class T>
 constexpr auto make_restriction(std::initializer_list<std::initializer_list<T>> const& il) {
 	return [il](multi::index i0, multi::index i1) { return il.begin()[i0].begin()[i1]; } ^ multi::extensions(il);  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
@@ -23,6 +29,10 @@ template<class T>
 constexpr auto make_restriction(std::initializer_list<std::initializer_list<std::initializer_list<T>>> const& il) {
 	return [il](multi::index i0, multi::index i1, multi::index i2) { return il.begin()[i0].begin()[i1].begin()[i2]; } ^ multi::extensions(il);  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 }
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 }  // namespace boost::multi::detail
 
