2843e84f07293433c38c194c49c63ed91f9839ed
Alfredo Correa <alfredo.correa@gmail.com>
2026-01-25 13:31:38 -0800

    add tests

--- test/restrict.cpp
+++ test/restrict.cpp
@@ -12,11 +12,12 @@ auto main() -> int {
 	{
 		auto const R1 = multi::restriction({2, 3}, [](auto ii, auto jj) { return ii + jj; });
 		auto const R2 = [](auto ii, auto jj) { return ii + jj; } ^ multi::extensions_t{2, 3};
+
 		auto const R3 = multi::restrict([](auto ii, auto jj) { return ii + jj; }, multi::extensions_t<2>{2, 3});
 		auto const R4 = multi::restrict([](auto ii, auto jj) { return ii + jj; }, multi::extensions_t{2, 3});
-		auto const R5 = multi::restrict([](auto ii, auto jj) { return ii + jj; }, multi::extensions_t<2>{2, 3});
-		auto const R6 = multi::restrict([](auto ii, auto jj) { return ii + jj; }, multi::extensions_t{2, 3});
-		auto const R7 = multi::restrict<2>([](auto ii, auto jj) { return ii + jj; }, {2, 3});
+		auto const R5 = multi::restrict<2>([](auto ii, auto jj) { return ii + jj; }, {2, 3});
+
+		auto const R6 = multi::restrict([](auto ii, auto jj) { return ii + jj; }, {2, 3});
 
 		BOOST_TEST( R1[1][1] == 2 );
 		BOOST_TEST( R2[1][1] == 2 );
@@ -24,7 +25,6 @@ auto main() -> int {
 		BOOST_TEST( R4[1][1] == 2 );
 		BOOST_TEST( R5[1][1] == 2 );
 		BOOST_TEST( R6[1][1] == 2 );
-		BOOST_TEST( R7[1][1] == 2 );
 	}
 
 	return boost::report_errors();
