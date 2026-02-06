45698d220180121c1837b4debce7d23a20f684bd
Alfredo Correa <alfredo.correa@gmail.com>
2026-02-06 00:59:25 -0800

    more tidy

--- test/dynamic_array.cpp
+++ test/dynamic_array.cpp
@@ -10,20 +10,20 @@ class copyable {
 };
 
 struct non_copyable {
-	non_copyable()                    = default;
+	non_copyable() = default;
 
 	non_copyable(non_copyable const&) = delete;
-	non_copyable(non_copyable&&) = default;
+	non_copyable(non_copyable&&)      = default;
 
 	auto operator=(non_copyable const&) -> non_copyable& = default;
-	auto operator=(non_copyable&&) -> non_copyable& = default;
+	auto operator=(non_copyable&&) -> non_copyable&      = default;
 
 	~non_copyable() = default;
 };
 
 struct non_default_constructible {
-	non_default_constructible(int /*unused*/) {}
-	non_default_constructible() = delete;	
+	explicit non_default_constructible(int /*unused*/) {}
+	non_default_constructible() = delete;
 };
 
 namespace multi = boost::multi;
@@ -33,11 +33,15 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		multi::dynamic_array<copyable, 2> arr({2, 2});
 
 		auto barr = arr;
+
+		BOOST_TEST( barr.size() == 2 );
 	}
 	{
 		multi::dynamic_array<non_copyable, 2> arr({2, 2});
 		// auto barr = arr;  // doesn't work (ok) because arr is not copy constructible
 		// auto barr = std::move(arr);  // doesn't work (ok) because arr is not copy constructible
+
+		BOOST_TEST( arr.size() == 2 );
 	}
 	{
 		// multi::dynamic_array<non_default_constructible, 2> arr({2, 2});  // ok, doesn't work, because it is not copy-constructible
