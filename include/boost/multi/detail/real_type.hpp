a4be58f9461c384101d167a07657820ea94810b0
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-15 04:39:16 +0100

    [CHORE] real_type container tweaks with new operator macro.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/boost/multi/detail/real_type.hpp
+++ include/boost/multi/detail/real_type.hpp
@@ -8,128 +8,109 @@
 #include <algorithm>
 #include <utility>
 
+#define BOOST_MULTI_DECL_OPERATOR(ID, OP) \
+  friend PrecisionType& operator ID (basic_real_type& lhs, const PrecisionType& rhs) { \
+lhs.pv_ ID rhs; \
+return lhs.pv_; \
+} \
+ \
+friend PrecisionType& operator OP (basic_real_type& lhs, const PrecisionType& rhs) { \
+  return lhs.pv_ OP rhs; \
+} \
+ friend PrecisionType& operator ID (basic_real_type& lhs, const basic_real_type& rhs) { \
+   lhs.pv_ ID rhs.pv_;                                                      \
+   return lhs.pv_;                                                      \
+ }                                                                      \
+                                                                        \
+ friend PrecisionType& operator OP (basic_real_type& lhs, const basic_real_type& rhs) { \
+   return lhs.pv_ OP rhs.pv_;                                               \
+ }
+
+
+
 namespace boost::multi {
 
-/// @brief This container represents a numeric type for the multi library. e.g: float, double, simd128, simd256, etc...
-template <typename PrecisionType>
-class basic_real_type final {
-	PrecisionType pv_{};
+  /// @brief This container represents a numeric type for the multi library. e.g: float, double, simd128, simd256, etc...
+  template <typename PrecisionType>
+  class basic_real_type final {
+    PrecisionType pv_{};
 
-public:
-  auto size() const {
-		return sizeof(PrecisionType);
-	}
+  public:
+    auto size() const {
+      return sizeof(PrecisionType);
+    }
 
-  auto& get() const {
-    return pv_;
+    auto& get() const {
+      return pv_;
+    }
+
+    basic_real_type(const PrecisionType& v) : pv_(v) {}
+
+    basic_real_type() = default;
+    ~basic_real_type() = default;
+
+    basic_real_type& operator=(const basic_real_type&) = default;
+    basic_real_type(const basic_real_type&) = default;
+
+  public:
+    BOOST_MULTI_DECL_OPERATOR(/=, /)
+    BOOST_MULTI_DECL_OPERATOR(*=, *)
+    BOOST_MULTI_DECL_OPERATOR(+=, +)
+    BOOST_MULTI_DECL_OPERATOR(-=, -)
+
+  public:
+
+    friend bool operator==(const basic_real_type& lhs, const PrecisionType& rhs) {
+      return lhs.pv_ == rhs;
+    }
+
+    friend bool operator!=(const basic_real_type& lhs, const PrecisionType& rhs) {
+      return lhs.pv_ != rhs;
+    }
+
+    friend bool operator==(const basic_real_type& lhs, const basic_real_type& rhs) {
+      return lhs.pv_ == rhs.pv_;
+    }
+
+    friend bool operator!=(const basic_real_type& lhs, const basic_real_type& rhs) {
+      return lhs.pv_ != rhs.pv_;
+    }
+
+    friend std::ofstream& operator<<(std::ofstream& os, const basic_real_type& ft) {
+      os << ft.pv_;
+      return os;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const basic_real_type& ft) {
+      os << ft.pv_;
+      return os;
+    }
+
+  };
+
+  using float_type = basic_real_type<float>;
+  using double_type = basic_real_type<double>;
+
+
+  inline constexpr auto get(const ::boost::multi::float_type& ft) {
+    return ft.get();
   }
 
-  basic_real_type(const PrecisionType& v) : pv_(v) {}
-
-  basic_real_type() = default;
-  ~basic_real_type() = default;
-
-  basic_real_type& operator=(const basic_real_type&) = default;
-  basic_real_type(const basic_real_type&) = default;
-
-public:
-	friend PrecisionType& operator/=(basic_real_type& lhs, const PrecisionType& rhs) {
-    lhs.pv_ /= rhs;
-    return lhs.pv_;
-	}
-
-	friend PrecisionType& operator/(basic_real_type& lhs, const PrecisionType& rhs) {
-    return lhs.pv_ / rhs;
-	}
-
-	friend PrecisionType& operator/=(basic_real_type& lhs, const basic_real_type& rhs) {
-    lhs.pv_ /= rhs.pv_;
-    return lhs.pv_;
-	}
-
-	friend PrecisionType& operator/(basic_real_type& lhs, const basic_real_type& rhs) {
-    return lhs.pv_ / rhs.pv_;
-	}
-
-  friend PrecisionType& operator*=(basic_real_type& lhs, const basic_real_type& rhs) {
-    lhs.pv_ *= rhs.pv_;
-    return lhs.pv_;
-	}
-
-	friend PrecisionType& operator*(basic_real_type& lhs, const basic_real_type& rhs) {
-    return lhs.pv_ * rhs.pv_;
-	}
-
-  friend PrecisionType& operator*=(basic_real_type& lhs, const PrecisionType& rhs) {
-    lhs.pv_ *= rhs;
-    return lhs;
-	}
-
-	friend PrecisionType& operator*(basic_real_type& lhs, const PrecisionType& rhs) {
-    return lhs.pv_ * rhs;
-	}
-
-  friend PrecisionType& operator-=(basic_real_type& lhs, const PrecisionType& rhs) {
-    lhs.pv_ -= rhs;
-    return lhs;
-	}
-
-	friend PrecisionType& operator-(basic_real_type& lhs, const PrecisionType& rhs) {
-    return lhs.pv_ - rhs;
-	}
-
-  friend PrecisionType& operator+=(basic_real_type& lhs, const PrecisionType& rhs) {
-    lhs.pv_ += rhs;
-    return lhs;
-	}
-
-public:
-	friend PrecisionType& operator+(basic_real_type& lhs, const PrecisionType& rhs) {
-    return lhs.pv_ + rhs;
-	}
-
-	friend bool operator==(const basic_real_type& lhs, const PrecisionType& rhs) {
-		return lhs.pv_ == rhs;
-	}
-
-	friend bool operator!=(const basic_real_type& lhs, const PrecisionType& rhs) {
-		return lhs.pv_ != rhs;
-	}
-
-	friend bool operator==(const basic_real_type& lhs, const basic_real_type& rhs) {
-		return lhs.pv_ == rhs.pv_;
-	}
-
-	friend bool operator!=(const basic_real_type& lhs, const basic_real_type& rhs) {
-		return lhs.pv_ != rhs.pv_;
-	}
-
-  friend std::ofstream& operator<<(std::ofstream& os, const basic_real_type& ft) {
-    os << ft.pv_;
-    return os;
+  inline constexpr auto get(const ::boost::multi::double_type& dt) {
+    return dt.get();
   }
-
-  friend std::ostream& operator<<(std::ostream& os, const basic_real_type& ft) {
-    os << ft.pv_;
-    return os;
-  }
-
-};
-
-using float_type = basic_real_type<float>;
-using double_type = basic_real_type<double>;
 
 } // end boost::multi
 
 namespace std {
 
-inline auto size(const ::boost::multi::float_type& ft) {
+  inline constexpr auto size(const ::boost::multi::float_type& ft) {
     return ft.size();
-}
+  }
 
-inline auto size(const ::boost::multi::double_type& dt) {
+  inline constexpr auto size(const ::boost::multi::double_type& dt) {
     return dt.size();
-}
+  }
 
 }
 
