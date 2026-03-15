187bb7f12d23f1a51cbd87fe05e7c6aa692d80d3
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-15 15:13:20 +0100

    [CHORE] Format two files (float_type, float_type_example).
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- examples/basic_real_type.cpp
+++ examples/basic_real_type.cpp
@@ -1,37 +1,40 @@
-#ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
-$CXX $0 -std=c++17 -o $0x -lboost_timer `pkg-config --libs tbb` &&$0x&&rm $0x;exit
+#ifdef COMPILATION  // -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;-*-
+$CXX $0 - std = c++ 17 - o $0x - lboost_timer `pkg - config-- libs tbb` && $0x && rm $0x;
+exit
 #endif
 
 // Copyright 2026 Amlal El Mahrouss
 
-#include <iostream>
-#include <cmath>
 #include <boost/multi/array.hpp>
 #include <boost/multi/detail/real_type.hpp>
+
 #include <boost/multiprecision/cpp_bin_float.hpp>
-#include <complex>
 #include <boost/timer/timer.hpp>
+
+#include <cmath>
+#include <complex>
+#include <iostream>
 
 namespace multi = boost::multi;
 using boost::multiprecision::cpp_bin_float_double;
 
 int main() {
-  boost::timer::auto_cpu_timer t;
-  {
-    boost::multi::double_type left{pow(M_E, M_PI)};
-    boost::multi::double_type right{1.0};
+	boost::timer::auto_cpu_timer t;
+	{
+		boost::multi::double_type left{pow(M_E, M_PI)};
+		boost::multi::double_type right{1.0};
 
-    cpp_bin_float_double f = left.get() + right.get();
-    std::cout.precision(250);
-    std::cout << f << std::endl;
-  }
-  {
-    boost::multi::double_type left{pow(M_E, M_PI)};
-    boost::multi::double_type right{1.0};
+		cpp_bin_float_double f = left.get() + right.get();
+		std::cout.precision(250);
+		std::cout << f << std::endl;
+	}
+	{
+		boost::multi::double_type left{pow(M_E, M_PI)};
+		boost::multi::double_type right{1.0};
 
-    cpp_bin_float_double f = left.get() + right.get();
-    std::cout.precision(500);
-    std::cout << f << std::endl;
-  }
-  return 0;
+		cpp_bin_float_double f = left.get() + right.get();
+		std::cout.precision(500);
+		std::cout << f << std::endl;
+	}
+	return 0;
 }
--- include/boost/multi/detail/real_type.hpp
+++ include/boost/multi/detail/real_type.hpp
@@ -11,121 +11,116 @@
 /// @brief Implements an operation for ID and OP.
 /// @param ID This could be /=.
 /// @param Op This could be /.
-#define BOOST_MULTI_DECL_OPERATOR(ID, OP) \
-  friend PrecisionType& operator ID (basic_real_type& lhs, const PrecisionType& rhs) { \
-lhs.pv_ ID rhs; \
-return lhs.pv_; \
-} \
- \
-friend PrecisionType& operator OP (basic_real_type& lhs, const PrecisionType& rhs) { \
-  return lhs.pv_ OP rhs; \
-} \
- friend PrecisionType& operator ID (const PrecisionType& rhs, basic_real_type& lhs) {        \
-   lhs.pv_ ID rhs;                                                      \
-   return lhs.pv_;                                                      \
- }                                                                      \
-                                                                        \
- friend PrecisionType& operator OP (const PrecisionType& rhs, basic_real_type& lhs) { \
-   return lhs.pv_ OP rhs;                                               \
- }                                                                      \
- friend PrecisionType& operator ID (basic_real_type& lhs, const basic_real_type& rhs) { \
-   lhs.pv_ ID rhs.pv_;                                                      \
-   return lhs.pv_;                                                      \
- }                                                                      \
-                                                                        \
- friend PrecisionType& operator OP (basic_real_type& lhs, const basic_real_type& rhs) { \
-   return lhs.pv_ OP rhs.pv_;                                               \
- }
-
-
+#define BOOST_MULTI_DECL_OPERATOR(ID, OP)                                                 \
+	friend PrecisionType& operator ID(basic_real_type& lhs, const PrecisionType& rhs) {   \
+		lhs.pv_ ID rhs;                                                                   \
+		return lhs.pv_;                                                                   \
+	}                                                                                     \
+                                                                                          \
+	friend PrecisionType& operator OP(basic_real_type& lhs, const PrecisionType& rhs) {   \
+		return lhs.pv_ OP rhs;                                                            \
+	}                                                                                     \
+	friend PrecisionType& operator ID(const PrecisionType& rhs, basic_real_type& lhs) {   \
+		lhs.pv_ ID rhs;                                                                   \
+		return lhs.pv_;                                                                   \
+	}                                                                                     \
+                                                                                          \
+	friend PrecisionType& operator OP(const PrecisionType& rhs, basic_real_type& lhs) {   \
+		return lhs.pv_ OP rhs;                                                            \
+	}                                                                                     \
+	friend PrecisionType& operator ID(basic_real_type& lhs, const basic_real_type& rhs) { \
+		lhs.pv_ ID rhs.pv_;                                                               \
+		return lhs.pv_;                                                                   \
+	}                                                                                     \
+                                                                                          \
+	friend PrecisionType& operator OP(basic_real_type& lhs, const basic_real_type& rhs) { \
+		return lhs.pv_ OP rhs.pv_;                                                        \
+	}
 
 namespace boost::multi {
 
-  /// @brief This container represents a numeric type for the multi library. e.g: float, double, simd128, simd256...
-  /// @author Amlal El Mahrouss
-  template <typename PrecisionType>
-  class basic_real_type final {
-    PrecisionType pv_{};
+/// @brief This container represents a numeric type for the multi library. e.g: float, double, simd128, simd256...
+/// @author Amlal El Mahrouss
+template<typename PrecisionType>
+class basic_real_type final {
+	PrecisionType pv_{};
 
-  public:
-    const auto width() const {
-      return sizeof(PrecisionType);
-    }
+ public:
+	auto const width() const {
+		return sizeof(PrecisionType);
+	}
 
-    const auto& get() const {
-      return pv_;
-    }
+	auto const& get() const {
+		return pv_;
+	}
 
-    basic_real_type(const PrecisionType& v) : pv_(v) {}
+	basic_real_type(PrecisionType const& v) : pv_(v) {}
 
-    basic_real_type() = default;
-    ~basic_real_type() = default;
+	basic_real_type()  = default;
+	~basic_real_type() = default;
 
-    basic_real_type& operator=(const basic_real_type&) = default;
-    basic_real_type(const basic_real_type&) = default;
+	basic_real_type& operator=(basic_real_type const&) = default;
+	basic_real_type(basic_real_type const&)            = default;
 
-  public:
-    BOOST_MULTI_DECL_OPERATOR(/=, /)
-    BOOST_MULTI_DECL_OPERATOR(*=, *)
-    BOOST_MULTI_DECL_OPERATOR(+=, +)
-    BOOST_MULTI_DECL_OPERATOR(-=, -)
-    BOOST_MULTI_DECL_OPERATOR(<=, <)
-    BOOST_MULTI_DECL_OPERATOR(>=, >)
+ public:
+	BOOST_MULTI_DECL_OPERATOR(/=, /)
+	BOOST_MULTI_DECL_OPERATOR(*=, *)
+	BOOST_MULTI_DECL_OPERATOR(+=, +)
+	BOOST_MULTI_DECL_OPERATOR(-=, -)
+	BOOST_MULTI_DECL_OPERATOR(<=, <)
+	BOOST_MULTI_DECL_OPERATOR(>=, >)
 
-  public:
+ public:
+	friend bool operator==(basic_real_type const& lhs, PrecisionType const& rhs) {
+		return lhs.pv_ == rhs;
+	}
 
-    friend bool operator==(const basic_real_type& lhs, const PrecisionType& rhs) {
-      return lhs.pv_ == rhs;
-    }
+	friend bool operator!=(basic_real_type const& lhs, PrecisionType const& rhs) {
+		return lhs.pv_ != rhs;
+	}
 
-    friend bool operator!=(const basic_real_type& lhs, const PrecisionType& rhs) {
-      return lhs.pv_ != rhs;
-    }
+	friend bool operator==(basic_real_type const& lhs, basic_real_type const& rhs) {
+		return lhs.pv_ == rhs.pv_;
+	}
 
-    friend bool operator==(const basic_real_type& lhs, const basic_real_type& rhs) {
-      return lhs.pv_ == rhs.pv_;
-    }
+	friend bool operator!=(basic_real_type const& lhs, basic_real_type const& rhs) {
+		return lhs.pv_ != rhs.pv_;
+	}
 
-    friend bool operator!=(const basic_real_type& lhs, const basic_real_type& rhs) {
-      return lhs.pv_ != rhs.pv_;
-    }
+	friend std::ofstream& operator<<(std::ofstream& os, basic_real_type const& ft) {
+		os << ft.pv_;
+		return os;
+	}
 
-    friend std::ofstream& operator<<(std::ofstream& os, const basic_real_type& ft) {
-      os << ft.pv_;
-      return os;
-    }
+	friend std::ostream& operator<<(std::ostream& os, basic_real_type const& ft) {
+		os << ft.pv_;
+		return os;
+	}
+};
 
-    friend std::ostream& operator<<(std::ostream& os, const basic_real_type& ft) {
-      os << ft.pv_;
-      return os;
-    }
+using float_type  = basic_real_type<float>;
+using double_type = basic_real_type<double>;
 
-  };
+inline constexpr auto get(::boost::multi::float_type const& ft) {
+	return ft.get();
+}
 
-  using float_type = basic_real_type<float>;
-  using double_type = basic_real_type<double>;
+inline constexpr auto get(::boost::multi::double_type const& dt) {
+	return dt.get();
+}
 
-
-  inline constexpr auto get(const ::boost::multi::float_type& ft) {
-    return ft.get();
-  }
-
-  inline constexpr auto get(const ::boost::multi::double_type& dt) {
-    return dt.get();
-  }
-
-} // end boost::multi
+}  // namespace boost::multi
 
 namespace std {
 
-  inline constexpr auto size(const ::boost::multi::float_type& ft) {
-    return ft.width();
-  }
-
-  inline constexpr auto size(const ::boost::multi::double_type& dt) {
-    return dt.width();
-  }
-
+inline constexpr auto size(::boost::multi::float_type const& ft) {
+	return ft.width();
 }
 
-#endif // BOOST_MULTI_DETAIL_REAL_TYPE_HPP
+inline constexpr auto size(::boost::multi::double_type const& dt) {
+	return dt.width();
+}
+
+}  // namespace std
+
+#endif  // BOOST_MULTI_DETAIL_REAL_TYPE_HPP
