0f615b4e5cff5c301e93a43e116c91576f581246
Alfredo Correa <alfredo.correa@gmail.com>
2026-03-20 21:13:57 -0700

    mrdocs attempt

---
+++ doc/mrdocs_driver.cpp
@@ -0,0 +1,8 @@
+// MrDocs driver: includes all public headers so that Clang can build a
+// compile_commands.json entry for each, even though the library is header-only.
+#include <boost/multi/array.hpp>
+#include <boost/multi/array_ref.hpp>
+#include <boost/multi/restriction.hpp>
+#include <boost/multi/utility.hpp>
+#include <boost/multi/elementwise.hpp>
+#include <boost/multi/io.hpp>
