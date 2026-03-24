1fdbb8ac7e967142b0bca6ab6bae849079feb54e
Theresa Pollinger <theresa.pollinger@a.riken.jp>
2026-03-24 04:59:35 +0000

    cublas context.hpp: add sgemm

--- include/boost/multi/adaptors/cuda/cublas/context.hpp
+++ include/boost/multi/adaptors/cuda/cublas/context.hpp
@@ -229,6 +229,8 @@ class context : private std::unique_ptr<typename std::pointer_traits<hicu(blasHa
 	>
 	void gemm(char transA, char transB, SSize m, SSize n, SSize k, ALPHA const* alpha, AAP aa, SSize lda, BBP bb, SSize ldb, BETA const* beta, CCP cc, SSize ldc) {
 		/*MULTI_MARK_SCOPE("cublasXgemm");*/
+
+		if(is_s<AA>{}) {sync_call<hicu(blasSgemm)>(cuda::cublas::operation{transA}, cuda::cublas::operation{transB}, static_cast<int>(m), static_cast<int>(n), static_cast<int>(k), (float         const*)alpha, (float         const*)::thrust::raw_pointer_cast(aa), static_cast<int>(lda), (float         const*)::thrust::raw_pointer_cast(bb), static_cast<int>(ldb), (float         const*)beta, (float        *)::thrust::raw_pointer_cast(cc), static_cast<int>(ldc));}
 		if(is_d<AA>{}) {sync_call<hicu(blasDgemm)>(cuda::cublas::operation{transA}, cuda::cublas::operation{transB}, static_cast<int>(m), static_cast<int>(n), static_cast<int>(k), (double        const*)alpha, (double        const*)::thrust::raw_pointer_cast(aa), static_cast<int>(lda), (double        const*)::thrust::raw_pointer_cast(bb), static_cast<int>(ldb), (double        const*)beta, (double       *)::thrust::raw_pointer_cast(cc), static_cast<int>(ldc));}
 		if(is_z<AA>{}) {sync_call<hicu(blasZgemm)>(cuda::cublas::operation{transA}, cuda::cublas::operation{transB}, static_cast<int>(m), static_cast<int>(n), static_cast<int>(k), (DoubleComplex const*)alpha, (DoubleComplex const*)::thrust::raw_pointer_cast(aa), static_cast<int>(lda), (DoubleComplex const*)::thrust::raw_pointer_cast(bb), static_cast<int>(ldb), (DoubleComplex const*)beta, (DoubleComplex*)::thrust::raw_pointer_cast(cc), static_cast<int>(ldc));}
 	}
