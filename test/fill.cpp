0fae0bde9c6714c18d5dddd55a7a3ea01862e47b
Alfredo Correa <alfredo.correa@gmail.com>
2026-03-27 13:52:04 -0700

    wa for circle

--- test/fill.cpp
+++ test/fill.cpp
@@ -191,8 +191,7 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 
 		multi::array<int, 2> r2D({5, 5});
 		std::for_each(  // NOLINT(llvm-use-ranges,modernize-use-ranges)
-			begin(r2D), end(r2D),
-			[&](decltype(r2D)::reference elem) { std::generate(begin(elem), end(elem), rand); }
+			begin(r2D), end(r2D), [&](decltype(r2D)::reference elem) { std::generate(begin(elem), end(elem), rand); }
 		);
 	}
 
