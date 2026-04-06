18bab74be9c12a4cfec7abc18ce4c09c619743e2
Alfredo Correa <alfredo.correa@gmail.com>
2026-04-06 00:56:45 -0700

    remove thrust adaptor

--- test/for_each.cpp
+++ test/for_each.cpp
@@ -2,7 +2,6 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/multi/adaptors/thrust.hpp>
 #include <boost/multi/array.hpp>
 
 #include <boost/core/lightweight_test.hpp>
