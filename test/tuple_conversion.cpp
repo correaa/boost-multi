fce5578c4823801506b952b5714e1322ffddf56f
Alfredo Correa <alfredo.correa@gmail.com>
2026-04-19 12:00:29 -0700

    more const

--- test/tuple_conversion.cpp
+++ test/tuple_conversion.cpp
@@ -12,18 +12,33 @@ namespace multi = boost::multi;
 
 auto main() -> int {
 	{
-		multi::array<int, 1> const A = {1, 2, 3, 4};
-		BOOST_TEST( A.size() == 4 );
+		multi::array<int, 1> const arr = {1, 2, 3, 4};
+		BOOST_TEST( arr.size() == 4 );
 	}
 	{
 		auto const tup = std::make_tuple(1, 2, 3);
-		multi::array<int, 1> const A(tup);
-		BOOST_TEST( A.size() == 3 );
+		multi::array<int, 1> const arr(tup);
+		BOOST_TEST( arr.size() == 3 );
 	}
 	{
-		multi::array<int, 2> const B({3, 2}, 0);
-		multi::array<int, 1> const Bsizes(B.sizes());
-		BOOST_TEST( Bsizes.size() == 2 );
+		multi::array<int, 2> const arr2d({3, 2}, 0);
+		multi::array<int, 1> const arr2d_sizes(arr2d.sizes());
+		BOOST_TEST( arr2d_sizes.size() == 2 );
+	}
+	{
+		auto const arr2d = multi::array<int, 2>({3, 2}, 0);
+		auto const arr2d_sizes = std::apply([](auto... szs) { return multi::array< multi::array<int, 2>::size_type, 1>({szs...}); }, arr2d.sizes());
+		BOOST_TEST( arr2d_sizes.size() == 2 );
+	}
+	{
+		auto const arr2d = multi::array<int, 2>({3, 2}, 0);
+		auto const arr2d_sizes = std::apply([](auto... szs) { return multi::array<multi::ssize_t, 1>({szs...}); }, arr2d.sizes());
+		BOOST_TEST( arr2d_sizes.size() == 2 );
+	}
+	{
+		auto const arr2d = multi::array<int, 2>({3, 2}, 0);
+		auto const arr2d_sizes = apply([](auto... ss) { return multi::array<multi::ssize_t, 1>({ss...}); }, arr2d.sizes());
+		BOOST_TEST( arr2d_sizes.size() == 2 );
 	}
 
 	return boost::report_errors();
