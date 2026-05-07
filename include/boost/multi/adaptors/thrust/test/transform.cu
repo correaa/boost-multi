81a8dea2c6f4cf9ab961b029a3420e6d97e462ef
Alfredo Correa <alfredo.correa@gmail.com>
2026-05-07 09:31:56 -0700

    add transform cu test

--- include/boost/multi/adaptors/thrust/test/transform.cu
+++ include/boost/multi/adaptors/thrust/test/transform.cu
@@ -7,12 +7,23 @@
 
 #include <boost/core/lightweight_test.hpp>
 
+#include <thrust/complex.h>
+#include <thrust/tabulate.h>
+#include <thrust/random.h>
+
 namespace multi = boost::multi;
 
 auto main() -> int {
 	namespace multi = boost::multi;
 
 	multi::thrust::universal_array<thrust::complex<double>, 3> const olap({5, 5, 3}, thrust::complex<double>{1.0, 3.0});
+
+	::thrust::tabulate(olap.elements().begin(), olap.elements().end(), [seed = 42] __host__ __device__ (auto i) {
+		thrust::default_random_engine rng(seed);
+		rng.discard(i);
+		thrust::uniform_real_distribution<double> dist(-1.0, 1.0);
+		return thrust::complex<double>(dist(rng), dist(rng));
+	});
 
 	multi::thrust::universal_array<double, 2> vel_gold;
 
@@ -32,6 +43,18 @@ auto main() -> int {
 		multi::array<double, 2> vel({5, 5});
 
 		thrust::transform(
+			olap.flatted().begin(), olap.flatted().end(),
+			vel.flatted().begin(),
+			[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
+		);
+
+		BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
+	}
+	{
+		multi::array<double, 2> vel({5, 5});
+
+		thrust::transform(
+			thrust::cuda::par,
 			olap.flatted().begin(), olap.flatted().end(),
 			vel.flatted().begin(),
 			[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
