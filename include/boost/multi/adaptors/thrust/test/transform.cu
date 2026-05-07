1e2079a0eff2573d59c78ee6b66980eadb34b98f
Alfredo Correa <alfredo.correa@gmail.com>
2026-05-07 12:53:22 -0700

    remove  thrust cases

--- include/boost/multi/adaptors/thrust/test/transform.cu
+++ include/boost/multi/adaptors/thrust/test/transform.cu
@@ -16,14 +16,14 @@ namespace multi = boost::multi;
 auto main() -> int {
 	namespace multi = boost::multi;
 
-	multi::thrust::universal_array<thrust::complex<double>, 3> const olap({5, 5, 3}, thrust::complex<double>{1.0, 3.0});
+	multi::thrust::universal_array<thrust::complex<double>, 3> olap({5, 5, 3});
 
-	::thrust::tabulate(olap.elements().begin(), olap.elements().end(), [seed = 42] __host__ __device__ (auto i) {
-		thrust::default_random_engine rng(seed);
-		rng.discard(i);
-		thrust::uniform_real_distribution<double> dist(-1.0, 1.0);
-		return thrust::complex<double>(dist(rng), dist(rng));
-	});
+	// ::thrust::tabulate(olap.elements().begin(), olap.elements().end(), [seed = 42] __device__ (multi::index i) {
+	// 	thrust::default_random_engine rng(seed);
+	// 	rng.discard(i);
+	// 	thrust::uniform_real_distribution<double> dist(-1.0, 1.0);
+	// 	return thrust::complex<double>(dist(rng), dist(rng));
+	// });
 
 	multi::thrust::universal_array<double, 2> vel_gold;
 
@@ -39,40 +39,40 @@ auto main() -> int {
 
 		vel_gold = vel;
 	}
-	{
-		multi::array<double, 2> vel({5, 5});
+	// {
+	// 	multi::thrust::universal_array<double, 2> vel({5, 5});
 
-		thrust::transform(
-			olap.flatted().begin(), olap.flatted().end(),
-			vel.flatted().begin(),
-			[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
-		);
+	// 	thrust::transform(
+	// 		olap.flatted().begin(), olap.flatted().end(),
+	// 		vel.flatted().begin(),
+	// 		[] __device__ (auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
+	// 	);
 
-		BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
-	}
-	{
-		multi::array<double, 2> vel({5, 5});
+	// 	BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
+	// }
+	// {
+	// 	multi::thrust::universal_array<double, 2> vel({5, 5});
 
-		thrust::transform(
-			thrust::cuda::par,
-			olap.flatted().begin(), olap.flatted().end(),
-			vel.flatted().begin(),
-			[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
-		);
+	// 	thrust::transform(
+	// 		thrust::cuda::par,
+	// 		olap.flatted().begin(), olap.flatted().end(),
+	// 		vel.flatted().begin(),
+	// 		[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
+	// 	);
 
-		BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
-	}
-	{
-		multi::array<double, 2> vel({5, 5});
+	// 	BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
+	// }
+	// {
+	// 	multi::thrust::universal_array<double, 2> vel({5, 5});
 
-		transform(
-			olap.flatted().begin(), olap.flatted().end(),
-			vel.flatted().begin(),
-			[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
-		);
+	// 	transform(
+	// 		olap.flatted().begin(), olap.flatted().end(),
+	// 		vel.flatted().begin(),
+	// 		[](auto const& e) { return norm(e[0]) + norm(e[1]) + norm(e[2]); }
+	// 	);
 
-		BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
-	}
+	// 	BOOST_TEST( std::abs(vel[2][3] - vel_gold[2][3]) < 1e-12  );
+	// }
 
 	return boost::report_errors();
 }
