39d7f052d575e90bc31d7947b4bd03bf3dbaf539
Alfredo Correa <alfredo.correa@gmail.com>
2026-06-07 02:19:59 -0700

    BMA compat private

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -1282,7 +1282,7 @@ class const_subarray : public array_types<T, D, ElementPtr, Layout> {
 	using const_elements_range = elements_range_t<element_const_ptr, layout_type>;
 	using elements_range       = elements_range_t<element_ptr, layout_type>;
 
- public:
+// public:
 	using index_gen [[deprecated("here to fulfill MultiArray concept")]]    = char*;
 	using extent_gen [[deprecated("here to fulfill MultiArray concept")]]   = void;
 	using extent_range [[deprecated("here to fulfill MultiArray concept")]] = void;
