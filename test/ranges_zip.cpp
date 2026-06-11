9460c4385c16e6f3d0c1b4f6addf8a785be4a9f8
Alfredo Correa <alfredo.correa@gmail.com>
2026-06-11 13:27:40 -0700

    tidy

--- test/ranges_zip.cpp
+++ test/ranges_zip.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/multi/array.hpp>
+#include <boost/multi/array.hpp>  // IWYU pragma: keep
 
 #include <boost/core/lightweight_test.hpp>
 
@@ -16,7 +16,7 @@
 #include <ranges>  // IWYU pragma: keep
 #endif
 
-namespace multi = boost::multi;
+namespace multi = boost::multi;  // NOLINT(misc-unused-alias-decls)
 
 auto main() -> int {  // NOLINT(bugprone-exception-escape)
 #if defined(__cpp_lib_ranges_zip) and (__cpp_lib_ranges_zip >= 202110L)
@@ -55,6 +55,31 @@ auto main() -> int {  // NOLINT(bugprone-exception-escape)
 
         BOOST_TEST( e1 == 5 );
         BOOST_TEST( e2 == 6 ); 
+    }
+    {
+		multi::array<int, 1> arr1({10}, 5);
+		multi::array<int, 1> arr2({10}, 6);
+
+        auto zp = std::views::zip(arr1(), arr2());
+
+        auto [e1, e2] = zp[3];
+
+        BOOST_TEST( e1 == 5 );
+        BOOST_TEST( e2 == 6 );
+    }
+    {
+		multi::array<int, 1> arr1({10}, 5);
+		multi::array<int, 1> arr2({10}, 6);
+
+        auto es1 = arr1().elements();
+        auto es2 = arr2().elements();
+
+        auto zp = std::views::zip(arr1, arr2);
+
+        auto [e1, e2] = zp[3];
+
+        BOOST_TEST( e1 == 5 );
+        BOOST_TEST( e2 == 6 );
     }
 #endif
 
