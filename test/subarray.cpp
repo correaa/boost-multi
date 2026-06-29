cffb6ac4aa6611070703379cce247cb75010556d
Alfredo Correa <alfredo.correa@gmail.com>
2026-06-29 11:47:06 -0700

    warnings on windows

--- test/subarray.cpp
+++ test/subarray.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019-2025 Alfredo A. Correa
+// Copyright 2019-2026 Alfredo A. Correa
 // Copyright 2024 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
@@ -7,6 +7,8 @@
 
 #include <boost/core/lightweight_test.hpp>
 
+#include <array>        // for array
+#include <tuple>        // for tuple
 #include <type_traits>  // for std::is_swappable_v
 #include <utility>      // for as_const
 
@@ -270,27 +272,28 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		// static_assert( std::is_trivially_relocatable_v<multi::array<int, 4>::iterator>);  // <==========
 	}
 	{
-		auto A = multi::array<int, 2>{
+		auto arr = multi::array<int, 2>{
 			{1, 2, 3},
 			{4, 5, 6},
 		};
 
 		using std::apply;
-		BOOST_TEST( &apply(A, std::tuple{1, 1}) == &A[1][1] );
-		BOOST_TEST( &apply(A, std::array<int, 2>{{1, 1}}) == &A[1][1] );
+		BOOST_TEST( &apply(arr, std::tuple{1, 1}) == &arr[1][1] );
+		BOOST_TEST( &apply(arr, std::array<int, 2>{{1, 1}}) == &arr[1][1] );
 
 		using indices_t = multi::array<int, 2>::indices_type;
-		BOOST_TEST( &apply(A, indices_t{1, 1}) == &A[1][1] );
+		BOOST_TEST( &apply(arr, indices_t{1, 1}) == &arr[1][1] );
 
-		BOOST_TEST( &A.apply(std::tuple{1, 1}) == &A[1][1] );
-		BOOST_TEST( &A.apply(std::array<int, 2>{{1, 1}}) == &A[1][1] );
+		BOOST_TEST( &arr.apply(std::tuple{1, 1}) == &arr[1][1] );
+		BOOST_TEST( &arr.apply(std::array<int, 2>{{1, 1}}) == &arr[1][1] );
 
-		BOOST_TEST( &A.apply(indices_t{1, 1}) == &A[1][1] );
+		BOOST_TEST( &arr.apply(indices_t{1, 1}) == &arr[1][1] );
 
-		BOOST_TEST( &A.apply({1, 1}) == &A[1][1] );
+		BOOST_TEST( &arr.apply({1, 1}) == &arr[1][1] );
+		// BOOST_TEST( &A.apply({1}) == &A[1][1] );  // error, can't deduce type
 
-		BOOST_TEST( &A.apply(std::tuple{1})[1] == &A[1][1] );
-		BOOST_TEST( &A.apply(std::array<int, 1>{{1}})[1] == &A[1][1] );
+		BOOST_TEST( &arr.apply(std::tuple{1})[1] == &arr[1][1] );
+		BOOST_TEST( &arr.apply(std::array<int, 1>{{1}})[1] == &arr[1][1] );
 	}
 
 	return boost::report_errors();
