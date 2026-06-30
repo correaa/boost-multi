5fcd91d903244c418b953c609d7b08b0ee2cdb74
Alfredo Correa <alfredo.correa@gmail.com>
2026-06-30 12:19:47 -0700

    format

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -3987,7 +3987,7 @@ class array_ref : public subarray<T, D, ElementPtr, Layout> {
 
  public:
 	/// returns a random-access range with all the elements of the array
-	constexpr auto elements() const& -> celements_type { return elements_aux_(); }	// cppcheck-suppress-begin duplInheritedMember ; to overwrite
+	constexpr auto elements() const& -> celements_type { return elements_aux_(); }  // cppcheck-suppress-begin duplInheritedMember ; to overwrite
 	constexpr auto elements() & -> elements_type { return elements_aux_(); }
 	constexpr auto elements() && -> elements_type { return elements_aux_(); }
 	// cppcheck-suppress-end duplInheritedMember ; to overwrite
