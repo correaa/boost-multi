09a99b856316be71aa1c4f20aee8e899f2b8463d
Alfredo Correa <alfredo.correa@gmail.com>
2026-07-11 17:06:59 -0700

    remove origin

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -890,12 +890,13 @@ struct                                                                          
 	/// Returns the base pointer of the array (the base of the layout, generally a pointer to the element with lowest indices)
 	constexpr auto base() const& -> typename dynamic_array::element_const_ptr { return typename dynamic_array::element_const_ptr{ref_::base()}; }
 
+ private:
+	[[deprecated("for compatibility with BMA")]]
 	constexpr auto origin() & -> typename dynamic_array::element_ptr { return ref_::origin(); }
+	[[deprecated("for compatibility with BMA")]]
 	constexpr auto origin() const& -> typename dynamic_array::element_const_ptr { return ref_::origin(); }
 
-	BOOST_MULTI_FRIEND_CONSTEXPR auto origin(dynamic_array& self) -> typename dynamic_array::element_ptr { return self.origin(); }
-	BOOST_MULTI_FRIEND_CONSTEXPR auto origin(dynamic_array const& self) -> typename dynamic_array::element_const_ptr { return self.origin(); }
-
+ public:
 	template<class TT, typename EElementPtr, class LLayout>
 	auto operator=(multi::const_subarray<TT, D, EElementPtr, LLayout> const& other) -> dynamic_array& {
 		ref_::operator=(other);  // TODO(correaa) : protect for self assigment
