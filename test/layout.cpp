68579961aa188d58e13eecff2e6f1420d67c301d
Alfredo Correa <alfredo.correa@gmail.com>
2026-08-09 11:54:05 -0700

    comment test

--- test/layout.cpp
+++ test/layout.cpp
@@ -177,22 +177,22 @@ auto main() -> int {  // NOLINT(readability-function-cognitive-complexity,bugpro
 		BOOST_TEST( vec == vec2 );
 	}
 
-	{
-		multi::array<double, 2> d2D = {
-			{150.0, 16.0, 17.0, 18.0, 19.0},
-			{ 30.0,  1.0,  2.0,  3.0,  4.0},
-			{100.0, 11.0, 12.0, 13.0, 14.0},
-			{ 50.0,  6.0,  7.0,  8.0,  9.0},
-		};
+	// {
+	// 	multi::array<double, 2> d2D = {
+	// 		{150.0, 16.0, 17.0, 18.0, 19.0},
+	// 		{ 30.0,  1.0,  2.0,  3.0,  4.0},
+	// 		{100.0, 11.0, 12.0, 13.0, 14.0},
+	// 		{ 50.0,  6.0,  7.0,  8.0,  9.0},
+	// 	};
 
-		// #if __has_cpp_attribute(no_unique_address) >=201803L and not defined(__NVCC__) and not defined(__PGI)
-		//  BOOST_TEST( sizeof(d2D)==sizeof(double*)+7*sizeof(std::size_t) );
-		// #endif
-		// BOOST_TEST( d2D.is_compact() );
-		// BOOST_TEST( d2D.rotated().is_compact() );
-		// BOOST_TEST( d2D[3].is_compact() );
-		// BOOST_TEST( !(d2D.rotated()[2].is_compact()) );
-	}
+	// 	// #if __has_cpp_attribute(no_unique_address) >=201803L and not defined(__NVCC__) and not defined(__PGI)
+	// 	//  BOOST_TEST( sizeof(d2D)==sizeof(double*)+7*sizeof(std::size_t) );
+	// 	// #endif
+	// 	// BOOST_TEST( d2D.is_compact() );
+	// 	// BOOST_TEST( d2D.rotated().is_compact() );
+	// 	// BOOST_TEST( d2D[3].is_compact() );
+	// 	// BOOST_TEST( !(d2D.rotated()[2].is_compact()) );
+	// }
 	// {
 	// 	multi::array<int, 2> d2D({5, 3});
 	// 	BOOST_TEST( d2D.is_compact() );
