fb48f5223bfd36fdf663063347e2b98d3d39f7af
Alfredo Correa <alfredo.correa@gmail.com>
2026-08-09 13:24:50 -0700

    remove dynamic cast

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -1471,7 +1471,8 @@ struct dynamic_array<T, 0, Alloc>  // NOLINT(misc-multiple-inheritance) : design
 	}
 
 	constexpr explicit operator subarray<value_type, 0, typename dynamic_array::element_const_ptr, typename dynamic_array::layout_type>() & {  // cppcheck-suppress duplInheritedMember ; to overwrite
-		return this->template dynamic_array_cast<value_type, typename dynamic_array::element_const_ptr>();                                     // cppcheck-suppress duplInheritedMember ; to overwrite
+		// cppcheck-suppress duplInheritedMember ; to overwrite
+		return this->template dynamic_array_cast<value_type, typename dynamic_array::element_const_ptr>();  // cppcheck-suppress duplInheritedMember ; to overwrite
 	}
 
 	template<class Archive>
