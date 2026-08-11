fb9c9c5af8633bb6dbc8b5ee6d295ad0e7a01b5b
Alfredo Correa <alfredo.correa@gmail.com>
2026-08-11 00:44:22 -0700

    fix redundant protected

--- include/boost/multi/array.hpp
+++ include/boost/multi/array.hpp
@@ -182,7 +182,6 @@ struct                                                                          
 	}
 
  protected:
- protected:
 	constexpr void uninitialized_default_construct() {
 		if constexpr(!std::is_trivially_default_constructible_v<typename dynamic_array::element> && !multi::force_element_trivial_default_construction<typename dynamic_array::element>) {
 			adl_alloc_uninitialized_default_construct_n(dynamic_array::alloc(), this->base_, this->num_elements());
