fde7939d56eadc0c8d83da15e6033c4fe4ac62f2
Alfredo Correa <alfredo.correa@gmail.com>
2026-08-25 09:12:29 -0700

    remove free extent

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -4524,8 +4524,8 @@ template<class T> constexpr auto cbegin(T&& rng) -> decltype(boost::multi::begin
 /// returns an const-iterator to the end of the given range (including r-value subarrays, this is for compatible with idioms like `using std::cend; ...cend(A2D[1])...`) 
 template<class T> constexpr auto cend(T&& rng) -> decltype(boost::multi::end(static_cast<T const&>(std::forward<T>(rng)))) { return boost::multi::end(static_cast<T const&>(std::forward<T>(rng))); }
 
-template<class T> constexpr auto                    extent(T const& rng) -> decltype(rng.extent()) { return rng.extent(); }
-template<class T> [[deprecated("use extent")]] auto extension(T const& rng) -> decltype(rng.extent()) { return rng.extent(); }
+// template<class T> constexpr auto                    extent(T const& rng) -> decltype(rng.extent()) { return rng.extent(); }
+// template<class T> [[deprecated("use extent")]] auto extension(T const& rng) -> decltype(rng.extent()) { return rng.extent(); }
 
 template<class T> constexpr auto stride(T const& rng) -> decltype(rng.stride()) { return rng.stride(); }
 
