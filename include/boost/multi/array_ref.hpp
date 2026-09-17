98673f48685d7d0014219d5268f495fe877f1697
Alfredo Correa <alfredo.correa@gmail.com>
2026-09-16 20:07:44 -0700

    format

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -2768,7 +2768,7 @@ class subarray : public const_subarray<T, D, ElementPtr, Layout> {
 			return static_cast<P2>(static_cast<void*>(base_ptr));  // NOLINT(bugprone-casting-through-void) direct reinterepret_cast doesn't work here
 		} else {
 			static_assert(sizeof(ElementPtr) == sizeof(P2));  // TODO(correaa) upgrade to bitcast C++20?
-			return detail::bit_cast_<P2>(base_ptr);                   // reinterpret_cast<P2 const&>(base_ptr);     // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast,bugprone-casting-through-void) direct reinterepret_cast doesn't work here
+			return detail::bit_cast_<P2>(base_ptr);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast,bugprone-casting-through-void)
 		}
 	}
 
