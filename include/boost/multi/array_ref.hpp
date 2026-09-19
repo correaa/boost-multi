ede440cf726102f19daf9ffa5da403a19700d856
Alfredo Correa <alfredo.correa@gmail.com>
2026-09-18 20:16:24 -0700

    format

--- include/boost/multi/array_ref.hpp
+++ include/boost/multi/array_ref.hpp
@@ -1848,7 +1848,7 @@ class const_subarray : public detail::array_types<T, D, ElementPtr, Layout> {
 	/// A transpose view \f$A^\mathrm{T}\f$, that exchanges the first two indices
 	BOOST_MULTI_HD constexpr auto transposed() const& -> const_subarray { return transposed_aux_(); }  // cppcheck-suppress duplInheritedMember ; to overwrite
 
-	BOOST_MULTI_HD constexpr auto operator~() const & -> const_subarray { return this->transposed(); }
+	BOOST_MULTI_HD constexpr auto operator~() const& -> const_subarray { return this->transposed(); }
 
 	// BOOST_MULTI_FRIEND_CONSTEXPR BOOST_MULTI_HD auto operator~(const_subarray const& self) -> const_subarray { return self.transposed(); }
 
